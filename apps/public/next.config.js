ac6e8fe15e3e840f389d5f0f280c40721aa18cc6
Jared White <jared@jaredwhite.com>
2019-07-24 22:41:27 +0000

    Add Styles module with Tailwindcss

--- apps/public/next.config.js
+++ apps/public/next.config.js
@@ -1,7 +1,8 @@
 const withTM = require("next-transpile-modules");
+const withSass = require('@zeit/next-sass')
 
 // Tell webpack to compile the ui components package
 // https://www.npmjs.com/package/next-transpile-modules
-module.exports = withTM({
-  transpileModules: ["@dahlia/ui-components"]
-});
+module.exports = withSass(withTM({
+  transpileModules: ["@dahlia"]
+}));
---
+++ apps/public/pages/_app.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import App, { Container } from 'next/app'
+import "@dahlia/styles/src/index.scss"
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    )
+  }
+}
+
+export default MyApp
