5034414da0a149107f5221847ae4fbc3b9c00bbc
Ben Kutler <ben@benkutler.com>
2019-07-28 13:59:03 -0700

    switch listings service from micro to koa

--- services/listings/src/index.ts
+++ services/listings/src/index.ts
@@ -1,18 +1,27 @@
-import { RequestHandler, send } from "micro";
-import { router, get } from "microrouter";
+import Application from "koa";
+// import helmet from "koa-helmet";
+// import cors from "@koa/cors";
 
-import { IncomingMessage, ServerResponse } from "http";
+const config = {
+  port: parseInt(process.env.NODE_PORT || "3001", 10)
+};
 
 const archer = require("../listings/archer.json");
 const gish = require("../listings/gish.json");
 const triton = require("../listings/triton.json");
+const data = { status: "ok", listings: [triton, gish, archer] };
 
-const service: RequestHandler = (req, res) => {
-  const data = {
-    status: "ok",
-    listings: [triton, gish, archer]
-  };
-  send(res, 200, data);
-};
+const app = new Application();
 
-module.exports = router(get("/", service));
+// TODO: app.use(logger(winston));
+//
+
+// app.use(helmet());
+// app.use(cors());
+
+app.use(ctx => {
+  ctx.body = data;
+});
+
+export const server = app.listen(config.port);
+console.log(`Server running on port ${config.port}`);
