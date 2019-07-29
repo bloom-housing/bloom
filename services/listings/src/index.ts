60fd4b3d74a7bc342daef347bb501a4f7300b693
Ben Kutler <ben@benkutler.com>
2019-07-28 22:49:42 -0500

    fix dependencies and general cleanup

--- services/listings/src/index.ts
+++ services/listings/src/index.ts
@@ -1,27 +1,26 @@
 import Application from "koa";
-// import helmet from "koa-helmet";
-// import cors from "@koa/cors";
+import cors from "@koa/cors";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
 
 const config = {
-  port: parseInt(process.env.NODE_PORT || "3001", 10)
+  port: parseInt(process.env.PORT || "3001", 10)
 };
 
-const archer = require("../listings/archer.json");
-const gish = require("../listings/gish.json");
-const triton = require("../listings/triton.json");
+import archer from "../listings/archer.json";
+import gish from "../listings/gish.json";
+import triton from "../listings/triton.json";
 const data = { status: "ok", listings: [triton, gish, archer] };
 
 const app = new Application();
 
 // TODO: app.use(logger(winston));
-//
-
-// app.use(helmet());
-// app.use(cors());
+app.use(cors());
 
 app.use(ctx => {
   ctx.body = data;
 });
 
-export const server = app.listen(config.port);
+const server = app.listen(config.port);
 console.log(`Server running on port ${config.port}`);
