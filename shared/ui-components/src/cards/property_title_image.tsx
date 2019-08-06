323aa6635dd2bb6f2e1cd7b18c8d52796ad637bf
Ben Kutler <ben@benkutler.com>
2019-08-06 10:58:00 -0500

    add the storybook viewport plugin and fix unrelated story errors

--- shared/ui-components/src/cards/property_title_image.tsx
+++ shared/ui-components/src/cards/property_title_image.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import Link from 'next/link';
+import * as React from "react";
+import Link from "next/link";
 
 interface PropertyTitleImageProps {
-  title: String;
-  imageUrl: String;
+  title: string;
+  imageUrl: string;
   listingId: Number;
 }
 
@@ -12,7 +12,7 @@ export const PropertyTitleImage = (props: PropertyTitleImageProps) => (
     <a>
       <figure className="relative">
         {props.imageUrl && <img src={props.imageUrl} alt={props.title} />}
-        {!props.imageUrl && <div style={{height: "300px", background: "#ccc"}}></div>}
+        {!props.imageUrl && <div style={{ height: "300px", background: "#ccc" }} />}
         <figcaption className="absolute inset-x-0 bottom-0">
           <h2 className="text-white text-center text-2xl uppercase t-alt-sans mb-3">{props.title}</h2>
         </figcaption>
