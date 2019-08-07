e0c63df6f2cd3d94e30ead27c1f82305062aa5a5
Jared White <jared@jaredwhite.com>
2019-08-07 15:11:57 -0700

    Move table sub-components into main component file

--- shared/ui-components/src/tables/basic_table.tsx
+++ shared/ui-components/src/tables/basic_table.tsx
@@ -1,6 +1,16 @@
 import * as React from 'react'
-import { HeaderCell, Cell } from './cell'
-import { Row } from './row'
+
+export const Row = (props: any) => (
+  <tr>{props.children}</tr>
+)
+
+export const HeaderCell = (props: any) => (
+  <th className="text-left uppercase bg-gray-200 p-5 font-semibold tracking-wider border-0 border-b border-blue-600">{props.children}</th>
+)
+
+export const Cell = (props: any) => (
+  <td data-label={props.headerLabel} className="p-5">{props.children}</td>
+)
 
 export const BasicTable = (props: any) => {
   const { headers, data } = props
