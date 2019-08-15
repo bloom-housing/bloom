8fab58431adc311f23b5c541bc4f5bc0b7f9cb82
Jared White <jared@jaredwhite.com>
2019-08-14 23:32:49 -0700

    Ability to load MDX content for pages or sections (#10)
    * set up a listing page responsive layout
    * sketch out header and footer layouts
    * remove experimental about page
    * add ability to load MDX content for pages or sections
    * use new default export of PageHeader
    * fix PageHeader location and streamline markdown component

--- shared/ui-components/src/footers/site_footer.tsx
+++ shared/ui-components/src/footers/site_footer.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react'
+import Link from "next/link"
 
 const SiteFooter = () => (
   <footer className="w-full bg-gray-700 text-white h-64 text-center pt-4">
     <div className="max-w-5xl m-auto">
+      <nav className="mb-5">
+        <Link href="/disclaimer"><a className="ml-5 text-white">Disclaimer</a></Link>
+        <Link href="/privacy"><a className="ml-5 text-white">Privacy Policy</a></Link>
+      </nav>
+
       This is a blank footer component, waiting to be implemented.
     </div>
   </footer>
--- shared/ui-components/src/headers/page_header/page_header.tsx
+++ shared/ui-components/src/headers/page_header/page_header.tsx
@@ -1,9 +1,19 @@
 import * as React from "react";
 
-const PageHeader = (props: any) => (
-  <header className="bg-gray-300 py-10 px-5">
-    <h1 className="title m-auto max-w-5xl">{props.children}</h1>
-  </header>
-);
+const PageHeader = (props: any) => {
+  const classNames = ["py-10", "px-5"]
+  if (props.inverse) {
+    classNames.push("bg-blue-700")
+    classNames.push("text-white")
+  } else {
+    classNames.push("bg-gray-300")
+  }
+
+  return (
+    <header className={classNames.join(" ")}>
+      <h1 className="title m-auto max-w-5xl">{props.children}</h1>
+    </header>
+  )
+}
 
 export default PageHeader;
