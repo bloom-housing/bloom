e138d91bff45669913b505c9677abdabcadf602e
Ben Kutler <ben@benkutler.com>
2019-08-27 14:50:35 -0700

    Update ESlint and other misc. DevOps chores (#25)
    * update eslint and other build tools
    * fix some open linter issues
    * switch cypress over to typescript and fix the sample test
    * update jest and fix config; update snapshot
    * remove extra @import spaces
    * unpin apps/public/webpack + regenerate full yarn.lock

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,20 @@
 module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parserOptions: {
+    project: ["./tsconfig.json", "./apps/public/tsconfig.json", "./services/**/tsconfig.json"],
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    tsconfigRootDir: "."
+  },
+  plugins: ["react", "@typescript-eslint"],
   extends: [
+    "eslint:recommended", // the set of rules which are recommended for all projects by the ESLint Team
+    "plugin:@typescript-eslint/eslint-recommended", // conflict resolution between above and below rulesets.
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:@typescript-eslint/recommended-requiring-type-checking", // additional rules that take a little longer to run
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended" // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
-  plugins: ["react", "@typescript-eslint"],
-  parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module" // Allows for the use of imports
-  },
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     "@typescript-eslint/explicit-function-return-type": "off",
@@ -17,4 +22,4 @@ module.exports = {
     "react/jsx-uses-vars": "warn",
     "react/jsx-uses-react": "warn"
   }
-};
+}
--- apps/public/cypress/integration/our_first_test.spec.ts
+++ apps/public/cypress/integration/our_first_test.spec.ts
@@ -1,14 +1,14 @@
-describe('Our First Test', function() {
+describe("Our First Test", function() {
   it("Loads the public app's homepage", function() {
-    cy.visit('http://localhost:3000')
+    cy.visit("http://localhost:3000")
 
     // Check that the "Hello World" text is present on the page
-    cy.contains('Hello World')
+    cy.contains("affordable housing")
 
     // Find the About page link and click it
-    cy.contains('About').click()
+    cy.contains("Disclaimer").click()
 
     // Should be on a new URL which includes '/about'
-    cy.url().should('include', '/about')
+    cy.url().should("include", "/disclaimer")
   })
 })
