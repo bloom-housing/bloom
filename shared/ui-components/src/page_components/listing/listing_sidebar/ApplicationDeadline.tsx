0debe751be3f28a05ac5dee69861e240ad3dc6f8
Marcin Jedras <mjjedras@gmail.com>
2019-10-01 21:55:22 +0200

    Add status label to listings list

--- shared/ui-components/src/page_components/listing/listing_sidebar/ApplicationDeadline.tsx
+++ shared/ui-components/src/page_components/listing/listing_sidebar/ApplicationDeadline.tsx
@@ -2,25 +2,28 @@ import * as React from "react"
 import * as moment from "moment"
 
 interface ApplicationDeadlineProps {
-  date: string
+  date: any
+  vivid?: boolean
 }
 
 const ApplicationDeadline = (props: ApplicationDeadlineProps) => {
   const dueDate = moment(props.date)
   const formattedDate = dueDate.format("MMM DD, YYYY") + " at " + dueDate.format("h:mm A")
+  const vivid = props.vivid || false
+  const textColor = vivid ? "text-white" : "text-gray-800"
   let bgColor, content
   // if due date is in future, listing is open
   if (moment() < dueDate) {
-    bgColor = "bg-blue-100"
+    bgColor = vivid ? "bg-blue-600" : "bg-blue-100"
     content = "Application Deadline"
   } else {
-    bgColor = "bg-red-200"
+    bgColor = vivid ? "bg-red-600" : "bg-red-200"
     content = "Applications Closed"
   }
 
   return (
-    <div className={"text-xs text-gray-800 p-4 " + bgColor}>
-      {content} {formattedDate}
+    <div className={`text-xs p-4 ${textColor} ${bgColor}`}>
+      {content}: {formattedDate}
     </div>
   )
 }
