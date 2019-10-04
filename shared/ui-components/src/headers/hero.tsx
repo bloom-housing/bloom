82e18582a4c7923e63aaad0e37a536e9692f7ed6
Marcin Jędras <mjjedras@gmail.com>
2019-10-04 04:04:20 +0200

    Add subheader when all listings are closed (#57)

--- shared/ui-components/src/headers/hero.tsx
+++ shared/ui-components/src/headers/hero.tsx
@@ -1,19 +1,38 @@
 import * as React from "react"
 import LinkButton from "@bloom/ui-components/src/atoms/LinkButton"
+import { Listing } from "@bloom/ui-components/src/types"
+import * as moment from "moment"
+import t from "@bloom/ui-components/src/helpers/translator"
 
 interface HeroProps {
   title: JSX.Element
   buttonTitle: string
   buttonLink: string
+  listings: Listing[]
 }
 
 const heroClasses = ["bg-blue-700", "py-20", "px-5", "text-white", "text-center"]
 
-const Hero = (props: HeroProps) => (
-  <div className={heroClasses.join(" ")}>
-    <h1 className="title mb-10">{props.title}</h1>
-    <LinkButton href={props.buttonLink}>{props.buttonTitle}</LinkButton>
-  </div>
-)
+const listingOpen = listing => {
+  return moment() < moment(listing.application_due_date)
+}
+
+const Hero = (props: HeroProps) => {
+  let subHeader
+  if (!props.listings.some(listingOpen)) {
+    subHeader = (
+      <h2 className="t-alt-sans text-gray-100 text-lg mb-4">
+        {t("welcome.all_application_closed")}
+      </h2>
+    )
+  }
+  return (
+    <div className={heroClasses.join(" ")}>
+      <h1 className="title mb-4">{props.title}</h1>
+      {subHeader}
+      <LinkButton href={props.buttonLink}>{props.buttonTitle}</LinkButton>
+    </div>
+  )
+}
 
 export default Hero
