8c86fad93a0354b2e384f62b58e4b7330755b40a
Ben Kutler <ben@benkutler.com>
2019-10-19 13:10:08 -0700

    add a mock window.matchMedia to ui-comp jest setup

--- shared/ui-components/.jest/setup-tests.js
+++ shared/ui-components/.jest/setup-tests.js
@@ -1,8 +1,22 @@
-import registerRequireContextHook from "babel-plugin-require-context-hook/register";
+import registerRequireContextHook from "babel-plugin-require-context-hook/register"
 
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import { configure } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
 
-registerRequireContextHook();
+registerRequireContextHook()
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() })
+
+// see: https://jestjs.io/docs/en/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+window.matchMedia = jest.fn().mockImplementation(query => {
+  return {
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn()
+  }
+})
