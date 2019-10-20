efccc3e16a7d3498f096f77adf2a876cd7372d00
Jared White <jared@jaredwhite.com>
2019-10-19 18:37:01 -0700

    Responsive navbar (#61)
    * Component consolidation
    * Use Bulma's Navbar component
    * Fix tailwind config for site and simplify CSS vars
    * Tailwind vars in sass, more React-izing Navbar
    * add CSS documentation outline
    * Improve mobile display of the top notice bar
    * Adding styles for navbar details
    * Fixing responsive menu styles and logo alignnment

---
+++ shared/styles/tailwind.tosass.js
@@ -0,0 +1,11 @@
+const tailwindToSassVars = bloomTheme => {
+  const bloomColorVars = Object.keys(bloomTheme.theme.colors).map(colorKey => {
+    return "$tailwind-" + colorKey + ": " + bloomTheme.theme.colors[colorKey] + ";"
+  })
+  const bloomScreenVars = Object.keys(bloomTheme.theme.screens).map(screenKey => {
+    return "$tailwind-screens-" + screenKey + ": " + bloomTheme.theme.screens[screenKey] + ";"
+  })
+  return bloomColorVars.concat(bloomScreenVars).join("\n")
+}
+
+module.exports = tailwindToSassVars
