b2305dae91649090480b02688b428a047e31210d
Jared White <jared@jaredwhite.com>
2019-11-13 19:04:11 +0000

    Migrate translation files to ui-components module

--- shared/ui-components/.storybook/config.ts
+++ shared/ui-components/.storybook/config.ts
@@ -1,6 +1,11 @@
 import { configure } from "@storybook/react"
 import "@bloom-housing/ui-components/styles/index.scss"
 
+// Set up translation file
+import { addTranslation } from "../src/helpers/translator"
+import general from "../static/locales/general.json"
+addTranslation(general)
+
 // automatically import all files ending in *.stories.tsx
 const req = require.context("../src", true, /stories\.tsx$/)
 
