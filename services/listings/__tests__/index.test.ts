1bfd79001ee4096114a2c3f7ce5199343948ddb6
Jared White <jared@jaredwhite.com>
2020-02-12 16:39:18 -0800

    Listings loader and filtering via jsonpath (#151)
    * Load JSON files from folder and optionally filter
    * Use Listing type
    * make sure listings are copied to dist folder
    * Update listings service test for jsonpath queries

--- services/listings/__tests__/index.test.ts
+++ services/listings/__tests__/index.test.ts
@@ -1,10 +1,36 @@
 import application from "../src/index"
 import request from "supertest"
+import listingsLoader from "../src/lib/listings_loader"
+import { Listing } from "@bloom-housing/core/src/listings"
 
-describe("GET / - a simple api endpoint", () => {
-  it("Hello API Request", async () => {
+describe("GET /", () => {
+  it("should return all listings", async () => {
     const req = await request(application).get("/")
-    expect(req.body.listings.length).toEqual(3)
+    const allListings = (await listingsLoader("listings")) as Listing[]
+    expect(req.body.listings.length).toEqual(allListings.length)
+  })
+
+  afterAll(async done => {
+    application.close()
+    await done()
+  })
+})
+
+describe("JSONPath queries", () => {
+  it("should return only the specified listings", async () => {
+    const query = "/?jsonpath=%24%5B%3F(%40.applicationAddress.city%3D%3D%22San+Jose%22)%5D"
+    const req = await request(application).get(query)
+    let sjListings = (await listingsLoader("listings")) as Listing[]
+    sjListings = sjListings.filter(item => {
+      return item.applicationAddress.city == "San Jose"
+    })
+    expect(req.body.listings.length).toEqual(sjListings.length)
+  })
+
+  it("shouldn't return any listings for incorrect query", async () => {
+    const query = "/?jsonpath=%24%5B%3F(%40.applicationNONSENSE.argh%3D%3D%22San+Jose%22)%5D"
+    const req = await request(application).get(query)
+    expect(req.body.listings.length).toEqual(0)
   })
 
   afterAll(async done => {
