7f22deb93c51f617e4ff8d758f34b0f9ef4e165e
Marcin Jedras <mjjedras@gmail.com>
2020-02-28 14:50:04 +0100

    Customizable footer component

--- shared/ui-components/src/footers/SiteFooter/SiteFooter.stories.tsx
+++ shared/ui-components/src/footers/SiteFooter/SiteFooter.stories.tsx
@@ -10,5 +10,52 @@ export default {
   component: SiteFooter,
   decorators: [withA11y]
 }
+const links = [{ href: "#", title: "Policy" }]
 
-export const footer = () => <SiteFooter />
+export const footer = () => (
+  <SiteFooter links={links} copyright="Alameda County © 2020 • All Rights Reserved" />
+)
+
+const logo = <img src="/images/logo_glyph.svg" alt="Alameda Logo" />
+
+export const withLogo = () => (
+  <SiteFooter links={links} copyright="Alameda County © 2020 • All Rights Reserved" logo={logo} />
+)
+
+const credits = (
+  <>
+    <p>
+      Alameda County Housing Portal is a project of the
+      <br />
+      <a href="https://www.acgov.org/cda/hcd/" target="_blank">
+        Alameda County - Housing and Community Development (HCD) Department
+      </a>
+    </p>
+
+    <p className="mt-10 text-sm">
+      For listing and application questions, please contact the Property Agent displayed on the
+      LISTING
+    </p>
+
+    <p className="text-sm">
+      For general program inquiries, you may call the Alameda County HCD at 510-670-5404.
+    </p>
+
+    <p className="mt-10 text-sm">
+      For additional Bay Area opportunities, please visit:
+      <br />
+      <a href="https://housing.sfgov.org" target="_blank">
+        San Francisco Housing Portal
+      </a>
+    </p>
+  </>
+)
+
+export const withLogoAndCredits = () => (
+  <SiteFooter
+    links={links}
+    copyright="Alameda County © 2020 • All Rights Reserved"
+    logo={logo}
+    credits={credits}
+  />
+)
--- shared/ui-components/src/footers/SiteFooter/SiteFooter.tsx
+++ shared/ui-components/src/footers/SiteFooter/SiteFooter.tsx
@@ -1,19 +1,41 @@
 import * as React from "react"
-import Link from "next/link"
+import "./SiteFooter.scss"
+import LocalizedLink from "../../atoms/LocalizedLink"
+import ExygyFooter from "../ExygyFooter"
 
-const SiteFooter = () => (
-  <footer className="w-full bg-gray-800 text-white h-64 text-center pt-4">
-    <div className="max-w-5xl m-auto">
-      <nav className="mb-5">
-        <Link href="/disclaimer">
-          <a className="ml-5 text-white">Disclaimer</a>
-        </Link>
-        <Link href="/privacy">
-          <a className="ml-5 text-white">Privacy Policy</a>
-        </Link>
-      </nav>
-      This is a blank footer component, waiting to be implemented.
-    </div>
+export interface LinkType {
+  href: string
+  title: string
+}
+
+export interface FooterProps {
+  copyright: string
+  credits?: JSX.Element
+  sole?: JSX.Element
+  links: LinkType[]
+  logo?: JSX.Element
+}
+
+const SiteFooter = (props: FooterProps) => (
+  <footer className="site-footer">
+    {props.logo && <div className="footer-logo">{props.logo}</div>}
+    {props.credits && <div className="footer-credits">{props.credits}</div>}
+
+    <section className="footer-sock">
+      <div className="footer-sock-inner">
+        <p className="footer-copyright">{props.copyright}</p>
+
+        <nav className="footer-nav">
+          {props.links.map((link, index) => (
+            <LocalizedLink key={index} href={link.href}>
+              {link.title}
+            </LocalizedLink>
+          ))}
+        </nav>
+      </div>
+    </section>
+
+    <section className="footer-sole">{props.sole || <ExygyFooter />}</section>
   </footer>
 )
 
