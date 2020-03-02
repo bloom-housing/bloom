3b3f7e2ce3f80c773393417f6f3101585b171d91
Marcin Jedras <mjjedras@gmail.com>
2020-03-02 17:57:42 +0100

    Update header notice

--- apps/public-sj/layouts/application.tsx
+++ apps/public-sj/layouts/application.tsx
@@ -7,17 +7,19 @@ import SiteFooter from "@bloom-housing/ui-components/src/footers/SiteFooter/Site
 import SVG from "react-inlinesvg"
 import WelcomeHeader from "../components/WelcomeHeader"
 
+const notice = (
+  <LocalizedLink href="https://docs.google.com/forms/d/e/1FAIpQLScFVVvd7FDa7puUN4iH2SBl_KAWBu8dRTXNuLIt8Ff9iYF3uA/viewform">
+    We'd love to get your feedback.
+  </LocalizedLink>
+)
+
 const Layout = props => (
   <div>
     <Head>
       <title>{t("nav.siteTitle")}</title>
     </Head>
     <WelcomeHeader />
-    <SiteHeader
-      logoSrc="/images/logo_glyph.svg"
-      notice="This is a preview of our new website. We're just getting started. We'd love to get your feedback."
-      title={t("nav.siteTitle")}
-    >
+    <SiteHeader logoSrc="/images/logo_glyph.svg" notice={notice} title={t("nav.siteTitle")}>
       <LocalizedLink href="/listings" className="navbar-item">
         {t("nav.listings")}
       </LocalizedLink>
