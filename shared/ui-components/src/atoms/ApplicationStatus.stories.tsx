b6e5b3ffd6a028e3951bbf5b2278f8c6b551b019
Jared White <jared@jaredwhite.com>
2020-03-04 14:29:49 -0800

    Add clock icon to ApplicationStatus

--- shared/ui-components/src/atoms/ApplicationStatus.stories.tsx
+++ shared/ui-components/src/atoms/ApplicationStatus.stories.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import moment from "moment"
 import ApplicationStatus from "./ApplicationStatus"
 import Archer from "@bloom-housing/listings-service/listings/archer.json"
+import SVG from "react-inlinesvg"
 
 /* eslint-disable @typescript-eslint/ban-ts-ignore */
 // @ts-ignore
@@ -11,12 +12,31 @@ export default {
 }
 /* eslint-enable @typescript-eslint/ban-ts-ignore */
 
+const svgInclude = <SVG src="/images/icons.svg" />
+
 const listing = Object.assign({}, Archer) as any
 listing.applicationOpenDate = ""
 listing.applicationDueDate = moment()
   .add(10, "days")
   .format()
-export const dueSoon = () => <ApplicationStatus listing={listing} />
+export const dueSoonAndVivid = () => (
+  <>
+    <ApplicationStatus listing={listing} vivid={true} />
+    {svgInclude}
+  </>
+)
+
+const listingPast = Object.assign({}, Archer) as any
+listingPast.applicationOpenDate = ""
+listingPast.applicationDueDate = moment()
+  .subtract(10, "days")
+  .format()
+export const pastDueAndVivid = () => (
+  <>
+    <ApplicationStatus listing={listingPast} vivid={true} />
+    {svgInclude}
+  </>
+)
 
 const listing2 = Object.assign({}, Archer) as any
 listing2.applicationOpenDate = moment()
@@ -25,7 +45,12 @@ listing2.applicationOpenDate = moment()
 listing2.applicationDueDate = moment()
   .add(10, "days")
   .format()
-export const openSoon = () => <ApplicationStatus listing={listing2} />
+export const openSoon = () => (
+  <>
+    <ApplicationStatus listing={listing2} />
+    {svgInclude}
+  </>
+)
 
 const listing3 = Object.assign({}, Archer) as any
 listing3.applicationOpenDate = moment()
@@ -34,11 +59,21 @@ listing3.applicationOpenDate = moment()
 listing3.applicationDueDate = moment()
   .add(10, "days")
   .format()
-export const openedAlready = () => <ApplicationStatus listing={listing3} />
+export const openedAlready = () => (
+  <>
+    <ApplicationStatus listing={listing3} />
+    {svgInclude}
+  </>
+)
 
 const listing4 = Object.assign({}, Archer) as any
 listing4.applicationOpenDate = moment()
   .subtract(15, "days")
   .format()
 listing4.applicationDueDate = ""
-export const openedWithNoDue = () => <ApplicationStatus listing={listing4} />
+export const openedWithNoDue = () => (
+  <>
+    <ApplicationStatus listing={listing4} />
+    {svgInclude}
+  </>
+)
