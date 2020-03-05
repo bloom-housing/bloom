7ca55ec94c1f377a8e40645f9cc61780b7c1cefa
Jared White <jared@jaredwhite.com>
2020-03-05 14:22:39 -0800

    Revert "Getting layout component ready for Google Analytics"
    This reverts commit 6d6631f7ac3ff59d8e3de08d3e4cc87b5d365c4a.

--- apps/public-reference/layouts/application.tsx
+++ apps/public-reference/layouts/application.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import * as React from "react"
 import Head from "next/head"
 import t from "@bloom-housing/ui-components/src/helpers/translator"
 import LocalizedLink from "@bloom-housing/ui-components/src/atoms/LocalizedLink"
@@ -9,45 +9,43 @@ import FooterNav from "@bloom-housing/ui-components/src/footers/FooterNav/Footer
 import FooterSection from "@bloom-housing/ui-components/src/footers/FooterSection/FooterSection"
 import ExygyFooter from "@bloom-housing/ui-components/src/footers/ExygyFooter"
 
-export default class extends Component {
-  public componentDidMount() {
-    //    console.log("DOING A THING!")
-  }
+const links = [
+  { href: "#", title: "Policy" },
+  { href: "/disclaimer", title: "Disclaimer" }
+]
+const Layout = props => (
+  <div>
+    <Head>
+      <title>{t("nav.siteTitle")}</title>
+    </Head>
+    <SiteHeader
+      logoSrc="/images/logo_glyph.svg"
+      notice="This is a preview of our new website. We're just getting started. We'd love to get your feedback."
+      title={t("nav.siteTitle")}
+    >
+      <LocalizedLink href="/listings" className="navbar-item">
+        {t("nav.listings")}
+      </LocalizedLink>
+      {/* Only show Get Assistance if housing counselor data is available */}
+      {process.env.housingCounselorServiceUrl && (
+        <LocalizedLink href="/housing-counselors" className="navbar-item">
+          {t("nav.getAssistance")}
+        </LocalizedLink>
+      )}
+    </SiteHeader>
+    <main>{props.children}</main>
 
-  public render() {
-    return (
-      <div>
-        <Head>
-          <title>{t("nav.siteTitle")}</title>
-        </Head>
-        <SiteHeader
-          logoSrc="/images/logo_glyph.svg"
-          notice="This is a preview of our new website. We're just getting started. We'd love to get your feedback."
-          title={t("nav.siteTitle")}
-        >
-          <LocalizedLink href="/listings" className="navbar-item">
-            {t("nav.listings")}
-          </LocalizedLink>
-          {/* Only show Get Assistance if housing counselor data is available */}
-          {process.env.housingCounselorServiceUrl && (
-            <LocalizedLink href="/housing-counselors" className="navbar-item">
-              {t("nav.getAssistance")}
-            </LocalizedLink>
-          )}
-        </SiteHeader>
-        <main>{this.props.children}</main>
+    <SiteFooter>
+      <FooterNav copyright="Alameda County © 2020 • All Rights Reserved">
+        <LocalizedLink href="#">Policy</LocalizedLink>
+        <LocalizedLink href="/disclaimer">Disclaimer</LocalizedLink>
+      </FooterNav>
+      <FooterSection className="bg-black" small>
+        <ExygyFooter />
+      </FooterSection>
+    </SiteFooter>
+    <SVG src="/images/icons.svg" />
+  </div>
+)
 
-        <SiteFooter>
-          <FooterNav copyright="Alameda County © 2020 • All Rights Reserved">
-            <LocalizedLink href="#">Policy</LocalizedLink>
-            <LocalizedLink href="/disclaimer">Disclaimer</LocalizedLink>
-          </FooterNav>
-          <FooterSection className="bg-black" small>
-            <ExygyFooter />
-          </FooterSection>
-        </SiteFooter>
-        <SVG src="/images/icons.svg" />
-      </div>
-    )
-  }
-}
+export default Layout
