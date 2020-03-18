5cbffabd89da17a897f9c36f4678a1baea2ae249
Jared White <jared@jaredwhite.com>
2020-03-18 12:32:14 -0700

    Move error messages to locale file

--- apps/public-reference/pages/_error.jsx
+++ apps/public-reference/pages/_error.jsx
@@ -6,7 +6,7 @@ import MarkdownSection from "@bloom-housing/ui-components/src/sections/markdown_
 import PageContent from "../page_content/homepage.mdx"
 
 export default () => {
-  const pageTitle = <>Page Not Found</>
+  const pageTitle = <>{t("error.notFound.title")}</>
 
   return (
     <Layout>
@@ -14,8 +14,7 @@ export default () => {
         <title>{pageTitle}</title>
       </Head>
       <Hero title={pageTitle} buttonTitle={t("welcome.seeRentalListings")} buttonLink="/listings">
-        Uh oh, we can’t seem to find the page you’re looking for. Try going back to the previous
-        page or click below to browse listings.
+        {t("error.notFound.message")}
       </Hero>
       <div className="homepage-extra">
         <MarkdownSection fullwidth={true}>
