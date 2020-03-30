ba1d95cf7cd15ac3143dd0c7cd0168bf8ee9bd7e
Ben Kutler <ben@benkutler.com>
2020-03-30 13:24:12 -0700

    Update dev and build dependencies (#194)
    * update dev and build dependencies
    
    * upgrade to cypress 4.2
    
    * update CircleCI docker image to match node version
    
    * update jest, babel-loader, etc.
    
    * update @typescript-eslint
    
    * Streamline and fix TS errors in Listings service
    
    Co-authored-by: Jared White <jared@jaredwhite.com>

--- services/listings/src/lib/listings_loader.ts
+++ services/listings/src/lib/listings_loader.ts
@@ -1,25 +1,18 @@
 import path from "path"
 import fs from "fs"
 
-const parseJsonFile = filePath => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, function(err, data) {
-      if (err) {
-        reject()
-        return
-      }
+const parseJsonFile = (filePath: string) => {
+  const data = fs.readFileSync(filePath)
 
-      resolve(JSON.parse(data.toString()))
-    })
-  })
+  return JSON.parse(data.toString())
 }
 
-export default function(folderName) {
+export default (folderName: string) => {
   return new Promise((resolve, reject) => {
     const listings = []
     const directoryPath = path.join(__dirname, "..", "..", folderName)
 
-    fs.readdir(directoryPath, async function(err, files) {
+    fs.readdir(directoryPath, (err, files) => {
       if (err) {
         console.log("Unable to scan directory: " + err)
         reject()
@@ -28,7 +21,7 @@ export default function(folderName) {
       for (const file of files) {
         const filePath = path.join(__dirname, "..", "..", folderName, file)
         if (/\.json$/.exec(filePath)) {
-          const listingJSON = await parseJsonFile(filePath)
+          const listingJSON = parseJsonFile(filePath)
           listings.push(listingJSON)
         }
       }
