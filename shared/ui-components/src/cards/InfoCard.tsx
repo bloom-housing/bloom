33f4b651a5693d4fc7caa2bf4af4d416c4bc0c92
Jared White <jared@jaredwhite.com>
2020-04-03 10:26:19 -0700

    Add external href to InfoCard (#207)

--- shared/ui-components/src/cards/InfoCard.tsx
+++ shared/ui-components/src/cards/InfoCard.tsx
@@ -3,13 +3,21 @@ import "./InfoCard.scss"
 
 export interface InfoCardProps {
   title: string
+  externalHref?: string
   children: JSX.Element
 }
 
 const InfoCard = (props: InfoCardProps) => (
   <div className="info-card">
-    <h4 className="info-card__title">{props.title}</h4>
-
+    {props.externalHref ? (
+      <h4 className="info-card__title">
+        <a href={props.externalHref} target="_blank">
+          {props.title}
+        </a>
+      </h4>
+    ) : (
+      <h4 className="info-card__title">{props.title}</h4>
+    )}
     {props.children}
   </div>
 )
