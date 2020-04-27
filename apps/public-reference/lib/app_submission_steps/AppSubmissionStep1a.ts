4bfdfd645b977e7c86b85c0a0dc6f37d8ef05764
Jared White <jared@jaredwhite.com>
2020-04-27 13:48:22 -0700

    updated comments

--- apps/public-reference/lib/app_submission_steps/AppSubmissionStep1a.ts
+++ apps/public-reference/lib/app_submission_steps/AppSubmissionStep1a.ts
@@ -10,14 +10,14 @@ export default class AppSubmissionStep1a {
   }
 
   save(formData) {
-    // Pull in all data
+    // Pull in all the form values that match application fields
     for (const [key, value] of Object.entries(formData)) {
       if (typeof this.application[key] != "undefined") {
         this.application[key] = value
       }
     }
 
-    // Transformations
+    // Custom data transformations
     this.application.name = `${formData.firstname} ${formData.lastname}`
 
     this.conductor.advanceToNextStep()
