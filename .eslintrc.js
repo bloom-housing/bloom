2aa0149792028e64fee080054bbf120e5f90c34c
Ben Kutler <ben@benkutler.com>
2020-05-06 17:06:26 -0700

    add eslint import plugin to spot hoisting problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,9 @@ module.exports = {
     "plugin:@typescript-eslint/eslint-recommended", // conflict resolution between above and below rulesets.
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "plugin:@typescript-eslint/recommended-requiring-type-checking", // additional rules that take a little longer to run
+    "plugin:import/errors", // check for imports not resolving correctly
+    "plugin:import/warnings",
+    "plugin:import/typescript",
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended" // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
