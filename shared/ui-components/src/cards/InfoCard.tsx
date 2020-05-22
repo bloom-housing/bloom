7ba1babb7d61515e10dccb7a47c49bdb91cf778c
Jesse Arnold <jessearnold@Jesses-MBP.hsd1.ca.comcast.net>
2020-05-22 16:54:24 -0700

    adding styles for info-card grid

--- shared/ui-components/src/cards/InfoCard.tsx
+++ shared/ui-components/src/cards/InfoCard.tsx
@@ -4,22 +4,28 @@ import "./InfoCard.scss"
 export interface InfoCardProps {
   title: string
   externalHref?: string
+  style: string
   children: JSX.Element
 }
 
-const InfoCard = (props: InfoCardProps) => (
-  <div className="info-card">
-    {props.externalHref ? (
-      <h4 className="info-card__title">
-        <a href={props.externalHref} target="_blank">
-          {props.title}
-        </a>
-      </h4>
-    ) : (
-      <h4 className="info-card__title">{props.title}</h4>
-    )}
-    {props.children}
-  </div>
-)
+const InfoCard = (props: InfoCardProps) => {
+  const wrapperClasses = ["info-card"]
+  wrapperClasses.push(props.style)
+
+  return ( 
+    <div className={wrapperClasses.join(" ")}>
+      {props.externalHref ? (
+        <h4 className="info-card__title">
+          <a href={props.externalHref} target="_blank">
+            {props.title}
+          </a>
+        </h4>
+      ) : (
+        <h4 className="info-card__title">{props.title}</h4>
+      )}
+      {props.children}
+    </div>
+  )
+}
 
 export { InfoCard as default, InfoCard }
