fc564443e847765bf191f300a8efa74e2e15da03
Ben Kutler <ben@benkutler.com>
2020-06-01 13:35:23 -0700

    add nextjs bundle analyzer (#310)

--- apps/public-reference/next.config.js
+++ apps/public-reference/next.config.js
@@ -4,6 +4,9 @@
 const withTM = require("next-transpile-modules")(["@bloom-housing"])
 const withSass = require("@zeit/next-sass")
 const withCSS = require("@zeit/next-css")
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
 const withMDX = require("@next/mdx")()
 const axios = require("axios")
 if (process.env.NODE_ENV !== "production") {
@@ -32,76 +35,78 @@ const tailwindVars = require("@bloom-housing/ui-components/tailwind.tosass.js")(
 // Tell webpack to compile the ui components package
 // https://www.npmjs.com/package/next-transpile-modules
 module.exports = withCSS(
-  withMDX(
-    withSass(
-      withTM({
-        env: {
-          listingServiceUrl: LISTING_SERVICE_URL,
-          mapBoxToken: MAPBOX_TOKEN,
-          housingCounselorServiceUrl: HOUSING_COUNSELOR_SERVICE_URL,
-          gtmKey: process.env.GTM_KEY || null,
-        },
-        sassLoaderOptions: {
-          prependData: tailwindVars,
-        },
-        // exportPathMap adapted from https://github.com/zeit/next.js/blob/canary/examples/with-static-export/next.config.js
-        async exportPathMap() {
-          // we fetch our list of listings, this allow us to dynamically generate the exported pages
-          let listings = []
-          try {
-            const response = await axios.get(LISTING_SERVICE_URL)
-            listings = response.data.listings
-          } catch (error) {
-            console.log(error)
-          }
+  withBundleAnalyzer(
+    withMDX(
+      withSass(
+        withTM({
+          env: {
+            listingServiceUrl: LISTING_SERVICE_URL,
+            mapBoxToken: MAPBOX_TOKEN,
+            housingCounselorServiceUrl: HOUSING_COUNSELOR_SERVICE_URL,
+            gtmKey: process.env.GTM_KEY || null,
+          },
+          sassLoaderOptions: {
+            prependData: tailwindVars,
+          },
+          // exportPathMap adapted from https://github.com/zeit/next.js/blob/canary/examples/with-static-export/next.config.js
+          async exportPathMap() {
+            // we fetch our list of listings, this allow us to dynamically generate the exported pages
+            let listings = []
+            try {
+              const response = await axios.get(LISTING_SERVICE_URL)
+              listings = response.data.listings
+            } catch (error) {
+              console.log(error)
+            }
 
-          // tranform the list of posts into a map of pages with the pathname `/post/:id`
-          const listingPaths = listings.reduce(
-            (listingPaths, listing) =>
-              Object.assign({}, listingPaths, {
-                [`/listing/${listing.id}/${listing.urlSlug}`]: {
-                  page: "/listing",
-                  query: { id: listing.id },
-                },
-                // Create a redirect so that the base ID redirects to the ID with URL slug
-                [`/listing/${listing.id}`]: {
-                  page: "/redirect",
-                  query: { to: `/listing/${listing.id}/${listing.urlSlug}` },
-                },
-              }),
-            {}
-          )
+            // tranform the list of posts into a map of pages with the pathname `/post/:id`
+            const listingPaths = listings.reduce(
+              (listingPaths, listing) =>
+                Object.assign({}, listingPaths, {
+                  [`/listing/${listing.id}/${listing.urlSlug}`]: {
+                    page: "/listing",
+                    query: { id: listing.id },
+                  },
+                  // Create a redirect so that the base ID redirects to the ID with URL slug
+                  [`/listing/${listing.id}`]: {
+                    page: "/redirect",
+                    query: { to: `/listing/${listing.id}/${listing.urlSlug}` },
+                  },
+                }),
+              {}
+            )
 
-          // define page paths for various available languages
-          const translatablePaths = Object.assign({}, listingPaths, {
-            "/": { page: "/" },
-            "/listings": { page: "/listings" },
-            "/housing-counselors": { page: "/HousingCounselors" },
-          })
-          const languages = ["es"] // add new language codes here
-          const languagePaths = {}
-          Object.entries(translatablePaths).forEach(([key, value]) => {
-            languagePaths[key] = value
-            languages.forEach((language) => {
-              const query = Object.assign({}, value.query)
-              query.language = language
-              languagePaths[`/${language}${key.replace(/^\/$/, "")}`] = {
-                ...value,
-                query: query,
-              }
+            // define page paths for various available languages
+            const translatablePaths = Object.assign({}, listingPaths, {
+              "/": { page: "/" },
+              "/listings": { page: "/listings" },
+              "/housing-counselors": { page: "/HousingCounselors" },
             })
-          })
+            const languages = ["es"] // add new language codes here
+            const languagePaths = {}
+            Object.entries(translatablePaths).forEach(([key, value]) => {
+              languagePaths[key] = value
+              languages.forEach((language) => {
+                const query = Object.assign({}, value.query)
+                query.language = language
+                languagePaths[`/${language}${key.replace(/^\/$/, "")}`] = {
+                  ...value,
+                  query: query,
+                }
+              })
+            })
 
-          // combine the map of all various types of page paths
-          return Object.assign({}, languagePaths, {
-            "/disclaimer": { page: "/disclaimer" },
-            "/privacy": { page: "/privacy" },
-            "/applications/new": { page: "/applications/new" },
-            "/applications/step2": { page: "/applications/step2" },
-            "/applications/complete": { page: "/applications/complete" },
-          })
-        },
-      })
+            // combine the map of all various types of page paths
+            return Object.assign({}, languagePaths, {
+              "/disclaimer": { page: "/disclaimer" },
+              "/privacy": { page: "/privacy" },
+              "/applications/new": { page: "/applications/new" },
+              "/applications/step2": { page: "/applications/step2" },
+              "/applications/complete": { page: "/applications/complete" },
+            })
+          },
+        })
+      )
     )
   )
 )
