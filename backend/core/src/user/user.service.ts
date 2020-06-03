89ef7d819e3abfac8888cb9040f764c383a75de6
Ben Peters <bencpeters@gmail.com>
2020-06-03 16:03:13 -0700

    Added password functions to User service using bcrypt

--- backend/core/src/user/user.service.ts
+++ backend/core/src/user/user.service.ts
@@ -2,12 +2,22 @@ import { Injectable } from "@nestjs/common"
 import { InjectRepository } from "@nestjs/typeorm"
 import { User } from "../entity/User"
 import { Repository } from "typeorm"
+import { compare, hash } from "bcrypt"
 
 @Injectable()
 export class UserService {
   constructor(@InjectRepository(User) private readonly repo: Repository<User>) {}
 
   public async findByEmail(email: string) {
-    return this.repo.findOneOrFail({ email })
+    return this.repo.findOne({ email })
+  }
+
+  public async storeUserPassword(user: User, password: string) {
+    const passwordHash = await hash(password, 10)
+    await this.repo.update({ passwordHash }, user)
+  }
+
+  public verifyUserPassword(user: User, password: string) {
+    return compare(password, user.passwordHash)
   }
 }
