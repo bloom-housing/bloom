4bdcf7e9f95e7d6c4d733faeeffb9a3c4ba78550
Ben Peters <bencpeters@gmail.com>
2020-06-03 16:58:20 -0700

    Added a basic request logger

--- backend/core/src/main.ts
+++ backend/core/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import { logger } from "./middleware/logger.middleware"
 
 let app
 async function bootstrap() {
   app = await NestFactory.create(AppModule)
   app.enableCors()
+  app.use(logger)
   await app.listen(process.env.PORT || 3001)
 }
 bootstrap()
