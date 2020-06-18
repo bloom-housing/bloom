94290adde93c019713f12c07e260ea035062063a
Jared White <jared@jaredwhite.com>
2020-06-17 17:43:30 -0700

    Update Cypress tests for application forms

--- apps/public-reference/cypress/integration/pages/ApplicationForms.ts
+++ apps/public-reference/cypress/integration/pages/ApplicationForms.ts
@@ -1,16 +1,14 @@
 describe("New applications page", function () {
   it("renders the new application form", function () {
-    cy.visit("/applications/new")
-    cy.contains("New Application")
+    cy.visit("/applications/start/choose-language")
     cy.get("form").should("have.length.of.at.least", 1)
-    cy.get("form").contains("First Name")
+    cy.get("h2.form-card__title").contains("Choose your language")
   })
 
   it("validates form fields", function () {
-    cy.visit("/applications/new")
+    cy.visit("/applications/contact/name")
 
-    // Click on the Disclaimer page link in the footer
-    cy.get("form input[name=firstname]").type("First Name")
+    cy.get("form input[name=firstName]").type("First Name")
 
     cy.get("form button").last().click()
 
@@ -19,7 +17,7 @@ describe("New applications page", function () {
     cy.get("form").should("not.contain", "Please enter a First Name")
   })
 
-  it("goes to step 2", function () {
+  /*  it("goes to step 2", function () {
     cy.visit("/applications/new")
     cy.get("form input[name=firstname]").type("First Name")
     cy.get("form input[name=lastname]").type("Last Name")
@@ -27,5 +25,5 @@ describe("New applications page", function () {
     cy.get("form#applications-new button").last().click()
 
     cy.get("form#applications-step2").contains("State")
-  })
+  }) */
 })
