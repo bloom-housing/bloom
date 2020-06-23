2b3dd34ce19260647422efcb817ef0027b85495a
Michal Plebanski <michalp@airnauts.com>
2020-06-23 11:18:09 +0200

    Add trivial GET /application test

---
+++ backend/core/src/applications/applications.e2e-spec.ts
@@ -0,0 +1,53 @@
+import { Test } from "@nestjs/testing"
+import { INestApplication, ValidationPipe } from "@nestjs/common"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { getConnection } from "typeorm"
+import { Application } from "../entity/application.entity"
+// Use require because of the CommonJS/AMD style export.
+// See https://www.typescriptlang.org/docs/handbook/modules.html#export--and-import--require
+import dbOptions = require("../../ormconfig.test")
+import supertest from "supertest"
+import { AppModule } from "../app.module"
+
+const clearDb = async (connection) => {
+  const entities = connection.entityMetadatas
+
+  for (const entity of entities) {
+    const repository = await connection.getRepository(entity.name)
+    await repository.query(`DELETE FROM "${entity.tableName}";`)
+  }
+}
+
+describe("Applications", () => {
+  let app: INestApplication
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        AppModule,
+        TypeOrmModule.forRoot(dbOptions),
+        TypeOrmModule.forFeature([Application]),
+      ],
+    }).compile()
+    await clearDb(getConnection())
+
+    app = moduleRef.createNestApplication()
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
+    await app.init()
+  })
+
+  it(`/GET `, async () => {
+    const res = await supertest(app.getHttpServer())
+      .get("/applications")
+      .expect(200)
+    expect(Array.isArray(res.body)).toBe(true)
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+})
