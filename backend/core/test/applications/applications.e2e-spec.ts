8de216f7c8dfc937d8b42949ed65f9b8136d281d
Michal Plebanski <michalp@airnauts.com>
2020-06-23 11:20:27 +0200

    Move applications e2e test to test/ directory

--- backend/core/test/applications/applications.e2e-spec.ts
+++ backend/core/test/applications/applications.e2e-spec.ts
@@ -2,21 +2,15 @@ import { Test } from "@nestjs/testing"
 import { INestApplication, ValidationPipe } from "@nestjs/common"
 import { TypeOrmModule } from "@nestjs/typeorm"
 import { getConnection } from "typeorm"
-import { Application } from "../entity/application.entity"
+import { Application } from "../../src/entity/application.entity"
 // Use require because of the CommonJS/AMD style export.
 // See https://www.typescriptlang.org/docs/handbook/modules.html#export--and-import--require
 import dbOptions = require("../../ormconfig.test")
 import supertest from "supertest"
-import { AppModule } from "../app.module"
+import { AppModule } from "../../src/app.module"
+import { clearDb } from "../utils/clearDb"
 
-const clearDb = async (connection) => {
-  const entities = connection.entityMetadatas
 
-  for (const entity of entities) {
-    const repository = await connection.getRepository(entity.name)
-    await repository.query(`DELETE FROM "${entity.tableName}";`)
-  }
-}
 
 describe("Applications", () => {
   let app: INestApplication
