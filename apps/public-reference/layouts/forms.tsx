7f30582fcfe1c4b1719e6370abeee3fc85021662
Jesse Arnold <jessearnold@Jesses-MacBook-Pro.local>
2020-06-23 13:59:09 -0700

    DOB widths

--- apps/public-reference/layouts/forms.tsx
+++ apps/public-reference/layouts/forms.tsx
@@ -6,7 +6,7 @@ export default (props) => {
       <section style={{ padding: "1px", background: "#f6f6f6" }}>
         <div className="md:mb-20 md:mt-12 mx-auto" style={{ maxWidth: "32rem" }}>
           {props.children}
-         </div>
+        </div>
       </section>
     </Layout>
   )
