09e119ae7f6ac64b535d6db209ab2c7239984a0b
Michal Plebanski <michalp@airnauts.com>
2020-07-03 09:30:59 +0200

    Move listings and applications related entities registration from root module to more appropriate modules

--- backend/core/src/app.module.ts
+++ backend/core/src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from "@nestjs/common"
 import { TypeOrmModule } from "@nestjs/typeorm"
-import { Listing } from "./entity/listing.entity"
-import { Attachment } from "./entity/attachment.entity"
-import { Unit } from "./entity/unit.entity"
-import { Preference } from "./entity/preference.entity"
 import { UserModule } from "./user/user.module"
 // Use require because of the CommonJS/AMD style export.
 // See https://www.typescriptlang.org/docs/handbook/modules.html#export--and-import--require
 import dbOptions = require("../ormconfig")
 import { AuthModule } from "./auth/auth.module"
-import { Application } from "./entity/application.entity"
 import { UserApplicationsModule } from "./user-applications/user-applications.module"
 import { ListingsModule } from "./listings/listings.module"
 import { ApplicationsModule } from "./applications/applications.module"
@@ -20,7 +15,6 @@ import { ApplicationsModule } from "./applications/applications.module"
       ...dbOptions,
       autoLoadEntities: true,
     }),
-    TypeOrmModule.forFeature([Listing, Attachment, Preference, Unit, Application]),
     UserModule,
     UserApplicationsModule,
     AuthModule,
--- backend/core/src/listings/listings.module.ts
+++ backend/core/src/listings/listings.module.ts
@@ -3,9 +3,12 @@ import { TypeOrmModule } from "@nestjs/typeorm"
 import { ListingsService } from "./listings.service"
 import { ListingsController } from "./listings.controller"
 import { Listing } from "../entity/listing.entity"
+import { Attachment } from "../entity/attachment.entity"
+import { Preference } from "../entity/preference.entity"
+import { Unit } from "../entity/unit.entity"
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Listing])],
+  imports: [TypeOrmModule.forFeature([Listing, Attachment, Preference, Unit])],
   providers: [ListingsService],
   exports: [ListingsService],
   controllers: [ListingsController],
