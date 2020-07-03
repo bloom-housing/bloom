09e59b42b8a981a43ceb913ce2d0092c8dfe5741
Michal Plebanski <michalp@airnauts.com>
2020-07-03 09:37:02 +0200

    Disable ignoring JWT token expiration

--- backend/core/src/auth/jwt.strategy.ts
+++ backend/core/src/auth/jwt.strategy.ts
@@ -17,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       passReqToCallback: true,
-      ignoreExpiration: true,
+      ignoreExpiration: false,
       secretOrKey: secretKey,
     })
   }
