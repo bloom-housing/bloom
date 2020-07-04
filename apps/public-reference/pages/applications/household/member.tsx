76289880995cdfe95cec3ea261e34d6a3a371c6c
Jesse Arnold <jessearnold@Jesses-MacBook-Pro.local>
2020-07-04 12:14:13 -0700

    fixing spacing and styling issues

--- apps/public-reference/pages/applications/household/member.tsx
+++ apps/public-reference/pages/applications/household/member.tsx
@@ -448,9 +448,9 @@ export default () => {
             <div className="form-card__pager">
               <div className="form-card__pager-row primary">
                 <Button
-                  big={true}
+                  big={false}
                   filled={true}
-                  className="w-full md:w-3/4"
+                  className=""
                   onClick={() => {
                     //
                   }}
