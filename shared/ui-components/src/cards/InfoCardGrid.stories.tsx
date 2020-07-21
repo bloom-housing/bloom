dc6e36e4ff3a627a495c3f7f52a60b3a336e5149
Jared White <jared@jaredwhite.com>
2020-07-21 13:16:05 -0700

    Add Markdown option to InfoCard (#430)
    * Add Markdown option to InfoCard
    * Change InfoCard to conditional Markdown, fix storyshots

--- shared/ui-components/src/cards/InfoCardGrid.stories.tsx
+++ shared/ui-components/src/cards/InfoCardGrid.stories.tsx
@@ -2,13 +2,14 @@ import React from "react"
 import { withA11y } from "@storybook/addon-a11y"
 import InfoCard from "./InfoCard"
 import InfoCardGrid from "./InfoCardGrid"
+import ExpandableText from "../atoms/ExpandableText"
 
 export default {
   title: "Cards|InfoCardGrid",
   decorators: [withA11y, (storyFn: any) => <div style={{ padding: "1rem" }}>{storyFn()}</div>],
 }
 
-export const TwoColumns = () => (
+export const ThreeColumns = () => (
   <InfoCardGrid
     title="Rentals"
     subtitle="Other rental housing funded by the City, as well as lists developed each month by community nonprofit agencies."
@@ -18,14 +19,37 @@ export const TwoColumns = () => (
       externalHref="http://google.com"
       className="is-normal-primary-lighter"
     >
-      <p>My content</p>
+      {`
+Paragraph content
+
+----
+
+More content
+      `}
     </InfoCard>
     <InfoCard
       title="My Card"
       externalHref="http://google.com"
       className="is-normal-primary-lighter"
     >
-      <p>My content</p>
+      {`
+### Header 3
+
+* A list
+* of items
+      `}
+    </InfoCard>
+
+    <InfoCard
+      title="My Card"
+      externalHref="http://google.com"
+      className="is-normal-primary-lighter"
+    >
+      <ExpandableText>
+        {`
+Text within _another_ component…
+      `}
+      </ExpandableText>
     </InfoCard>
   </InfoCardGrid>
 )
