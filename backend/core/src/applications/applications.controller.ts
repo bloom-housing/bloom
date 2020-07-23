b6417aae583b1cbe020daef023abf46df3281993
Michał Plebański <michalp@airnauts.com>
2020-07-23 18:14:18 +0200

    Application terms (#442)

--- backend/core/src/applications/applications.controller.ts
+++ backend/core/src/applications/applications.controller.ts
@@ -13,38 +13,47 @@ import {
 } from "@nestjs/common"
 import { ApplicationDto, ApplicationsListQueryParams } from "./applications.dto"
 import { ApplicationsService } from "./applications.service"
-import { ApiBearerAuth } from "@nestjs/swagger"
+import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger"
 import { DefaultAuthGuard } from "../auth/default.guard"
 import { ApplicationCreateDto } from "./application.create.dto"
 import { ApplicationUpdateDto } from "./application.update.dto"
 import { TransformInterceptor } from "../interceptors/transform.interceptor"
+import { OptionalAuthGuard } from "../auth/optional-auth.guard"
 
 @Controller("applications")
-@UseGuards(DefaultAuthGuard)
+@ApiTags("applications")
 @ApiBearerAuth()
 @UseInterceptors(ClassSerializerInterceptor)
 export class ApplicationsController {
   constructor(private readonly applicationsService: ApplicationsService) {}
 
   @Get()
+  @UseGuards(DefaultAuthGuard)
+  @ApiOperation({ summary: "List applications", operationId: "list" })
   @UseInterceptors(new TransformInterceptor(ApplicationDto))
   async list(@Query() params: ApplicationsListQueryParams): Promise<ApplicationDto[]> {
     return await this.applicationsService.list(params)
   }
 
   @Post()
+  @ApiOperation({ summary: "Create application", operationId: "create" })
+  @UseGuards(OptionalAuthGuard)
   @UseInterceptors(new TransformInterceptor(ApplicationDto))
   async create(@Body() applicationCreateDto: ApplicationCreateDto): Promise<ApplicationDto> {
     return await this.applicationsService.create(applicationCreateDto)
   }
 
   @Get(`:applicationId`)
+  @UseGuards(DefaultAuthGuard)
+  @ApiOperation({ summary: "Get application by id", operationId: "retrieve" })
   @UseInterceptors(new TransformInterceptor(ApplicationDto))
   async retrieve(@Param("applicationId") applicationId: string): Promise<ApplicationDto> {
     return await this.applicationsService.findOne(applicationId)
   }
 
   @Put(`:applicationId`)
+  @UseGuards(DefaultAuthGuard)
+  @ApiOperation({ summary: "Update application by id", operationId: "update" })
   @UseInterceptors(new TransformInterceptor(ApplicationDto))
   async update(
     @Param("applicationId") applicationId: string,
@@ -54,6 +63,8 @@ export class ApplicationsController {
   }
 
   @Delete(`:applicationId`)
+  @UseGuards(DefaultAuthGuard)
+  @ApiOperation({ summary: "Delete application by id", operationId: "delete" })
   async delete(@Param("applicationId") applicationId: string) {
     await this.applicationsService.delete(applicationId)
   }
