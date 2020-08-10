efbba42e1d3a433e16fdae02126ce6f1934e3b48
Ben Peters <bencpeters@gmail.com>
2020-08-09 18:26:35 -0700

    Fixed unit tests to all pass

--- backend/core/src/auth/constants.ts
+++ backend/core/src/auth/constants.ts
@@ -1,5 +1,9 @@
 // Ensure that a secret key is defined if we're not in dev mode
-if (process.env.NODE_ENV !== "development" && !process.env.APP_SECRET) {
+if (
+  process.env.NODE_ENV !== "development" &&
+  process.env.NODE_ENV !== "test" &&
+  !process.env.APP_SECRET
+) {
   throw new Error("APP_SECRET not found! This is a required config variable in production.")
 }
 
