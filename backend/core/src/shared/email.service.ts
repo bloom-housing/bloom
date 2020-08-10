3f163af92cde89134233211cb047ea2b23637e1a
Marcin Jedras <mjjedras@gmail.com>
2020-08-11 00:12:14 +0200

    Initial i18n add for email service

--- backend/core/src/shared/email.service.ts
+++ backend/core/src/shared/email.service.ts
@@ -3,18 +3,49 @@ import { SendGridService } from "@anchan828/nest-sendgrid"
 import Handlebars from "handlebars"
 import path from "path"
 import { User } from "../entity/user.entity"
+import Polyglot from "node-polyglot"
 
 @Injectable()
 export class EmailService {
-  constructor(private readonly sendGrid: SendGridService) {}
+  polyglot: Polyglot
+
+  constructor(private readonly sendGrid: SendGridService) {
+    this.polyglot = new Polyglot({
+      phrases: { register: { welcome: "Welcome" } },
+    })
+
+    // Handlebars.registerHelper("t", function (phrase: string, options?: any) {
+    //   return this.polyglot.t(phrase, options)
+    // })
+  }
 
   public async welcome(user: User) {
     const template = Handlebars.compile(path.join(__dirname, "views/register-email.hbs"))
-    await this.sendGrid.send({
-      to: user.email,
-      from: "test@example.com",
-      subject: "Welcome to Bloom",
-      html: template({ user: user }),
-    })
+    const t = Handlebars.compile("asdsdads{{test}}")
+    const resp = t({ test: "abd" })
+    console.log(resp)
+    // const response = await this.sendGrid.send({
+    //   to: user.email,
+    //   from: "test@example.com",
+    //   subject: "Welcome to Bloom",
+    //   text: "and easy to do anywhere, even with Node.js",
+    //   // html: template({ user: user }),
+    //   html: "Email body",
+    // })
+
+    await this.sendGrid.send(
+      {
+        to: user.email,
+        // from: "test",
+        subject: "Welcome to Bloom",
+        text: "and easy to do anywhere, even with Node.js",
+        // html: template({ user: user }),
+        html: "Email body",
+      },
+      false,
+      (error, info) => {
+        console.error(error)
+      }
+    )
   }
 }
