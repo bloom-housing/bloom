0fab6990eac54fbb2c8bf73d5dcfbb7c6bbb4db1
Marcin Jedras <mjjedras@gmail.com>
2020-08-13 23:40:49 +0200

    Fix email service, add translations to email service and retry

--- backend/core/src/shared/email.service.ts
+++ backend/core/src/shared/email.service.ts
@@ -5,54 +5,56 @@ import Handlebars from "handlebars"
 import path from "path"
 import { User } from "../entity/user.entity"
 import Polyglot from "node-polyglot"
+import fs from "fs"
 
 @Injectable()
 export class EmailService {
   polyglot: Polyglot
 
   constructor(private readonly sendGrid: SendGridService) {
-    this.polyglot = new Polyglot({
+    const polyglot = new Polyglot({
+      // TODO Add translations loaded from a file
       phrases: { register: { welcome: "Welcome" } },
     })
+    this.polyglot = polyglot
 
-    // Handlebars.registerHelper("t", function (phrase: string, options?: any) {
-    //   return this.polyglot.t(phrase, options)
-    // })
+    Handlebars.registerHelper("t", function (phrase: string, options?: any) {
+      return polyglot.t(phrase, options)
+    })
   }
 
   public async welcome(user: User) {
     Logger.log(
       `Preparing to send a welcome email to ${user.email} from ${process.env.EMAIL_FROM_ADDRESS}...`
     )
-    const template = Handlebars.compile(path.join(__dirname, "views/register-email.hbs"))
-    const t = Handlebars.compile("asdsdads{{test}}")
-    const resp = t({ test: "abd" })
-    console.log(resp)
-    // const response = await this.sendGrid.send({
-    //   to: user.email,
-    //   from: "test@example.com",
-    //   subject: "Welcome to Bloom",
-    //   text: "and easy to do anywhere, even with Node.js",
-    //   // html: template({ user: user }),
-    //   html: "Email body",
-    // })
+    // TODO set locale for user
+    // polyglot.locale(user.lang)
+    await this.send(user.email, "Welcome to Bloom", this.template("register-email")({ user: user }))
+  }
 
+  private template(view: string) {
+    return Handlebars.compile(fs.readFileSync(path.join(__dirname, `/views/${view}.hbs`), "utf8"))
+  }
+
+  private async send(to: string, subject: string, body: string, retry?: number) {
     await this.sendGrid.send(
       {
-        to: user.email,
+        to: to,
         from: process.env.EMAIL_FROM_ADDRESS,
-        subject: "Welcome to Bloom",
-        text: "and easy to do anywhere, even with Node.js",
-        // html: template({ user: user }),
-        html: "Email body",
+        subject: subject,
+        html: body,
       },
       false,
       (error, info) => {
-        console.error(error)
         if (error instanceof ResponseError) {
           const { message, code, response } = error
           const { headers, body } = response
-          console.error(`Error sending email! Error body: ${body}`)
+          console.error(`Error sending email to: ${to}! Error body: ${body}`)
+          if (!retry) {
+            retry = 3
+          }
+          // Retries, if sending failed
+          this.send(to, subject, body, retry - 1)
         }
       }
     )
