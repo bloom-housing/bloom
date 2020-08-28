db7b3c15d7a32667ccd2884eb8939849897459f1
Ben Kutler <ben@benkutler.com>
2020-08-28 12:07:21 -0700

    add preferred unit helper text

--- apps/public-reference/pages/applications/household/preferred-units.tsx
+++ apps/public-reference/pages/applications/household/preferred-units.tsx
@@ -67,6 +67,7 @@ export default () => {
           <h2 className="form-card__title is-borderless">
             {t("application.household.preferredUnit.title")}
           </h2>
+          <p className="mt-4 field-note">{t("application.household.preferredUnit.subTitle")}</p>
         </div>
 
         <form onSubmit={handleSubmit(onSubmit)}>
