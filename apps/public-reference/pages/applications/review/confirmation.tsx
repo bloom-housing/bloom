596c9d6f29dd76326f6ae9812afbb0f8c0bd9e06
Jared White <jared@jaredwhite.com>
2020-09-02 22:27:35 -0700

    Update the copy on the Application Confirmation page (#569)
    * update the copy on the Application Confirmation page
    * update storyshot refugee from #592

--- apps/public-reference/pages/applications/review/confirmation.tsx
+++ apps/public-reference/pages/applications/review/confirmation.tsx
@@ -3,7 +3,7 @@
 Application confirmation with lottery number (confirmation number) 
 */
 import Link from "next/link"
-import Router from "next/router"
+import { useRouter } from "next/router"
 import { Button, FormCard, t } from "@bloom-housing/ui-components"
 import FormsLayout from "../../../layouts/forms"
 import { AppSubmissionContext } from "../../../lib/AppSubmissionContext"
@@ -11,6 +11,7 @@ import { useContext } from "react"
 
 export default () => {
   const { listing } = useContext(AppSubmissionContext)
+  const router = useRouter()
 
   return (
     <FormsLayout>
@@ -37,23 +38,6 @@ export default () => {
           <h3 className="form-card__paragraph-title">
             {t("application.review.confirmation.whatExpectTitle")}
           </h3>
-
-          <p className="field-note mt-1">
-            {t("application.review.confirmation.whatExpectFirstParagraph.held")}
-            {/* TODO: replace with real date */}
-            ###
-            {t("application.review.confirmation.whatExpectFirstParagraph.attend")}
-            {/* TODO: url slug seems to be not completed */}
-            {listing?.urlSlug && (
-              <Link
-                href={`listing/id=${listing.id}`}
-                as={`/listing/${listing.id}/${listing.urlSlug}`}
-              >
-                <a>{t("application.review.confirmation.whatExpectFirstParagraph.listing")}</a>
-              </Link>
-            )}
-            {t("application.review.confirmation.whatExpectFirstParagraph.refer")}
-          </p>
 
           <p className="field-note mt-2">
             {t("application.review.confirmation.whatExpectSecondparagraph")}
@@ -93,7 +77,7 @@ export default () => {
             <Button
               filled={true}
               onClick={() => {
-                Router.push("/create-account").then(() => window.scrollTo(0, 0))
+                router.push("/create-account").then(() => window.scrollTo(0, 0))
               }}
             >
               {t("application.form.general.createAccount")}
