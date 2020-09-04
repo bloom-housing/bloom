fbd2fca23f1e3e843f95c5c8cfd755b33ad17df3
Michal Plebanski <michalp@airnauts.com>
2020-09-04 15:18:21 +0200

    Add /listings DELETE,PUT and /listings/:id GET

--- backend/core/src/listings/listings.controller.ts
+++ backend/core/src/listings/listings.controller.ts
@@ -1,9 +1,21 @@
-import { Body, Controller, Get, Post, Query, UseInterceptors } from "@nestjs/common"
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+  UseInterceptors,
+} from "@nestjs/common"
 import { ListingsService } from "./listings.service"
 import { ApiOperation } from "@nestjs/swagger"
 import { TransformInterceptor } from "../interceptors/transform.interceptor"
 import { ListingCreateDto } from "./listing.create.dto"
 import { ListingDto, ListingExtendedDto } from "./listing.dto"
+import { ListingUpdateDto } from "./listings.update.dto"
 
 @Controller("listings")
 export class ListingsController {
@@ -20,5 +32,28 @@ export class ListingsController {
   @UseInterceptors(new TransformInterceptor(ListingDto))
   async create(@Body() listingDto: ListingCreateDto): Promise<ListingDto> {
     return this.listingsService.create(listingDto)
+  }
+
+  @Get(`:listingId`)
+  @ApiOperation({ summary: "Get listing by id", operationId: "retrieve" })
+  @UseInterceptors(new TransformInterceptor(ListingDto))
+  async retrieve(@Param("listingId") listingId: string): Promise<ListingDto> {
+    return await this.listingsService.findOne(listingId)
+  }
+
+  @Put(`:listingId`)
+  @ApiOperation({ summary: "Update listing by id", operationId: "update" })
+  @UseInterceptors(new TransformInterceptor(ListingDto))
+  async update(
+    @Param("listingId") listingId: string,
+    @Body() listingUpdateDto: ListingUpdateDto
+  ): Promise<ListingDto> {
+    return await this.listingsService.update(listingUpdateDto)
+  }
+
+  @Delete(`:listingId`)
+  @ApiOperation({ summary: "Delete listing by id", operationId: "delete" })
+  async delete(@Param("listingId") listingId: string) {
+    await this.listingsService.delete(listingId)
   }
 }
--- backend/core/src/listings/listings.service.ts
+++ backend/core/src/listings/listings.service.ts
@@ -4,10 +4,11 @@ import { transformUnits } from "../lib/unit_transformations"
 import { listingUrlSlug } from "../lib/url_helper"
 import jp from "jsonpath"
 
-import { Listing } from "../entity/listing.entity"
 import { plainToClass } from "class-transformer"
 import { ListingCreateDto } from "./listing.create.dto"
 import { ListingExtendedDto } from "./listing.dto"
+import { Listing } from "../entity/listing.entity"
+import { ListingUpdateDto } from "./listings.update.dto"
 
 export enum ListingsResponseStatus {
   ok = "ok",
@@ -44,5 +45,30 @@ export class ListingsService {
     const listing = plainToClass(Listing, listingDto)
     await listing.save()
     return listing
+  }
+
+  async update(listingDto: ListingUpdateDto) {
+    const listing = await Listing.findOneOrFail({
+      where: { id: listingDto.id },
+    })
+    Object.assign(listing, listingDto)
+    await listing.save()
+    return listing
+  }
+
+  async delete(listingId: string) {
+    const listing = await Listing.findOneOrFail({
+      where: { id: listingId },
+    })
+    return await Listing.remove(listing)
+  }
+
+  async findOne(listingId: string) {
+    return await Listing.findOneOrFail({
+      where: {
+        id: listingId,
+      },
+      relations: ["units", "preferences", "assets", "applicationMethods"],
+    })
   }
 }
