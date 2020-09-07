46545edb6c6926a9ca262fd3fc3a9ee11eee3b6d
dominikx96 <dominik@airnauts.com>
2020-09-07 09:12:12 +0200

    Made fields type as optional

--- shared/ui-components/src/forms/FieldGroup.tsx
+++ shared/ui-components/src/forms/FieldGroup.tsx
@@ -14,7 +14,7 @@ interface FieldGroupProps {
   name: string
   type?: string
   groupLabel?: string
-  fields: FieldSingle[]
+  fields?: FieldSingle[]
   groupNote?: string
   register: any
   validation?: Record<string, any>
@@ -37,24 +37,23 @@ const FieldGroup = ({
       {groupNote && <p className="field-note mb-4">{groupNote}</p>}
 
       <div className={`field ${error && "error"}`}>
-        {fields &&
-          fields.map((item) => (
-            <div className={`field`} key={item.id}>
-              <input
-                aria-describedby={`${name}-error`}
-                aria-invalid={!!error || false}
-                type={type}
-                id={item.id}
-                value={item.value || item.id}
-                name={name}
-                defaultChecked={item.defaultChecked || false}
-                ref={register(validation)}
-              />
-              <label htmlFor={item.id} className="font-semibold">
-                {item.label}
-              </label>
-            </div>
-          ))}
+        {fields?.map((item) => (
+          <div className={`field`} key={item.id}>
+            <input
+              aria-describedby={`${name}-error`}
+              aria-invalid={!!error || false}
+              type={type}
+              id={item.id}
+              value={item.value || item.id}
+              name={name}
+              defaultChecked={item.defaultChecked || false}
+              ref={register(validation)}
+            />
+            <label htmlFor={item.id} className="font-semibold">
+              {item.label}
+            </label>
+          </div>
+        ))}
 
         {error && errorMessage && (
           <ErrorMessage id={`${name}-error`} error={error}>
