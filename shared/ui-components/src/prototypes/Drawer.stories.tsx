6387be7bf2440edf027d1850162256acb2d760c9
Jesse Arnold <jessearnold@Jesses-MacBook-Pro.local>
2020-09-08 14:23:23 -0700

    fixing scroll panel

--- shared/ui-components/src/prototypes/Drawer.stories.tsx
+++ shared/ui-components/src/prototypes/Drawer.stories.tsx
@@ -10,7 +10,7 @@ export default {
 }
 
 export const Drawer = () => (
-  <div className="drawer is-left">
+  <div className="drawer is-left" aria-labelledby="Drawer Title" aria-describedby="Drawer description">
     <header className="drawer__header">
       <h1 className="drawer__title">My Drawer</h1>
       <button className="drawer__close" aria-label="Close" tabindex="0">
@@ -19,6 +19,12 @@ export const Drawer = () => (
     </header>
     <div className="drawer__body">
       <div className="drawer__content">
+        <section className="border p-8 bg-white mb-8">
+          <p>Test</p>
+        </section>
+        <section className="border p-8 bg-white mb-8">
+          <p>Test</p>
+        </section>
         <section className="border p-8 bg-white">
           <p>Test</p>
         </section>
