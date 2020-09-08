68d73391f66339fdbcc3a8dbe3719a9c593e4e73
dominikx96 <dominikx96@gmail.com>
2020-09-08 21:16:11 +0200

    add origin to show full listing link on print view (#605)

--- apps/public-reference/pages/applications/view.tsx
+++ apps/public-reference/pages/applications/view.tsx
@@ -18,13 +18,20 @@ export default () => {
     return moment().format(DATE_FORMAT)
   }, [])
 
+  const origin = typeof window !== "undefined" ? window.location.origin : ""
+
   return (
     <FormsLayout>
       <FormCard header="Confirmation">
         <div className="py-2">
-          <Link href={`listing/id=${listing.id}`} as={`/listing/${listing.id}/${listing.urlSlug}`}>
-            <a className="lined text-tiny">{t("application.confirmation.viewOriginalListing")}</a>
-          </Link>
+          {listing && (
+            <Link
+              href={`listing/id=${listing.id}`}
+              as={`${origin}/listing/${listing.id}/${listing.urlSlug}`}
+            >
+              <a className="lined text-tiny">{t("application.confirmation.viewOriginalListing")}</a>
+            </Link>
+          )}
         </div>
       </FormCard>
 
@@ -48,7 +55,7 @@ export default () => {
 
         <FormSummaryDetails application={application} editMode={false} />
 
-        <div className="form-card__pager">
+        <div className="form-card__pager hide-for-print">
           <div className="form-card__pager-row py-6">
             <a href="#" className="lined text-tiny" onClick={() => window.print()}>
               {t("application.confirmation.printCopy")}
