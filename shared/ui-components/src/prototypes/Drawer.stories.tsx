3c3b3192e77a375ee9677981a79a3156f4da6cef
Jesse Arnold <jessearnold@Jesses-MacBook-Pro.local>
2020-09-09 16:12:12 -0700

    adding names to fields

--- shared/ui-components/src/prototypes/Drawer.stories.tsx
+++ shared/ui-components/src/prototypes/Drawer.stories.tsx
@@ -71,56 +71,56 @@ export const DrawerFieldTest = () => (
                 <div className="field">
                   <label className="label" htmlFor="field-1">Label</label>
                   <div className="control">
-                    <input className="input" id="field-1" placeholder="Enter text" />
+                    <input className="input" id="field-1" name="field-1" placeholder="Enter text" />
                   </div>
                 </div>
 
                 <div className="field">
                   <label className="label" htmlFor="field-2">Label</label>
                   <div className="control">
-                    <input className="input" id="field-2" placeholder="Enter text" />
+                    <input className="input" id="field-2" name="field-2" placeholder="Enter text" />
                   </div>
                 </div>
 
                 <div className="field">
                   <label className="label" htmlFor="field-3">Label</label>
                   <div className="control">
-                    <input className="input" id="field-3" placeholder="Enter text" />
+                    <input className="input" id="field-3" name="field-3" placeholder="Enter text" />
                   </div>
                 </div>
 
                 <div className="field">
                   <label className="label" htmlFor="field-4">Label</label>
                   <div className="control">
-                    <input className="input" id="field-4" placeholder="Enter text" />
+                    <input className="input" id="field-4" name="field-4" placeholder="Enter text" />
                   </div>
                 </div>
 
                 <div className="field">
                   <label className="label" htmlFor="field-5">Label</label>
                   <div className="control">
-                    <input className="input" id="field-5" placeholder="Enter text" />
+                    <input className="input" id="field-5" name="field-5" placeholder="Enter text" />
                   </div>
                 </div>
 
                 <div className="field">
                   <label className="label" htmlFor="field-6">Label</label>
                   <div className="control">
-                    <input className="input" id="field-6" placeholder="Enter text" />
+                    <input className="input" id="field-6" name="field-6" placeholder="Enter text" />
                   </div>
                 </div>
 
                 <div className="field">
                   <label className="label" htmlFor="field-7">Label</label>
                   <div className="control">
-                    <input className="input" id="field-7" placeholder="Enter text" />
+                    <input className="input" id="field-7" name="field-7" placeholder="Enter text" />
                   </div>
                 </div>
 
                 <div className="field">
                   <label className="label" htmlFor="field-8">Label</label>
                   <div className="control">
-                    <input className="input" id="field-8" placeholder="Enter text" />
+                    <input className="input" id="field-8" name="field-8" placeholder="Enter text" />
                   </div>
                 </div>
               </div>
