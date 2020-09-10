11e7dd0bc41a640fa1a44f9cf2be3165fef08b3b
Jared White <jared@jaredwhite.com>
2020-09-09 19:33:25 -0700

    Guard against missing listings

--- shared/ui-components/src/page_components/listing/ListingsList.tsx
+++ shared/ui-components/src/page_components/listing/ListingsList.tsx
@@ -12,7 +12,7 @@ export interface ListingsProps {
 }
 
 const imageUrlFromListing = (listing: Listing) => {
-  return listing.assets.find((asset) => asset.label == "building")?.fileId
+  return listing?.assets?.find((asset) => asset.label == "building")?.fileId
 }
 
 const ListingsList = (props: ListingsProps) => {
