2e327213f06625c83a5c7a564d17bfe3b4de3566
Jesse Arnold <jessearnold@Jesses-MacBook-Pro.local>
2020-09-11 16:02:22 -0700

    css formatting

--- shared/ui-components/src/prototypes/SideNav.stories.tsx
+++ shared/ui-components/src/prototypes/SideNav.stories.tsx
@@ -10,6 +10,16 @@ export default {
 export const SideNav = () => (
   <nav className="side-nav" aria-label="Secondary navigation">
     <ul>
+      <li><a href="#">One</a></li>
+      <li><a href="#">Two</a></li>
+      <li><a href="#">Three</a></li>
+    </ul>
+  </nav>
+)
+
+export const SideNavCurrent = () => (
+  <nav className="side-nav" aria-label="Secondary navigation">
+    <ul>
       <li><a href="#" className="is-current">One</a></li>
       <li><a href="#">Two</a></li>
       <li><a href="#">Three</a></li>
