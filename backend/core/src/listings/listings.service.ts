ee3a47b0d6061a50708150be23c7d8c8e63ad6eb
Marcin Jedras <mjjedras@gmail.com>
2020-09-15 16:36:40 +0200

    Order unit types in tables

--- backend/core/src/listings/listings.service.ts
+++ backend/core/src/listings/listings.service.ts
@@ -16,9 +16,16 @@ export enum ListingsResponseStatus {
 @Injectable()
 export class ListingsService {
   public async list(jsonpath?: string): Promise<any> {
-    let listings = await Listing.find({
-      relations: ["units", "preferences", "assets", "applicationMethods"],
-    })
+    let listings = await Listing.createQueryBuilder("listings")
+      .leftJoinAndSelect("listings.units", "units")
+      .leftJoinAndSelect("listings.preferences", "preferences")
+      .leftJoinAndSelect("listings.assets", "assets")
+      .leftJoinAndSelect("listings.applicationMethods", "applicationMethods")
+      .orderBy({
+        "listings.id": "DESC",
+        "units.max_occupancy": "ASC",
+      })
+      .getMany()
 
     if (jsonpath) {
       listings = jp.query(listings, jsonpath)
