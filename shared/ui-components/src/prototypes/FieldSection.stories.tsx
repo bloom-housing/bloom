353d1aaf874cdc21f4754637eaddf37aa3821055
Jesse Arnold <jessearnold@Jesses-MacBook-Pro.local>
2020-09-16 15:08:22 -0700

    adding higher level wrapper components

--- shared/ui-components/src/prototypes/FieldSection.stories.tsx
+++ shared/ui-components/src/prototypes/FieldSection.stories.tsx
@@ -1,78 +1,75 @@
 import React from "react"
 import { Field, t } from "@bloom-housing/ui-components"
 import { SimpleTable } from "./SimpleTable"
-
-import "./FieldSection.scss"
+import { FieldSection } from "./FieldSection"
 
 export default {
   title: "Prototypes/FieldSection",
   decorators: [(storyFn: any) => <div style={{ padding: "1rem" }}>{storyFn()}</div>],
 }
 
-export const FieldSectionFourColumns = () => (
-  <div className="field-section">
-    <header className="field-section__header">
-      <h2 className="field-section__title">My Section</h2>
-    </header>  
-
-    <div className="field-grid md:grid md:grid-cols-4 md:gap-8">
-      <div className="field">
-        <label className="label" htmlFor="field-1">Label</label>
-        <div className="control">
-          <input className="input" id="field-1" placeholder="Enter text" />
-        </div>
-      </div>
-
-      <div className="field">
-        <label className="label" htmlFor="field-2">Label</label>
-        <div className="control">
-          <input className="input" id="field-2" placeholder="Enter text" />
-        </div>
-      </div>
-
-      <div className="field">
-        <label className="label" htmlFor="field-3">Label</label>
-        <div className="control">
-          <input className="input" id="field-3" placeholder="Enter text" />
-        </div>
-      </div>
-
-      <div className="field">
-        <label className="label" htmlFor="field-4">Label</label>
-        <div className="control">
-          <input className="input" id="field-4" placeholder="Enter text" />
-        </div>
-      </div>
-
-      <div className="field">
-        <label className="label" htmlFor="field-5">Label</label>
-        <div className="control">
-          <input className="input" id="field-5" placeholder="Enter text" />
-        </div>
-      </div>
-
-      <div className="field">
-        <label className="label" htmlFor="field-6">Label</label>
-        <div className="control">
-          <input className="input" id="field-6" placeholder="Enter text" />
-        </div>
-      </div>
-
-      <div className="field">
-        <label className="label" htmlFor="field-7">Label</label>
-        <div className="control">
-          <input className="input" id="field-7" placeholder="Enter text" />
-        </div>
-      </div>
-
-      <div className="field">
-        <label className="label" htmlFor="field-8">Label</label>
-        <div className="control">
-          <input className="input" id="field-8" placeholder="Enter text" />
-        </div>
+export const FourColumns = () => (
+  <FieldSection
+    title="Section Title"
+    tinted={true}
+    className="md:grid md:grid-cols-4 md:gap-8"
+  >
+    <div className="field">
+      <label className="label" htmlFor="field-1">Label</label>
+      <div className="control">
+        <input className="input" id="field-1" placeholder="Enter text" />
       </div>
     </div>
-  </div>
+
+    <div className="field">
+      <label className="label" htmlFor="field-2">Label</label>
+      <div className="control">
+        <input className="input" id="field-2" placeholder="Enter text" />
+      </div>
+    </div>
+
+    <div className="field">
+      <label className="label" htmlFor="field-3">Label</label>
+      <div className="control">
+        <input className="input" id="field-3" placeholder="Enter text" />
+      </div>
+    </div>
+
+    <div className="field">
+      <label className="label" htmlFor="field-4">Label</label>
+      <div className="control">
+        <input className="input" id="field-4" placeholder="Enter text" />
+      </div>
+    </div>
+
+    <div className="field">
+      <label className="label" htmlFor="field-5">Label</label>
+      <div className="control">
+        <input className="input" id="field-5" placeholder="Enter text" />
+      </div>
+    </div>
+
+    <div className="field">
+      <label className="label" htmlFor="field-6">Label</label>
+      <div className="control">
+        <input className="input" id="field-6" placeholder="Enter text" />
+      </div>
+    </div>
+
+    <div className="field">
+      <label className="label" htmlFor="field-7">Label</label>
+      <div className="control">
+        <input className="input" id="field-7" placeholder="Enter text" />
+      </div>
+    </div>
+
+    <div className="field">
+      <label className="label" htmlFor="field-8">Label</label>
+      <div className="control">
+        <input className="input" id="field-8" placeholder="Enter text" />
+      </div>
+    </div>
+  </FieldSection>
 )
 
 export const FieldGroupAddress = () => (
--- shared/ui-components/src/prototypes/ViewSection.stories.tsx
+++ shared/ui-components/src/prototypes/ViewSection.stories.tsx
@@ -1,166 +1,150 @@
 import React from "react"
 import Link from "next/link"
 import { SimpleTable } from "./SimpleTable"
-
-import "./ViewSection.scss"
+import { ViewItem } from "./ViewItem"
+import { ViewSection } from "./ViewSection"
 
 export default {
   title: "Prototypes/ViewSection",
   decorators: [(storyFn: any) => <div style={{ padding: "1rem" }}>{storyFn()}</div>],
 }
 
-export const ViewSectionFourColumns = () => (
-  <div className="view-section">
-    <header className="view-header">
-      <h2 className="view-title">My Section</h2>
-    </header>
+export const FourColumns = () => (
+  <ViewSection
+    title="Section Title"
+    tinted={true}
+    className="md:grid md:grid-cols-4 md:gap-8"
+  >
+    <ViewItem
+        label="First Name"
+        value="Lisa"
+      />
 
-    <div className="view-grid md:grid md:grid-cols-4 md:gap-8 bg-primary-lighter">
-      <p className="view-item">
-        <span className="view-item__label">First Name</span>
-        <span className="view-item__value">Lisa</span>
-      </p>
+      <ViewItem
+        label="Middle Name"
+        value="S"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Middle Name</span>
-        <span className="view-item__value">S</span>
-      </p>
+      <ViewItem
+        label="Last Name"
+        value="Jones"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Last Name</span>
-        <span className="view-item__value">Jones</span>
-      </p>
+      <ViewItem
+        label="Date of Birth"
+        value="01/01/1985"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Date of Birth</span>
-        <span className="view-item__value">01/01/1985</span>
-      </p>
+      <ViewItem
+        label="Email"
+        value="lisa@gmail.com"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Email</span>
-        <span className="view-item__value">lisa@gmail.com</span>
-      </p>
+      <ViewItem
+        label="Phone"
+        value="111-222-3333"
+        helper="Cell"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Phone</span>
-        <span className="view-item__value">111-222-3333</span>
-        <span className="view-item__helper">Cell</span>
-      </p>
+      <ViewItem
+        label="Second Phone"
+        value="111-222-3333"
+        helper="Work"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Second Phone</span>
-        <span className="view-item__value">222-333-4444</span>
-        <span className="view-item__helper">Work</span>
-      </p>
-
-      <p className="view-item">
-        <span className="view-item__label">Perferred Contact</span>
-        <span className="view-item__value">Phone</span>
-      </p>
-    </div>
-  </div>
+      <ViewItem
+        label="Perferred Contact"
+        value="Phone"
+      />
+  </ViewSection>
 )
 
-export const ViewSectionFourColumnsEdit = () => (
-  <div className="view-section">
-    <header className="view-header">
-      <h2 className="view-title">My Section</h2>
-      <span className="ml-auto">
-        <a className="edit-link">Edit</a>
-      </span>
-    </header>
+export const FourColumnsEdit = () => (
+  <ViewSection
+    title="Section Title"
+    edit="Edit"
+    tinted={true}
+    className="md:grid md:grid-cols-4 md:gap-8"
+  >
+    <ViewItem
+      label="First Name"
+      value="Lisa"
+    />
 
-    <div className="view-grid md:grid md:grid-cols-4 md:gap-8 bg-primary-lighter">
-      <p className="view-item">
-        <span className="view-item__label">Label</span>
-        <span className="view-item__value">Value</span>
-        <span className="view-item__helper">Helper</span>
-      </p>
+    <ViewItem
+      label="Middle Name"
+      value="S"
+    />
 
-      <p className="view-item">
-        <span className="view-item__label">Label</span>
-        <span className="view-item__value">Value</span>
-        <span className="view-item__helper">Helper</span>
-      </p>
+    <ViewItem
+      label="Last Name"
+      value="Jones"
+    />
 
-      <p className="view-item">
-        <span className="view-item__label">Label</span>
-        <span className="view-item__value">Value</span>
-        <span className="view-item__helper">Helper</span>
-      </p>
+    <ViewItem
+      label="Date of Birth"
+      value="01/01/1985"
+    />
 
-      <p className="view-item">
-        <span className="view-item__label">Label</span>
-        <span className="view-item__value">Value</span>
-        <span className="view-item__helper">Helper</span>
-      </p>
+    <ViewItem
+      label="Email"
+      value="lisa@gmail.com"
+    />
 
-      <p className="view-item">
-        <span className="view-item__label">Label</span>
-        <span className="view-item__value">Value</span>
-        <span className="view-item__helper">Helper</span>
-      </p>
+    <ViewItem
+      label="Phone"
+      value="111-222-3333"
+      helper="Cell"
+    />
 
-      <p className="view-item">
-        <span className="view-item__label">Label</span>
-        <span className="view-item__value">Value</span>
-        <span className="view-item__helper">Helper</span>
-      </p>
+    <ViewItem
+      label="Second Phone"
+      value="111-222-3333"
+      helper="Work"
+    />
 
-      <p className="view-item">
-        <span className="view-item__label">Label</span>
-        <span className="view-item__value">Value</span>
-        <span className="view-item__helper">Helper</span>
-      </p>
-
-      <p className="view-item">
-        <span className="view-item__label">Label</span>
-        <span className="view-item__value">Value</span>
-        <span className="view-item__helper">Helper</span>
-      </p>
-    </div>
-  </div>
+    <ViewItem
+      label="Perferred Contact"
+      value="Phone"
+    />
+  </ViewSection>
 )
 
-export const ViewSectionAddress = () => (
+export const Address = () => (
   <div className="view-section">
     <header className="view-header">
       <h2 className="view-title">My Section</h2>
-    </header>
-
-    <div className="view-grid md:grid md:grid-cols-2 md:gap-8 bg-primary-lighter">
+    </header>  
+    <div className="view-grid md:grid md:grid-cols-1 gap-y-4 bg-primary-lighter">
       <div className="view-group">
-        <h3 className="view-group__title">My Group</h3>
+        <h3 className="view-group__title">Residence Address</h3>
         <div className="view-subgrid md:grid md:grid-cols-4 md:gap-8">
-          <p className="view-item md:col-span-2">
-            <span className="view-item__label">Label</span>
-            <span className="view-item__value">Value</span>
-            <span className="view-item__helper">Helper</span>
-          </p>
+          <ViewItem
+            label="Street Address"
+            value="112 Springfield St."
+            className="md:col-span-3"
+          />
 
-          <p className="view-item md:col-span-2">
-            <span className="view-item__label">Label</span>
-            <span className="view-item__value">Value</span>
-            <span className="view-item__helper">Helper</span>
-          </p>
+          <ViewItem
+            label="Apt Unit #"
+            value="1"
+          />
 
-          <p className="view-item md:col-span-2">
-            <span className="view-item__label">Label</span>
-            <span className="view-item__value">Value</span>
-            <span className="view-item__helper">Helper</span>
-          </p>
+          <ViewItem
+            label="City"
+            value="Oakland"
+            className="md:col-span-2"
+          />
 
-          <p className="view-item">
-            <span className="view-item__label">Label</span>
-            <span className="view-item__value">Value</span>
-            <span className="view-item__helper">Helper</span>
-          </p>
+          <ViewItem
+            label="State"
+            value="CA"
+          />
 
-          <p className="view-item">
-            <span className="view-item__label">Label</span>
-            <span className="view-item__value">Value</span>
-            <span className="view-item__helper">Helper</span>
-          </p>
+          <ViewItem
+            label="Zip"
+            value="94577"
+          />
         </div>
       </div>
     </div>
@@ -172,47 +156,47 @@ export const ViewSectionSingleColumn = () => (
     <h3 className="view-group__title">My Group</h3>
 
     <div className="view-grid md:grid md:grid-cols-1 gap-y-4 bg-primary-lighter">
-      <p className="view-item">
-        <span className="view-item__label">First Name</span>
-        <span className="view-item__value">Lisa</span>
-      </p>
+      <ViewItem
+        label="First Name"
+        value="Lisa"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Middle Name</span>
-        <span className="view-item__value">S</span>
-      </p>
+      <ViewItem
+        label="Middle Name"
+        value="S"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Last Name</span>
-        <span className="view-item__value">Jones</span>
-      </p>
+      <ViewItem
+        label="Last Name"
+        value="Jones"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Date of Birth</span>
-        <span className="view-item__value">01/01/1985</span>
-      </p>
+      <ViewItem
+        label="Date of Birth"
+        value="01/01/1985"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Email</span>
-        <span className="view-item__value">lisa@gmail.com</span>
-      </p>
+      <ViewItem
+        label="Email"
+        value="lisa@gmail.com"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Phone</span>
-        <span className="view-item__value">111-222-3333</span>
-        <span className="view-item__helper">Cell</span>
-      </p>
+      <ViewItem
+        label="Phone"
+        value="111-222-3333"
+        helper="Cell"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Second Phone</span>
-        <span className="view-item__value">222-333-4444</span>
-        <span className="view-item__helper">Work</span>
-      </p>
+      <ViewItem
+        label="Second Phone"
+        value="111-222-3333"
+        helper="Work"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Perferred Contact</span>
-        <span className="view-item__value">Phone</span>
-      </p>
+      <ViewItem
+        label="Perferred Contact"
+        value="Phone"
+      />
     </div>
   </div>
 )
@@ -236,47 +220,51 @@ export const ViewSectionSingleColumnWarn = () => (
     <h3 className="view-group__title">My Group</h3>
 
     <div className="view-grid md:grid md:grid-cols-1 gap-y-4 bg-primary-lighter">
-      <p className="view-item is-flagged">
-        <span className="view-item__label">First Name</span>
-        <span className="view-item__value">Lisa</span>
-      </p>
+      <ViewItem
+        label="First Name"
+        value="Lisa"
+        flagged={true}
+      />
 
-      <p className="view-item is-flagged">
-        <span className="view-item__label">Middle Name</span>
-        <span className="view-item__value">S</span>
-      </p>
+      <ViewItem
+        label="Middle Name"
+        value="S"
+        flagged={true}
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Last Name</span>
-        <span className="view-item__value">Jones</span>
-      </p>
+      <ViewItem
+        label="Last Name"
+        value="Jones"
+        flagged={true}
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Date of Birth</span>
-        <span className="view-item__value">01/01/1985</span>
-      </p>
+      <ViewItem
+        label="Date of Birth"
+        value="01/01/1985"
+        flagged={true}
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Email</span>
-        <span className="view-item__value">lisa@gmail.com</span>
-      </p>
+      <ViewItem
+        label="Email"
+        value="lisa@gmail.com"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Phone</span>
-        <span className="view-item__value">111-222-3333</span>
-        <span className="view-item__helper">Cell</span>
-      </p>
+      <ViewItem
+        label="Phone"
+        value="111-222-3333"
+        helper="Cell"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Second Phone</span>
-        <span className="view-item__value">222-333-4444</span>
-        <span className="view-item__helper">Work</span>
-      </p>
+      <ViewItem
+        label="Second Phone"
+        value="111-222-3333"
+        helper="Work"
+      />
 
-      <p className="view-item">
-        <span className="view-item__label">Perferred Contact</span>
-        <span className="view-item__value">Phone</span>
-      </p>
+      <ViewItem
+        label="Perferred Contact"
+        value="Phone"
+      />
     </div>
   </div>
 )
