2a45941a5755f2a62f34f861435cf2f0040385f8
Jared White <jared@jaredwhite.com>
2020-09-15 17:54:20 -0700

    Use client detection for back link

--- apps/public-reference/src/forms/applications/FormBackLink.tsx
+++ apps/public-reference/src/forms/applications/FormBackLink.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link"
-import { t } from "@bloom-housing/ui-components"
+import { OnClientSide, t } from "@bloom-housing/ui-components"
 import ApplicationConductor from "../../../lib/ApplicationConductor"
 
 const FormBackLink = (props: { conductor: ApplicationConductor }) => (
   <p className="form-card__back">
-    {props.conductor.config.steps.length > 1 && (
+    {OnClientSide() && props.conductor.config.steps.length > 1 && (
       <strong>
         <Link href={props.conductor.determinePreviousUrl()}>
           <a>{t("t.back")}</a>
