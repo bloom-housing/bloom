2b3e972d36349525595ffd5939db931ceb2ab507
Ben Peters <bencpeters@gmail.com>
2020-09-23 12:16:00 -0700

    Add Backend Authorization and Roles (#603)
    See `docs/Authentication.md` for more details on how to protect routes & implement permissions.

--- backend/core/src/applications/applications.module.ts
+++ backend/core/src/applications/applications.module.ts
@@ -3,13 +3,14 @@ import { TypeOrmModule } from "@nestjs/typeorm"
 import { Application } from "../entity/application.entity"
 import { ApplicationsService } from "./applications.service"
 import { ApplicationsController } from "./applications.controller"
+import { AuthModule } from "../auth/auth.module"
 import { EmailService } from "../shared/email.service"
 import { ListingsService } from "../listings/listings.service"
 import { CsvEncoder } from "../services/csv-encoder.service"
 import { CsvBuilder } from "../services/csv-builder.service"
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Application])],
+  imports: [TypeOrmModule.forFeature([Application]), AuthModule],
   providers: [ApplicationsService, EmailService, ListingsService, CsvEncoder, CsvBuilder],
   exports: [ApplicationsService],
   controllers: [ApplicationsController],
--- backend/core/src/auth/optional-auth.guard.ts
+++ backend/core/src/auth/optional-auth.guard.ts
@@ -3,7 +3,9 @@ import { Injectable } from "@nestjs/common"
 
 @Injectable()
 export class OptionalAuthGuard extends DefaultAuthGuard {
-  handleRequest(err, user, info) {
-    return user
+  handleRequest(err, user) {
+    // User is literally "false" here when not logged in - return `undefined` instead so that req.user will be
+    // undefined in the not-logged-in case.
+    return user ? user : undefined
   }
 }
