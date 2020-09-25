48ab83eb1f5d604dc17508ca4d7dd3679d83a8eb
Ben Kutler <ben@benkutler.com>
2020-09-25 11:13:43 -0700

    begin to re-enable login flow for public-reference (#722)

--- apps/public-reference/pages/applications/start/choose-language.tsx
+++ apps/public-reference/pages/applications/start/choose-language.tsx
@@ -112,7 +112,7 @@ export default () => {
             </div>
           </Form>
 
-          {/* <div className="form-card__pager-row primary px-4 border-t border-gray-450">
+          <div className="form-card__pager-row primary px-4 border-t border-gray-450">
             <h2 className="form-card__title w-full border-none pt-0 mt-0">
               {t("application.chooseLanguage.haveAnAccount")}
             </h2>
@@ -124,7 +124,7 @@ export default () => {
                 {t("nav.signIn")}
               </LinkButton>
             </div>
-          </div> */}
+          </div>
         </div>
       </FormCard>
     </FormsLayout>
