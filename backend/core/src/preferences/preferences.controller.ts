3ff8866d1bafa7100cb291121f5ac9bf94ac75e3
Michal Plebanski <michalp@airnauts.com>
2020-09-30 12:17:59 +0200

    Make use of nestjsx/CRUD for /preferences

--- backend/core/src/preferences/preferences.controller.ts
+++ backend/core/src/preferences/preferences.controller.ts
@@ -1,67 +1,33 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-  UseGuards,
-  UseInterceptors,
-} from "@nestjs/common"
-import { PreferencesService } from "../preferences/preferences.service"
-import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger"
-import { TransformInterceptor } from "../interceptors/transform.interceptor"
-import { PreferenceDto } from "../preferences/preference.dto"
-import { Preference } from "../entity/preference.entity"
-import { PreferenceCreateDto } from "../preferences/preference.create.dto"
-import { PreferenceUpdateDto } from "../preferences/preference.update.dto"
-import { DefaultAuthGuard } from "../auth/default.guard"
 
 // TODO Add Admin role check
 
-@Controller("/preferences")
-@ApiTags("preferences")
-@ApiBearerAuth()
+import { Controller, UseGuards } from "@nestjs/common"
+import { Crud } from "@nestjsx/crud"
+import { Preference } from "../entity/preference.entity"
+import { PreferenceCreateDto } from "./preference.create.dto"
+import { ResourceType } from "../auth/resource_type.decorator"
+import AuthzGuard from "../auth/authz.guard"
+import { PreferencesService } from "./preferences.service"
+import { DefaultAuthGuard } from "../auth/default.guard"
+
+@Crud({
+  model: {
+    type: Preference,
+  },
+  dto: {
+    create: PreferenceCreateDto,
+  },
+  params: {
+    id: {
+      field: "id",
+      type: "uuid",
+      primary: true,
+    },
+  },
+})
+@Controller("preferences")
+@ResourceType("preference")
+@UseGuards(DefaultAuthGuard, AuthzGuard)
 export class PreferencesController {
-  constructor(private readonly preferencesService: PreferencesService) {}
-
-  @Get()
-  @UseGuards(DefaultAuthGuard)
-  @ApiOperation({ summary: "List preferences", operationId: "list" })
-  @UseInterceptors(new TransformInterceptor(PreferenceDto))
-  async list(): Promise<Preference[]> {
-    return await this.preferencesService.list()
-  }
-
-  @Post()
-  @UseGuards(DefaultAuthGuard)
-  @ApiOperation({ summary: "Create preference", operationId: "create" })
-  @UseInterceptors(new TransformInterceptor(PreferenceDto))
-  async create(@Body() preference: PreferenceCreateDto): Promise<Preference> {
-    return this.preferencesService.create(preference)
-  }
-
-  @Put(`:preferenceId`)
-  @UseGuards(DefaultAuthGuard)
-  @ApiOperation({ summary: "Update preference", operationId: "update" })
-  @UseInterceptors(new TransformInterceptor(PreferenceDto))
-  async update(@Body() preference: PreferenceUpdateDto): Promise<Preference> {
-    return this.preferencesService.update(preference)
-  }
-
-  @Get(`:preferenceId`)
-  @UseGuards(DefaultAuthGuard)
-  @ApiOperation({ summary: "Get preference by id", operationId: "retrieve" })
-  @UseInterceptors(new TransformInterceptor(PreferenceDto))
-  async retrieve(@Param("preferenceId") preferenceId: string): Promise<Preference> {
-    return await this.preferencesService.findOne(preferenceId)
-  }
-
-  @Delete(`:preferenceId`)
-  @UseGuards(DefaultAuthGuard)
-  @ApiOperation({ summary: "Delete preference by id", operationId: "delete" })
-  async delete(@Param("preferenceId") preferenceId: string): Promise<void> {
-    await this.preferencesService.delete(preferenceId)
-  }
+  constructor(public service: PreferencesService) {}
 }
