1e9a02e3d0ffbd1bfa68b48135ad6817d3e5efa3
Michal Plebanski <michalp@airnauts.com>
2020-09-30 11:56:37 +0200

    Make use of nestjsx/CRUD for /assets

--- backend/core/src/assets/assets.controller.ts
+++ backend/core/src/assets/assets.controller.ts
@@ -1,60 +1,30 @@
-import {
-  Body,
-  ClassSerializerInterceptor,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-  UseInterceptors,
-} from "@nestjs/common"
-import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger"
-import { TransformInterceptor } from "../interceptors/transform.interceptor"
+import { Controller, UseGuards } from "@nestjs/common"
 import { AssetsService } from "./assets.service"
 import { Asset } from "../entity/asset.entity"
+import { Crud } from "@nestjsx/crud"
+import { ResourceType } from "../auth/resource_type.decorator"
 import { AssetCreateDto } from "./asset.create.dto"
-import { AssetUpdateDto } from "./asset.update.dto"
-import { AssetDto } from "./asset.dto"
+import AuthzGuard from "../auth/authz.guard"
+import { DefaultAuthGuard } from "../auth/default.guard"
 
-// TODO Add Admin role check
-
+@Crud({
+  model: {
+    type: Asset,
+  },
+  dto: {
+    create: AssetCreateDto,
+  },
+  params: {
+    id: {
+      field: "id",
+      type: "uuid",
+      primary: true,
+    },
+  },
+})
 @Controller("assets")
-@ApiTags("assets")
-@ApiBearerAuth()
+@ResourceType("asset")
+@UseGuards(DefaultAuthGuard, AuthzGuard)
 export class AssetsController {
-  constructor(private readonly assetsService: AssetsService) {}
-
-  @Get()
-  @ApiOperation({ summary: "List assets", operationId: "list" })
-  @UseInterceptors(new TransformInterceptor(AssetDto))
-  async list(): Promise<Asset[]> {
-    return await this.assetsService.list()
-  }
-
-  @Post()
-  @ApiOperation({ summary: "Create asset", operationId: "create" })
-  @UseInterceptors(new TransformInterceptor(AssetDto))
-  async create(@Body() asset: AssetCreateDto): Promise<AssetDto> {
-    return this.assetsService.create(asset)
-  }
-
-  @Put(`:assetId`)
-  @ApiOperation({ summary: "Update asset", operationId: "update" })
-  @UseInterceptors(new TransformInterceptor(AssetDto))
-  async update(@Body() asset: AssetUpdateDto): Promise<AssetDto> {
-    return this.assetsService.update(asset)
-  }
-
-  @Get(`:assetId`)
-  @ApiOperation({ summary: "Get asset by id", operationId: "retrieve" })
-  @UseInterceptors(new TransformInterceptor(AssetDto))
-  async retrieve(@Param("assetId") assetId: string): Promise<AssetDto> {
-    return await this.assetsService.findOne(assetId)
-  }
-  @Delete(`:assetId`)
-  @ApiOperation({ summary: "Delete asset by id", operationId: "delete" })
-  async delete(@Param("assetId") assetId: string): Promise<void> {
-    await this.assetsService.delete(assetId)
-  }
+  constructor(public service: AssetsService) {}
 }
--- backend/core/src/assets/assets.service.ts
+++ backend/core/src/assets/assets.service.ts
@@ -1,43 +1,11 @@
 import { Injectable } from "@nestjs/common"
 import { Asset } from "../entity/asset.entity"
-import { AssetCreateDto } from "./asset.create.dto"
-import { plainToClass } from "class-transformer"
-import { AssetUpdateDto } from "./asset.update.dto"
+import { TypeOrmCrudService } from "@nestjsx/crud-typeorm"
+import { InjectRepository } from "@nestjs/typeorm"
 
 @Injectable()
-export class AssetsService {
-  async list(): Promise<Asset[]> {
-    return Asset.find()
-  }
-
-  async create(assetDto: AssetCreateDto): Promise<Asset> {
-    const asset = plainToClass(Asset, assetDto)
-    await asset.save()
-    return asset
-  }
-
-  async findOne(assetId: string): Promise<Asset> {
-    return Asset.findOneOrFail({
-      where: {
-        id: assetId,
-      },
-      relations: ["listing"],
-    })
-  }
-
-  async delete(assetId: string) {
-    return Asset.delete(assetId)
-  }
-
-  async update(assetDto: AssetUpdateDto) {
-    const asset = await Asset.findOneOrFail({
-      where: {
-        id: assetDto.id,
-      },
-      relations: ["listing"],
-    })
-    Object.assign(asset, assetDto)
-    await asset.save()
-    return asset
+export class AssetsService extends TypeOrmCrudService<Asset> {
+  constructor(@InjectRepository(Asset) repo) {
+    super(repo)
   }
 }
