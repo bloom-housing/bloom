e244deb29eca73736f80d9ba6df131b24dbd77dd
Jared White <jared@jaredwhite.com>
2020-10-03 03:48:08 +0000

    Relocate main stylesheets to global folder

--- apps/partners-reference/pages/_app.jsx
+++ apps/partners-reference/pages/_app.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import App from "next/app"
-import "@bloom-housing/ui-components/styles/index.scss"
+import "@bloom-housing/ui-components/src/global/index.scss"
 import {
   addTranslation,
   ConfigProvider,
--- shared/ui-components/.storybook/preview.ts
+++ shared/ui-components/.storybook/preview.ts
@@ -1,4 +1,4 @@
-import "@bloom-housing/ui-components/styles/index.scss"
+import "@bloom-housing/ui-components/src/global/index.scss"
 
 // Set up translation file
 import { addTranslation } from "../src/helpers/translator"
