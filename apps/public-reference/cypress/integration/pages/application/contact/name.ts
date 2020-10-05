5b435f5c94c90132566d6fe45a79cf4ee6aedf11
dominikx96 <dominik@airnauts.com>
2020-10-05 10:37:04 +0200

    Created fixture & updated test to check context value

--- apps/public-reference/cypress/integration/pages/application/contact/name.ts
+++ apps/public-reference/cypress/integration/pages/application/contact/name.ts
@@ -1,5 +1,6 @@
 describe("Form contact/name", function () {
   beforeEach(() => {
+    cy.fixture("name.json").as("valuesJSON")
     cy.visit("/applications/contact/name")
   })
 
@@ -13,21 +14,10 @@ describe("Form contact/name", function () {
     cy.goNext()
 
     // check errors
-    cy.getByID("applicant.firstName-error")
-      .should("be.visible")
-      .and("contain", "Please enter a First Name")
-
-    cy.getByID("applicant.lastName-error")
-      .should("be.visible")
-      .and("contain", "Please enter a Last Name")
-
-    cy.get(".error-message")
-      .should("be.visible")
-      .and("contain", "Please enter a valid Date of Birth")
-
-    cy.getByID("applicant.emailAddress-error")
-      .should("be.visible")
-      .and("contain", "Please enter an email address")
+    cy.getByID("applicant.firstName-error").should("be.visible").and("not.to.be.empty")
+    cy.getByID("applicant.lastName-error").should("be.visible").and("not.to.be.empty")
+    cy.getByID("applicant.dateOfBirth-error").should("be.visible").and("not.to.be.empty")
+    cy.getByID("applicant.emailAddress-error").should("be.visible").and("not.to.be.empty")
   })
 
   it("Should test incorrect email value", function () {
@@ -35,9 +25,7 @@ describe("Form contact/name", function () {
 
     cy.goNext()
 
-    cy.getByID("applicant.emailAddress-error")
-      .should("be.visible")
-      .and("contain", "Please enter an email address")
+    cy.getByID("applicant.emailAddress-error").should("be.visible").and("not.to.be.empty")
   })
 
   it("Shouldn't notify email error when checkbox is selected", function () {
@@ -48,18 +36,31 @@ describe("Form contact/name", function () {
     cy.getByID("applicant.emailAddress-error").should("not.exist")
   })
 
-  it("should save form values", function () {
-    cy.getByID("applicant.firstName").type("Name")
-    cy.getByID("applicant.middleName").type("Middle Name")
-    cy.getByID("applicant.lastName").type("Last Name")
-    cy.getByID("applicant.birthMonth").type("07")
-    cy.getByID("applicant.birthDay").type("17")
-    cy.getByID("applicant.birthYear").type("1996")
-    cy.getByID("applicant.emailAddress").type("test@bloom.com")
+  it("should save form values and redirect to the next step", function () {
+    cy.getByID("applicant.firstName").type(this.valuesJSON["applicant.firstName"])
+    cy.getByID("applicant.middleName").type(this.valuesJSON["applicant.middleName"])
+    cy.getByID("applicant.lastName").type(this.valuesJSON["applicant.lastName"])
+    cy.getByID("applicant.birthMonth").type(this.valuesJSON["applicant.birthMonth"])
+    cy.getByID("applicant.birthDay").type(this.valuesJSON["applicant.birthDay"])
+    cy.getByID("applicant.birthYear").type(this.valuesJSON["applicant.birthYear"])
+    cy.getByID("applicant.emailAddress").type(this.valuesJSON["applicant.emailAddress"])
 
     cy.goNext()
 
     // no errors should be visible
     cy.get(".error-message").should("not.exist")
+
+    // check context values
+    cy.getSubmissionContext().its("applicant").should("deep.include", {
+      birthDay: this.valuesJSON["applicant.birthDay"],
+      birthMonth: this.valuesJSON["applicant.birthMonth"],
+      birthYear: this.valuesJSON["applicant.birthYear"],
+      emailAddress: this.valuesJSON["applicant.emailAddress"],
+      firstName: this.valuesJSON["applicant.firstName"],
+      lastName: this.valuesJSON["applicant.lastName"],
+      middleName: this.valuesJSON["applicant.middleName"],
+    })
+
+    // TODO: check next step (when steps config will be ready)
   })
 })
