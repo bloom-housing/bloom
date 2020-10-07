93916b869689174da84a3ed36ca64c64f2897bfc
Michal Plebanski <michalp@airnauts.com>
2020-10-07 14:09:01 +0200

    Optimize controller decorators

--- backend/core/src/assets/assets.controller.ts
+++ backend/core/src/assets/assets.controller.ts
@@ -12,7 +12,6 @@ import {
 import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger"
 import { TransformInterceptor } from "../interceptors/transform.interceptor"
 import { AssetsService } from "./assets.service"
-import { Asset } from "../entity/asset.entity"
 import { AssetCreateDto } from "./asset.create.dto"
 import { AssetUpdateDto } from "./asset.update.dto"
 import { AssetDto } from "./asset.dto"
@@ -23,35 +22,32 @@ import { ResourceType } from "../auth/resource_type.decorator"
 @Controller("assets")
 @ApiTags("assets")
 @ApiBearerAuth()
-@UseGuards(DefaultAuthGuard, AuthzGuard)
 @ResourceType("asset")
+@UseGuards(DefaultAuthGuard, AuthzGuard)
+@UseInterceptors(new TransformInterceptor(AssetDto))
 export class AssetsController {
   constructor(private readonly assetsService: AssetsService) {}
 
   @Get()
   @ApiOperation({ summary: "List assets", operationId: "list" })
-  @UseInterceptors(new TransformInterceptor(AssetDto))
-  async list(): Promise<Asset[]> {
+  async list(): Promise<AssetDto[]> {
     return await this.assetsService.list()
   }
 
   @Post()
   @ApiOperation({ summary: "Create asset", operationId: "create" })
-  @UseInterceptors(new TransformInterceptor(AssetDto))
   async create(@Body() asset: AssetCreateDto): Promise<AssetDto> {
     return this.assetsService.create(asset)
   }
 
   @Put(`:assetId`)
   @ApiOperation({ summary: "Update asset", operationId: "update" })
-  @UseInterceptors(new TransformInterceptor(AssetDto))
   async update(@Body() asset: AssetUpdateDto): Promise<AssetDto> {
     return this.assetsService.update(asset)
   }
 
   @Get(`:assetId`)
   @ApiOperation({ summary: "Get asset by id", operationId: "retrieve" })
-  @UseInterceptors(new TransformInterceptor(AssetDto))
   async retrieve(@Param("assetId") assetId: string): Promise<AssetDto> {
     return await this.assetsService.findOne(assetId)
   }
