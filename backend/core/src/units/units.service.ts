65bd76233eddb82459310ec9741ec9a9a41e5b97
Michał Plebański <michalp@airnauts.com>
2020-10-08 22:12:28 +0200

    Add AbstractService and use it for assets, units, preferences and application methods (#744)

--- backend/core/src/units/units.service.ts
+++ backend/core/src/units/units.service.ts
@@ -1,39 +1,12 @@
+import { InjectRepository } from "@nestjs/typeorm"
+import { AbstractService } from "../shared/abstract-service"
 import { Unit } from "../entity/unit.entity"
-import { plainToClass } from "class-transformer"
 import { UnitCreateDto } from "./unit.create.dto"
 import { UnitUpdateDto } from "./unit.update.dto"
+import { Repository } from "typeorm"
 
-export class UnitsService {
-  async list(): Promise<Unit[]> {
-    return Unit.find()
-  }
-
-  async create(unitDto: UnitCreateDto): Promise<Unit> {
-    const unit = plainToClass(Unit, unitDto)
-    await unit.save()
-    return unit
-  }
-
-  async findOne(unitId: string): Promise<Unit> {
-    return Unit.findOneOrFail({
-      where: {
-        id: unitId,
-      },
-    })
-  }
-
-  async delete(unitId: string) {
-    await Unit.delete(unitId)
-  }
-
-  async update(unitDto: UnitUpdateDto) {
-    const unit = await Unit.findOneOrFail({
-      where: {
-        id: unitDto.id,
-      },
-    })
-    Object.assign(unit, unitDto)
-    await unit.save()
-    return unit
+export class UnitsService extends AbstractService<Unit, UnitCreateDto, UnitUpdateDto> {
+  constructor(@InjectRepository(Unit) protected readonly repository: Repository<Unit>) {
+    super(repository)
   }
 }
