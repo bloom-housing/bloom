741a5ffac1aa97f17fb8a20d8d9658301abf2d8f
dominikx96 <dominik@airnauts.com>
2020-10-16 13:23:54 +0200

    Updated test to check agency field

--- apps/public-reference/cypress/integration/pages/application/contact/alternate-contact-name.ts
+++ apps/public-reference/cypress/integration/pages/application/contact/alternate-contact-name.ts
@@ -1,16 +1,20 @@
 describe("applications/contact/alternate-contact-name", function () {
   beforeEach(() => {
-    cy.loadConfig()
     cy.fixture("applications/alternate-contact-name.json").as("data")
-    cy.visit("/applications/contact/alternate-contact-name")
   })
 
   it("Should render form", function () {
+    cy.loadConfig()
+    cy.visit("/applications/contact/alternate-contact-name")
+
     cy.get("form").should("be.visible")
     cy.location("pathname").should("include", "applications/contact/alternate-contact-name")
   })
 
-  it("Should display initial form errors", function () {
+  it("Should display initial form errors without agency field", function () {
+    cy.loadConfig()
+    cy.visit("/applications/contact/alternate-contact-name")
+
     cy.goNext()
 
     cy.checkErrorAlert("be.visible")
@@ -19,11 +23,33 @@ describe("applications/contact/alternate-contact-name", function () {
     cy.getByID("lastName-error").should("be.visible").and("not.to.be.empty")
   })
 
-  // TODO: check agency if has been set in the previous step
+  it("Should display initial form errors with agency field", function () {
+    // set initial value for type to show additional agency step
+    cy.loadConfig({
+      "alternateContact.type": "caseManager",
+    })
+    cy.visit("/applications/contact/alternate-contact-name")
+
+    cy.getByID("agency").should("be.visible")
+
+    cy.goNext()
+
+    cy.checkErrorAlert("be.visible")
+
+    cy.getByID("firstName-error").should("be.visible").and("not.to.be.empty")
+    cy.getByID("lastName-error").should("be.visible").and("not.to.be.empty")
+    cy.getByID("agency-error").should("be.visible").and("not.to.be.empty")
+  })
 
   it("Should save form values and redirect to the next step", function () {
+    cy.loadConfig({
+      "alternateContact.type": "caseManager",
+    })
+    cy.visit("/applications/contact/alternate-contact-name")
+
     cy.getByID("firstName").type(this.data["firstName"])
     cy.getByID("lastName").type(this.data["lastName"])
+    cy.getByID("agency").type(this.data["agency"])
 
     cy.goNext()
 
@@ -38,6 +64,7 @@ describe("applications/contact/alternate-contact-name", function () {
     cy.getSubmissionContext().its("alternateContact").should("deep.nested.include", {
       firstName: this.data["firstName"],
       lastName: this.data["lastName"],
+      agency: this.data["agency"],
     })
   })
 })
