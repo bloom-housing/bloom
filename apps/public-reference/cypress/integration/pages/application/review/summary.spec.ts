2cd506e09b16fbac02f970f942280ac1f34c4bbd
dominikx96 <dominik@airnauts.com>
2020-10-19 11:37:59 +0200

    Updated summary test

--- apps/public-reference/cypress/integration/pages/application/review/summary.spec.ts
+++ apps/public-reference/cypress/integration/pages/application/review/summary.spec.ts
@@ -1,3 +1,15 @@
 describe("applications/review/summary", function () {
   const route = "/applications/review/summary"
+
+  beforeEach(() => {
+    cy.fixture("applications/summary.json").as("data")
+  })
+
+  it("Should render 'You' section and check displaled values", function () {
+    cy.loadConfig({}, "filledApplication.json")
+    cy.visit(route)
+
+    cy.getByID("applicantName").should("include.text", this.data.you.name)
+    cy.getByID("applicantbirthDay").should("include.text", this.data.you.dateOfBirth)
+  })
 })
