bb856af776647fe350e9a3be7b97e97d05c5e16b
Michal Plebanski <michalp@airnauts.com>
2020-10-19 13:52:43 +0200

    Update frontend client

--- shared/ui-components/src/authentication/UserContext.tsx
+++ shared/ui-components/src/authentication/UserContext.tsx
@@ -8,7 +8,7 @@ import {
 } from "react"
 import Router, { useRouter } from "next/router"
 import { createAction, createReducer } from "typesafe-actions"
-import { User, CreateUserDto } from "@bloom-housing/backend-core"
+import { User } from "@bloom-housing/backend-core"
 import { clearToken, getToken, getTokenTtl, setToken } from "./token"
 import {
   createAxiosInstance,
@@ -19,10 +19,11 @@ import {
 } from "./api_requests"
 import { ConfigContext } from "../config/ConfigContext"
 import { t } from "@bloom-housing/ui-components"
+import { UserCreateDto } from "@bloom-housing/backend-core/client"
 // External interface this context provides
 type ContextProps = {
   login: (email: string, password: string) => Promise<User>
-  createUser: (user: CreateUserDto) => Promise<User>
+  createUser: (user: UserCreateDto) => Promise<User>
   signOut: () => void
   // True when an API request is processing
   loading: boolean
@@ -154,7 +155,7 @@ export const UserProvider: FunctionComponent = ({ children }) => {
         dispatch(stopLoading())
       }
     },
-    createUser: async (user: CreateUserDto) => {
+    createUser: async (user: UserCreateDto) => {
       dispatch(startLoading())
       try {
         const { accessToken, user: profile } = await register(apiUrl, user)
