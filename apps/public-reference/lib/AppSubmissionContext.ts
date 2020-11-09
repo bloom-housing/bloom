147a049e72a163c4337411e6aeb8a45f26123c0e
dominikx96 <dominik@airnauts.com>
2020-11-09 17:07:07 +0100

    Moved blank application to shared

--- apps/public-reference/lib/AppSubmissionContext.ts
+++ apps/public-reference/lib/AppSubmissionContext.ts
@@ -1,6 +1,6 @@
 import React from "react"
 import ApplicationConductor from "./ApplicationConductor"
-import { ApplicationData } from "@bloom-housing/core"
+import { BlankApplicationFields } from "@bloom-housing/ui-components"
 
 export const retrieveApplicationConfig = () => {
   // Note: this whole function will eventually be replaced with one that reads this from the backend.
@@ -73,95 +73,7 @@ export const blankApplication = () => {
   return {
     loaded: false,
     completedSections: 0,
-    applicant: {
-      firstName: "",
-      middleName: "",
-      lastName: "",
-      birthMonth: 0,
-      birthDay: 0,
-      birthYear: 0,
-      emailAddress: "",
-      noEmail: false,
-      phoneNumber: "",
-      phoneNumberType: "",
-      noPhone: false,
-      workInRegion: null,
-      address: {
-        street: "",
-        street2: "",
-        city: "",
-        state: "",
-        zipCode: "",
-        county: "",
-        latitude: null,
-        longitude: null,
-      },
-      workAddress: {
-        street: "",
-        street2: "",
-        city: "",
-        state: "",
-        zipCode: "",
-        county: "",
-        latitude: null,
-        longitude: null,
-      },
-    },
-    additionalPhone: false,
-    additionalPhoneNumber: "",
-    additionalPhoneNumberType: "",
-    contactPreferences: [],
-    householdSize: 0,
-    housingStatus: "",
-    sendMailToMailingAddress: false,
-    mailingAddress: {
-      street: "",
-      street2: "",
-      city: "",
-      state: "",
-      zipCode: "",
-    },
-    alternateAddress: {
-      street: "",
-      street2: "",
-      city: "",
-      state: "",
-      zipCode: "",
-    },
-    alternateContact: {
-      type: "",
-      otherType: "",
-      firstName: "",
-      lastName: "",
-      agency: "",
-      phoneNumber: "",
-      emailAddress: "",
-      mailingAddress: {
-        street: "",
-        city: "",
-        state: "",
-        zipCode: "",
-      },
-    },
-    accessibility: {
-      mobility: null,
-      vision: null,
-      hearing: null,
-    },
-    incomeVouchers: null,
-    income: null,
-    incomePeriod: null,
-    householdMembers: [],
-    preferredUnit: [],
-    demographics: {
-      ethnicity: "",
-      race: "",
-      gender: "",
-      sexualOrientation: "",
-      howDidYouHear: "",
-    },
-    preferences: {} as Record<string, any>,
-    confirmationId: "",
+    ...BlankApplicationFields,
   }
 }
 
