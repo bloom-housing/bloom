58e496e2354836240a66c0153b41876d4136b4e6
Michal Plebanski <michalp@airnauts.com>
2020-11-10 14:42:37 +0100

    Work in progress

--- backend/core/src/entity/application.entity.ts
+++ backend/core/src/entity/application.entity.ts
@@ -5,6 +5,9 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   ManyToOne,
+  OneToOne,
+  JoinColumn,
+  OneToMany,
 } from "typeorm"
 import { User } from "./user.entity"
 import { Listing } from "./listing.entity"
@@ -52,62 +55,95 @@ export class HousingCounselor {
   website: string | null
 }
 
+@Entity()
 export class Applicant {
+  @PrimaryGeneratedColumn("uuid")
+  @Expose()
+  @IsString()
+  @IsUUID()
+  id: string
+
+  @CreateDateColumn()
+  @Expose()
+  @IsDate()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  @Expose()
+  @IsDate()
+  updatedAt: Date
+
+  @Column()
   @Expose()
   @IsString()
   firstName: string
 
+  @Column()
   @Expose()
   @IsString()
   middleName: string
 
+  @Column()
   @Expose()
   @IsString()
   lastName: string
 
+  @Column()
   @Expose()
   @IsNumber()
   birthMonth: number
 
+  @Column()
   @Expose()
   @IsNumber()
   birthDay: number
 
+  @Column()
   @Expose()
   @IsNumber()
   birthYear: number
 
+  @Column()
   @Expose()
   @IsString()
   emailAddress: string
 
+  @Column()
   @Expose()
   @IsBoolean()
   noEmail: boolean
 
+  @Column()
   @Expose()
   @IsString()
   phoneNumber: string
 
+  @Column()
   @Expose()
   @IsString()
   phoneNumberType: string
 
+  @Column()
   @Expose()
   @IsBoolean()
   noPhone: boolean
 
+  @Column()
   @Expose()
   @IsOptional()
   @IsBoolean()
   workInRegion: boolean | null
 
+  @OneToOne(() => Address, { eager: true, cascade: true })
+  @JoinColumn()
   @Expose()
   @IsDefined()
   @ValidateNested()
   @Type(() => Address)
   workAddress: Address
 
+  @OneToOne(() => Address, { eager: true, cascade: true })
+  @JoinColumn()
   @Expose()
   @IsDefined()
   @ValidateNested()
@@ -116,34 +152,58 @@ export class Applicant {
 }
 
 export class AlternateContact {
+  @PrimaryGeneratedColumn("uuid")
+  @Expose()
+  @IsString()
+  @IsUUID()
+  id: string
+
+  @CreateDateColumn()
+  @Expose()
+  @IsDate()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  @Expose()
+  @IsDate()
+  updatedAt: Date
+
+  @Column()
   @Expose()
   @IsString()
   type: string
 
+  @Column()
   @Expose()
   @IsString()
   otherType: string
 
+  @Column()
   @Expose()
   @IsString()
   firstName: string
 
+  @Column()
   @Expose()
   @IsString()
   lastName: string
 
+  @Column()
   @Expose()
   @IsString()
   agency: string
 
+  @Column()
   @Expose()
   @IsString()
   phoneNumber: string
 
+  @Column()
   @Expose()
   @IsString()
   emailAddress: string
 
+  @OneToOne(() => Address, { eager: true, cascade: true })
   @Expose()
   @IsDefined()
   @ValidateNested()
@@ -152,16 +212,35 @@ export class AlternateContact {
 }
 
 export class Accessibility {
+  @PrimaryGeneratedColumn("uuid")
+  @Expose()
+  @IsString()
+  @IsUUID()
+  id: string
+
+  @CreateDateColumn()
+  @Expose()
+  @IsDate()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  @Expose()
+  @IsDate()
+  updatedAt: Date
+
+  @Column({ type: "boolean", nullable: true })
   @Expose()
   @IsOptional()
   @IsBoolean()
   mobility: boolean | null
 
+  @Column({ type: "boolean", nullable: true })
   @Expose()
   @IsOptional()
   @IsBoolean()
   vision: boolean | null
 
+  @Column({ type: "boolean", nullable: true })
   @Expose()
   @IsOptional()
   @IsBoolean()
@@ -169,98 +248,154 @@ export class Accessibility {
 }
 
 export class Demographics {
+  @PrimaryGeneratedColumn("uuid")
+  @Expose()
+  @IsString()
+  @IsUUID()
+  id: string
+
+  @CreateDateColumn()
+  @Expose()
+  @IsDate()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  @Expose()
+  @IsDate()
+  updatedAt: Date
+
+  @Column()
   @Expose()
   @IsString()
   ethnicity: string
 
+  @Column()
   @Expose()
   @IsString()
   gender: string
 
+  @Column()
   @Expose()
   @IsString()
   sexualOrientation: string
 
+  @Column()
   @Expose()
   @IsString()
   howDidYouHear: string
 
+  @Column()
   @Expose()
   @IsString()
   race: string
 }
 
 export class HouseholdMember {
+  @PrimaryGeneratedColumn("uuid")
+  @Expose()
+  @IsString()
+  @IsUUID()
+  id: string
+
+  @CreateDateColumn()
+  @Expose()
+  @IsDate()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  @Expose()
+  @IsDate()
+  updatedAt: Date
+
+  @Column({ type: "integer", nullable: true })
   @Expose()
   @IsOptional()
   @IsNumber()
-  id?: number | null
+  // TODO Add on frontend
+  orderId?: number | null
 
+  @OneToOne(() => Address, { eager: true, cascade: true })
+  @JoinColumn()
   @Expose()
   @IsDefined()
   @ValidateNested()
   @Type(() => Address)
   address: Address
 
+  @Column()
   @Expose()
   @IsString()
   firstName: string
 
+  @Column()
   @Expose()
   @IsString()
   middleName: string
 
+  @Column()
   @Expose()
   @IsString()
   lastName: string
 
+  @Column()
   @Expose()
   @IsNumber()
   birthMonth: number
 
+  @Column()
   @Expose()
   @IsNumber()
   birthDay: number
 
+  @Column()
   @Expose()
   @IsNumber()
   birthYear: number
 
+  @Column()
   @Expose()
   @IsString()
   emailAddress: string
 
+  @Column()
   @Expose()
   @IsBoolean()
   noEmail: boolean
 
+  @Column()
   @Expose()
   @IsString()
   phoneNumber: string
 
+  @Column()
   @Expose()
   @IsString()
   phoneNumberType: string
 
+  @Column()
   @Expose()
   @IsBoolean()
   noPhone: boolean
 
+  @Column({ type: "boolean", nullable: true })
   @Expose()
   @IsOptional()
   @IsBoolean()
   sameAddress?: boolean | null
 
+  @Column({ type: "text", nullable: true })
   @Expose()
   @IsOptional()
   @IsString()
   relationship?: string | null
 
-  @Expose()
+  @Column({ type: "boolean", nullable: true })
   @IsOptional()
   @IsBoolean()
   workInRegion?: boolean | null
 
+  @OneToOne(() => Address, { eager: true, cascade: true })
+  @JoinColumn()
   @Expose()
   @IsOptional()
   @ValidateNested()
@@ -268,96 +403,144 @@ export class HouseholdMember {
   workAddress?: Address | null
 }
 
+@Entity()
 export class ApplicationData {
+  @PrimaryGeneratedColumn("uuid")
+  @Expose()
+  @IsString()
+  @IsUUID()
+  id: string
+
+  @CreateDateColumn()
+  @Expose()
+  @IsDate()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  @Expose()
+  @IsDate()
+  updatedAt: Date
+
+  @OneToOne(() => Applicant, { eager: true, cascade: true })
+  @JoinColumn()
   @Expose()
   @ValidateNested()
   @Type(() => Applicant)
   applicant: Applicant
 
+  @Column()
   @Expose()
   @IsBoolean()
   additionalPhone: boolean
 
+  @Column()
   @Expose()
   @IsString()
   additionalPhoneNumber: string
 
+  @Column()
   @Expose()
   @IsString()
   additionalPhoneNumberType: string
 
+  @Column("text", { array: true })
   @Expose()
   @IsString({ each: true })
-  contactPreferences: Array<string>
+  contactPreferences: string[]
 
+  @Column()
   @Expose()
   @IsNumber()
   householdSize: number
 
+  @Column()
   @Expose()
   @IsString()
   housingStatus: string
 
+  @Column()
   @Expose()
   @IsBoolean()
   sendMailToMailingAddress: boolean
 
+  @OneToOne(() => Address, { eager: true, cascade: true })
+  @JoinColumn()
   @Expose()
   @IsDefined()
   @ValidateNested()
   @Type(() => Address)
   mailingAddress: Address
 
+  @OneToOne(() => Address, { eager: true, cascade: true })
+  @JoinColumn()
   @Expose()
   @IsDefined()
   @ValidateNested()
   @Type(() => Address)
   alternateAddress: Address
 
+  @OneToOne(() => AlternateContact, { eager: true, cascade: true })
+  @JoinColumn()
   @Expose()
   @IsDefined()
   @ValidateNested()
   @Type(() => AlternateContact)
   alternateContact: AlternateContact
 
+  @OneToOne(() => Accessibility, { eager: true, cascade: true })
+  @JoinColumn()
   @Expose()
   @IsDefined()
   @ValidateNested()
   @Type(() => Accessibility)
   accessibility: Accessibility
 
+  @OneToOne(() => Demographics, { eager: true, cascade: true })
+  @JoinColumn()
   @Expose()
   @IsDefined()
   @ValidateNested()
   @Type(() => Demographics)
   demographics: Demographics
 
+  @Column()
   @Expose()
   @IsString()
   incomeVouchers: string
 
+  @Column()
   @Expose()
   @IsString()
   income: string
 
+  @Column()
   @Expose()
   @IsString()
   incomePeriod: string
 
+  @OneToMany(() => HouseholdMember, (householdMember) => householdMember.applicationData, {
+    eager: true,
+    cascade: true,
+  })
   @Expose()
   @ValidateNested({ each: true })
   @Type(() => HouseholdMember)
-  householdMembers: Array<HouseholdMember>
+  householdMembers: HouseholdMember[]
 
+  @Column({ type: "text", array: true })
   @Expose()
   @IsString({ each: true })
-  preferredUnit: Array<string>
+  preferredUnit: string[]
 
+  // TODO Make preferences easy to work with SQL
+  @Column({ type: "jsonb" })
   @Expose()
   @IsDefined()
   @IsObject()
   preferences: Record<string, any>
 }
+
+export class 
 
 @Entity({ name: "applications" })
 export class Application {
@@ -388,7 +571,8 @@ export class Application {
   @ManyToOne(() => Listing, (listing) => listing.applications)
   listing: Listing
 
-  @Column({ type: "jsonb", nullable: true })
+  @OneToOne(() => ApplicationData, { eager: true, cascade: true })
+  @JoinColumn()
   @Expose()
   @IsDefined()
   @ValidateNested()
--- backend/core/src/shared/dto/address.dto.ts
+++ backend/core/src/shared/dto/address.dto.ts
@@ -1,47 +1,73 @@
 import { Expose } from "class-transformer"
-import { IsDefined, IsNumber, IsOptional, IsString } from "class-validator"
+import { IsDate, IsDefined, IsNumber, IsOptional, IsString, IsUUID } from "class-validator"
+import { Column, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
 
 export class Address {
+  @PrimaryGeneratedColumn("uuid")
+  @Expose()
+  @IsString()
+  @IsUUID()
+  id: string
+
+  @CreateDateColumn()
+  @Expose()
+  @IsDate()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  @Expose()
+  @IsDate()
+  updatedAt: Date
+
+  @Column({ type: "text", nullable: true })
   @Expose()
   @IsOptional()
   @IsString()
   placeName?: string
 
+  @Column()
   @Expose()
   @IsDefined()
   @IsString()
   city: string
 
+  @Column({ type: "text", nullable: true })
   @Expose()
   @IsOptional()
   @IsString()
   county?: string
 
+  @Column()
   @Expose()
   @IsDefined()
   @IsString()
   state: string
 
+  @Column()
   @Expose()
   @IsDefined()
   @IsString()
   street: string
 
+  @Column({ type: "text", nullable: true })
   @Expose()
   @IsOptional()
   @IsString()
   street2?: string
 
+  @Column()
   @Expose()
   @IsDefined()
   @IsString()
   zipCode: string
 
+  @Column({ type: "number", nullable: true })
   @Expose()
   @IsOptional()
   @IsNumber()
   latitude?: number
 
+  @Column({ type: "number", nullable: true })
   @Expose()
   @IsOptional()
   @IsNumber()
