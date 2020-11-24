4922214c75c3cd3f6058de9ed31c6f038230e673
dominikx96 <dominikx96@gmail.com>
2020-11-24 22:49:57 +0100

    Feature/applications table cleanup (#849)
    * Updated income cell
    
    * Defined helper to format income
    
    * Added missing translations for applications table
    
    * Updated application table data
    
    * Updated columns data
    
    * Use period type from the backend
    
    * Set applicant address as mailing when user doesn't have mailing address & cleanup
    
    * Defined applications table translations
    
    * Moved col definitions to separated file
    
    * Updated translations & imported col defs
    
    * Show only one income type & prevent render undefined data on pagination use
    
    Co-authored-by: dominikx96 <dominik@airnauts.com>

--- apps/partners-reference/pages/applications.tsx
+++ apps/partners-reference/pages/applications.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, useRef } from "react"
+import React, { useState, useEffect, useContext, useRef, useMemo } from "react"
 import { useRouter } from "next/router"
 import moment from "moment"
 import Head from "next/head"
@@ -15,6 +15,7 @@ import { useApplicationsData } from "../lib/hooks"
 import Layout from "../layouts/application"
 import { useForm } from "react-hook-form"
 import { AgGridReact } from "ag-grid-react"
+import { getColDefs } from "../src/applications/applicationsColDefs"
 
 const ApplicationsList = () => {
   const metaDescription = t("pageDescription.welcome", { regionName: t("region.name") })
@@ -67,7 +68,6 @@ const ApplicationsList = () => {
   }
 
   const onExport = async () => {
-    console.log("exporting...")
     const content = await applicationsService.listAsCsv({ listingId, includeHeaders: true })
     const now = new Date()
     const dateString = moment(now).format("YYYY-MM-DD_HH:mm:ss")
@@ -106,342 +106,22 @@ const ApplicationsList = () => {
     maxWidth: 300,
   }
 
-  const columnDefs = [
-    {
-      headerName: "Application Submission Date",
-      field: "createdAt",
-      sortable: true,
-      filter: false,
-      pinned: "left",
-      width: 200,
-      minWidth: 150,
-      sort: "asc",
-      valueFormatter: ({ value }) => {
-        const date = moment(value).format("MM/DD/YYYY")
-        const time = moment(value).format("HH:mm:ssA")
+  // get the highest value from householdSize and limit to 6
+  const maxHouseholdSize = useMemo(() => {
+    let max = 1
 
-        return `${date} ${t("t.at")} ${time}`
-      },
-    },
-    {
-      headerName: "Application Number",
-      field: "id",
-      sortable: false,
-      filter: false,
-      width: 150,
-      minWidth: 120,
-      pinned: "left",
-      type: "rightAligned",
-      cellRenderer: "formatLinkCell",
-    },
-    {
-      headerName: "First Name",
-      field: "application.applicant.firstName",
-      sortable: true,
-      filter: false,
-      pinned: "left",
-      width: 125,
-      minWidth: 100,
-    },
-    {
-      headerName: "Last Name",
-      field: "application.applicant.lastName",
-      sortable: true,
-      filter: "agTextColumnFilter",
-      pinned: "left",
-      width: 125,
-      minWidth: 100,
-    },
-    {
-      headerName: "Household Size",
-      field: "application.householdSize",
-      sortable: false,
-      filter: false,
-      width: 125,
-      minWidth: 120,
-      type: "rightAligned",
-    },
-    {
-      headerName: "Declared Annual Income",
-      field: "application.income",
-      sortable: false,
-      filter: false,
-      width: 180,
-      minWidth: 150,
-      type: "rightAligned",
-    },
-    {
-      headerName: "Requested ADA",
-      field: "application.incomeVouchers",
-      sortable: false,
-      filter: false,
-      width: 120,
-      minWidth: 100,
-      valueFormatter: (data) => (data.value ? t("t.yes") : t("t.no")),
-    },
-    {
-      headerName: "Preference Claimed",
-      field: "application.preferences.youHaveClaimed",
-      sortable: true,
-      filter: false,
-      width: 150,
-      minWidth: 100,
-    },
-    {
-      headerName: "Primary DOB",
-      field: "application.household.member.dateOfBirth",
-      sortable: false,
-      filter: false,
-      width: 150,
-      minWidth: 100,
-    },
-    {
-      headerName: "Email",
-      field: "application.applicant.emailAddress",
-      sortable: false,
-      filter: false,
-      width: 150,
-      minWidth: 100,
-    },
-    {
-      headerName: "Phone",
-      field: "application.applicant.phoneNumber",
-      sortable: false,
-      filter: false,
-      width: 150,
-      minWidth: 100,
-    },
-    {
-      headerName: "Residence Address",
-      field: "application.applicant.address.street",
-      sortable: false,
-      filter: false,
-      width: 175,
-      minWidth: 150,
-    },
-    {
-      headerName: "Residence City",
-      field: "application.applicant.address.city",
-      sortable: false,
-      filter: false,
-      width: 150,
-      minWidth: 120,
-    },
-    {
-      headerName: "Residence State",
-      field: "application.applicant.address.state",
-      sortable: false,
-      filter: false,
-      width: 120,
-      minWidth: 100,
-    },
-    {
-      headerName: "Residence Zip",
-      field: "application.applicant.address.zipCode",
-      sortable: false,
-      filter: false,
-      width: 120,
-      minWidth: 100,
-    },
-    {
-      headerName: "Mailing Address",
-      field: "application.applicant.mailingAddress.street",
-      sortable: false,
-      filter: false,
-      width: 175,
-      minWidth: 150,
-    },
-    {
-      headerName: "Mailing City",
-      field: "application.applicant.mailingAddress.city",
-      sortable: false,
-      filter: false,
-      width: 150,
-      minWidth: 120,
-    },
-    {
-      headerName: "Mailing State",
-      field: "application.applicant.mailingAddress.state",
-      sortable: false,
-      filter: false,
-      width: 120,
-      minWidth: 100,
-    },
-    {
-      headerName: "Mailing Zip",
-      field: "application.applicant.mailingAddress.zipCode",
-      sortable: false,
-      filter: false,
-      width: 120,
-      minWidth: 100,
-    },
-    {
-      headerName: "Work Address",
-      field: "application.applicant.workAddress.street",
-      sortable: false,
-      filter: false,
-      width: 175,
-      minWidth: 150,
-    },
-    {
-      headerName: "Work City",
-      field: "application.applicant.workAddress.city",
-      sortable: false,
-      filter: false,
-      width: 150,
-      minWidth: 120,
-    },
-    {
-      headerName: "Work State",
-      field: "application.applicant.workAddress.state",
-      sortable: false,
-      filter: false,
-      width: 120,
-      minWidth: 100,
-    },
-    {
-      headerName: "Work Zip",
-      field: "application.applicant.workAddress.zipCode",
-      sortable: false,
-      filter: false,
-      width: 120,
-      minWidth: 100,
-    },
-    {
-      headerName: "Alt Contact First Name",
-      field: "application.alternateContact.firstName",
-      sortable: false,
-      filter: false,
-      width: 125,
-      minWidth: 100,
-    },
-    {
-      headerName: "Alt Contact Last Name",
-      field: "application.alternateContact.lastName",
-      sortable: false,
-      filter: false,
-      width: 125,
-      minWidth: 100,
-    },
-    {
-      headerName: "Alt Contact Email",
-      field: "application.alternateContact.emailAddress",
-      sortable: false,
-      filter: false,
-      width: 150,
-      minWidth: 100,
-    },
-    {
-      headerName: "Alt Contact Phone",
-      field: "application.alternateContact.phoneNumber",
-      sortable: false,
-      filter: false,
-      width: 150,
-      minWidth: 100,
-    },
-    {
-      headerName: "HH Member First Name",
-      field: "application.householdMember.firstName",
-      sortable: false,
-      filter: false,
-      width: 125,
-      minWidth: 100,
-    },
-    {
-      headerName: "HH Member Last Name",
-      field: "application.householdMember.lastName",
-      sortable: false,
-      filter: false,
-      width: 125,
-      minWidth: 100,
-    },
-    {
-      headerName: "HH Member Email",
-      field: "application.householdMember.emailAddress",
-      sortable: false,
-      filter: false,
-      width: 150,
-      minWidth: 100,
-    },
-    {
-      headerName: "HH Member Phone",
-      field: "application.householdMember.phoneNumber",
-      sortable: false,
-      filter: false,
-      width: 150,
-      minWidth: 100,
-    },
-    {
-      headerName: "HH Member Address",
-      field: "application.householdMember.address.street",
-      sortable: false,
-      filter: false,
-      width: 175,
-      minWidth: 150,
-    },
-    {
-      headerName: "HH Member City",
-      field: "application.householdMember.address.city",
-      sortable: false,
-      filter: false,
-      width: 150,
-      minWidth: 120,
-    },
-    {
-      headerName: "HH Member State",
-      field: "application.householdMember.address.state",
-      sortable: false,
-      filter: false,
-      width: 120,
-      minWidth: 100,
-    },
-    {
-      headerName: "HH Member Zip",
-      field: "application.householdMember.address.zipCode",
-      sortable: false,
-      filter: false,
-      width: 120,
-      minWidth: 100,
-    },
-    {
-      headerName: "HH Member Work Address",
-      field: "application.householdMember.workAddress.street",
-      sortable: false,
-      filter: false,
-      width: 175,
-      minWidth: 150,
-    },
-    {
-      headerName: "HH Member Work City",
-      field: "application.householdMember.workAddress.city",
-      sortable: false,
-      filter: false,
-      width: 150,
-      minWidth: 120,
-    },
-    {
-      headerName: "HH Member Work State",
-      field: "application.householdMember.workAddress.state",
-      sortable: false,
-      filter: false,
-      width: 120,
-      minWidth: 100,
-    },
-    {
-      headerName: "HH Member Work Zip",
-      field: "application.householdMember.workAddress.zipCode",
-      sortable: false,
-      filter: false,
-      width: 120,
-      minWidth: 100,
-    },
-    {
-      headerName: "Listing",
-      field: "listing.name",
-      sortable: false,
-      filter: false,
-    },
-  ]
+    appsData?.items.forEach((item) => {
+      if (item.application.householdSize > max) {
+        max = item.application.householdSize
+      }
+    })
+
+    return max < 6 ? max : 6
+  }, [appsData])
+
+  const columnDefs = useMemo(() => {
+    return getColDefs(maxHouseholdSize)
+  }, [maxHouseholdSize])
 
   return (
     <Layout>
@@ -456,7 +136,7 @@ const ApplicationsList = () => {
           <div className="ag-theme-alpine ag-theme-bloom">
             <div className="flex justify-between">
               <div className="w-56">
-                <Field name="filter-input" register={register} placeholder="Filter" />
+                <Field name="filter-input" register={register} placeholder={t("t.filter")} />
               </div>
 
               <div className="flex-row">
---
+++ apps/partners-reference/src/applications/applicationsColDefs.ts
@@ -0,0 +1,553 @@
+import moment from "moment"
+import { t, formatIncome } from "@bloom-housing/ui-components"
+import { IncomePeriod } from "@bloom-housing/core"
+
+export function getColDefs(maxHouseholdSize: number) {
+  const defs = [
+    {
+      headerName: t("application.details.submittedDate"),
+      field: "createdAt",
+      sortable: true,
+      filter: false,
+      pinned: "left",
+      width: 200,
+      minWidth: 150,
+      sort: "asc",
+      valueFormatter: ({ value }) => {
+        if (!value) return ""
+
+        const date = moment(value).format("MM/DD/YYYY")
+        const time = moment(value).format("HH:mm:ss A")
+
+        return `${date} ${t("t.at")} ${time}`
+      },
+    },
+    {
+      headerName: t("application.details.number"),
+      field: "id",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 120,
+      pinned: "left",
+      type: "rightAligned",
+      cellRenderer: "formatLinkCell",
+    },
+    {
+      headerName: t("application.name.firstName"),
+      field: "application.applicant.firstName",
+      sortable: true,
+      filter: false,
+      pinned: "left",
+      width: 125,
+      minWidth: 100,
+    },
+    {
+      headerName: t("application.name.lastName"),
+      field: "application.applicant.lastName",
+      sortable: true,
+      filter: "agTextColumnFilter",
+      pinned: "left",
+      width: 125,
+      minWidth: 100,
+    },
+    {
+      headerName: t("application.details.householdSize"),
+      field: "application.householdSize",
+      sortable: true,
+      filter: false,
+      width: 125,
+      minWidth: 120,
+      type: "rightAligned",
+    },
+    {
+      headerName: t("applications.table.declaredAnnualIncome"),
+      field: "application.income",
+      sortable: true,
+      filter: false,
+      width: 180,
+      minWidth: 150,
+      type: "rightAligned",
+      valueFormatter: ({ data, value }) => {
+        if (!value) return ""
+
+        return data.application.incomePeriod === IncomePeriod.perYear
+          ? formatIncome(value, data.application.incomePeriod, IncomePeriod.perYear)
+          : t("t.n/a")
+      },
+    },
+    {
+      headerName: t("applications.table.declaredMonthlyIncome"),
+      field: "application.income",
+      sortable: true,
+      filter: false,
+      width: 180,
+      minWidth: 150,
+      type: "rightAligned",
+      valueFormatter: ({ data, value }) => {
+        if (!value) return ""
+
+        return data.application.incomePeriod === IncomePeriod.perMonth
+          ? formatIncome(value, data.application.incomePeriod, IncomePeriod.perMonth)
+          : t("t.n/a")
+      },
+    },
+    {
+      headerName: t("applications.table.subsidyOrVoucher"),
+      field: "application.incomeVouchers",
+      sortable: true,
+      filter: false,
+      width: 120,
+      minWidth: 100,
+      valueFormatter: (data) => {
+        if (!data.value) return ""
+
+        return data.value ? t("t.yes") : t("t.no")
+      },
+    },
+    {
+      headerName: t("applications.table.requestAda"),
+      field: "application.accessibility",
+      sortable: true,
+      filter: false,
+      width: 120,
+      minWidth: 100,
+      valueFormatter: (data) => {
+        if (!data.value) return ""
+
+        const posiviveValues = Object.entries(data.value).reduce((acc, curr) => {
+          if (curr[1]) {
+            acc.push(t(`application.ada.${curr[0]}`))
+          }
+
+          return acc
+        }, [])
+
+        return posiviveValues.length ? posiviveValues.join(", ") : t("t.none")
+      },
+    },
+    {
+      headerName: t("applications.table.preferenceClaimed"),
+      field: "application.preferences",
+      sortable: true,
+      filter: false,
+      width: 150,
+      minWidth: 100,
+      valueFormatter: (data) => {
+        if (!data.value) return ""
+
+        const posiviveValues = Object.entries(data.value).reduce((acc, curr) => {
+          if (curr[0] !== "none" && curr[1]) {
+            acc.push(t(`application.preferences.options.${curr[0]}`))
+          }
+
+          return acc
+        }, [])
+
+        return posiviveValues.length ? posiviveValues.join(", ") : t("t.none")
+      },
+    },
+    {
+      headerName: t("applications.table.primaryDob"),
+      field: "application.applicant",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 100,
+      valueFormatter: ({ value }) => {
+        if (!value) return ""
+        return `${value.birthMonth}/${value.birthDay}/${value.birthYear}`
+      },
+    },
+    {
+      headerName: t("t.email"),
+      field: "application.applicant.emailAddress",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 100,
+    },
+    {
+      headerName: t("t.phone"),
+      field: "application.applicant.phoneNumber",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 100,
+    },
+    {
+      headerName: t("applications.table.phoneType"),
+      field: "application.applicant.phoneNumberType",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 100,
+      valueFormatter: ({ value }) => {
+        if (!value) return ""
+        return t(`application.contact.phoneNumberTypes.${value}`)
+      },
+    },
+    {
+      headerName: t("t.additionalPhone"),
+      field: "application.additionalPhoneNumber",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 100,
+      valueFormatter: ({ value }) => {
+        if (!value) return ""
+        return value ? value : t("t.none")
+      },
+    },
+    {
+      headerName: t("applications.table.additionalPhoneType"),
+      field: "application.additionalPhoneNumberType",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 100,
+      valueFormatter: ({ value }) => {
+        if (!value) return ""
+        return value ? t(`application.contact.phoneNumberTypes.${value}`) : t("t.none")
+      },
+    },
+    {
+      headerName: t("applications.table.residenceStreet"),
+      field: "application.applicant.address.street",
+      sortable: false,
+      filter: false,
+      width: 175,
+      minWidth: 150,
+    },
+    {
+      headerName: t("applications.table.residenceCity"),
+      field: "application.applicant.address.city",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 120,
+    },
+    {
+      headerName: t("applications.table.residenceState"),
+      field: "application.applicant.address.state",
+      sortable: false,
+      filter: false,
+      width: 120,
+      minWidth: 100,
+    },
+    {
+      headerName: t("applications.table.residenceZip"),
+      field: "application.applicant.address.zipCode",
+      sortable: false,
+      filter: false,
+      width: 120,
+      minWidth: 100,
+    },
+    {
+      headerName: t("applications.table.mailingStreet"),
+      field: "application.mailingAddress.street",
+      sortable: false,
+      filter: false,
+      width: 175,
+      minWidth: 150,
+      valueFormatter: function ({ data, value }) {
+        if (!value) return ""
+        return `${
+          data.application.sendMailToMailingAddress
+            ? value
+            : data.application.applicant.address.street
+        }`
+      },
+    },
+    {
+      headerName: t("applications.table.mailingCity"),
+      field: "application.mailingAddress.city",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 120,
+      valueFormatter: function ({ data, value }) {
+        if (!value) return ""
+
+        return `${
+          data.application.sendMailToMailingAddress
+            ? value
+            : data.application.applicant.address.city
+        }`
+      },
+    },
+    {
+      headerName: t("applications.table.mailingState"),
+      field: "application.mailingAddress.state",
+      sortable: false,
+      filter: false,
+      width: 120,
+      minWidth: 100,
+      valueFormatter: function ({ data, value }) {
+        if (!value) return ""
+
+        return `${
+          data.application.sendMailToMailingAddress
+            ? value
+            : data.application.applicant.address.state
+        }`
+      },
+    },
+    {
+      headerName: t("applications.table.mailingZip"),
+      field: "application.mailingAddress.zipCode",
+      sortable: false,
+      filter: false,
+      width: 120,
+      minWidth: 100,
+      valueFormatter: function ({ data, value }) {
+        if (!value) return ""
+
+        return `${
+          data.application.sendMailToMailingAddress
+            ? value
+            : data.application.applicant.address.zipCode
+        }`
+      },
+    },
+    {
+      headerName: t("applications.table.workStreet"),
+      field: "application.applicant.workAddress.street",
+      sortable: false,
+      filter: false,
+      width: 175,
+      minWidth: 150,
+    },
+    {
+      headerName: t("applications.table.workCity"),
+      field: "application.applicant.workAddress.city",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 120,
+    },
+    {
+      headerName: t("applications.table.workState"),
+      field: "application.applicant.workAddress.state",
+      sortable: false,
+      filter: false,
+      width: 120,
+      minWidth: 100,
+    },
+    {
+      headerName: t("applications.table.workZip"),
+      field: "application.applicant.workAddress.zipCode",
+      sortable: false,
+      filter: false,
+      width: 120,
+      minWidth: 100,
+    },
+    {
+      headerName: t("applications.table.altContactFirstName"),
+      field: "application.alternateContact.firstName",
+      sortable: false,
+      filter: false,
+      width: 125,
+      minWidth: 100,
+    },
+    {
+      headerName: t("applications.table.altContactLastName"),
+      field: "application.alternateContact.lastName",
+      sortable: false,
+      filter: false,
+      width: 125,
+      minWidth: 100,
+    },
+    {
+      headerName: t("applications.table.altContactRelationship"),
+      field: "application.alternateContact.type",
+      sortable: false,
+      filter: false,
+      width: 125,
+      minWidth: 100,
+      valueFormatter: ({ data, value }) => {
+        if (!value) return ""
+
+        return value == "other"
+          ? data.application.alternateContact.otherType
+          : t(`application.alternateContact.type.options.${value}`)
+      },
+    },
+    {
+      headerName: t("applications.table.altContactAgency"),
+      field: "application.alternateContact.agency",
+      sortable: false,
+      filter: false,
+      width: 125,
+      minWidth: 100,
+      valueFormatter: ({ value }) => {
+        if (!value) return ""
+        return value?.length ? value : t("t.none")
+      },
+    },
+    {
+      headerName: t("applications.table.altContactEmail"),
+      field: "application.alternateContact.emailAddress",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 100,
+    },
+    {
+      headerName: t("applications.table.altContactPhone"),
+      field: "application.alternateContact.phoneNumber",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 100,
+    },
+
+    {
+      headerName: t("applications.table.altContactStreetAddress"),
+      field: "application.alternateContact.mailingAddress.street",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 100,
+    },
+    {
+      headerName: t("applications.table.altContactCity"),
+      field: "application.alternateContact.mailingAddress.city",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 100,
+    },
+    {
+      headerName: t("applications.table.altContactState"),
+      field: "application.alternateContact.mailingAddress.state",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 100,
+    },
+    {
+      headerName: t("applications.table.altContactZip"),
+      field: "application.alternateContact.mailingAddress.zipCode",
+      sortable: false,
+      filter: false,
+      width: 150,
+      minWidth: 100,
+    },
+  ]
+
+  const householdCols = []
+  // householdSize property includes primary applicant, so we have to exclude it
+  for (let i = 0; i < maxHouseholdSize - 1; i++) {
+    const householdIndex = i + 1
+
+    householdCols.push(
+      {
+        headerName: `${t("applications.table.householdFirstName")} ${householdIndex}`,
+        field: "application.householdMembers",
+        sortable: false,
+        filter: false,
+        width: 125,
+        minWidth: 100,
+        valueFormatter: ({ value }) => {
+          if (!value) return ""
+
+          return value[i] ? value[i].firstName : ""
+        },
+      },
+      {
+        headerName: `${t("applications.table.householdLastName")} ${householdIndex}`,
+        field: "application.householdMembers",
+        sortable: false,
+        filter: false,
+        width: 125,
+        minWidth: 100,
+        valueFormatter: ({ value }) => {
+          if (!value) return ""
+
+          return value[i] ? value[i].lastName : ""
+        },
+      },
+      {
+        headerName: `${t("applications.table.householdRelationship")} ${householdIndex}`,
+        field: "application.householdMembers",
+        sortable: false,
+        filter: false,
+        width: 125,
+        minWidth: 100,
+        valueFormatter: ({ value }) => {
+          if (!value) return ""
+
+          return value[i] ? t(`application.form.options.relationship.${value[i].relationship}`) : ""
+        },
+      },
+      {
+        headerName: `${t("applications.table.householdDob")} ${householdIndex}`,
+        field: "application.householdMembers",
+        sortable: false,
+        filter: false,
+        width: 125,
+        minWidth: 100,
+        valueFormatter: ({ value }) => {
+          if (!value) return ""
+
+          return value[i] ? `${value[i].birthMonth}/${value[i].birthDay}/${value[i].birthYear}` : ""
+        },
+      },
+      {
+        headerName: `${t("applications.table.householdStreetAddress")} ${householdIndex}`,
+        field: "application.householdMembers",
+        sortable: false,
+        filter: false,
+        width: 125,
+        minWidth: 100,
+        valueFormatter: ({ value }) => {
+          if (!value) return ""
+
+          return value[i] ? value[i].address.street : ""
+        },
+      },
+      {
+        headerName: `${t("applications.table.householdCity")} ${householdIndex}`,
+        field: "application.householdMembers",
+        sortable: false,
+        filter: false,
+        width: 125,
+        minWidth: 100,
+        valueFormatter: ({ value }) => {
+          if (!value) return ""
+
+          return value[i] ? value[i].address.city : ""
+        },
+      },
+      {
+        headerName: `${t("applications.table.householdState")} ${householdIndex}`,
+        field: "application.householdMembers",
+        sortable: false,
+        filter: false,
+        width: 125,
+        minWidth: 100,
+        valueFormatter: ({ value }) => {
+          if (!value) return ""
+
+          return value[i] ? value[i].address.state : ""
+        },
+      },
+      {
+        headerName: `${t("applications.table.householdZip")} ${householdIndex}`,
+        field: "application.householdMembers",
+        sortable: false,
+        filter: false,
+        width: 125,
+        minWidth: 100,
+        valueFormatter: ({ value }) => {
+          if (!value) return ""
+
+          return value[i] ? value[i].address.zipCode : ""
+        },
+      }
+    )
+  }
+
+  return [...defs, ...householdCols]
+}
