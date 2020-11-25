76d5f0e3e4105cb5bc5d8e481d1869c94ef4ec72
dominikx96 <dominik@airnauts.com>
2020-11-25 10:49:07 +0100

    Created initial page fot adding application

---
+++ apps/partners-reference/pages/applications/add.tsx
@@ -0,0 +1,35 @@
+import React, { Fragment, useMemo } from "react"
+import { useRouter } from "next/router"
+import moment from "moment"
+import Head from "next/head"
+import {
+  PageHeader,
+  t,
+  Tag,
+  GridSection,
+  ViewItem,
+  GridCell,
+  MinimalTable,
+  InlineButton,
+} from "@bloom-housing/ui-components"
+import { useSingleApplicationData } from "../../lib/hooks"
+import Layout from "../../layouts/application"
+import EditApplication from "../../src/applications/editApplication"
+
+const NewApplication = () => {
+  const router = useRouter()
+
+  return (
+    <Layout>
+      <Head>
+        <title>{t("nav.siteTitle")}</title>
+      </Head>
+      {/* <MetaTags title={t("nav.siteTitle")} image={metaImage} description={metaDescription} /> */}
+      <PageHeader>Applications Received</PageHeader>
+
+      <EditApplication isEditable={false} />
+    </Layout>
+  )
+}
+
+export default NewApplication
