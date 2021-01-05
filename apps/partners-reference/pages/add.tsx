d3947bbbbe4fa16d4c01f734b4738896009439dc
dominikx96 <dominik@airnauts.com>
2021-01-05 12:50:59 +0100

    Renamed directory

--- apps/partners-reference/pages/add.tsx
+++ apps/partners-reference/pages/add.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import Head from "next/head"
 import { PageHeader, t, MetaTags } from "@bloom-housing/ui-components"
 import Layout from "../layouts/application"
-import ApplicationForm from "../src/applications/ApplicationForm/ApplicationForm"
+import PaperApplicationForm from "../src/applications/PaperApplicationForm/PaperApplicationForm"
 
 const NewApplication = () => {
   const metaDescription = ""
@@ -16,7 +16,7 @@ const NewApplication = () => {
       <MetaTags title={t("nav.siteTitle")} image={metaImage} description={metaDescription} />
       <PageHeader>{t("applications.newApplication")}</PageHeader>
 
-      <ApplicationForm isEditable={false} />
+      <PaperApplicationForm />
     </Layout>
   )
 }
