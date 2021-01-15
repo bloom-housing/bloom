d6edc59c63e0591a2e7305505b9e0bbf3a9a93c3
dominikx96 <dominik@airnauts.com>
2021-01-15 15:38:34 +0100

    Added sign out button

--- sites/partners/layouts/application.tsx
+++ sites/partners/layouts/application.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, { useContext } from "react"
 import Head from "next/head"
 import {
   LocalizedLink,
@@ -8,45 +8,55 @@ import {
   FooterSection,
   ExygyFooter,
   t,
+  UserContext,
 } from "@bloom-housing/ui-components"
 import SVG from "react-inlinesvg"
 
-const Layout = (props) => (
-  <div className="site-wrapper">
-    <div className="site-content site-content--wide-content">
-      <Head>
-        <title>{t("nav.siteTitle")}</title>
-      </Head>
-      <SiteHeader
-        skip={t("nav.skip")}
-        logoSrc="/images/logo_glyph.svg"
-        notice=""
-        title={t("nav.siteTitle")}
-      >
-        <LocalizedLink href="/properties" className="navbar-item">
-          {t("nav.properties")}
-        </LocalizedLink>
-        <LocalizedLink href="/listings" className="navbar-item">
-          {t("nav.listings")}
-        </LocalizedLink>
-        <LocalizedLink href="/applications" className="navbar-item">
-          {t("nav.applications")}
-        </LocalizedLink>
-      </SiteHeader>
-      <main>{props.children}</main>
+const Layout = (props) => {
+  const { profile, signOut } = useContext(UserContext)
 
-      <SiteFooter>
-        <FooterNav copyright="© 2020 • All Rights Reserved">
-          <LocalizedLink href="/privacy">{t("pageTitle.privacy")}</LocalizedLink>
-          <LocalizedLink href="/disclaimer">{t("pageTitle.disclaimer")}</LocalizedLink>
-        </FooterNav>
-        <FooterSection className="bg-black" small>
-          <ExygyFooter />
-        </FooterSection>
-      </SiteFooter>
-      <SVG src="/images/icons.svg" />
+  return (
+    <div className="site-wrapper">
+      <div className="site-content site-content--wide-content">
+        <Head>
+          <title>{t("nav.siteTitle")}</title>
+        </Head>
+        <SiteHeader
+          skip={t("nav.skip")}
+          logoSrc="/images/logo_glyph.svg"
+          notice=""
+          title={t("nav.siteTitle")}
+        >
+          <LocalizedLink href="/properties" className="navbar-item">
+            {t("nav.properties")}
+          </LocalizedLink>
+          <LocalizedLink href="/listings" className="navbar-item">
+            {t("nav.listings")}
+          </LocalizedLink>
+          <LocalizedLink href="/applications" className="navbar-item">
+            {t("nav.applications")}
+          </LocalizedLink>
+          {!!profile && (
+            <a href="#" className="navbar-item" onClick={signOut}>
+              {t("nav.signOut")}
+            </a>
+          )}
+        </SiteHeader>
+        <main>{props.children}</main>
+
+        <SiteFooter>
+          <FooterNav copyright="© 2020 • All Rights Reserved">
+            <LocalizedLink href="/privacy">{t("pageTitle.privacy")}</LocalizedLink>
+            <LocalizedLink href="/disclaimer">{t("pageTitle.disclaimer")}</LocalizedLink>
+          </FooterNav>
+          <FooterSection className="bg-black" small>
+            <ExygyFooter />
+          </FooterSection>
+        </SiteFooter>
+        <SVG src="/images/icons.svg" />
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Layout
