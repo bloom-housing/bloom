bfe20a1ebae8727d7d0477bd4b60b11ace5c36b4
dominikx96 <dominikx96@gmail.com>
2021-01-29 22:27:41 +0100

    Temporary disable email validators (#977)
    Co-authored-by: dominikx96 <dominik@airnauts.com>

--- backend/core/src/applications/entities/alternate-contact.entity.ts
+++ backend/core/src/applications/entities/alternate-contact.entity.ts
@@ -1,14 +1,7 @@
 import { Column, Entity, JoinColumn, OneToOne } from "typeorm"
 import { AbstractEntity } from "../../shared/entities/abstract.entity"
 import { Expose, Type } from "class-transformer"
-import {
-  IsDefined,
-  IsEmail,
-  IsOptional,
-  IsString,
-  MaxLength,
-  ValidateNested,
-} from "class-validator"
+import { IsDefined, IsOptional, IsString, MaxLength, ValidateNested } from "class-validator"
 import { Address } from "../../shared/entities/address.entity"
 import { ValidationsGroupsEnum } from "../../shared/validations-groups.enum"
 
@@ -60,7 +53,7 @@ export class AlternateContact extends AbstractEntity {
   @Expose()
   @IsOptional({ groups: [ValidationsGroupsEnum.partners] })
   @IsString({ groups: [ValidationsGroupsEnum.default] })
-  @IsEmail({}, { groups: [ValidationsGroupsEnum.default] })
+  // @IsEmail({}, { groups: [ValidationsGroupsEnum.default] })
   emailAddress?: string | null
 
   @OneToOne(() => Address, { eager: true, cascade: true })
