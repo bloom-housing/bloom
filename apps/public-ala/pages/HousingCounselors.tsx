55526851c57a236e88a4bbd80cecba6a04c6dc76
Michal Plebanski <michalp@airnauts.com>
2021-02-02 16:49:43 +0100

    Fix HousingCounselors.tsx export

--- apps/public-ala/pages/HousingCounselors.tsx
+++ apps/public-ala/pages/HousingCounselors.tsx
@@ -32,7 +32,7 @@ export default class extends Component<HousingCounselorsProps> {
         <PageHeader inverse={true} subtitle={t("housingCounselors.subtitle")}>
           {t("pageTitle.housingCounselors")}
         </PageHeader>
-        {this.props.counselors.map((c) => {
+        {this.props.counselors?.map((c) => {
           return (
             <article key={c.name} className="flex-row flex-wrap max-w-5xl m-auto py-8 border-b-2">
               <HousingCounselor counselor={c} />
