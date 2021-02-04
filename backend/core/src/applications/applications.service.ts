b9bde6c119b9a287eba494679ff0d2a3fc79aa6c
dominikx96 <dominik@airnauts.com>
2021-02-04 18:08:34 +0100

    Prevent id overwrite

--- backend/core/src/applications/applications.service.ts
+++ backend/core/src/applications/applications.service.ts
@@ -72,7 +72,10 @@ export class ApplicationsService {
         where: { id: applicationUpdateDto.id },
         relations: ["listing", "user"],
       }))
-    Object.assign(application, applicationUpdateDto)
+    Object.assign(application, {
+      ...applicationUpdateDto,
+      id: application.id,
+    })
 
     await this.repository.save(application)
     return application
