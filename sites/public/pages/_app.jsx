f5eebf696f6f66016d266be1ca129f49fcc9ba9a
Netra Patil <netrapatil@Netras-MacBook-Pro-2.local>
2021-02-08 14:59:15 -0800

    Add Alameda Specific Translation to 0.3 Alameda

--- sites/public/pages/_app.jsx
+++ sites/public/pages/_app.jsx
@@ -61,7 +61,9 @@ class MyApp extends App {
       custom: {
         general: await import("../page_content/locale_overrides/general.json"),
         // Uncomment to add additional language overrides
-        // es: await import("../page_content/locale_overrides/es.json")
+        es: await import("../page_content/locale_overrides/es.json"),
+        zh: await import("../page_content/locale_overrides/zh.json"),
+        vi: await import("../page_content/locale_overrides/vi.json"),
       },
     }
     return { pageProps, translations }
