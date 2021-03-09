7cccaab6edaba286b4407a07429c7d29f9a51fc7
Marcin Jedras <mjjedras@gmail.com>
2021-03-09 21:25:57 +0100

    Fixes to date format, allow markup for street, add lottery time

--- ui-components/src/page_components/listing/listing_sidebar/events/PublicLotteryEvent.tsx
+++ ui-components/src/page_components/listing/listing_sidebar/events/PublicLotteryEvent.tsx
@@ -1,18 +1,14 @@
 import * as React from "react"
 import { ListingEvent } from "@bloom-housing/backend-core/types"
 import { t } from "../../../../helpers/translator"
-import moment from "moment"
+import { EventDateSection } from "./EventDateSection"
 
 const PublicLotteryEvent = (props: { event: ListingEvent }) => {
   const { event } = props
   return (
     <section className="aside-block -mx-4 pt-0 md:mx-0 md:pt-4">
       <h4 className="text-caps-underline">{t("listings.publicLottery.header")}</h4>
-      <p className="text text-gray-800 pb-3 flex justify-between items-center">
-        <span className="inline-block text-tiny uppercase">
-          {moment(props.event.startTime).format("MMMM D, YYYY")}
-        </span>
-      </p>
+      <EventDateSection event={props.event} />
       {event.url && (
         <p className="text text-gray-800 pb-3">
           <a href={event.url}>{t("listings.publicLottery.seeVideo")}</a>
