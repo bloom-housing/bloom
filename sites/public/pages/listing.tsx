a538948af04bdea388174d4c3ec2293c8e2b3cb5
Netra Patil <netrapatil@Netras-MacBook-Pro-2.local>
2021-03-26 15:14:53 -0700

    fix houshole maximum income translations on mobile view

--- sites/public/pages/listing.tsx
+++ sites/public/pages/listing.tsx
@@ -80,7 +80,13 @@ export default class extends Component<ListingProps> {
         return percentInt
       })
       .sort()
-    const hmiHeaders = listing.property.unitsSummarized.hmi.columns as TableHeaders
+
+    const deriveHmiHeaders = listing.property.unitsSummarized.hmi.columns as TableHeaders
+    const hmiHeaders = {}
+    for (const [key, value] of Object.entries(deriveHmiHeaders)) {
+      hmiHeaders[key] = t(value)
+    }
+
     const hmiData = listing.property.unitsSummarized.hmi.rows.map((row) => {
       return { ...row, householdSize: <strong>{row["householdSize"]}</strong> }
     })
