75a1934d94444d20089408202dcf4e1032de288e
dominikx96 <dominik@airnauts.com>
2021-04-12 12:14:13 +0200

    Added flags navigation

--- ui-components/src/headers/PageHeader.tsx
+++ ui-components/src/headers/PageHeader.tsx
@@ -1,15 +1,26 @@
 import * as React from "react"
+import { useRouter } from "next/router"
 import "./PageHeader.scss"
+import { Tab, TabNav } from "../navigation/TabNav"
 
+type TabNavItem = {
+  label: string
+  path: string
+  content?: React.ReactNode
+}
 export interface PageHeaderProps {
   className?: string
   inverse?: boolean
   title: React.ReactNode
   subtitle?: string
   children?: React.ReactNode
+  tabNav?: TabNavItem[]
 }
 
 const PageHeader = (props: PageHeaderProps) => {
+  const router = useRouter()
+  const currentPath = router?.asPath
+
   const classNames = ["page-header"]
   if (props.className) {
     classNames.push(...props.className.split(" "))
@@ -27,9 +38,23 @@ const PageHeader = (props: PageHeaderProps) => {
       <hgroup className="page-header__group">
         <h1 className="page-header__title">{props.title}</h1>
         {props.subtitle && <p className="page-header__lead"> {props.subtitle}</p>}
-      </hgroup>
+        {props.children}
 
-      {props.children}
+        {props?.tabNav && (
+          <TabNav className="page-header__secondary-nav">
+            {props.tabNav?.map((tab) => (
+              <Tab
+                key={tab.path}
+                tagContent={tab?.content}
+                current={tab.path === currentPath}
+                href={tab.path}
+              >
+                {tab.label}
+              </Tab>
+            ))}
+          </TabNav>
+        )}
+      </hgroup>
     </header>
   )
 }
