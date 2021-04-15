54cf4c530b4f388946c8c49a6148c60abc581265
Michal Plebanski <michalp@airnauts.com>
2021-04-15 10:38:20 +0200

    Add missing REDIS_TLS_URL in config

--- backend/core/src/shared/shared.module.ts
+++ backend/core/src/shared/shared.module.ts
@@ -14,6 +14,7 @@ import Joi from "joi"
           .valid("development", "staging", "production", "test")
           .default("development"),
         DATABASE_URL: Joi.string().required(),
+        REDIS_TLS_URL: Joi.string().required(),
       }),
     }),
     SendGridModule.forRootAsync({
