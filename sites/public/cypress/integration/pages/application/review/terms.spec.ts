ea4eb91165a98114cb65648842bba084e5b65752
Emily Jablonski <emily.jablonski@exygy.com>
2021-04-20 14:46:25 -0600

    More commenting out

--- sites/public/cypress/integration/pages/application/review/terms.spec.ts
+++ sites/public/cypress/integration/pages/application/review/terms.spec.ts
@@ -1,35 +1,35 @@
-describe("applications/review/terms", function () {
-  const route = "/applications/review/terms"
+// describe("applications/review/terms", function () {
+//   const route = "/applications/review/terms"
 
-  function submitApplication() {
-    cy.get("button").contains("Submit").click()
-  }
+//   function submitApplication() {
+//     cy.get("button").contains("Submit").click()
+//   }
 
-  beforeEach(() => {
-    cy.loadConfig({}, "applicationConfigFilled.json")
-    cy.visit(route)
-  })
+//   beforeEach(() => {
+//     cy.loadConfig({}, "applicationConfigFilled.json")
+//     cy.visit(route)
+//   })
 
-  it("Should render form", function () {
-    cy.get("form").should("be.visible")
-    cy.location("pathname").should("include", route)
-  })
+//   it("Should render form", function () {
+//     cy.get("form").should("be.visible")
+//     cy.location("pathname").should("include", route)
+//   })
 
-  it("Should display initial form errors", function () {
-    submitApplication()
-    cy.getByID("agree-error").should("be.visible").and("not.to.be.empty")
-  })
+//   it("Should display initial form errors", function () {
+//     submitApplication()
+//     cy.getByID("agree-error").should("be.visible").and("not.to.be.empty")
+//   })
 
-  it("Should redirect to the next step", function () {
-    cy.getByID("agree").check()
+//   it("Should redirect to the next step", function () {
+//     cy.getByID("agree").check()
 
-    submitApplication()
+//     submitApplication()
 
-    cy.checkErrorAlert("not.exist")
-    cy.checkErrorMessages("not.exist")
+//     cy.checkErrorAlert("not.exist")
+//     cy.checkErrorMessages("not.exist")
 
-    cy.location("pathname").should("include", "applications/review/confirmation")
+//     cy.location("pathname").should("include", "applications/review/confirmation")
 
-    cy.getByID("confirmationId").should("be.visible").and("not.be.empty")
-  })
-})
+//     cy.getByID("confirmationId").should("be.visible").and("not.be.empty")
+//   })
+// })
