d39af02687f892344f6409b59345b157b7e2ce17
Emily Jablonski <emily.jablonski@exygy.com>
2021-04-23 10:43:46 -0600

    Update the tests

--- sites/public/cypress/integration/navigation.ts
+++ sites/public/cypress/integration/navigation.ts
@@ -13,28 +13,12 @@ describe("Navigating around the site", () => {
     cy.contains("Rent affordable housing")
   })
 
-  it("Loads a listing page directly by id", () => {
+  it("Loads a listing page", () => {
     cy.visit("/listings")
     cy.get("article.listings-row a")
       .first()
       .then(function ($a) {
         cy.visit($a.prop("href"))
-        // Check that the listing page sidebar apply section text is present on the page
-        cy.contains("Apply Online")
-
-        // Check that the URL got re-written with a URL slug
-        cy.location().should((loc) => {
-          expect(loc.pathname).to.contain("_55_triton_park_lane_foster_city_ca")
-        })
-      })
-  })
-
-  it("Loads a listing page directly with a full url", () => {
-    cy.visit("/listings")
-    cy.get("article.listings-row a")
-      .first()
-      .then(function ($a) {
-        cy.visit(`${$a.prop("href")}/test_listing_1_pref_55_triton_park_lane_foster_city_ca`)
         // Check that the listing page sidebar apply section text is present on the page
         cy.contains("Apply Online")
       })
