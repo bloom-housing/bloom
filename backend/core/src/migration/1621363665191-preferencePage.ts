8f0e01c37619d5b1aa52df21e759fa252e7827a6
Emily Jablonski <emily.jablonski@exygy.com>
2021-05-18 17:52:03 -0600

    maybe an update to the migration

--- backend/core/src/migration/1621363665191-preferencePage.ts
+++ backend/core/src/migration/1621363665191-preferencePage.ts
@@ -4,7 +4,7 @@ export class preferencePage1621363665191 implements MigrationInterface {
   name = "preferencePage1621363665191"
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "preferences" ADD "page" integer`)
+    await queryRunner.query(`ALTER TABLE "preferences" ADD "page" integer NOT NULL DEFAULT 1`)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
