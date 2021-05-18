5f2737393f8d38880e3a328256d1f594f54307e7
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2021-05-18 11:20:32 -0600

    feature: new styles for tag component (#1240)

--- ui-components/src/text/Tag.tsx
+++ ui-components/src/text/Tag.tsx
@@ -6,12 +6,14 @@ export interface TagProps extends AppearanceProps {
   className?: string
   pillStyle?: boolean
   children: React.ReactNode
+  fillContainer?: boolean
 }
 
 const Tag = (props: TagProps) => {
   const tagClasses = ["tag"].concat(classNamesForAppearanceTypes(props))
 
   if (props.pillStyle) tagClasses.push("is-pill")
+  if (props.fillContainer) tagClasses.push("fill-container")
   if (props.className) tagClasses.push(props.className)
 
   return <span className={tagClasses.join(" ")}>{props.children}</span>
