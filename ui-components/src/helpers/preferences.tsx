cdca24cb0b737f2abf5b0757d90adb0ee69a1904
Emily Jablonski <emily.jablonski@exygy.com>
2021-05-20 15:39:23 -0600

    remove optional chaining

--- ui-components/src/helpers/preferences.tsx
+++ ui-components/src/helpers/preferences.tsx
@@ -234,8 +234,10 @@ export const FormAddress = ({
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const mapPreferencesToApi = (data: Record<string, any>) => {
   const CLAIMED_KEY = "claimed"
-  const preferencesFormData = data.application?.preferences?.options
+  const preferencesFormData = data.application.preferences.options
 
+  // I think this is where it's failing once it deploys with:
+  // Cannot convert undefined or null to object at Function.keys
   const keys = Object.keys(preferencesFormData)
 
   return keys.map((key) => {
