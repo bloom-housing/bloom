953925e08e2a7369c1598ce6b7245a03ad104a59
Sean Albert <smabert@gmail.com>
2021-05-21 10:30:20 -0700

    updates translation overrides and _app.tsx to add them in for "en" as well

--- sites/public/src/translations.ts
+++ sites/public/src/translations.ts
@@ -4,6 +4,11 @@ import spanishTranslations from "@bloom-housing/ui-components/src/locales/es.jso
 import chineseTranslations from "@bloom-housing/ui-components/src/locales/zh.json"
 import vietnameseTranslations from "@bloom-housing/ui-components/src/locales/vi.json"
 
+import additionalGeneralTranslations from "../page_content/locale_overrides/general.json"
+import additionalSpanishTranslations from "../page_content/locale_overrides/es.json"
+import additionalChineseTranslations from "../page_content/locale_overrides/zh.json"
+import additionalViatnameseTranslations from "../page_content/locale_overrides/vi.json"
+
 export const translations = {
   general: generalTranslations,
   es: spanishTranslations,
@@ -12,5 +17,8 @@ export const translations = {
 } as Record<string, any>
 
 export const overrideTranslations = {
-  //  zh: additionalChineseTranslations
+  en: additionalGeneralTranslations,
+  es: additionalSpanishTranslations,
+  zh: additionalChineseTranslations,
+  vi: additionalViatnameseTranslations,
 } as Record<string, any>
