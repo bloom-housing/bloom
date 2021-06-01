a1cdd758de9cfd7df8d4fbecbc3cec3110e68f32
dominikx96 <dominik@airnauts.com>
2021-06-01 17:36:36 +0200

    Pagination improvements on mobile

--- ui-components/src/global/vendor/AgPagination.tsx
+++ ui-components/src/global/vendor/AgPagination.tsx
@@ -42,74 +42,90 @@ const AgPagination = ({
   }
 
   return (
-    <div className="data-pager">
-      <Button
-        className="data-pager__previous data-pager__control"
-        onClick={onPrevClick}
-        disabled={currentPage === 1}
-      >
-        {t("t.previous")}
-      </Button>
+    <div className="data-pager flex flex-col md:flex-row">
+      <div className="hidden md:block">
+        <Button
+          className="data-pager__previous data-pager__control"
+          onClick={onPrevClick}
+          disabled={currentPage === 1}
+        >
+          {t("t.previous")}
+        </Button>
+      </div>
 
-      <div className="data-pager__control-group">
-        <span className="data-pager__control">
+      <div className="data-pager__control-group ml-0 md:ml-auto w-full md:w-auto md:flex md:items-center">
+        <div className="data-pager__control">
           <span className="field-label" id="lbTotalPages">
             {totalItems}
           </span>
           {quantityLabel && <span className="field-label">{quantityLabel}</span>}
-        </span>
+        </div>
 
-        <span className="field data-pager__control">
-          <label className="field-label font-sans" htmlFor="page-size">
-            {t("t.show")}
-          </label>
-          <select
-            name="page-size"
-            id="page-size"
-            value={itemsPerPage}
-            onChange={({ target }) => onRowLimitChange(target.value)}
-          >
-            {AG_PER_PAGE_OPTIONS.map((item) => (
-              <option key={item} value={item}>
-                {item}
-              </option>
-            ))}
-          </select>
-        </span>
+        <div className="flex mt-5 md:mt-0 md:items-center">
+          <div className="field data-pager__control md:mb-0">
+            <label className="field-label font-sans" htmlFor="page-size">
+              {t("t.show")}
+            </label>
+            <select
+              name="page-size"
+              id="page-size"
+              value={itemsPerPage}
+              onChange={({ target }) => onRowLimitChange(target.value)}
+            >
+              {AG_PER_PAGE_OPTIONS.map((item) => (
+                <option key={item} value={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </div>
 
-        <span className="field data-pager__control">
-          <label className="field-label font-sans" htmlFor="page-jump">
-            {t("t.jumpTo")}
-          </label>
-          <select
-            name="page-jump"
-            id="page-jump"
-            onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
-              setCurrentPage(parseInt(e.target.value))
-            }
-            value={currentPage}
-          >
-            {Array(totalPages)
-              .fill(totalPages)
-              .map((_, i) => {
-                const value = i + 1
-                return (
-                  <option key={value} value={value}>
-                    {value}
-                  </option>
-                )
-              })}
-          </select>
-        </span>
+          <div className="field data-pager__control">
+            <label className="field-label font-sans" htmlFor="page-jump">
+              {t("t.jumpTo")}
+            </label>
+            <select
+              name="page-jump"
+              id="page-jump"
+              onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                setCurrentPage(parseInt(e.target.value))
+              }
+              value={currentPage}
+            >
+              {Array(totalPages)
+                .fill(totalPages)
+                .map((_, i) => {
+                  const value = i + 1
+                  return (
+                    <option key={value} value={value}>
+                      {value}
+                    </option>
+                  )
+                })}
+            </select>
+          </div>
+        </div>
       </div>
 
-      <Button
-        className="data-pager__next data-pager__control"
-        onClick={onNextClick}
-        disabled={totalPages === currentPage}
-      >
-        {t("t.next")}
-      </Button>
+      <div className="w-full md:w-auto flex justify-between mt-5 md:mt-0 ">
+        <div className="md:hidden">
+          <Button
+            className="data-pager__previous data-pager__control"
+            onClick={onPrevClick}
+            disabled={currentPage === 1}
+          >
+            {t("t.previous")}
+          </Button>
+        </div>
+
+        <Button
+          className="data-pager__next data-pager__control"
+          onClick={onNextClick}
+          disabled={totalPages === currentPage}
+        >
+          {t("t.next")}
+        </Button>
+      </div>
     </div>
   )
 }
