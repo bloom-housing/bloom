f23bf39779fa207a10eeaa27148402655df0980d
Michal Plebanski <michalp@airnauts.com>
2021-06-01 14:34:10 +0200

    Add routing for V2

--- backend/core/src/routes.ts
+++ backend/core/src/routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from "nest-router"
 import { AppModule } from "./v1/app.module"
+import { AppModule as AppModuleV2 } from "./v2/app.module"
 
 export const routes: Routes = [
   {
-    path: '/',
+    path: "/",
     module: AppModule,
   },
-];
+  {
+    path: "/v2",
+    module: AppModuleV2,
+  },
+]
 
 export {}
