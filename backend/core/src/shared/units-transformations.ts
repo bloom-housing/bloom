54ad89f7f46e3e56d3313a289602f032353a571b
Marcin Jędras <mjjedras@gmail.com>
2021-06-02 11:30:32 +0200

    Replace toPrecision with toFixed (#1304)

--- backend/core/src/shared/units-transformations.ts
+++ backend/core/src/shared/units-transformations.ts
@@ -129,8 +129,8 @@ const getUnitSummary = (unit: Unit, existingSummary?: UnitSummary) => {
     parseFloat(unit.monthlyIncomeMin)
   )
   summary.minIncomeRange = {
-    min: minIncomeRange.min.toPrecision(2),
-    max: minIncomeRange.max.toPrecision(2),
+    min: minIncomeRange.min.toFixed(2),
+    max: minIncomeRange.max.toFixed(2),
   }
 
   summary.occupancyRange = minMaxValue(summary.occupancyRange, unit.minOccupancy, unit.maxOccupancy)
@@ -146,8 +146,8 @@ const getUnitSummary = (unit: Unit, existingSummary?: UnitSummary) => {
     Number.parseFloat(unit.monthlyRent)
   )
   summary.rentRange = {
-    min: rentRange.min.toPrecision(2),
-    max: rentRange.max.toPrecision(2),
+    min: rentRange.min.toFixed(2),
+    max: rentRange.max.toFixed(2),
   }
   if (unit.floor) {
     summary.floorRange = minMaxValue(summary.floorRange, unit.floor)
