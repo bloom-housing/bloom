447d1bd1a1b260a36fcb3f7a49f51ecb4fc8ec79
Michal Plebanski <michalp@airnauts.com>
2021-06-07 11:29:54 +0200

    Fix unit tests

--- backend/core/src/assets/assets.controller.spec.ts
+++ backend/core/src/assets/assets.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from "@nestjs/testing"
 import { AssetsController } from "./assets.controller"
+import { AuthModule } from "../auth/auth.module"
+import dbOptions = require("../../ormconfig.test")
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { AssetsService } from "./services/assets.service"
 
 describe("AssetsController", () => {
   let controller: AssetsController
@@ -7,6 +11,8 @@ describe("AssetsController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AssetsController],
+      imports: [TypeOrmModule.forRoot(dbOptions), AuthModule],
+      providers: [{ provide: AssetsService, useValue: {} }],
     }).compile()
 
     controller = module.get<AssetsController>(AssetsController)
--- backend/core/src/assets/services/assets.service.spec.ts
+++ backend/core/src/assets/services/assets.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from "@nestjs/testing"
 import { AssetsService } from "./assets.service"
+import { getRepositoryToken } from "@nestjs/typeorm"
+import { Asset } from "../entities/asset.entity"
+import { UploadService } from "./upload.service"
 
 describe("AssetsService", () => {
   let service: AssetsService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AssetsService],
+      providers: [
+        AssetsService,
+        { provide: getRepositoryToken(Asset), useValue: {} },
+        { provide: UploadService, useValue: {} },
+      ],
     }).compile()
 
     service = module.get<AssetsService>(AssetsService)
