43f2a0f650bb6e3cc6c16d5e32b0aa7c590e509c
Michal Plebanski <michalp@airnauts.com>
2021-06-24 15:51:04 +0200

    Add reserved_community_type seed migration

---
+++ backend/core/src/migration/1624542123483-seed-reserved-community-type.ts
@@ -0,0 +1,15 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class seedReservedCommunityType1624542123483 implements MigrationInterface {
+
+    reservedCommunityTypes = ["specialNeeds", "senior"]
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        for (const reservedCommunityType of this.reservedCommunityTypes) {
+            await queryRunner.query(`INSERT INTO reserved_community_types (name) VALUES ($1)`, [reservedCommunityType])
+        }
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DELETE FROM reserved_community_types WHERE name = ANY($1)`, [this.reservedCommunityTypes])
+    }
+}
