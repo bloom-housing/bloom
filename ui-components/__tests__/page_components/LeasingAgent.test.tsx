9f84c48c8fdf8619d04932ac482d3f3cd4dda176
Lint Action <lint-action@samuelmeuli.com>
2021-07-08 00:58:12 +0000

    Fix code style issues with Prettier

--- ui-components/__tests__/page_components/LeasingAgent.test.tsx
+++ ui-components/__tests__/page_components/LeasingAgent.test.tsx
@@ -12,15 +12,23 @@ describe("<LeasingAgent>", () => {
     const listing = Object.assign({}, ArcherListing) as Listing
     const { getByText } = render(<LeasingAgent listing={listing} />)
     expect(listing.leasingAgentName && getByText(listing.leasingAgentName)).toBeTruthy()
-    expect(listing.leasingAgentPhone && getByText(listing.leasingAgentPhone, { exact: false })).toBeTruthy()
-    expect(listing.leasingAgentOfficeHours && getByText(listing.leasingAgentOfficeHours)).toBeTruthy()
+    expect(
+      listing.leasingAgentPhone && getByText(listing.leasingAgentPhone, { exact: false })
+    ).toBeTruthy()
+    expect(
+      listing.leasingAgentOfficeHours && getByText(listing.leasingAgentOfficeHours)
+    ).toBeTruthy()
   })
   it("renders nothing if application is not open", () => {
     const listing = Object.assign({}, ArcherListing) as Listing
     listing.applicationOpenDate = new Date(moment().add(10, "days").format())
     const { queryByText } = render(<LeasingAgent listing={listing} />)
     expect(listing.leasingAgentName && queryByText(listing.leasingAgentName)).toBeNull()
-    expect(listing.leasingAgentPhone && queryByText(listing.leasingAgentPhone, { exact: false })).toBeNull()
-    expect(listing.leasingAgentOfficeHours && queryByText(listing.leasingAgentOfficeHours)).toBeNull()
+    expect(
+      listing.leasingAgentPhone && queryByText(listing.leasingAgentPhone, { exact: false })
+    ).toBeNull()
+    expect(
+      listing.leasingAgentOfficeHours && queryByText(listing.leasingAgentOfficeHours)
+    ).toBeNull()
   })
 })
