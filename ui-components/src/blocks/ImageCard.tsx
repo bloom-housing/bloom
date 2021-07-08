f5fcbcd86be2badd45d168e68335c19925c0fde9
Anthony Kong <anthony.kong@sfgov.org>
2021-07-08 11:09:14 -0700

    DAH-830 - add image component variation with tag

--- ui-components/src/blocks/ImageCard.tsx
+++ ui-components/src/blocks/ImageCard.tsx
@@ -3,6 +3,8 @@ import { LocalizedLink } from "../actions/LocalizedLink"
 import { ApplicationStatus } from "../notifications/ApplicationStatus"
 import "./ImageCard.scss"
 import { Listing } from "@bloom-housing/backend-core/types"
+import { Tag } from "../text/Tag"
+import { AppearanceStyleType } from "../global/AppearanceTypes"
 
 export interface ImageCardProps {
   imageUrl: string
@@ -10,10 +12,12 @@ export interface ImageCardProps {
   title: string
   href?: string
   listing?: Listing
+  tagLabel?: string
 }
 
 const ImageCard = (props: ImageCardProps) => {
   let statusLabel
+  let tag
 
   if (props.listing) {
     statusLabel = (
@@ -23,8 +27,17 @@ const ImageCard = (props: ImageCardProps) => {
     )
   }
 
+  if (props.tagLabel) {
+    tag = (
+      <Tag styleType={AppearanceStyleType.warning} className="image-card__tag">
+        {props.tagLabel}
+      </Tag>
+    )
+  }
+
   const image = (
     <div className="image-card__wrapper">
+      {tag}
       <figure className="image-card">
         {props.imageUrl && <img src={props.imageUrl} alt={props.title} />}
         {!props.imageUrl && <div style={{ height: "300px", background: "#ccc" }}></div>}
