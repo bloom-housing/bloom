7c47c1f4c094ca5d33067b0ec69bb5f5038029bb
seanmalbert <smabert@gmail.com>
2021-07-13 17:38:16 -0700

    fixes tests

--- ui-components/__tests__/page_components/SidebarAddress.test.tsx
+++ ui-components/__tests__/page_components/SidebarAddress.test.tsx
@@ -2,14 +2,13 @@ import React from "react"
 import { render, cleanup } from "@testing-library/react"
 import { SidebarAddress } from "../../src/page_components/listing/listing_sidebar/SidebarAddress"
 import { ArcherListing } from "@bloom-housing/backend-core/types/src/archer-listing"
-import moment from "moment"
 
 afterEach(cleanup)
 
 describe("<SidebarAddress>", () => {
   it("renders with address", () => {
     const { getByText } = render(<SidebarAddress address={ArcherListing.applicationAddress} />)
-    expect(getByText(ArcherListing.applicationAddress.street || "", { exact: false })).toBeTruthy()
+    expect(getByText(ArcherListing.applicationAddress?.street || "", { exact: false })).toBeTruthy()
     expect(getByText("Get Directions")).toBeTruthy()
   })
   it("renders with office hours and no address", () => {
