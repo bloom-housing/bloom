2a9bd0dea8a345a5e6ff30c5dd933acdee4d4414
Abbie Farr <afarr@google.com>
2021-07-15 09:59:26 -0400

    Update migration script

--- backend/core/src/migration/1626207809474-unit-status-enum.ts
+++ backend/core/src/migration/1626207809474-unit-status-enum.ts
@@ -4,15 +4,13 @@ export class unitStatusEnum1626207809474 implements MigrationInterface {
     name = 'unitStatusEnum1626207809474'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "units" DROP COLUMN "status"`);
         await queryRunner.query(`CREATE TYPE "units_status_enum" AS ENUM('unknown', 'available', 'occupied')`);
-        await queryRunner.query(`ALTER TABLE "units" ADD "status" "units_status_enum" NOT NULL DEFAULT 'unknown'`);
+        await queryRunner.query(`ALTER TABLE "units" ALTER COLUMN "status" TYPE "units_status_enum" using "status"::"units_status_enum"`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "units" DROP COLUMN "status"`);
+        await queryRunner.query(`ALTER TABLE "units" ALTER COLUMN "status" TYPE text`);
         await queryRunner.query(`DROP TYPE "units_status_enum"`);
-        await queryRunner.query(`ALTER TABLE "units" ADD "status" text`);
     }
 
 }
