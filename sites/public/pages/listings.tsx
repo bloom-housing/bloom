31b540e3f674ece9e2da1d269fa76b935f60e7a3
Austin Valeske <avaleske@users.noreply.github.com>
2021-07-16 11:46:14 -0700

    Add modal to hold filters (#167)
    * initial experiments
    
    * Updated model with select dropdown and close button
    
    * Used Bloom Select to update model <select> filters
    
    * updated strings in general.json and removed strings in listings model
    
    * resolved strings from general.json
    
    Co-authored-by: pfeeny5 <82653098+pfeeny5@users.noreply.github.com>

--- sites/public/pages/listings.tsx
+++ sites/public/pages/listings.tsx
@@ -1,5 +1,19 @@
 import Head from "next/head"
-import { ListingsList, PageHeader, AgPagination, t } from "@bloom-housing/ui-components"
+import {
+  ListingsList,
+  PageHeader,
+  Button,
+  AppearanceSizeType,
+  Modal,
+  Drawer,
+  AppearanceStyleType,
+  AppearanceBorderType,
+  t,
+  Select,
+  AgPagination,
+  Form,
+} from "@bloom-housing/ui-components"
+import { useForm } from "react-hook-form"
 import Layout from "../layouts/application"
 import { MetaTags } from "../src/MetaTags"
 import React, { useEffect, useState } from "react"
@@ -32,13 +46,25 @@ const ListingsPage = () => {
     if (router.query.page && Number(router.query.page) != currentPage) {
       setCurrentPage(Number(router.query.page))
     }
-  }, [router.query.page])
+  }, [currentPage, router.query.page])
 
   const { listingsData, listingsLoading } = useListingsData(currentPage, itemsPerPage)
 
   const pageTitle = `${t("pageTitle.rent")} - ${t("nav.siteTitle")}`
   const metaDescription = t("pageDescription.welcome", { regionName: t("region.name") })
   const metaImage = "" // TODO: replace with hero image
+  const [filterModalVisible, setFilterModalVisible] = useState<boolean>(false)
+  const [filterDrawerVisible, setFilterDrawerVisible] = useState<boolean>(false)
+
+  const preferredUnitOptions = ["1", "2", "3", "4", "studio"]
+  const accessibilityOptions = ["n", "y"]
+  const communityOptions = ["general", "senior", "assisted"]
+  /* Form Handler */
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  const { handleSubmit, register } = useForm()
+  const onSubmit = () => {
+    // Not yet implemented.
+  }
 
   return (
     <Layout>
@@ -47,6 +73,96 @@ const ListingsPage = () => {
       </Head>
       <MetaTags title={t("nav.siteTitle")} image={metaImage} description={metaDescription} />
       <PageHeader title={t("pageTitle.rent")} />
+      <Modal
+        open={filterModalVisible}
+        title={t("listingFilters.modalTitle")}
+        actions={[
+          <Button
+            onClick={() => setFilterModalVisible(false)}
+            styleType={AppearanceStyleType.primary}
+          >
+            Apply
+          </Button>,
+          <Button
+            onClick={() => setFilterModalVisible(false)}
+            styleType={AppearanceStyleType.secondary}
+            border={AppearanceBorderType.borderless}
+          >
+            Close
+          </Button>,
+        ]}
+        hideCloseIcon
+      >
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-card__group">
+            <p className="field-note mb-4">{t("listingFilters.modalHeader")}</p>
+            <Select
+              id="filter.unitOptions"
+              name="filter.unitOptions"
+              label={t("listingFilters.unitOptions.label")}
+              validation={{ required: true }}
+              register={register}
+              controlClassName="control"
+              options={preferredUnitOptions}
+              keyPrefix="listingFilters.unitOptions.unitOptionsTypes"
+            />
+            <Select
+              id="filter.accessibilityOptions"
+              name="filter.accessibilityOptions"
+              label={t("listingFilters.accessibilityOptions.label")}
+              validation={{ required: true }}
+              register={register}
+              controlClassName="control"
+              options={accessibilityOptions}
+              keyPrefix="listingFilters.accessibilityOptions.accessibilityOptionsTypes"
+            />
+            <Select
+              id="filter.communityOptions"
+              name="filter.communityOptions"
+              label={t("listingFilters.communityOptions.label")}
+              validation={{ required: true }}
+              register={register}
+              controlClassName="control"
+              options={communityOptions}
+              keyPrefix="listingFilters.communityOptions.communityOptionsTypes"
+            />
+          </div>
+        </Form>
+      </Modal>
+      <Drawer
+        open={filterDrawerVisible}
+        title="Drawer Title"
+        onClose={() => setFilterDrawerVisible(false)}
+        actions={[
+          <Button
+            key={0}
+            onClick={() => setFilterDrawerVisible(false)}
+            styleType={AppearanceStyleType.primary}
+          >
+            Submit
+          </Button>,
+          <Button
+            key={1}
+            onClick={() => setFilterDrawerVisible(false)}
+            styleType={AppearanceStyleType.secondary}
+            border={AppearanceBorderType.borderless}
+          >
+            Cancel
+          </Button>,
+        ]}
+      >
+        <p>Placeholder for future text</p>
+      </Drawer>
+      <div className="max-w-3xl m-auto">
+        <Button size={AppearanceSizeType.small} onClick={() => setFilterModalVisible(true)}>
+          {/* TODO:avaleske make this a string */}
+          Filter listings
+        </Button>
+        <Button size={AppearanceSizeType.small} onClick={() => setFilterDrawerVisible(true)}>
+          {/* TODO:avaleske make this a string */}
+          Filter listings (drawer)
+        </Button>
+      </div>
       {!listingsLoading && (
         <div>
           {listingsData && <ListingsList listings={listingsData.items} />}
