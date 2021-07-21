a2293b2e948652c4fcd765922b90f86fe1d5d791
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2021-07-21 15:26:38 -0600

    manually run prettier (#1553)

--- backend/core/src/migration/1624542123483-seed-reserved-community-type.ts
+++ backend/core/src/migration/1624542123483-seed-reserved-community-type.ts
@@ -1,15 +1,18 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class seedReservedCommunityType1624542123483 implements MigrationInterface {
-
-    reservedCommunityTypes = ["specialNeeds", "senior"]
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        for (const reservedCommunityType of this.reservedCommunityTypes) {
-            await queryRunner.query(`INSERT INTO reserved_community_types (name) VALUES ($1)`, [reservedCommunityType])
-        }
+  reservedCommunityTypes = ["specialNeeds", "senior"]
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    for (const reservedCommunityType of this.reservedCommunityTypes) {
+      await queryRunner.query(`INSERT INTO reserved_community_types (name) VALUES ($1)`, [
+        reservedCommunityType,
+      ])
     }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DELETE FROM reserved_community_types WHERE name = ANY($1)`, [this.reservedCommunityTypes])
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DELETE FROM reserved_community_types WHERE name = ANY($1)`, [
+      this.reservedCommunityTypes,
+    ])
+  }
 }
--- backend/core/src/migration/1624624546037-add-image-relation-to-listing.ts
+++ backend/core/src/migration/1624624546037-add-image-relation-to-listing.ts
@@ -1,16 +1,19 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class addImageRelationToListing1624624546037 implements MigrationInterface {
-    name = 'addImageRelationToListing1624624546037'
+  name = "addImageRelationToListing1624624546037"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "listings" ADD "image_id" uuid`);
-        await queryRunner.query(`ALTER TABLE "listings" ADD CONSTRAINT "FK_ecc271b96bd18df0efe47b85186" FOREIGN KEY ("image_id") REFERENCES "assets"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "listings" ADD "image_id" uuid`)
+    await queryRunner.query(
+      `ALTER TABLE "listings" ADD CONSTRAINT "FK_ecc271b96bd18df0efe47b85186" FOREIGN KEY ("image_id") REFERENCES "assets"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "listings" DROP CONSTRAINT "FK_ecc271b96bd18df0efe47b85186"`);
-        await queryRunner.query(`ALTER TABLE "listings" DROP COLUMN "image_id"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "listings" DROP CONSTRAINT "FK_ecc271b96bd18df0efe47b85186"`
+    )
+    await queryRunner.query(`ALTER TABLE "listings" DROP COLUMN "image_id"`)
+  }
 }
