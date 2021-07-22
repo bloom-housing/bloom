5d8880645b90deecae6668f7f1078c01e0a2065c
elenm <16543000+elenm@users.noreply.github.com>
2021-07-22 11:19:59 -0400

    [Eligibility Questionnaire] Add income and section 8 pages (#214)
    Add the income and section 8 pages to the eligibility questionnaire.

--- sites/public/pages/eligibility/income.tsx
+++ sites/public/pages/eligibility/income.tsx
@@ -4,9 +4,70 @@ Prompts the user for their annual income.
 */
 import FormsLayout from "../../layouts/forms"
 import React from "react"
+import { FormCard } from "@bloom-housing/ui-components/src/blocks/FormCard"
+import { t } from "@bloom-housing/ui-components/src/helpers/translator"
+import { ProgressNav } from "@bloom-housing/ui-components/src/navigation/ProgressNav"
+import { ELIGIBILITY_ROUTE, ELIGIBILITY_SECTIONS } from "../../lib/constants"
+import { Form } from "@bloom-housing/ui-components/src/forms/Form"
+import { Button } from "@bloom-housing/ui-components/src/actions/Button"
+import { AppearanceStyleType, Select } from "@bloom-housing/ui-components"
+import { useRouter } from "next/router"
+import { useForm } from "react-hook-form"
 
 const EligibilityIncome = () => {
-  return <FormsLayout />
+  const router = useRouter()
+
+  const incomeRanges = ["below10k", "10kTo20k", "30kTo40k", "40kTo50k", "over50k"]
+
+  /* Form Handler */
+  const { handleSubmit, register } = useForm()
+  const onSubmit = () => {
+    // Not yet implemented.
+  }
+
+  return (
+    <FormsLayout>
+      <FormCard header={t("eligibility.progress.header")}>
+        <ProgressNav
+          currentPageSection={5}
+          completedSections={4}
+          labels={ELIGIBILITY_SECTIONS.map((label) => t(`eligibility.progress.sections.${label}`))}
+        />
+      </FormCard>
+      <FormCard>
+        <div className="form-card__lead pb-0">
+          <h2 className="form-card__title is-borderless">{t("eligibility.income.prompt")}</h2>
+        </div>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-card__group">
+            <p className="field-note mb-4">{t("eligibility.income.description")}</p>
+            <Select
+              id="eligibility.income"
+              name="eligibility.income"
+              placeholder={t("eligibility.income.label")}
+              label={t("eligibility.income.label")}
+              validation={{ required: true }}
+              defaultValue={t("eligibility.income.ranges")}
+              register={register}
+              controlClassName="control"
+              options={incomeRanges}
+              keyPrefix="eligibility.income.ranges"
+            />
+          </div>
+          <div className="form-card__pager">
+            <div className="form-card__pager-row primary">
+              <Button
+                styleType={AppearanceStyleType.primary}
+                onClick={() => router.push(`/${ELIGIBILITY_ROUTE}/${ELIGIBILITY_SECTIONS[5]}`)}
+              >
+                {t("t.next")}
+              </Button>
+            </div>
+          </div>
+        </Form>
+      </FormCard>
+    </FormsLayout>
+  )
 }
 
 export default EligibilityIncome
--- sites/public/pages/eligibility/section8.tsx
+++ sites/public/pages/eligibility/section8.tsx
@@ -4,9 +4,76 @@ Asks whether the user has a section 8 voucher.
 */
 import FormsLayout from "../../layouts/forms"
 import React from "react"
+import { FormCard } from "@bloom-housing/ui-components/src/blocks/FormCard"
+import { t } from "@bloom-housing/ui-components/src/helpers/translator"
+import { ProgressNav } from "@bloom-housing/ui-components/src/navigation/ProgressNav"
+import { ELIGIBILITY_SECTIONS } from "../../lib/constants"
+import { Form } from "@bloom-housing/ui-components/src/forms/Form"
+import { Field } from "@bloom-housing/ui-components/src/forms/Field"
+import { Button } from "@bloom-housing/ui-components/src/actions/Button"
+import { AppearanceStyleType } from "@bloom-housing/ui-components"
+import { useForm } from "react-hook-form"
 
 const EligibilitySection8 = () => {
-  return <FormsLayout />
+  /* Form Handler */
+  const { handleSubmit, register } = useForm()
+  const onSubmit = () => {
+    // Not yet implemented.
+  }
+
+  return (
+    <FormsLayout>
+      <FormCard header={t("eligibility.progress.header")}>
+        <ProgressNav
+          currentPageSection={6}
+          completedSections={5}
+          labels={ELIGIBILITY_SECTIONS.map((label) => t(`eligibility.progress.sections.${label}`))}
+        />
+      </FormCard>
+      <FormCard>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-card__lead pb-0">
+            <h2 className="form-card__title is-borderless">{t("eligibility.section8.prompt")}</h2>
+          </div>
+          <div className="form-card__group">
+            <fieldset>
+              <Field
+                type="radio"
+                id="section8No"
+                name="section8No"
+                label={t("t.no")}
+                register={register}
+                validation={{ required: true }}
+                inputProps={{
+                  value: "no",
+                }}
+              />
+
+              <Field
+                type="radio"
+                id="section8Yes"
+                name="section8Yes"
+                label={t("t.yes")}
+                register={register}
+                validation={{ required: true }}
+                inputProps={{
+                  value: "yes",
+                }}
+              />
+            </fieldset>
+          </div>
+          <div className="form-card__pager">
+            <div className="form-card__pager-row primary">
+              {/*TODO: Implement onClick method to display results.*/}
+              <Button styleType={AppearanceStyleType.primary} onClick={() => {}}>
+                {t("t.done")}
+              </Button>
+            </div>
+          </div>
+        </Form>
+      </FormCard>
+    </FormsLayout>
+  )
 }
 
 export default EligibilitySection8
