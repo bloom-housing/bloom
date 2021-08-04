b258b0256bdb4e379c5a01d49c481713dbee472a
Anthony Kong <anthony.kong@sfgov.org>
2021-08-04 00:36:41 -0700

    DAH-848 - update notifications/ApplicationStatus to include post lottery state and allow exclusion of icon

--- ui-components/src/notifications/ApplicationStatus.tsx
+++ ui-components/src/notifications/ApplicationStatus.tsx
@@ -7,17 +7,29 @@ export interface ApplicationStatusProps {
   content: string
   status?: ApplicationStatusType
   vivid?: boolean
+  withIcon?: boolean
 }
 
 const ApplicationStatus = (props: ApplicationStatusProps) => {
   let bgColor = ""
   // determine styling
   const vivid = props.vivid || false
-  const textColor = vivid ? "text-white" : "text-gray-800"
+  let textColor = vivid ? "text-white" : "text-gray-800"
   const textSize = vivid ? "text-xs" : "text-sm"
 
   const status = props.status || ApplicationStatusType.Open
   const content = props.content || "Application status content"
+  const withIcon = props.withIcon ?? true
+
+  let icon
+
+  if (withIcon) {
+    icon = (
+      <span>
+        <Icon size="medium" symbol="clock" fill={vivid ? IconFillColors.white : undefined} /> &nbsp;
+      </span>
+    )
+  }
 
   switch (status) {
     case ApplicationStatusType.Open:
@@ -26,13 +38,17 @@ const ApplicationStatus = (props: ApplicationStatusProps) => {
     case ApplicationStatusType.Closed:
       bgColor = vivid ? "bg-alert" : "bg-alert-light"
       break
+    case ApplicationStatusType.PostLottery:
+      bgColor = "bg-gray-850"
+      textColor = "text-white"
+      break
     default:
       bgColor = "bg-primary"
   }
 
   return (
     <div className={`application-status ${textSize} ${textColor} ${bgColor}`}>
-      <Icon size="medium" symbol="clock" fill={vivid ? IconFillColors.white : undefined} /> &nbsp;
+      {icon}
       {content}
     </div>
   )
