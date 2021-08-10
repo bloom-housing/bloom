92a114fe6f12c2c6e02133971fb26995ef79ecab
Abbie Farr <afarr@google.com>
2021-08-10 13:37:13 -0400

    migrate partners from leasing agent table

--- backend/core/src/migration/1628543278484-partner-role.ts
+++ backend/core/src/migration/1628543278484-partner-role.ts
@@ -5,10 +5,12 @@ export class partnerRole1628543278484 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user_roles" ("is_admin" boolean NOT NULL DEFAULT false, "is_partner" boolean NOT NULL DEFAULT false, "user_id" uuid NOT NULL, CONSTRAINT "REL_87b8888186ca9769c960e92687" UNIQUE ("user_id"), CONSTRAINT "PK_87b8888186ca9769c960e926870" PRIMARY KEY ("user_id"))`);
-        await queryRunner.query(`INSERT INTO "user_roles" ("user_id") SELECT "id" FROM "user_accounts"`)
-        // Initially, all existing users have partner access. This should be revoked over time.
+        // Add all partners to the table.
+        await queryRunner.query(`INSERT INTO "user_roles" ("user_id") SELECT DISTINCT "user_accounts_id" FROM "listings_leasing_agents_user_accounts"`)
+        // Add any admins to the table.
+        await queryRunner.query(`INSERT INTO "user_roles" ("user_id", "is_admin") SELECT "id", "is_admin" FROM "user_accounts" WHERE "user_accounts"."is_admin" = TRUE`)
+        // Give everyone partner permissions.
         await queryRunner.query(`UPDATE "user_roles" SET "is_partner" = TRUE`)
-        await queryRunner.query(`UPDATE "user_roles" SET "is_admin" = "user_accounts"."is_admin" FROM "user_accounts" WHERE "user_accounts"."id" = "user_roles"."user_id"`)
         await queryRunner.query(`ALTER TABLE "user_roles" ADD CONSTRAINT "FK_87b8888186ca9769c960e926870" FOREIGN KEY ("user_id") REFERENCES "user_accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "user_accounts" DROP COLUMN "is_admin"`);
     }
