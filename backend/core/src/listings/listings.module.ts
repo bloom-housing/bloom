bb37290e38b89494cefd0ffd60f0befc5ac13c9f
Sean Albert <smabert@gmail.com>
2021-08-11 12:43:48 -0700

    Revert redis (#1659)
    * force quit redis connection on app close
    
    * pulls back redis changes until they're ready

--- backend/core/src/listings/listings.module.ts
+++ backend/core/src/listings/listings.module.ts
@@ -1,6 +1,5 @@
-import { CacheModule, CACHE_MANAGER, Inject, Module, OnModuleDestroy } from "@nestjs/common"
+import { CacheModule, Module } from "@nestjs/common"
 import { TypeOrmModule } from "@nestjs/typeorm"
-import * as redisStore from "cache-manager-redis-store"
 import { ListingsService } from "./listings.service"
 import { ListingsController } from "./listings.controller"
 import { Listing } from "./entities/listing.entity"
@@ -9,28 +8,12 @@ import { Preference } from "../preferences/entities/preference.entity"
 import { AuthModule } from "../auth/auth.module"
 import { User } from "../auth/entities/user.entity"
 import { Property } from "../property/entities/property.entity"
-import { Store } from "cache-manager"
-import Redis from "redis"
-
-interface RedisCache extends Cache {
-  store: RedisStore
-}
-
-interface RedisStore extends Store {
-  name: "redis"
-  getClient: () => Redis.RedisClient
-  isCacheableValue: (value: unknown) => boolean
-}
 
 @Module({
   imports: [
     CacheModule.register({
       ttl: 24 * 60 * 60,
-      store: redisStore,
-      url: process.env.REDIS_USE_TLS === "0" ? process.env.REDIS_URL : process.env.REDIS_TLS_URL,
-      tls: {
-        rejectUnauthorized: false,
-      },
+      max: 10,
     }),
     TypeOrmModule.forFeature([Listing, Preference, Unit, User, Property]),
     AuthModule,
@@ -39,17 +22,4 @@ interface RedisStore extends Store {
   exports: [ListingsService],
   controllers: [ListingsController],
 })
-// We have to manually disconnect from redis on app close
-export class ListingsModule implements OnModuleDestroy {
-  redisClient: Redis.RedisClient
-  constructor(@Inject(CACHE_MANAGER) private cacheManager: RedisCache) {
-    this.redisClient = this.cacheManager.store.getClient()
-
-    this.redisClient.on("error", (error) => {
-      console.log("redis error = ", error)
-    })
-  }
-  onModuleDestroy() {
-    this.redisClient.quit()
-  }
-}
+export class ListingsModule {}
