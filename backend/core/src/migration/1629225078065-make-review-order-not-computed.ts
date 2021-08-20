5cd901a5c20f7a1e462910ce655b578870c1cb07
Emily Jablonski <emily.jablonski@exygy.com>
2021-08-20 16:40:47 -0600

    migration for existing data

--- backend/core/src/migration/1629225078065-make-review-order-not-computed.ts
+++ backend/core/src/migration/1629225078065-make-review-order-not-computed.ts
@@ -10,6 +10,25 @@ export class makeReviewOrderNotComputed1629225078065 implements MigrationInterfa
     await queryRunner.query(
       `ALTER TABLE "listings" ADD "review_order_type" "listings_review_order_type_enum"`
     )
+
+    const listingsIDsWithoutReviewOrderType = await queryRunner.query(
+      "SELECT id FROM listings WHERE review_order_type IS NULL"
+    )
+
+    for (const listing of listingsIDsWithoutReviewOrderType) {
+      const listingEventTypes = await queryRunner.query(
+        `SELECT type FROM listing_events WHERE listing_id = '${listing.id}'`
+      )
+      if (listingEventTypes.some((eventType) => eventType.type === "publicLottery")) {
+        await queryRunner.query(
+          `UPDATE listings SET review_order_type = 'lottery' where id = '${listing.id}'`
+        )
+      } else {
+        await queryRunner.query(
+          `UPDATE listings SET review_order_type = 'firstComeFirstServe' where id = '${listing.id}'`
+        )
+      }
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
