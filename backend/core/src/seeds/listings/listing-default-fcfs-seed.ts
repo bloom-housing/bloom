b868e98f49c67b9d3c94754a5180bef72b874280
Emily Jablonski <emily.jablonski@exygy.com>
2021-08-20 16:49:06 -0600

    update seed data

--- backend/core/src/seeds/listings/listing-default-fcfs-seed.ts
+++ backend/core/src/seeds/listings/listing-default-fcfs-seed.ts
@@ -1,4 +1,5 @@
 import { ListingDefaultSeed } from "./listing-default-seed"
+import { ListingReviewOrder } from "../../listings/types/listing-review-order-enum"
 
 export class ListingDefaultFCFSSeed extends ListingDefaultSeed {
   async seed() {
@@ -6,6 +7,7 @@ export class ListingDefaultFCFSSeed extends ListingDefaultSeed {
     return await this.listingRepository.save({
       ...listing,
       name: "Test: Default, FCFS",
+      reviewOrderType: "firstComeFirstServe" as ListingReviewOrder,
       applicationDueDate: null,
       events: [],
     })
