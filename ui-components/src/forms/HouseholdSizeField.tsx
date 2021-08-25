8593d374b31957b3591a56a1acd413aad9f9a5f7
Emily Jablonski <emily.jablonski@exygy.com>
2021-08-25 08:58:13 -0600

    fix cypress test

--- ui-components/src/forms/HouseholdSizeField.tsx
+++ ui-components/src/forms/HouseholdSizeField.tsx
@@ -29,7 +29,7 @@ const HouseholdSizeField = (props: HouseholdSizeFieldProps) => {
 
   return (
     <>
-      {householdSizeMin && householdSizeMax && validate && (
+      {householdSizeMax && validate && (
         <>
           <span className="hidden">
             <input
