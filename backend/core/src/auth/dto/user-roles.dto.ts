465bed7848a1bec6c8e66bfed40d392e95f3e1ee
Michal Plebanski <michalp@airnauts.com>
2021-08-26 12:22:18 +0200

    Add /user tests for updating roles property

--- backend/core/src/auth/dto/user-roles.dto.ts
+++ backend/core/src/auth/dto/user-roles.dto.ts
@@ -2,7 +2,3 @@ import { OmitType } from "@nestjs/swagger"
 import { UserRoles } from "../entities/user-roles.entity"
 
 export class UserRolesDto extends OmitType(UserRoles, ["user"] as const) {}
-
-export class UserRolesCreateDto extends OmitType(UserRolesDto, ["isAdmin", "isPartner"] as const) {}
-
-export class UserRolesUpdateDto extends OmitType(UserRolesDto, ["isAdmin", "isPartner"] as const) {}
--- backend/core/test/listings/listings.e2e-spec.ts
+++ backend/core/test/listings/listings.e2e-spec.ts
@@ -75,8 +75,7 @@ describe("Listings", () => {
 
   // TODO: replace jsonpath with SQL-level filtering
   it("should return only the specified listings", async () => {
-    const query =
-      "/?jsonpath=%24%5B%3F%28%40.applicationAddress.city%3D%3D%22Foster%20City%22%29%5D"
+    const query = "?jsonpath=%24%5B%3F%28%40.applicationAddress.city%3D%3D%22Foster%20City%22%29%5D"
     const res = await supertest(app.getHttpServer()).get(`/listings${query}`).expect(200)
     expect(res.body.items.length).toEqual(1)
     expect(res.body.items[0].applicationAddress.city).toEqual("Foster City")
