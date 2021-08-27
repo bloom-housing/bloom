f0058a3825a73699ce611a69b97f59be77110beb
Anders Schneider <anderseschneider@gmail.com>
2021-08-27 11:42:29 -0400

    Add override translations (#493)

--- sites/public/.jest/setup-tests.js
+++ sites/public/.jest/setup-tests.js
@@ -1,4 +1,6 @@
 import { addTranslation } from "../../../ui-components/src/helpers/translator"
 import general from "../../../ui-components/src/locales/general.json"
+import general_overrides from "../page_content/locale_overrides/general.json"
 
 addTranslation(general)
+addTranslation(general_overrides)
