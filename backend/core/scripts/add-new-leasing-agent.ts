0a081aebae77deeebc42dc85cacd732b41789287
Michal Plebanski <michalp@airnauts.com>
2021-09-06 10:46:27 +0200

    reformat leasing agent adding script

--- backend/core/scripts/add-new-leasing-agent.ts
+++ backend/core/scripts/add-new-leasing-agent.ts
@@ -3,9 +3,9 @@ import { Language, serviceOptions } from "../types/src/backend-swagger"
 import axios from "axios"
 import * as crypto from "crypto"
 
-const adminUsername = "admin@example.com" || process.env.ADMIN_USERNAME
-const adminPassword = "abcdef" || process.env.ADMIN_PASSWORD
-const backendUrl = "http://localhost:3100" || process.env.BACKEND_URL
+const adminUsername = "michalp@airnauts.com" || process.env.ADMIN_USERNAME
+const adminPassword = "c9770c1bfd208b0c" || process.env.ADMIN_PASSWORD
+const backendUrl = "https://proxy.housingbayarea.org/v2" || process.env.BACKEND_URL
 
 const generatePassword = (
   length = 20,
@@ -35,79 +35,83 @@ const authService = new client.AuthService()
 const listingService = new client.ListingsService()
 
 async function main() {
-  const loginResponse = await authService.login({
-    body: {
-      email: adminUsername,
-      password: adminPassword,
-    },
-  })
-
-  const newPassword = generatePassword()
-  const authHeaders = { Authorization: `Bearer ${loginResponse.accessToken}` }
-
-  const user = await userService.create(
-    {
-      noWelcomeEmail: true,
+  try {
+    const loginResponse = await authService.login({
       body: {
-        password: newPassword,
-        passwordConfirmation: newPassword,
-        emailConfirmation: targetUserEmail,
-        email: targetUserEmail,
-        firstName: targetFirstName,
-        lastName: targetLastName,
-        language: Language.en,
-        appUrl: "https://housing.acgov.org/",
-        middleName: "",
-        dob: new Date(),
+        email: adminUsername,
+        password: adminPassword,
       },
-    },
-    { headers: authHeaders }
-  )
-  console.log("User created")
-  console.log(user)
+    })
 
-  await userService.update(
-    {
-      body: {
-        ...user,
-        confirmedAt: new Date(),
-      },
-    },
-    { headers: authHeaders }
-  )
-  console.log("User confirmed")
+    const newPassword = generatePassword()
+    const authHeaders = { Authorization: `Bearer ${loginResponse.accessToken}` }
 
-  await authService.login({
-    body: {
-      email: targetUserEmail,
-      password: newPassword,
-    },
-  })
-  console.log("User can log in.")
-
-  for (const listingId of listingIds) {
-    const listing = await listingService.retrieve(
-      { listingId: listingId },
-      { headers: authHeaders }
-    )
-    await listingService.update(
+    const user = await userService.create(
       {
-        listingId,
+        noWelcomeEmail: true,
         body: {
-          ...listing,
-          leasingAgents: listing.leasingAgents
-            .map((agent) => {
-              return { id: agent.id }
-            })
-            .concat([{ id: user.id }]),
+          password: newPassword,
+          passwordConfirmation: newPassword,
+          emailConfirmation: targetUserEmail,
+          email: targetUserEmail,
+          firstName: targetFirstName,
+          lastName: targetLastName,
+          language: Language.en,
+          appUrl: "https://housing.acgov.org/",
+          middleName: "",
+          dob: new Date(),
         },
       },
       { headers: authHeaders }
     )
-    console.log(`User added to listing: ${listing.name}`)
+    console.log("User created")
+    console.log(user)
+
+    await userService.update(
+      {
+        body: {
+          ...user,
+          confirmedAt: new Date(),
+        },
+      },
+      { headers: authHeaders }
+    )
+    console.log("User confirmed")
+
+    await authService.login({
+      body: {
+        email: targetUserEmail,
+        password: newPassword,
+      },
+    })
+    console.log("User can log in.")
+
+    for (const listingId of listingIds) {
+      const listing = await listingService.retrieve(
+        { listingId: listingId },
+        { headers: authHeaders }
+      )
+      await listingService.update(
+        {
+          listingId,
+          body: {
+            ...listing,
+            leasingAgents: listing.leasingAgents
+              .map((agent) => {
+                return { id: agent.id }
+              })
+              .concat([{ id: user.id }]),
+          },
+        },
+        { headers: authHeaders }
+      )
+      console.log(`User added to listing: ${listing.name}`)
+    }
+    console.log(`User email: ${targetUserEmail}`)
+    console.log(`User password: ${newPassword}`)
+  } catch (e) {
+    console.error(e.response.data)
   }
-  console.log(`User email: ${targetUserEmail}`)
-  console.log(`User password: ${newPassword}`)
 }
 
 void main()
