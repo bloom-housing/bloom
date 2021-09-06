d3a4b9eb9025d24c928b0a8b778ef7b46c00f3d2
Dominik Barcikowski <dominik@airnauts.com>
2021-09-06 10:59:57 +0200

    Prevent unit name undefined

--- backend/core/src/shared/units-transformations.ts
+++ backend/core/src/shared/units-transformations.ts
@@ -300,7 +300,7 @@ export const summarizeUnitsByTypeAndRent = (units: Units): UnitSummary[] => {
   units.forEach((unit) => {
     const currentUnitType = unit.unitType
     const currentUnitRent = unit.monthlyRentAsPercentOfIncome
-    const thisKey = currentUnitType.name.concat(currentUnitRent)
+    const thisKey = currentUnitType?.name.concat(currentUnitRent)
     if (!(thisKey in unitMap)) unitMap[thisKey] = []
     unitMap[thisKey].push(unit)
   })
