6a48260326e0493a87fca53e617a5f9e1edb15e5
Dominik Barcikowski <dominik@airnauts.com>
2021-09-15 16:46:12 +0200

    Filter users by isPartner

--- sites/partners/lib/hooks.ts
+++ sites/partners/lib/hooks.ts
@@ -7,6 +7,7 @@ import {
   EnumApplicationsApiExtraModelOrder,
   EnumApplicationsApiExtraModelOrderBy,
   EnumListingFilterParamsComparison,
+  EnumUserFilterParamsComparison
 } from "@bloom-housing/backend-core/types"
 
 interface PaginationProps {
@@ -273,6 +274,12 @@ export function useUserList({ page, limit }: UseUserListProps) {
     userService.list({
       page,
       limit,
+      filter: [
+        {
+          isPartner: true,
+          $comparison: EnumUserFilterParamsComparison["="]
+        }
+      ]
     })
 
   const { data, error } = useSWR(
