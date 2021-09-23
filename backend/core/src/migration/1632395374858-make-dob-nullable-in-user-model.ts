5dfaa9b8fd1c94531d80c66acd1f087b62d8d6d6
Lint Action <lint-action@samuelmeuli.com>
2021-09-23 11:15:35 +0000

    Fix code style issues with Prettier

--- backend/core/src/migration/1632395374858-make-dob-nullable-in-user-model.ts
+++ backend/core/src/migration/1632395374858-make-dob-nullable-in-user-model.ts
@@ -1,14 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class makeDobNullableInUserModel1632395374858 implements MigrationInterface {
-    name = 'makeDobNullableInUserModel1632395374858'
+  name = "makeDobNullableInUserModel1632395374858"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user_accounts" ALTER COLUMN "dob" DROP NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user_accounts" ALTER COLUMN "dob" DROP NOT NULL`)
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user_accounts" ALTER COLUMN "dob" SET NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user_accounts" ALTER COLUMN "dob" SET NOT NULL`)
+  }
 }
