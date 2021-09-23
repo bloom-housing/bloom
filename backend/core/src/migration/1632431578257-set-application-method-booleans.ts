e1ccb80afcd932429f1d7b8aa17f2063526db127
Lint Action <lint-action@samuelmeuli.com>
2021-09-23 21:50:38 +0000

    Fix code style issues with Prettier

--- backend/core/src/migration/1632431578257-set-application-method-booleans.ts
+++ backend/core/src/migration/1632431578257-set-application-method-booleans.ts
@@ -1,46 +1,55 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class setApplicationMethodBooleans1632431578257 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        // get application methods
-        const methods = await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // get application methods
+    const methods = await queryRunner.query(`
             SELECT type, listing_id
             FROM application_methods
         `)
 
-        for (const method of methods) {
-            if (method.type = "Internal") {
-                await queryRunner.query(`
+    for (const method of methods) {
+      if ((method.type = "Internal")) {
+        await queryRunner.query(
+          `
                     UPDATE listings
                     SET digital_application = $1,
                         common_digital_application = $2
                     WHERE id = $3
-                `, [true, true, method.listing_id])
-            } else if (method.type = "ExternalLink") {
-                await queryRunner.query(`
+                `,
+          [true, true, method.listing_id]
+        )
+      } else if ((method.type = "ExternalLink")) {
+        await queryRunner.query(
+          `
                     UPDATE listings
                     SET digital_application = $1,
                         common_digital_application = $2
                     WHERE id = $3
-                `, [true, false, method.listing_id])
-            } else if (method.type = "PaperPickup") {
-                await queryRunner.query(`
+                `,
+          [true, false, method.listing_id]
+        )
+      } else if ((method.type = "PaperPickup")) {
+        await queryRunner.query(
+          `
                     UPDATE listings
                     SET paper_application = $1
                     WHERE id = $2
-                `, [true, method.listing_id])
-            } else if (method.type = "Referral") {
-                await queryRunner.query(`
+                `,
+          [true, method.listing_id]
+        )
+      } else if ((method.type = "Referral")) {
+        await queryRunner.query(
+          `
                     UPDATE listings
                     SET referral_opportunity = $1
                     WHERE id = $2
-                `, [true, method.listing_id])
-            }
-        }
+                `,
+          [true, method.listing_id]
+        )
+      }
     }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
