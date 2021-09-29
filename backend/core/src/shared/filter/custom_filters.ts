dd2ab4cec79d21eccd3591e6cbb7b97cf8066aab
Abbie Farr <afarr@google.com>
2021-09-29 16:18:22 -0400

    Don't include nulls in senior housing filter

--- backend/core/src/shared/filter/custom_filters.ts
+++ backend/core/src/shared/filter/custom_filters.ts
@@ -5,25 +5,16 @@ import {
 } from "../../listings/types/listing-filter-keys-enum"
 import { filterTypeToFieldMap } from "../../listings/dto/listing.dto"
 
-export function addSeniorHousingQuery(
-  qb: WhereExpression,
-  filterValue: string,
-  includeNulls?: boolean
-) {
+export function addSeniorHousingQuery(qb: WhereExpression, filterValue: string) {
   const whereParameterName = ListingFilterKeys.seniorHousing
   const seniorHousingCommunityType = "senior62"
   const reservedCommunityTypeColumnName = `LOWER(CAST(${
     filterTypeToFieldMap[ListingFilterKeys.seniorHousing]
   } as text))`
   if (filterValue == "true") {
-    qb.andWhere(
-      `(${reservedCommunityTypeColumnName} = LOWER(:${whereParameterName})  ${
-        includeNulls ? `OR ${reservedCommunityTypeColumnName} IS NULL` : ""
-      })`,
-      {
-        [whereParameterName]: seniorHousingCommunityType,
-      }
-    )
+    qb.andWhere(`${reservedCommunityTypeColumnName} = LOWER(:${whereParameterName})`, {
+      [whereParameterName]: seniorHousingCommunityType,
+    })
   } else if (filterValue == "false") {
     qb.andWhere(
       `(${reservedCommunityTypeColumnName} IS NULL OR ${reservedCommunityTypeColumnName} <> LOWER(:${whereParameterName}))`,
