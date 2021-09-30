1cf83f6c3fe420d602e3a0c6370beb0e14260e80
seanmalbert <smabert@gmail.com>
2021-09-29 18:15:34 -0700

    fix lint issues

--- sites/public/src/ListingView.tsx
+++ sites/public/src/ListingView.tsx
@@ -54,8 +54,10 @@ export const ListingView = (props: ListingProps) => {
   let buildingSelectionCriteria, preferencesSection
   const { listing } = props
 
-  const { content: appStatusContent, subContent: appStatusSubContent } =
-    useGetApplicationStatusProps(listing)
+  const {
+    content: appStatusContent,
+    subContent: appStatusSubContent,
+  } = useGetApplicationStatusProps(listing)
 
   const appOpenInFuture = openInFuture(listing)
   const hasNonReferralMethods = listing?.applicationMethods
