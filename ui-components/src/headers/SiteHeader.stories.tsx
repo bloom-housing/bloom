606d237baea0ec52d7221345a2a8343ab909a4b4
Emily Jablonski <emily.jablonski@exygy.com>
2021-10-04 14:58:23 -0600

    fix: super small mobile views

--- ui-components/src/headers/SiteHeader.stories.tsx
+++ ui-components/src/headers/SiteHeader.stories.tsx
@@ -41,7 +41,7 @@ export const withLanguage = () => (
   />
 )
 
-export const withLanguageAndNotice = () => (
+export const withLanguageAndMobileNotice = () => (
   <SiteHeader
     homeURL={"/"}
     languages={[
@@ -49,6 +49,7 @@ export const withLanguageAndNotice = () => (
       { label: "Español", onClick: () => console.log("Clicked Español"), active: false },
       { label: "中文", onClick: () => console.log("Clicked 中文"), active: false },
     ]}
+    noticeMobile={true}
     notice="We're just getting started. We'd love to get your feedback."
     logoSrc="/images/logo_glyph.svg"
     title="Alameda County Housing Portal"
@@ -335,5 +336,56 @@ export const imageOnlyLogo = () => (
     notice="We're just getting started. We'd love to get your feedback."
     logoSrc="https://bit.ly/3me4iUC"
     menuLinks={[]}
+  />
+)
+
+export const DAHLIAToggleSet = () => (
+  <SiteHeader
+    homeURL={"/"}
+    languages={[
+      { label: "English", onClick: () => console.log("Clicked English"), active: true },
+      { label: "Español", onClick: () => console.log("Clicked Español"), active: false },
+      { label: "中文", onClick: () => console.log("Clicked 中文"), active: false },
+    ]}
+    notice="We'd love to get your feedback."
+    noticeMobile={true}
+    mobileDrawer={true}
+    flattenSubMenus={true}
+    imageOnly={true}
+    mobileText={true}
+    logoWidth={"medium"}
+    logoSrc="https://bit.ly/3me4iUC"
+    dropdownItemClassName={"text-xs"}
+    menuLinks={[
+      {
+        title: "My Account",
+        subMenuLinks: [
+          {
+            title: "My Dashboard",
+            href: "/account/dashboard",
+            iconSrc: "https://bit.ly/2YoHrxA",
+            iconClassName: "w-6 pr-2",
+          },
+          {
+            title: "My Applications",
+            href: "/account/dashboard",
+            iconSrc: "https://bit.ly/2YoHrxA",
+            iconClassName: "w-6 pr-2",
+          },
+          {
+            title: "Account Settings",
+            href: "/account/edit",
+            iconSrc: "https://bit.ly/2YoHrxA",
+            iconClassName: "w-6 pr-2",
+          },
+          {
+            title: "Sign Out",
+            onClick: () => {},
+            iconSrc: "https://bit.ly/2YoHrxA",
+            iconClassName: "w-6 pr-2",
+          },
+        ],
+      },
+    ]}
   />
 )
