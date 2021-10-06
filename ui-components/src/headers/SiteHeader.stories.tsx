c2a9f2f15ccba2f4f3609cb70564ec18473f73aa
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2021-10-06 13:50:52 -0600

    refactor: re-write header menu bar, remove bulma (#1885)

--- ui-components/src/headers/SiteHeader.stories.tsx
+++ ui-components/src/headers/SiteHeader.stories.tsx
@@ -1,171 +1,391 @@
 import * as React from "react"
 
-import { SiteHeader, NavbarDropdown } from "./SiteHeader"
-import { Button } from "../actions/Button"
-import { AppearanceSizeType } from "../global/AppearanceTypes"
+import { SiteHeader } from "./SiteHeader"
 
 export default {
   title: "Headers/Site Header",
 }
 
-export const standard = () => (
+export const basic = () => (
   <SiteHeader
+    homeURL={"/"}
+    // languages={[{ label: "English", onClick: () => console.log("Clicked English"), active: true }]}
+    logoSrc="/images/logo_glyph.svg"
+    // notice="This is a preview of our new website. We're just getting started. We'd love to get your feedback."
+    title="Alameda County Housing Portal"
+    menuLinks={[]}
+  />
+)
+
+export const withNotice = () => (
+  <SiteHeader
+    homeURL={"/"}
     logoSrc="/images/logo_glyph.svg"
     notice="This is a preview of our new website. We're just getting started. We'd love to get your feedback."
     title="Alameda County Housing Portal"
-    skip="Skip to content"
-  >
-    <a href="#" className="navbar-item">
-      Hello World
-    </a>
-  </SiteHeader>
+    menuLinks={[]}
+  />
 )
 
-export const standardSlim = () => (
+export const withLanguage = () => (
   <SiteHeader
+    homeURL={"/"}
+    languages={[
+      { label: "English", onClick: () => console.log("Clicked English"), active: true },
+      { label: "Español", onClick: () => console.log("Clicked Español"), active: false },
+      { label: "中文", onClick: () => console.log("Clicked 中文"), active: false },
+    ]}
     logoSrc="/images/logo_glyph.svg"
-    notice="This is a preview of our new website. We're just getting started. We'd love to get your feedback."
+    title="Alameda County Housing Portal"
+    menuLinks={[]}
+  />
+)
+
+export const withLanguageAndMobileNotice = () => (
+  <SiteHeader
+    homeURL={"/"}
+    languages={[
+      { label: "English", onClick: () => console.log("Clicked English"), active: true },
+      { label: "Español", onClick: () => console.log("Clicked Español"), active: false },
+      { label: "中文", onClick: () => console.log("Clicked 中文"), active: false },
+    ]}
+    noticeMobile={true}
+    notice="We're just getting started. We'd love to get your feedback."
+    logoSrc="/images/logo_glyph.svg"
+    title="Alameda County Housing Portal"
+    menuLinks={[]}
+  />
+)
+
+export const slimLogo = () => (
+  <SiteHeader
+    homeURL={"/"}
+    languages={[
+      { label: "English", onClick: () => console.log("Clicked English"), active: true },
+      { label: "Español", onClick: () => console.log("Clicked Español"), active: false },
+      { label: "中文", onClick: () => console.log("Clicked 中文"), active: false },
+    ]}
+    logoWidth={"slim"}
+    notice="We're just getting started. We'd love to get your feedback."
+    logoSrc="/images/logo_glyph.svg"
     title="Housing Portal"
-    skip="Skip to content"
-    logoWidth={"slim"}
-  >
-    <a href="#" className="navbar-item">
-      Hello World
-    </a>
-  </SiteHeader>
+    menuLinks={[]}
+  />
 )
 
-export const standardMedium = () => (
+export const mediumLogo = () => (
   <SiteHeader
-    logoSrc="/images/logo_glyph.svg"
-    notice="This is a preview of our new website. We're just getting started. We'd love to get your feedback."
-    title="Alameda County Housing Portal"
-    skip="Skip to content"
+    homeURL={"/"}
+    languages={[
+      { label: "English", onClick: () => console.log("Clicked English"), active: true },
+      { label: "Español", onClick: () => console.log("Clicked Español"), active: false },
+      { label: "中文", onClick: () => console.log("Clicked 中文"), active: false },
+    ]}
     logoWidth={"medium"}
-  >
-    <a href="#" className="navbar-item">
-      Hello World
-    </a>
-  </SiteHeader>
-)
-
-export const standardWide = () => (
-  <SiteHeader
+    notice="We're just getting started. We'd love to get your feedback."
     logoSrc="/images/logo_glyph.svg"
-    notice="This is a preview of our new website. We're just getting started. We'd love to get your feedback."
-    title="Dahlia San Francisco Housing Portal"
-    skip="Skip to content"
+    title="Housing Portal"
+    menuLinks={[]}
+  />
+)
+
+export const wideLogo = () => (
+  <SiteHeader
+    homeURL={"/"}
+    languages={[
+      { label: "English", onClick: () => console.log("Clicked English"), active: true },
+      { label: "Español", onClick: () => console.log("Clicked Español"), active: false },
+      { label: "中文", onClick: () => console.log("Clicked 中文"), active: false },
+    ]}
     logoWidth={"wide"}
-  >
-    <a href="#" className="navbar-item">
-      Hello World
-    </a>
-  </SiteHeader>
+    notice="We're just getting started. We'd love to get your feedback."
+    logoSrc="/images/logo_glyph.svg"
+    title="Housing Portal"
+    menuLinks={[]}
+  />
 )
 
-export const imageOnlySlim = () => (
+export const withMenuLinks = () => (
   <SiteHeader
-    logoSrc="/images/listing.jpg"
-    imageOnly={true}
-    title={"Image Alt Text"}
-    notice="This is a preview of our new website. We're just getting started. We'd love to get your feedback."
-    skip="Skip to content"
-    logoWidth={"slim"}
-  >
-    <a href="#" className="navbar-item">
-      Hello World
-    </a>
-  </SiteHeader>
+    homeURL={"/"}
+    languages={[
+      { label: "English", onClick: () => console.log("Clicked English"), active: true },
+      { label: "Español", onClick: () => console.log("Clicked Español"), active: false },
+      { label: "中文", onClick: () => console.log("Clicked 中文"), active: false },
+    ]}
+    notice="We're just getting started. We'd love to get your feedback."
+    logoSrc="/images/logo_glyph.svg"
+    title="Alameda County Housing Portal"
+    menuLinks={[
+      {
+        title: "Listings",
+        href: "/",
+      },
+      {
+        title: "Get Assistance",
+        href: "/",
+      },
+      {
+        title: "Sign In",
+        href: "/",
+      },
+    ]}
+  />
 )
 
-export const imageOnlyMedium = () => (
+export const withMenuDropdowns = () => (
   <SiteHeader
-    logoSrc="/images/listing.jpg"
-    imageOnly={true}
-    title={"Image Alt Text"}
-    notice="This is a preview of our new website. We're just getting started. We'd love to get your feedback."
-    skip="Skip to content"
+    homeURL={"/"}
+    languages={[
+      { label: "English", onClick: () => console.log("Clicked English"), active: true },
+      { label: "Español", onClick: () => console.log("Clicked Español"), active: false },
+      { label: "中文", onClick: () => console.log("Clicked 中文"), active: false },
+    ]}
+    notice="We're just getting started. We'd love to get your feedback."
+    logoSrc="/images/logo_glyph.svg"
+    title="Alameda County Housing Portal"
+    menuLinks={[
+      {
+        title: "My Account",
+        subMenuLinks: [
+          {
+            title: "My Dashboard",
+            href: "/account/dashboard",
+          },
+          {
+            title: "My Applications",
+            href: "/account/dashboard",
+          },
+          {
+            title: "Account Settings",
+            href: "/account/edit",
+          },
+          {
+            title: "Sign Out",
+            onClick: () => {},
+          },
+        ],
+      },
+    ]}
+  />
+)
+
+export const withMobileDrawer = () => (
+  <SiteHeader
+    homeURL={"/"}
+    languages={[
+      { label: "English", onClick: () => console.log("Clicked English"), active: true },
+      { label: "Español", onClick: () => console.log("Clicked Español"), active: false },
+      { label: "中文", onClick: () => console.log("Clicked 中文"), active: false },
+    ]}
+    notice="We're just getting started. We'd love to get your feedback."
+    logoSrc="/images/logo_glyph.svg"
+    title="Alameda County Housing Portal"
+    menuLinks={[
+      {
+        title: "Listings",
+        href: "/",
+      },
+      {
+        title: "Get Assistance",
+        href: "/",
+      },
+      {
+        title: "Sign In",
+        href: "/",
+      },
+    ]}
+    mobileDrawer={true}
+  />
+)
+
+export const withMobileText = () => (
+  <SiteHeader
+    homeURL={"/"}
+    languages={[
+      { label: "English", onClick: () => console.log("Clicked English"), active: true },
+      { label: "Español", onClick: () => console.log("Clicked Español"), active: false },
+      { label: "中文", onClick: () => console.log("Clicked 中文"), active: false },
+    ]}
+    mobileText={true}
+    notice="We're just getting started. We'd love to get your feedback."
+    logoSrc="/images/logo_glyph.svg"
+    title="Alameda County Housing Portal"
+    menuLinks={[
+      {
+        title: "Listings",
+        href: "/",
+      },
+      {
+        title: "Get Assistance",
+        href: "/",
+      },
+      {
+        title: "Sign In",
+        href: "/",
+      },
+    ]}
+    mobileDrawer={true}
+  />
+)
+
+export const withNoticeOnMobile = () => (
+  <SiteHeader
+    homeURL={"/"}
+    logoSrc="/images/logo_glyph.svg"
+    notice="We'd love to get your feedback."
+    title="Alameda County Housing Portal"
+    menuLinks={[]}
+    noticeMobile={true}
+  />
+)
+
+export const withFlattenedMobileSubMenus = () => (
+  <SiteHeader
+    homeURL={"/"}
+    languages={[
+      { label: "English", onClick: () => console.log("Clicked English"), active: true },
+      { label: "Español", onClick: () => console.log("Clicked Español"), active: false },
+      { label: "中文", onClick: () => console.log("Clicked 中文"), active: false },
+    ]}
+    flattenSubMenus={true}
+    notice="We're just getting started. We'd love to get your feedback."
+    logoSrc="/images/logo_glyph.svg"
+    title="Alameda County Housing Portal"
+    menuLinks={[
+      {
+        title: "My Account",
+        subMenuLinks: [
+          {
+            title: "My Dashboard",
+            href: "/account/dashboard",
+          },
+          {
+            title: "My Applications",
+            href: "/account/dashboard",
+          },
+          {
+            title: "Account Settings",
+            href: "/account/edit",
+          },
+          {
+            title: "Sign Out",
+            onClick: () => {},
+          },
+        ],
+      },
+    ]}
+  />
+)
+
+export const withDropdownIcons = () => (
+  <SiteHeader
+    homeURL={"/"}
+    languages={[
+      { label: "English", onClick: () => console.log("Clicked English"), active: true },
+      { label: "Español", onClick: () => console.log("Clicked Español"), active: false },
+      { label: "中文", onClick: () => console.log("Clicked 中文"), active: false },
+    ]}
+    notice="We're just getting started. We'd love to get your feedback."
+    logoSrc="/images/logo_glyph.svg"
+    title="Alameda County Housing Portal"
+    dropdownItemClassName={"text-xs"}
+    menuLinks={[
+      {
+        title: "My Account",
+        subMenuLinks: [
+          {
+            title: "My Dashboard",
+            href: "/account/dashboard",
+            iconSrc: "https://bit.ly/2YoHrxA",
+            iconClassName: "w-6 pr-2",
+          },
+          {
+            title: "My Applications",
+            href: "/account/dashboard",
+            iconSrc: "https://bit.ly/2YoHrxA",
+            iconClassName: "w-6 pr-2",
+          },
+          {
+            title: "Account Settings",
+            href: "/account/edit",
+            iconSrc: "https://bit.ly/2YoHrxA",
+            iconClassName: "w-6 pr-2",
+          },
+          {
+            title: "Sign Out",
+            onClick: () => {},
+            iconSrc: "https://bit.ly/2YoHrxA",
+            iconClassName: "w-6 pr-2",
+          },
+        ],
+      },
+    ]}
+  />
+)
+export const imageOnlyLogo = () => (
+  <SiteHeader
+    homeURL={"/"}
+    languages={[
+      { label: "English", onClick: () => console.log("Clicked English"), active: true },
+      { label: "Español", onClick: () => console.log("Clicked Español"), active: false },
+      { label: "中文", onClick: () => console.log("Clicked 中文"), active: false },
+    ]}
+    imageOnly
     logoWidth={"medium"}
-  >
-    <a href="#" className="navbar-item">
-      Hello World
-    </a>
-  </SiteHeader>
+    notice="We're just getting started. We'd love to get your feedback."
+    logoSrc="https://bit.ly/3me4iUC"
+    menuLinks={[]}
+  />
 )
 
-export const imageOnlyWide = () => (
+export const DAHLIAToggleSet = () => (
   <SiteHeader
-    logoSrc="/images/listing.jpg"
+    homeURL={"/"}
+    languages={[
+      { label: "English", onClick: () => console.log("Clicked English"), active: true },
+      { label: "Español", onClick: () => console.log("Clicked Español"), active: false },
+      { label: "中文", onClick: () => console.log("Clicked 中文"), active: false },
+    ]}
+    notice="We'd love to get your feedback."
+    noticeMobile={true}
+    mobileDrawer={true}
+    flattenSubMenus={true}
     imageOnly={true}
-    title={"Image Alt Text"}
-    notice="This is a preview of our new website. We're just getting started. We'd love to get your feedback."
-    skip="Skip to content"
-    logoWidth={"wide"}
-  >
-    <a href="#" className="navbar-item">
-      Hello World
-    </a>
-  </SiteHeader>
-)
-
-export const withDropdownAndButton = () => (
-  <SiteHeader
-    logoSrc="/images/logo_glyph.svg"
-    notice="This is a preview of our new website. We're just getting started. We'd love to get your feedback."
-    title="Alameda County Housing Portal"
-    skip="Skip to content"
-  >
-    <a href="#" className="navbar-item">
-      Hello World
-    </a>
-    <NavbarDropdown menuTitle="Testing Menu">
-      <a href="#" className="navbar-item">
-        My Favorites
-      </a>
-      <a href="#" className="navbar-item">
-        Get Assistance
-      </a>
-      <a href="#" className="navbar-item">
-        Sign In
-      </a>
-    </NavbarDropdown>
-    <div className="navbar-item">
-      <div className="buttons">
-        <Button
-          size={AppearanceSizeType.small}
-          onClick={() => {
-            alert("Hello!")
-          }}
-        >
-          Example Button
-        </Button>
-      </div>
-    </div>
-  </SiteHeader>
-)
-
-export const withLanguageNavigation = () => (
-  <SiteHeader
-    logoSrc="/images/logo_glyph.svg"
-    notice="This is a preview of our new website. We're just getting started. We'd love to get your feedback."
-    title="Alameda County Housing Portal"
-    skip="Skip to content"
-    language={{
-      list: [
-        {
-          prefix: "",
-          label: "English",
-        },
-        {
-          prefix: "es",
-          label: "Spanish",
-        },
-      ],
-      codes: [],
-    }}
-  >
-    <a href="#" className="navbar-item">
-      Hello World
-    </a>
-  </SiteHeader>
+    mobileText={true}
+    logoWidth={"medium"}
+    logoSrc="https://bit.ly/3me4iUC"
+    dropdownItemClassName={"text-xs"}
+    menuLinks={[
+      {
+        title: "My Account",
+        subMenuLinks: [
+          {
+            title: "My Dashboard",
+            href: "/account/dashboard",
+            iconSrc: "https://bit.ly/2YoHrxA",
+            iconClassName: "w-6 pr-2",
+          },
+          {
+            title: "My Applications",
+            href: "/account/dashboard",
+            iconSrc: "https://bit.ly/2YoHrxA",
+            iconClassName: "w-6 pr-2",
+          },
+          {
+            title: "Account Settings",
+            href: "/account/edit",
+            iconSrc: "https://bit.ly/2YoHrxA",
+            iconClassName: "w-6 pr-2",
+          },
+          {
+            title: "Sign Out",
+            onClick: () => {},
+            iconSrc: "https://bit.ly/2YoHrxA",
+            iconClassName: "w-6 pr-2",
+          },
+        ],
+      },
+    ]}
+  />
 )
