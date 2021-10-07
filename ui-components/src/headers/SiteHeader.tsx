577f1e337646b6a7fba10fecd60a89e8e2c821b4
Emily Jablonski <emily.jablonski@exygy.com>
2021-10-07 14:42:03 -0600

    fix: patch test 2

--- ui-components/src/headers/SiteHeader.tsx
+++ ui-components/src/headers/SiteHeader.tsx
@@ -38,7 +38,7 @@ export interface SiteHeaderProps {
 }
 
 const SiteHeader = (props: SiteHeaderProps) => {
-  // Adding a patch change 6
+  // Adding a patch change 7
   const [activeMenus, setActiveMenus] = useState<string[]>([])
   const [activeMobileMenus, setActiveMobileMenus] = useState<string[]>([])
   const [isDesktop, setIsDesktop] = useState(true)
