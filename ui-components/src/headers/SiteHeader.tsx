f7b4d3545b8bc03ec447a0ed94da1da49d89b7f8
Emily Jablonski <emily.jablonski@exygy.com>
2021-10-07 16:21:06 -0600

    style: breaking
    BREAKING CHANGE: breaking

--- ui-components/src/headers/SiteHeader.tsx
+++ ui-components/src/headers/SiteHeader.tsx
@@ -38,7 +38,7 @@ export interface SiteHeaderProps {
 }
 
 const SiteHeader = (props: SiteHeaderProps) => {
-  // Adding a patch change  18
+  // Adding a patch change  19
   const [activeMenus, setActiveMenus] = useState<string[]>([])
   const [activeMobileMenus, setActiveMobileMenus] = useState<string[]>([])
   const [isDesktop, setIsDesktop] = useState(true)
