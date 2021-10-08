6341c3a10d6f5525de4b18277b199e956d008ae9
Pat Plunkett <patplunkett@google.com>
2021-10-08 14:21:30 -0700

    Add missing routePrefix to locales.
    The routePrefix config controls when to apply the is-active class to LanguageNav components.

--- ui-components/__tests__/navigation/LanguageNav.test.tsx
+++ ui-components/__tests__/navigation/LanguageNav.test.tsx
@@ -1,29 +1,54 @@
 import React from "react"
 import { render, cleanup } from "@testing-library/react"
 import { LanguageNav } from "../../src/navigation/LanguageNav"
+import { t } from "../../src/helpers/translator"
+import { mocked } from "ts-jest/utils"
+
+jest.mock("../../src/helpers/translator");
+const mockedT = mocked(t, true)
 
 afterEach(cleanup)
 
 describe("<LanguageNav>", () => {
   it("renders without error", () => {
-    const props = {
-      language: {
-        list: [
-          {
-            prefix: "",
-            label: "English",
-          },
-          {
-            prefix: "es",
-            label: "Spanish",
-          },
-        ],
-        codes: ["en", "es"],
-      },
+    const language = {
+      list: [
+        {
+          prefix: "",
+          label: "English",
+        },
+        {
+          prefix: "es",
+          label: "Spanish",
+        },
+      ],
+      codes: ["en", "es"],
     }
 
-    const { getByText } = render(<LanguageNav language={props.language} />)
+    const { getByText } = render(<LanguageNav language={language} />)
     expect(getByText("English")).toBeTruthy()
     expect(getByText("Spanish")).toBeTruthy()
+  })
+
+  it("sets is-active on correct item", () => {
+    const language = {
+      list: [
+        {
+          prefix: "",
+          label: "English",
+        },
+        {
+          prefix: "es",
+          label: "Spanish",
+        },
+      ],
+      codes: ["en", "es"],
+    }
+
+    mockedT.mockReturnValue("es")
+
+    const { getByText } = render(<LanguageNav language={language} />)
+    expect(getByText("English").className).not.toContain("is-active")
+    expect(getByText("Spanish").className).toContain("is-active")
   })
 })
