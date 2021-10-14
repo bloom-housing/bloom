9f9070d98cdcddbd5735f993129b7c2881717514
Sean Albert <smabert@gmail.com>
2021-10-13 21:25:52 -0700

    refactor: cleanup and makes csvBuilder not dependent on applications

--- backend/core/src/csv/csv-builder.service.ts
+++ backend/core/src/csv/csv-builder.service.ts
@@ -1,145 +1,75 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common"
+import { Injectable, Scope } from "@nestjs/common"
 import dayjs from "dayjs"
-import { CsvEncoder } from "./csv-encoder.service"
 import { ApplicationDto } from "../applications/dto/application.dto"
 import { capitalizeFirstLetter } from "../libs/stringLib"
 
 const HEADER_COUNT_REG = new RegExp(/\s\(\d\)/)
 
-@Injectable()
+interface MappedFields {
+  [key: string]: string
+}
+
+@Injectable({ scope: Scope.REQUEST })
 export class CsvBuilder {
   headers: { [key: string]: number }
-  data: { header: string; mappedField: string; val: any }[]
+  parsedData: { header: string; mappedField: string; val: any }[]
   inProgress: boolean
   excludeKeys: string[]
-  mappedFields: { [key: string]: string }
-  constructor(private readonly csvEncoder: CsvEncoder) {
+  mappedFields: MappedFields
+  constructor() {
     this.headers = {}
-    this.data = []
+    this.parsedData = []
     this.inProgress = false
-    this.excludeKeys = [
-      " id",
-      "appUrl",
-      "createdAt",
-      "confirmationCode",
-      "deletedAt",
-      "listingId",
-      "noEmail",
-      "noPhone",
-      " orderId",
-      "updatedAt",
-      "userId",
-    ]
-    // preface fields with _[A-Z]_ to make sorting easier later
-    this.mappedFields = {
-      id: "_A_Application Number",
-      submissionType: "_B_Application Type",
-      submissionDate: "_C_Application Submission Date",
-      ...this.mapPrimaryApplicantFields("D"),
-      additionalPhoneNumber: "_E_Primary Applicant Additional Phone Number",
-      contactPreferences: "_F_Primary Applicant Preferred Contact Type",
-      ...this.mapAddressFields("applicant address", "Primary Applicant Address", "G"),
-      ...this.mapAddressFields("mailingAddress", "Primary Applicant Mailing Address", "H"),
-      ...this.mapAddressFields("applicant workAddress", "Primary Applicant Work Address", "I"),
-      ...this.mapAlternateContactFields("J"),
-      ...this.mapAddressFields(
-        "alternateContact mailingAddress",
-        "Alternate Contact Mailing Address",
-        "K"
-      ),
-      income: "_I_Income",
-      accessibility: "_L_ADA",
-      incomeVouchers: "_M_Vouchers or Subsidies",
-      preferredUnit: "_N_Requested Unit Type",
-      preferences: "_O_Preferences",
-      householdSize: "_P_Household Size",
-      householdMembers: "_Q_Household Members",
-      markedAsDuplicate: "_R_Marked As Duplicate",
-      flagged: "_S_Flagged As Duplicate",
-      demographics: "_T_Demographics",
-    }
+    this.excludeKeys = []
+    this.mappedFields = {}
   }
 
-  private mapAddressFields(oldKey: string, newKey: string, sort) {
-    const obj = {}
-    const fields = [
-      "street",
-      "street2",
-      "city",
-      "zipCode",
-      "county",
-      "state",
-      "placeName",
-      "latitude",
-      "longitude",
-    ]
-    fields.forEach((field, i) => {
-      obj[`${oldKey} ${field}`] = `_${sort}${i}_${newKey} ${capitalizeFirstLetter(field)}`
-    })
-
-    return obj
-  }
-
-  private mapPrimaryApplicantFields(sort: string) {
-    const obj = {}
-    const fields = [
-      "firstName",
-      "middleName",
-      "lastName",
-      "birthMonth",
-      "birthDay",
-      "birthYear",
-      "emailAddress",
-      "phoneNumber",
-      "phoneNumberType",
-    ]
-    fields.forEach((field, i) => {
-      obj[`applicant ${field}`] = `_${sort}${i}_Primary Applicant ${this.capAndSplit(field)}`
-    })
-
-    return obj
-  }
-
-  private mapAlternateContactFields(sort: string) {
-    const obj = {}
-    const fields = [
-      "firstName",
-      "middleName",
-      "lastName",
-      "type",
-      "agency",
-      "otherType",
-      "emailAddress",
-      "phoneNumber",
-    ]
-
-    fields.forEach((field, i) => {
-      obj[`alternateContact ${field}`] = `_${sort}${i}_Alternate Contact ${this.capAndSplit(field)}`
-    })
-
-    return obj
-  }
-
-  private capAndSplit(str: string): string {
+  public capAndSplit(str: string): string {
     let newStr = capitalizeFirstLetter(str)
     newStr = newStr.split(/(?=[A-Z])/).join(" ")
     return newStr
   }
 
+  public arrayToCapitalizedString(arr: string[]): string {
+    return arr.map((val) => this.capAndSplit(val)).join(" ")
+  }
+
+  /**
+   * this tests two cases of a header, static and dynamic
+   * *
+   * Headers like "applicant firstName" or "houseHoldMembers firstName (1)" are static
+   * and headers like "preference <some other strings>" where it's defined off of the data.
+   * So in the case of dynamic we're just checking the first string to see if it matches
+   */
+  private isHeaderInMappedFields(name): [string, string] {
+    const staticHeader = this.mappedFields[name.replace(HEADER_COUNT_REG, "")]
+    const dynamicHeader = this.mappedFields[name.split(" ")[0]]
+
+    return [staticHeader, dynamicHeader]
+  }
+
+  /**
+   * This builder supports mapped (static and dynamic) and non mapped keys.
+   * For an example, using a mapped static header, we have the "applicant" object, which has
+   * "firstName" as a key. Since we only call setHeader when we've gotten down to non objects,
+   * it's finally called with setHeader("applicant firstName").
+   * Since "applicant firstName" is a mappedField, meaning that mappedField["applicant firstName"] = "Primary Applicant First Name", we want to set the mappedStr to this and pass along to add to the pasredData object.
+   * So that later, we can know what column the corresponding data goes in by that mapped field.
+   */
   private setHeader(header: string, index: number): [string, string?] {
     let newKey = header
     // we pass along the mapped key for reference, so we can more easily set data to the correct column
-    const test1 = this.mappedFields[header]
-    const test2 = this.mappedFields[header.split(" ")[0]]
-    let mappedStr = test1 ?? test2
-    if (!test1 && test2) {
-      const [, ...fields] = header.split(" ")
-      if (fields) {
-        fields.forEach((field) => {
-          mappedStr += " " + this.capAndSplit(field)
-        })
+    const [staticHeader, dynamicHeader] = this.isHeaderInMappedFields(header)
+    let mappedStr = staticHeader ?? dynamicHeader
+    if (!staticHeader && dynamicHeader) {
+      const [, ...names] = header.split(" ")
+      if (names) {
+        mappedStr += " " + this.arrayToCapitalizedString(names)
       }
+    }
+    if (!mappedStr) {
+      newKey = this.arrayToCapitalizedString(newKey.split(" "))
     }
 
     if (index !== 0) {
@@ -154,51 +84,51 @@ export class CsvBuilder {
     return [newKey, mappedStr]
   }
 
-  private resetData() {
-    this.headers = {}
-    this.data = []
-  }
-
-  private setProgress(inProgress: boolean) {
-    this.inProgress = inProgress
-  }
-
   private getExcludeKeysRegex() {
     return new RegExp(this.excludeKeys.join("|"))
   }
 
-  private addToExlucdedKeys(key) {
+  public addToExcludedKeys(key) {
     this.excludeKeys.push(key)
   }
 
-  private parseApplication(key: string, val: any, header: string, index = 0): void {
+  public setExcludedKeys(keys: string[]) {
+    this.excludeKeys = keys
+  }
+
+  public setMappedFields(obj: MappedFields) {
+    this.mappedFields = obj
+  }
+
+  private parseData(key: string, val: any, header: string, index = 0): void {
     if (Array.isArray(val)) {
       val.forEach((val2, i) => {
-        // preferences are a special case
-        if (key === "preferences") {
+        // preferences and programs are a special case
+        // TODO: think of a better way to handle this case
+        if (key === "preferences" || key === "programs") {
           val2.options?.forEach((preference) => {
-            this.parseApplication(
+            this.parseData(
               preference.key,
               preference.checked ? "Yes" : "No",
               `${header} ${val2.key} ${preference.key}`
             )
           })
         } else {
-          this.parseApplication(key, val2, header, i + 1)
+          this.parseData(key, val2, header, i + 1)
         }
       })
     } else if (val instanceof Object) {
       // dates are objects
       if (dayjs(val).isValid()) {
         const [newKey, mappedField] = this.setHeader(header, index)
-        this.data.push({
+        this.parsedData.push({
           header: newKey,
           mappedField,
           val: dayjs(val).format("DD-MM-YYYY h:mm:ss A"),
         })
       } else {
         for (let i = 0, keys = Object.keys(val); i < keys.length; i++) {
-          this.parseApplication(
+          this.parseData(
             keys[i],
             val[keys[i]],
             header.length ? `${header} ${keys[i]}` : keys[i],
@@ -213,7 +143,7 @@ export class CsvBuilder {
         val = val ? "Yes" : "No"
       }
       // use JSON.stringify to escape double and single quotes
-      this.data.push({
+      this.parsedData.push({
         header: newKey,
         mappedField,
         val: val !== undefined && val !== null ? JSON.stringify(val) : "",
@@ -221,51 +151,36 @@ export class CsvBuilder {
     }
   }
 
-  public build(arr: any[], includeDemographics?: boolean): string {
-    if (this.inProgress === true) {
-      throw new HttpException(
-        {
-          status: HttpStatus.TOO_MANY_REQUESTS,
-          error: "Download Already in Progress",
-        },
-        HttpStatus.TOO_MANY_REQUESTS
-      )
-    }
-    this.setProgress(true)
-    let dataString = ""
+  public build(arr: any[]): string {
+    let csvString = ""
     try {
-      if (!includeDemographics) {
-        this.addToExlucdedKeys("demographics")
-      }
       arr.forEach((row: ApplicationDto) => {
-        this.parseApplication("", row, "")
-        this.data.push({ header: "_endRow", mappedField: undefined, val: "_endRow" })
+        this.parseData("", row, "")
+        this.parsedData.push({ header: "_endRow", mappedField: undefined, val: "_endRow" })
       })
-
-      // filter out excluded keys and fields not in mappedFields
+      // filter out excluded keys and fields not in mappedFields (if mappedFields is not empty)
+      const hasMappedFields = Object.keys(this.mappedFields).length > 0
+      const hasExcludeKeys = this.excludeKeys.length > 0
       let headerArray = Object.keys(this.headers).filter((header) => {
-        const mappedField =
-          this.mappedFields[header.replace(HEADER_COUNT_REG, "")] ||
-          this.mappedFields[header.split(" ")[0]]
-        return !this.getExcludeKeysRegex().test(header) && mappedField
+        const mappedField = hasMappedFields
+          ? this.mappedFields[header.replace(HEADER_COUNT_REG, "")] !== undefined ||
+            this.mappedFields[header.split(" ")[0]] !== undefined
+          : true
+        return (hasExcludeKeys === false || !this.getExcludeKeysRegex().test(header)) && mappedField
       })
-
       // initiate arrays to insert data
       const rows = Array.from({ length: arr.length }, () => Array(headerArray.length))
 
       headerArray = headerArray
         .map((column) => {
           let columnString = ""
-          const test1 = this.mappedFields[column.replace(HEADER_COUNT_REG, "")]
-          const test2 = this.mappedFields[column.split(" ")[0]]
-          let mappedField = test1 ?? test2
+          const [staticHeader, dynamicHeader] = this.isHeaderInMappedFields(column)
+          let mappedField = staticHeader ?? dynamicHeader
           if (mappedField) {
-            if (!test1 && test2) {
-              const [, ...fields] = column.split(" ")
-              fields.forEach((field) => {
-                mappedField += " " + this.capAndSplit(field)
-              })
-            } else if (test1) {
+            if (!staticHeader && dynamicHeader) {
+              const [, ...names] = column.split(" ")
+              mappedField += " " + this.arrayToCapitalizedString(names)
+            } else if (staticHeader) {
               const columnCount = HEADER_COUNT_REG.exec(column)
               if (columnCount !== null) {
                 mappedField += columnCount[0]
@@ -274,23 +189,14 @@ export class CsvBuilder {
             columnString = mappedField
           } else {
             // format header names
-            let columnArray = column.split(" ")
-            columnArray = columnArray.map((str) => {
-              let newStr = ""
-
-              // capitalize and split camel cased
-              newStr = this.capAndSplit(str)
-              return newStr
-            })
-
-            columnString = columnArray.join(" ")
+            const columnArray = column.split(" ")
+            columnString = this.arrayToCapitalizedString(columnArray)
           }
           return columnString
         })
         .sort((a, b) => (a < b ? -1 : a > b ? 1 : 0))
-
       let row = 0
-      this.data.forEach((obj) => {
+      this.parsedData.forEach((obj) => {
         if (obj.val === "_endRow") {
           row++
         } else {
@@ -306,25 +212,22 @@ export class CsvBuilder {
       })
 
       // turn headers into csv format
-      dataString = headerArray.map((key) => key.replace(/^_[A-Z]\d?_/, "")).join(",")
+      csvString = headerArray.map((key) => key.replace(/^_[A-Z]\d?_/, "")).join(",")
       if (headerArray.length) {
-        dataString += "\n"
+        csvString += "\n"
       }
 
       // turn rows into csv format
       rows.forEach((row) => {
         if (row.length) {
-          dataString += row.join(",")
-          dataString += "\n"
+          csvString += row.join(",")
+          csvString += "\n"
         }
       })
     } catch (error) {
       console.log("CSV Export Error = ", error)
     }
 
-    this.resetData()
-    this.setProgress(false)
-
-    return dataString
+    return csvString
   }
 }
--- backend/core/src/csv/csv-builder.spec.ts
+++ backend/core/src/csv/csv-builder.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from "@nestjs/testing"
 import { CsvBuilder } from "./csv-builder.service"
-import { CsvEncoder } from "./csv-encoder.service"
+/* import { ApplicationCsvExporter } from "./application-csv-exporter"
+import { ApplicationStatus } from "../applications/types/application-status-enum"
+import { ApplicationSubmissionType } from "../applications/types/application-submission-type-enum" */
 
 // Cypress brings in Chai types for the global expect, but we want to use jest
 // expect here so we need to re-declare it.
@@ -12,9 +14,9 @@ describe("CSVBuilder", () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CsvEncoder, CsvBuilder],
+      providers: [CsvBuilder],
     }).compile()
-    service = module.get<CsvBuilder>(CsvBuilder)
+    service = await module.resolve<CsvBuilder>(CsvBuilder)
   })
 
   it("should be defined", () => {
@@ -24,6 +26,164 @@ describe("CSVBuilder", () => {
   it("create empty response", () => {
     const response = service.build([])
     expect(response).toBe("")
+  })
+  it("create correctly escaped CSV for correct data", () => {
+    const response = service.build([{ foo: "foo", bar: "bar" }])
+    expect(response).toBe('Bar,Foo\n"bar","foo"\n')
+  })
+  it("create CSV and convert boolean to yes/no", () => {
+    const response = service.build([{ foo: true, bar: false }])
+    expect(response).toBe('Bar,Foo\n"No","Yes"\n')
+  })
+  it("create CSV with escaped double quotes", () => {
+    const response = service.build([{ foo: '"', bar: "bar" }])
+    expect(response).toBe('Bar,Foo\n"bar","\\""\n')
+  })
+  it("create CSV with comma in value", () => {
+    const response = service.build([{ foo: "with, comma", bar: "should work," }])
+    expect(response).toBe('Bar,Foo\n"should work,","with, comma"\n')
+  })
+  it("create an CSV for undefined fields", () => {
+    const response = service.build([{}])
+    expect(response).toBe("")
+  })
+  it("create a CSV with nested objects", () => {
+    const response = service.build([{ foo: { bar: 1, baz: 2 } }])
+    expect(response).toBe("Foo Bar,Foo Baz\n1,2\n")
+  })
+  it("create a CSV with an array of strings", () => {
+    const response = service.build([{ arr: ["foo", "bar"] }])
+    expect(response).toBe('Arr (1),Arr (2)\n"foo","bar"\n')
+  })
+  it("create a correct CSV for array type of metadata", () => {
+    const response = service.build([{ arr: [{ fooBar: "foo" }, { fooBar: "foo 2" }] }])
+    expect(response).toBe('Arr Foo Bar (1),Arr Foo Bar (2)\n"foo","foo 2"\n')
+  })
+  it("create an empty CSV for array type of metadata for undefined input array", () => {
+    const response = service.build([])
+    expect(response).toBe("")
+  })
+  it("create a CSV for array type of metadata for null input array", () => {
+    const response = service.build([{ arr: null }])
+    expect(response).toBe("Arr\n\n")
+  })
+  it("create an empty CSV for array type of metadata for empty input array", () => {
+    const response = service.build([{ arr: [] }])
+    expect(response).toBe("")
+  })
+  it("create an empty CSV if nested objects are empty", () => {
+    const response = service.build([{ arr: [{}] }])
+    expect(response).toBe("")
+  })
+  it("create a correct CSV for array type of metadata for single malformed input array", () => {
+    const response = service.build([{ arr: [{ foo: null }] }])
+    expect(response).toBe("Arr Foo (1)\n\n")
+  })
+})
+
+// TODO: add tests specific to ApplicationCsvExporter
+/* describe("ApplicationCsvExporter", () => {
+  let service: ApplicationCsvExporter
+  const now = new Date()
+
+  const BASE_ADDRESS = {
+    city: "city",
+    state: "state",
+    street: "street",
+    zipCode: "zipcode",
+  }
+
+  const BASE_APPLICATIONS = [
+    {
+      id: "app_1",
+      listingId: "listing_1",
+      applicant: {
+        id: "applicant_1",
+        firstName: "first name",
+        middleName: "middle name",
+        lastName: "last name",
+        address: {
+          id: "address_1",
+          createdAt: now,
+          updatedAt: now,
+          ...BASE_ADDRESS,
+        },
+        workAddress: {
+          id: "work_address_1",
+          createdAt: now,
+          updatedAt: now,
+          ...BASE_ADDRESS,
+        },
+        createdAt: now,
+        updatedAt: now,
+      },
+      contactPreferences: [],
+      updatedAt: now,
+      createdAt: now,
+      mailingAddress: {
+        id: "mailing_address_1",
+        createdAt: now,
+        updatedAt: now,
+        ...BASE_ADDRESS,
+      },
+      alternateAddress: {
+        id: "alternate_address_1",
+        createdAt: now,
+        updatedAt: now,
+        ...BASE_ADDRESS,
+      },
+      alternateContact: {
+        id: "alternate_contact_1",
+        createdAt: now,
+        updatedAt: now,
+        mailingAddress: {
+          id: "mailing_address_2",
+          createdAt: now,
+          updatedAt: now,
+          ...BASE_ADDRESS,
+        },
+      },
+      accessibility: {
+        id: "accessibility_1",
+        createdAt: now,
+        updatedAt: now,
+      },
+      demographics: {
+        howDidYouHear: ["ears"],
+        id: "demographics_1",
+        createdAt: now,
+        updatedAt: now,
+      },
+      householdMembers: [],
+      preferredUnit: [],
+      preferences: [],
+      status: ApplicationStatus.submitted,
+      submissionType: ApplicationSubmissionType.electronical,
+      markedAsDuplicate: false,
+      flagged: false,
+      confirmationCode: "code_1",
+    },
+  ]
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [CsvBuilder, ApplicationCsvExporter],
+    }).compile()
+    service = module.get<ApplicationCsvExporter>(ApplicationCsvExporter)
+  })
+
+  it("should be defined", () => {
+    expect(service).toBeDefined()
+  })
+
+  it("create empty response", () => {
+    const response = service.export([])
+    expect(response).toBe("")
+  })
+
+  it("export application with base fields", () => {
+    const response = service.export(BASE_APPLICATIONS, false)
+    console.log("response = ", response)
   })
   it("create correctly escaped CSV for correct data", () => {
     const response = service.build([{ id: "foo", income: "bar" }])
@@ -91,4 +251,4 @@ describe("CSVBuilder", () => {
     const response = service.build([{ submissionType: "bar", id: "foo" }])
     expect(response).toBe('Application Number,Application Type\n"foo","bar"\n')
   })
-})
+}) */
