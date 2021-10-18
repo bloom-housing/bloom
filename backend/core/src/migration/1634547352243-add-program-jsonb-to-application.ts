1136e9a0501d19e38078524a913d9b581f004f45
Lint Action <lint-action@samuelmeuli.com>
2021-10-18 09:04:40 +0000

    Fix code style issues with Prettier

--- backend/core/src/migration/1634547352243-add-program-jsonb-to-application.ts
+++ backend/core/src/migration/1634547352243-add-program-jsonb-to-application.ts
@@ -1,16 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class addProgramJsonbToApplication1634547352243 implements MigrationInterface {
-    name = 'addProgramJsonbToApplication1634547352243'
+  name = "addProgramJsonbToApplication1634547352243"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "applications" ADD "programs" jsonb`);
-        await queryRunner.query(`ALTER TABLE "programs" ADD "form_metadata" jsonb`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "applications" ADD "programs" jsonb`)
+    await queryRunner.query(`ALTER TABLE "programs" ADD "form_metadata" jsonb`)
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "programs" DROP COLUMN "form_metadata"`);
-        await queryRunner.query(`ALTER TABLE "applications" DROP COLUMN "programs"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "programs" DROP COLUMN "form_metadata"`)
+    await queryRunner.query(`ALTER TABLE "applications" DROP COLUMN "programs"`)
+  }
 }
