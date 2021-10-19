3f9ae763cf4d223758c724833abada136650e4d8
Lint Action <lint-action@samuelmeuli.com>
2021-10-19 17:48:34 +0000

    Fix code style issues with Prettier

--- backend/core/src/migration/1634664593091-adds-jurisdiction-index-to-listings.ts
+++ backend/core/src/migration/1634664593091-adds-jurisdiction-index-to-listings.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class addsJurisdictionIndexToListings1634664593091 implements MigrationInterface {
-    name = 'addsJurisdictionIndexToListings1634664593091'
+  name = "addsJurisdictionIndexToListings1634664593091"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE INDEX "IDX_ba0026e02ecfe91791aed1a481" ON "listings" ("jurisdiction_id") `);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE INDEX "IDX_ba0026e02ecfe91791aed1a481" ON "listings" ("jurisdiction_id") `
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "IDX_ba0026e02ecfe91791aed1a481"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX "IDX_ba0026e02ecfe91791aed1a481"`)
+  }
 }
