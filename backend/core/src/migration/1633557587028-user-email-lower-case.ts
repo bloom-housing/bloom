0f423891e2c1ab185e710e4532ca64e0e9bbe749
Yazeed Loonat <yazeedloonat@gmail.com>
2021-10-19 16:50:16 -0700

    fix: migration update

--- backend/core/src/migration/1633557587028-user-email-lower-case.ts
+++ backend/core/src/migration/1633557587028-user-email-lower-case.ts
@@ -4,10 +4,22 @@ export class userEmailLowerCase1633557587028 implements MigrationInterface {
   name = "userEmailLowerCase1633557587028"
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`UPDATE "user_accounts" SET email = lower(email)`)
+    await queryRunner.query(`
+      UPDATE "user_accounts" SET email = lower(email);
+      UPDATE user_accounts SET email = lower(email);
+      UPDATE listings SET leasing_agent_email = lower(leasing_agent_email);
+      UPDATE applicant SET email_address = lower(email_address);
+      UPDATE alternate_contact SET email_address = lower(email_address);
+    `)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`UPDATE "user_accounts" SET email = lower(email)`)
+    await queryRunner.query(`
+      UPDATE "user_accounts" SET email = lower(email);
+      UPDATE user_accounts SET email = lower(email);
+      UPDATE listings SET leasing_agent_email = lower(leasing_agent_email);
+      UPDATE applicant SET email_address = lower(email_address);
+      UPDATE alternate_contact SET email_address = lower(email_address);
+    `)
   }
 }
