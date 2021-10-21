634ecf459fb1e71f03ca1456af6028cbf7e2ab07
Lint Action <lint-action@samuelmeuli.com>
2021-10-21 16:00:53 +0000

    Fix code style issues with Prettier

--- backend/core/src/migration/1634830165326-remove-page-from-listing-program-entity.ts
+++ backend/core/src/migration/1634830165326-remove-page-from-listing-program-entity.ts
@@ -1,14 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class removePageFromListingProgramEntity1634830165326 implements MigrationInterface {
-    name = 'removePageFromListingProgramEntity1634830165326'
+  name = "removePageFromListingProgramEntity1634830165326"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "listing_programs" DROP COLUMN "page"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "listing_programs" DROP COLUMN "page"`)
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "listing_programs" ADD "page" integer`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "listing_programs" ADD "page" integer`)
+  }
 }
