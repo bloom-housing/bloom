9c3967f0b74526db39df4f5dbc7ad9a52741a6ea
Sean Albert <smabert@gmail.com>
2021-11-11 16:33:53 -0700

    fix: recalculate units available on listing update
    picked form dev f1a3dbce6478b16542ed61ab20de5dfb9b797262

--- backend/core/src/listings/listings.service.ts
+++ backend/core/src/listings/listings.service.ts
@@ -154,11 +154,16 @@ export class ListingsService {
     if (!listing) {
       throw new NotFoundException()
     }
+    let availableUnits = 0
     listingDto.units.forEach((unit) => {
       if (!unit.id) {
         delete unit.id
       }
+      if (unit.status === "available") {
+        availableUnits++
+      }
     })
+    listingDto.unitsAvailable = availableUnits
     Object.assign(listing, {
       ...plainToClass(Listing, listingDto, { excludeExtraneousValues: true }),
       property: plainToClass(
