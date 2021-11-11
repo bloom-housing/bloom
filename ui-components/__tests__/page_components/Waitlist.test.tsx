3d36c86f84714a55209524821a48143ddafc5294
Jared White <jared@jaredwhite.com>
2021-11-10 23:33:19 -0800

    test: fix the waitlist tests

--- ui-components/__tests__/page_components/Waitlist.test.tsx
+++ ui-components/__tests__/page_components/Waitlist.test.tsx
@@ -6,19 +6,15 @@ afterEach(cleanup)
 
 describe("<Waitlist>", () => {
   it("renders with a closed waitlist", () => {
-    const { getByText, getAllByText } = render(
+    const { container } = render(
       <Waitlist
         isWaitlistOpen={false}
         waitlistMaxSize={500}
         waitlistCurrentSize={300}
         waitlistOpenSpots={200}
-        unitsAvailable={5}
       />
     )
-    expect(getByText("Waitlist Closed")).toBeTruthy()
-    expect(getByText("Current Waitlist Size"))
-    expect(getByText("Final Waitlist Size"))
-    expect(getAllByText("300").length).toBe(1)
+    expect(container.innerHTML).toEqual("")
   })
   it("renders with an open waitlist", () => {
     const { getByText } = render(
@@ -27,15 +23,20 @@ describe("<Waitlist>", () => {
         waitlistMaxSize={100}
         waitlistCurrentSize={40}
         waitlistOpenSpots={60}
-        unitsAvailable={0}
       />
     )
-    expect(getByText("Waitlist is open")).toBeTruthy()
+    expect(getByText("Available Units and Waitlist")).toBeTruthy()
     expect(getByText("Current Waitlist Size"))
     expect(getByText("40")).toBeTruthy()
+    expect(getByText("Open Waitlist Slots"))
+    expect(getByText("60")).toBeTruthy()
     expect(getByText("Final Waitlist Size"))
     expect(getByText("100")).toBeTruthy()
-    expect(getByText("Submit an application for an open slot on the waitlist.")).toBeTruthy()
+    expect(
+      getByText(
+        "Once ranked applicants fill all available units, the remaining ranked applicants will be placed on a waitlist for those same units."
+      )
+    ).toBeTruthy()
   })
   it("renders with open spots", () => {
     const { getByText } = render(
@@ -44,7 +45,6 @@ describe("<Waitlist>", () => {
         waitlistMaxSize={10}
         waitlistCurrentSize={0}
         waitlistOpenSpots={10}
-        unitsAvailable={5}
       />
     )
     expect(
