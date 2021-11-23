90d99a192ad8f91c9a440763dba4efae6bc1bffe
Emily Jablonski <emily.jablonski@exygy.com>
2021-11-23 13:56:48 -0700

    fix: import

--- sites/public/pages/applications/household/programs.tsx
+++ sites/public/pages/applications/household/programs.tsx
@@ -10,7 +10,6 @@ import {
   t,
   Button,
   AppearanceStyleType,
-  OnClientSide,
 } from "@bloom-housing/ui-components"
 import FormsLayout from "../../../layouts/forms"
 import FormBackLink from "../../../src/forms/applications/FormBackLink"
@@ -19,6 +18,7 @@ import {
   mapProgramToApi,
   getProgramOptionName,
   getProgramOptionDescription,
+  OnClientSide,
 } from "@bloom-housing/shared-helpers"
 
 const ApplicationPrograms = () => {
