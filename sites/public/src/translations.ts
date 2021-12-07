eebd6df6cf78f1f57e884487f0cf3ea7e6e5a159
Emily Jablonski <emily.jablonski@exygy.com>
2021-12-06 19:24:56 -0700

    feat: tagalog language

--- sites/public/src/translations.ts
+++ sites/public/src/translations.ts
@@ -2,14 +2,14 @@ import generalTranslations from "@bloom-housing/ui-components/src/locales/genera
 import spanishTranslations from "@bloom-housing/ui-components/src/locales/es.json"
 import chineseTranslations from "@bloom-housing/ui-components/src/locales/zh.json"
 import vietnameseTranslations from "@bloom-housing/ui-components/src/locales/vi.json"
+import tagalogTranslations from "@bloom-housing/ui-components/src/locales/tl.json"
 
 export const translations = {
   general: generalTranslations,
   es: spanishTranslations,
   zh: chineseTranslations,
   vi: vietnameseTranslations,
+  tl: tagalogTranslations,
 } as Record<string, any>
 
-export const overrideTranslations = {
-  //  zh: additionalChineseTranslations
-} as Record<string, any>
+export const overrideTranslations = {} as Record<string, any>
