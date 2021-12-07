08475a8a97fb3fa2a0de1b84d8cd183ea2eaadaa
Yazeed Loonat <yazeedloonat@gmail.com>
2021-12-07 15:17:39 -0700

    test: fixes test issues

--- backend/core/test/user/user.e2e-spec.ts
+++ backend/core/test/user/user.e2e-spec.ts
@@ -745,7 +745,7 @@ describe("Applications", () => {
       .get(`/user/list`)
       .set(...setAuthorization(adminAccessToken))
       .expect(200)
-    expect(allUsersListRes.body.meta.totalItems >= totalUsersCount).toBe(true)
+    expect(allUsersListRes.body.meta.totalItems).toBe(totalUsersCount)
 
     const portalUsersFilter = [
       {
