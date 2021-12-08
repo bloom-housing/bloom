4235ddc0ea3577a246479fc681fc3cd1911e662e
Lint Action <lint-action@samuelmeuli.com>
2021-12-08 00:21:22 +0000

    Fix code style issues with Prettier

--- backend/core/src/migration/1637710104539-add-listing-features.ts
+++ backend/core/src/migration/1637710104539-add-listing-features.ts
@@ -1,20 +1,29 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class addListingFeatures1637710104539 implements MigrationInterface {
-    name = 'addListingFeatures1637710104539'
+  name = "addListingFeatures1637710104539"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "listing_features" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "elevator" boolean, "wheelchair_ramp" boolean, "service_animals_allowed" boolean, "accessible_parking" boolean, "parking_on_site" boolean, "in_unit_washer_dryer" boolean, "laundry_in_building" boolean, "barrier_free_entrance" boolean, "roll_in_shower" boolean, "grab_bars" boolean, "heating_in_unit" boolean, "ac_in_unit" boolean, CONSTRAINT "PK_88e4fe3e46d21d8b4fdadeb7599" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "listings" ADD "features_id" uuid`);
-        await queryRunner.query(`ALTER TABLE "listings" ADD CONSTRAINT "UQ_ac59a58a02199c57a588f045830" UNIQUE ("features_id")`);
-        await queryRunner.query(`ALTER TABLE "listings" ADD CONSTRAINT "FK_ac59a58a02199c57a588f045830" FOREIGN KEY ("features_id") REFERENCES "listing_features"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "listing_features" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "elevator" boolean, "wheelchair_ramp" boolean, "service_animals_allowed" boolean, "accessible_parking" boolean, "parking_on_site" boolean, "in_unit_washer_dryer" boolean, "laundry_in_building" boolean, "barrier_free_entrance" boolean, "roll_in_shower" boolean, "grab_bars" boolean, "heating_in_unit" boolean, "ac_in_unit" boolean, CONSTRAINT "PK_88e4fe3e46d21d8b4fdadeb7599" PRIMARY KEY ("id"))`
+    )
+    await queryRunner.query(`ALTER TABLE "listings" ADD "features_id" uuid`)
+    await queryRunner.query(
+      `ALTER TABLE "listings" ADD CONSTRAINT "UQ_ac59a58a02199c57a588f045830" UNIQUE ("features_id")`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "listings" ADD CONSTRAINT "FK_ac59a58a02199c57a588f045830" FOREIGN KEY ("features_id") REFERENCES "listing_features"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "listings" DROP CONSTRAINT "FK_ac59a58a02199c57a588f045830"`);
-        await queryRunner.query(`ALTER TABLE "listings" DROP CONSTRAINT "UQ_ac59a58a02199c57a588f045830"`);
-        await queryRunner.query(`ALTER TABLE "listings" DROP COLUMN "features_id"`);
-        await queryRunner.query(`DROP TABLE "listing_features"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "listings" DROP CONSTRAINT "FK_ac59a58a02199c57a588f045830"`
+    )
+    await queryRunner.query(
+      `ALTER TABLE "listings" DROP CONSTRAINT "UQ_ac59a58a02199c57a588f045830"`
+    )
+    await queryRunner.query(`ALTER TABLE "listings" DROP COLUMN "features_id"`)
+    await queryRunner.query(`DROP TABLE "listing_features"`)
+  }
 }
