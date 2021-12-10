830469c171933ea59fb49db0abcbb25954e957b8
Lint Action <lint-action@samuelmeuli.com>
2021-12-10 11:35:40 +0000

    Fix code style issues with Prettier

--- backend/core/src/migration/1639135587230-add-partner-terms-to-jurisdiction.ts
+++ backend/core/src/migration/1639135587230-add-partner-terms-to-jurisdiction.ts
@@ -1,13 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class addPartnerTermsToJurisdiction1639135587230 implements MigrationInterface {
-    name = 'addPartnerTermsToJurisdiction1639135587230'
+  name = "addPartnerTermsToJurisdiction1639135587230"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "jurisdictions" ADD "partner_terms" text`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "jurisdictions" ADD "partner_terms" text`)
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "jurisdictions" DROP COLUMN "partner_terms"`);
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "jurisdictions" DROP COLUMN "partner_terms"`)
+  }
 }
