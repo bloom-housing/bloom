da343842c763be06d904c0be48d01b91d0a55bab
Lint Action <lint-action@samuelmeuli.com>
2021-12-11 01:06:38 +0000

    Fix code style issues with Prettier

--- backend/core/src/cron/cron.service.ts
+++ backend/core/src/cron/cron.service.ts
@@ -1,26 +1,24 @@
 import { Injectable } from "@nestjs/common"
 import { Cron, CronExpression } from "@nestjs/schedule"
-import { UserListQueryParams } from "src/auth/dto/user-list-query-params";
-import { UserService } from "src/auth/services/user.service";
-import { AuthContext } from "src/auth/types/auth-context";
-import { Compare } from "src/shared/dto/filter.dto";
-import { EmailService } from "src/shared/email/email.service";
+import { UserListQueryParams } from "src/auth/dto/user-list-query-params"
+import { UserService } from "src/auth/services/user.service"
+import { AuthContext } from "src/auth/types/auth-context"
+import { Compare } from "src/shared/dto/filter.dto"
+import { EmailService } from "src/shared/email/email.service"
 import { ListingsService } from "../listings/listings.service"
 
 @Injectable()
 export class CronService {
-
   constructor(
     private readonly emailService: EmailService,
     private readonly listingsService: ListingsService,
-    private readonly userService: UserService,
-
+    private readonly userService: UserService
   ) {}
 
   @Cron(CronExpression.EVERY_1ST_DAY_OF_MONTH_AT_NOON)
   async handleCron() {
-     if (!process.env.SEND_NOTIFICATIONS_FOR_UPDATE_LISTINGS_REMINDER) {
-      return;
+    if (!process.env.SEND_NOTIFICATIONS_FOR_UPDATE_LISTINGS_REMINDER) {
+      return
     }
 
     const userQueryParams: UserListQueryParams = {
@@ -31,20 +29,19 @@ export class CronService {
         },
       ],
     }
-    const users = this.userService.list(userQueryParams, new AuthContext());
-    const allListings = await this.listingsService.list({});
-    
+    const users = this.userService.list(userQueryParams, new AuthContext())
+    const allListings = await this.listingsService.list({})
+
     // For each listing, check whether the listed leasing agents are in the list of partner users.
     // If the leasing agent is a partner and has their email notifications turned on, send the reminder email.
-    for (var listing of allListings.items){
-      for (var leasingAgent of listing.leasingAgents){
+    for (var listing of allListings.items) {
+      for (var leasingAgent of listing.leasingAgents) {
         if ((await users).items.includes(leasingAgent)) {
           if (leasingAgent.preferences.sendEmailNotifications == true) {
-            this.emailService.updateListingReminder(listing, leasingAgent);
+            this.emailService.updateListingReminder(listing, leasingAgent)
           }
         }
       }
     }
   }
 }
-
