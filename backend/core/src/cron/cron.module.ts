3bf0140a49125be97372da133a48977e74d1816a
Lint Action <lint-action@samuelmeuli.com>
2021-12-13 18:19:08 +0000

    Fix code style issues with Prettier

--- backend/core/src/cron/cron.module.ts
+++ backend/core/src/cron/cron.module.ts
@@ -1,9 +1,8 @@
 import { Module } from "@nestjs/common"
 import { CronService } from "./cron.service"
-import { UserService } from "../auth/services/user.service";
+import { UserService } from "../auth/services/user.service"
 import { EmailService } from "../shared/email/email.service"
 import { ListingsService } from "../listings/listings.service"
-
 
 @Module({
   imports: [UserService, EmailService, ListingsService],
