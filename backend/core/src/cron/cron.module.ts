cfa900fe45c004f8daf1b7562ba515d9ab066aca
Aisiri Murulidhar <aisirim@google.com>
2021-12-13 21:50:10 -0800

    Add more providers

--- backend/core/src/cron/cron.module.ts
+++ backend/core/src/cron/cron.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common"
+import { UserService } from "../auth/services/user.service"
+import { ListingsService } from "../listings/listings.service"
 import { EmailService } from "../shared/email/email.service"
 import { CronService } from "./cron.service"
 
 @Module({
-  providers: [CronService, EmailService],
+  providers: [CronService, EmailService, ListingsService, UserService],
 })
 export class CronModule {}
