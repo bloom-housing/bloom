577f6a1fa401d7c8464183a00c5f7fafa0e833a4
Aisiri Murulidhar <aisirim@google.com>
2021-12-13 16:27:09 -0800

    Remove imports

--- backend/core/src/cron/cron.module.ts
+++ backend/core/src/cron/cron.module.ts
@@ -1,10 +1,7 @@
 import { Module } from "@nestjs/common"
 import { CronService } from "./cron.service"
-import { EmailService } from "../shared/email/email.service"
-import { ListingsService } from "../listings/listings.service"
 
 @Module({
-  imports: [ EmailService, ListingsService],
   providers: [CronService],
 })
 export class CronModule {}
