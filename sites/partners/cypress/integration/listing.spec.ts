b35d20b58bfa8001711dcd1ce250a947e00d127b
Yazeed Loonat <yazeedloonat@gmail.com>
2021-12-15 13:01:57 -0700

    test: update to spec

--- sites/partners/cypress/integration/listing.spec.ts
+++ sites/partners/cypress/integration/listing.spec.ts
@@ -220,11 +220,8 @@ describe("Listing Management Tests", () => {
     cy.getByTestId("nameField").type(" (Edited)")
     cy.getByTestId("saveAndExitButton").contains("Save & Exit").click()
     cy.getByTestId("listingIsAlreadyLiveButton").contains("Save").click()
-  })
-
-  it("verify the changes saved successfully", () => {
     cy.fixture("listing").then((listing) => {
-      cy.get("#name").contains(`${listing["name"]}`)
+      cy.get("#name").contains(`${listing["name"]} (Edited)`)
     })
   })
 })
