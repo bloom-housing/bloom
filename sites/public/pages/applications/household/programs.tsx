3cb56f370fc35ce3f10f690c5bde18691e21b53c
Emily Jablonski <emily.jablonski@exygy.com>
2021-12-14 18:31:56 -0700

    fix: remove extra render

--- sites/public/pages/applications/household/programs.tsx
+++ sites/public/pages/applications/household/programs.tsx
@@ -87,7 +87,7 @@ const ApplicationPrograms = () => {
           currentPageSection={currentPageSection}
           completedSections={application.completedSections}
           labels={conductor.config.sections.map((label) => t(`t.${label}`))}
-          mounted={OnClientSide()}
+          mounted={clientLoaded}
         />
       </FormCard>
 
