2001155dc16f96007ee814acdeb08f1fc331454b
Emily Jablonski <emily.jablonski@exygy.com>
2022-01-12 12:27:03 -0700

    fix: migration wip

--- backend/core/src/migration/1641855882656-remove-application-due-time.ts
+++ backend/core/src/migration/1641855882656-remove-application-due-time.ts
@@ -4,20 +4,34 @@ export class removeApplicationDueTime1641855882656 implements MigrationInterface
   name = "removeApplicationDueTime1641855882656"
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const listings = await queryRunner.query(`SELECT id from listings`)
+    const listings = await queryRunner.query(
+      `SELECT id, application_due_time, application_due_date from listings`
+    )
 
     for (const listing of listings) {
-      const existingDate = new Date(listing.application_due_date)
-      const existingTime = new Date(listing.application_due_time)
+      let dateTimeString = null
 
-      // Add 5pm default
-      const consolidatedDateTime = listing.application_due_time
-        ? new Date(existingDate.setTime(existingTime.getTime())).toISOString()
-        : listing.application_due_date
+      // If existing due date, pull in existing time or set time as 5pm
+      if (listing["application_due_date"]) {
+        let dueDate = new Date(listing["application_due_date"])
+        if (listing["application_due_time"]) {
+          const dueTime = new Date(listing["application_due_time"])
+          dueDate.setHours(dueTime.getHours())
+        } else {
+          dueDate.setHours(17, 0, 0, 0)
+        }
+        // Format date into db input format
+        const modifiedDateString = dueDate.toISOString()
+        const timeDelimiter = modifiedDateString.indexOf("T")
+        dateTimeString = `${modifiedDateString.substr(
+          0,
+          timeDelimiter
+        )} ${modifiedDateString.substr(timeDelimiter + 1)}`
+      }
 
       await queryRunner.query("UPDATE listings SET application_due_date = ($1) WHERE id = ($2)", [
-        consolidatedDateTime,
-        listing.id,
+        dateTimeString,
+        listing["id"],
       ])
     }
     await queryRunner.query(`ALTER TABLE "listings" DROP COLUMN "application_due_time"`)
