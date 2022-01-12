cc216b02d4c824cf4ec71ebd5d074b39a597ea8a
Emily Jablonski <emily.jablonski@exygy.com>
2022-01-11 20:15:15 -0700

    fix: migration wip

--- backend/core/src/migration/1641855882656-remove-application-due-time.ts
+++ backend/core/src/migration/1641855882656-remove-application-due-time.ts
@@ -4,6 +4,22 @@ export class removeApplicationDueTime1641855882656 implements MigrationInterface
   name = "removeApplicationDueTime1641855882656"
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    const listings = await queryRunner.query(`SELECT id from listings`)
+
+    for (const listing of listings) {
+      const existingDate = new Date(listing.application_due_date)
+      const existingTime = new Date(listing.application_due_time)
+
+      // Add 5pm default
+      const consolidatedDateTime = listing.application_due_time
+        ? new Date(existingDate.setTime(existingTime.getTime())).toISOString()
+        : listing.application_due_date
+
+      await queryRunner.query("UPDATE listings SET application_due_date = ($1) WHERE id = ($2)", [
+        consolidatedDateTime,
+        listing.id,
+      ])
+    }
     await queryRunner.query(`ALTER TABLE "listings" DROP COLUMN "application_due_time"`)
   }
 
