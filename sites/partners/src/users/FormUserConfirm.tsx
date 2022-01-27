3e8903c4e789af2ab48e86b95a1a7cea0e9b1d73
Emily Jablonski <emily.jablonski@exygy.com>
2022-01-27 16:05:11 -0500

    feat: add terms to confirm page

--- sites/partners/src/users/FormUserConfirm.tsx
+++ sites/partners/src/users/FormUserConfirm.tsx
@@ -13,13 +13,17 @@ import {
   useMutate,
   AuthContext,
   AlertBox,
+  Modal,
+  FieldGroup,
 } from "@bloom-housing/ui-components"
 import { useForm } from "react-hook-form"
 import { LoginResponse } from "@bloom-housing/backend-core/types"
+import Markdown from "markdown-to-jsx"
 
 type FormUserConfirmFields = {
   password: string
   passwordConfirmation: string
+  agree: boolean
 }
 
 const MIN_PASSWORD_LENGTH = 8
@@ -40,6 +44,14 @@ const FormUserConfirm = () => {
   password.current = watch("password", "")
 
   const [isLoginLoading, setLoginLoading] = useState(false)
+  const [termsModal, setTermsModal] = useState(null)
+
+  const agreeField = [
+    {
+      id: "agree",
+      label: "I accept the Terms of Service",
+    },
+  ]
 
   const onSubmit = async (data: FormUserConfirmFields) => {
     resetMutation()
@@ -83,87 +95,141 @@ const FormUserConfirm = () => {
   }
 
   return (
-    <FormCard>
-      <div className="form-card__lead text-center border-b mx-0 px-5">
-        <Icon size="2xl" symbol="settings" />
-        <h2 className="form-card__title">{t("users.addPassword")}</h2>
-        <p className="mt-4 field-note">{t("users.needUniquePassword")}</p>
+    <>
+      <FormCard>
+        <div className="form-card__lead text-center border-b mx-0 px-5">
+          <Icon size="2xl" symbol="settings" />
+          <h2 className="form-card__title">{t("users.addPassword")}</h2>
+          <p className="mt-4 field-note">{t("users.needUniquePassword")}</p>
 
-        {isError && (
-          <AlertBox className="mt-5" type="alert" closeable>
-            {t(`errors.alert.badRequest`)}
-          </AlertBox>
-        )}
-      </div>
-
-      <Form id="update-password" onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-card__group is-borderless">
-          <fieldset>
-            <legend className="field-label--caps">
-              {t("authentication.createAccount.password")}
-            </legend>
-            <p className="field-note mb-4">{t("users.makeNote")}</p>
-
-            <div className="mt-5">
-              <Field
-                type="password"
-                name="password"
-                label={t("account.settings.newPassword")}
-                note={t("authentication.createAccount.passwordInfo")}
-                placeholder={t("authentication.createAccount.mustBe8Chars")}
-                validation={{
-                  required: true,
-                  minLength: MIN_PASSWORD_LENGTH,
-                  pattern: passwordRegex,
-                }}
-                error={!!errors?.password}
-                errorMessage={t("authentication.signIn.passwordError")}
-                register={register}
-                className={"mb-1"}
-                inputProps={{
-                  autoComplete: "off",
-                }}
-              />
-            </div>
-
-            <div className="mt-5">
-              <Field
-                type="password"
-                name="passwordConfirmation"
-                label={t("account.settings.confirmNewPassword")}
-                placeholder={t("authentication.createAccount.mustBe8Chars")}
-                validation={{
-                  required: true,
-                  validate: (value) =>
-                    value === password.current ||
-                    t("authentication.createAccount.errors.passwordMismatch"),
-                }}
-                error={!!errors?.passwordConfirmation}
-                errorMessage={t("authentication.createAccount.errors.passwordMismatch")}
-                register={register}
-                className={"mb-1"}
-                inputProps={{
-                  autoComplete: "off",
-                }}
-              />
-            </div>
-          </fieldset>
+          {isError && (
+            <AlertBox className="mt-5" type="alert" closeable>
+              {t(`errors.alert.badRequest`)}
+            </AlertBox>
+          )}
         </div>
 
-        <div className="form-card__pager mt-8">
-          <div className="form-card__pager-row primary">
-            <Button
-              type="submit"
-              styleType={AppearanceStyleType.primary}
-              className={"items-center"}
-              loading={isConfirmLoading || isLoginLoading}
-            >
-              {t("users.confirmAccount")}
-            </Button>
+        <Form id="update-password" onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-card__group is-borderless">
+            <fieldset>
+              <legend className="field-label--caps">
+                {t("authentication.createAccount.password")}
+              </legend>
+              <p className="field-note mb-4">{t("users.makeNote")}</p>
+
+              <div className="mt-5">
+                <Field
+                  type="password"
+                  name="password"
+                  label={t("account.settings.newPassword")}
+                  note={t("authentication.createAccount.passwordInfo")}
+                  placeholder={t("authentication.createAccount.mustBe8Chars")}
+                  validation={{
+                    required: true,
+                    minLength: MIN_PASSWORD_LENGTH,
+                    pattern: passwordRegex,
+                  }}
+                  error={!!errors?.password}
+                  errorMessage={t("authentication.signIn.passwordError")}
+                  register={register}
+                  className={"mb-1"}
+                  inputProps={{
+                    autoComplete: "off",
+                  }}
+                />
+              </div>
+
+              <div className="mt-5">
+                <Field
+                  type="password"
+                  name="passwordConfirmation"
+                  label={t("account.settings.confirmNewPassword")}
+                  placeholder={t("authentication.createAccount.mustBe8Chars")}
+                  validation={{
+                    required: true,
+                    validate: (value) =>
+                      value === password.current ||
+                      t("authentication.createAccount.errors.passwordMismatch"),
+                  }}
+                  error={!!errors?.passwordConfirmation}
+                  errorMessage={t("authentication.createAccount.errors.passwordMismatch")}
+                  register={register}
+                  className={"mb-1"}
+                  inputProps={{
+                    autoComplete: "off",
+                  }}
+                />
+              </div>
+
+              <legend className="field-label--caps pt-8">Terms</legend>
+              <p className="field-note mb-4">
+                To continue you must accept the{" "}
+                <button
+                  onClick={() => setTermsModal(true)}
+                  className={"text-primary underline font-semibold"}
+                >
+                  Terms of Service
+                </button>
+                .
+              </p>
+
+              <FieldGroup
+                name="agree"
+                type="checkbox"
+                fields={agreeField}
+                register={register}
+                validation={{ required: true }}
+                error={!!errors.agree}
+                errorMessage={t("errors.agreeError")}
+                fieldLabelClassName={"text-primary"}
+                dataTestId={"account-terms-agree"}
+              />
+            </fieldset>
           </div>
-        </div>
-      </Form>
-    </FormCard>
+
+          <div className="form-card__pager mt-8">
+            <div className="form-card__pager-row primary">
+              <Button
+                type="submit"
+                styleType={AppearanceStyleType.primary}
+                className={"items-center"}
+                loading={isConfirmLoading || isLoginLoading}
+              >
+                {t("users.confirmAccount")}
+              </Button>
+            </div>
+          </div>
+        </Form>
+      </FormCard>
+
+      <Modal
+        open={!!termsModal}
+        title={"Terms"}
+        onClose={() => setTermsModal(null)}
+        actions={[
+          <Button
+            styleType={AppearanceStyleType.primary}
+            onClick={() => {
+              setTermsModal(null)
+            }}
+          >
+            Ok
+          </Button>,
+        ]}
+        slim={true}
+      >
+        <Markdown options={{ disableParsingRawHTML: false }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris commodo enim sed felis
+          iaculis, in mattis diam dictum. Quisque consequat tellus lorem, et pharetra nibh facilisis
+          a. Curabitur vel viverra felis, sed vulputate magna. Nunc ut orci iaculis, placerat nunc
+          non, dignissim purus. Vivamus tristique, sapien ac gravida cursus, augue ex fringilla leo,
+          in dignissim lacus quam nec mauris. Cras a lacus quis nisl eleifend ornare. Ut sagittis
+          eros libero, ac accumsan nibh lobortis ut. Mauris tempor mauris ac vulputate bibendum. Ut
+          placerat lacinia molestie. Aliquam diam sem, lobortis ac velit aliquam, feugiat venenatis
+          metus.
+        </Markdown>
+      </Modal>
+    </>
   )
 }
 
