bloom-housing/bloom

commit d0f0644e51316a791df384573f485f4580804f7a

refactor: store email from address on jurisdiction entity (#2485)
* refactor: store email from address on jurisdiction entity

* test: fixup backend test

* test: fixup backend test 2

backend/core/src/email/email.service.ts
@@ -41,12 +41,11 @@ export class EmailService {
   }
 
   public async welcome(user: User, appUrl: string, confirmationUrl: string) {
+    const jurisdiction = await this.jurisdictionResolverService.getJurisdiction()
     await this.loadTranslationsForUser(user)
     if (this.configService.get<string>("NODE_ENV") === "production") {
       Logger.log(
-        `Preparing to send a welcome email to ${user.email} from ${this.configService.get<string>(
-          "EMAIL_FROM_ADDRESS"
-        )}...`
+        `Preparing to send a welcome email to ${user.email} from ${jurisdiction.emailFromAddress}...`
       )
     }
     await this.send(
@@ -88,9 +87,7 @@ export class EmailService {
 
     if (this.configService.get<string>("NODE_ENV") == "production") {
       Logger.log(
-        `Preparing to send a confirmation email to ${
-          application.applicant.emailAddress
-        } from ${this.configService.get<string>("EMAIL_FROM_ADDRESS")}...`
+        `Preparing to send a confirmation email to ${application.applicant.emailAddress} from ${jurisdiction.emailFromAddress}...`
       )
     }
 
@@ -133,9 +130,7 @@ export class EmailService {
 
     if (this.configService.get<string>("NODE_ENV") == "production") {
       Logger.log(
-        `Preparing to send a forget password email to ${user.email} from ${this.configService.get<
-          string
-        >("EMAIL_FROM_ADDRESS")}...`
+        `Preparing to send a forget password email to ${user.email} from ${jurisdiction.emailFromAddress}...`
       )
     }
 
@@ -197,10 +192,11 @@ export class EmailService {
   }
 
   private async send(to: string, subject: string, body: string, retry = 3) {
+    const jurisdiction = await this.jurisdictionResolverService.getJurisdiction()
     await this.sendGrid.send(
       {
         to: to,
-        from: this.configService.get<string>("EMAIL_FROM_ADDRESS"),
+        from: jurisdiction.emailFromAddress,
         subject: subject,
         html: body,
       },

backend/core/types/src/backend-swagger.ts
@@ -2445,6 +2445,9 @@ export interface Jurisdiction {
 
   /**  */
   publicUrl: string
+
+  /**  */
+  emailFromAddress: string
 }
 
 export interface AmiChart {
@@ -4236,6 +4239,9 @@ export interface JurisdictionCreate {
   publicUrl: string
 
   /**  */
+  emailFromAddress: string
+
+  /**  */
   programs: Id[]
 
   /**  */
@@ -4266,6 +4272,9 @@ export interface JurisdictionUpdate {
 
   /**  */
   publicUrl: string
+
+  /**  */
+  emailFromAddress: string
 
   /**  */
   programs: Id[]

.circleci/config.yml
@@ -26,7 +26,6 @@ executors:
     environment:
       PORT: "3100"
       EMAIL_API_KEY: "SG.SOME-LONG-SECRET-KEY"
-      EMAIL_FROM_ADDRESS: "Bloom Dev Housing Portal <bloom-no-reply@exygy.dev>"
       APP_SECRET: "CI-LONG-SECRET-KEY"
       # DB URL for migration and seeds:
       DATABASE_URL: "postgres://bloom-ci@localhost:5432/bloom"
@@ -94,7 +93,6 @@ jobs:
           environment:
             PORT: "3100"
             EMAIL_API_KEY: "SG.SOME-LONG-SECRET-KEY"
-            EMAIL_FROM_ADDRESS: "Bloom Dev Housing Portal <bloom-no-reply@exygy.dev>"
             APP_SECRET: "CI-LONG-SECRET-KEY"
             # DB URL for migration and seeds:
             DATABASE_URL: "postgres://bloom-ci@localhost:5432/bloom"

backend/core/.env.template
@@ -8,7 +8,6 @@ REDIS_USE_TLS=0
 THROTTLE_TTL=60
 THROTTLE_LIMIT=2
 EMAIL_API_KEY='SOME-LONG-SECRET-KEY'
-EMAIL_FROM_ADDRESS='Bloom Dev Housing Portal <bloom-no-reply@exygy.dev>'
 APP_SECRET='SOME-LONG-SECRET-KEY'
 CLOUDINARY_SECRET=CLOUDINARY_SECRET
 CLOUDINARY_KEY=CLOUDINARY_KEY

backend/core/archer.ts
@@ -19,6 +19,7 @@ export const SanMateoHUD2019: AmiChart = {
     createdAt: new Date(),
     updatedAt: new Date(),
     name: "Alameda",
+    emailFromAddress: "Alameda Housing Email",
     programs: [],
     languages: [EnumJurisdictionLanguages.en],
     preferences: [],

backend/core/src/jurisdictions/entities/jurisdiction.entity.ts
@@ -60,4 +60,9 @@ export class Jurisdiction extends AbstractEntity {
   @Expose()
   @IsString({ groups: [ValidationsGroupsEnum.default] })
   publicUrl: string
+
+  @Column({ nullable: true, type: "text" })
+  @Expose()
+  @IsString({ groups: [ValidationsGroupsEnum.default] })
+  emailFromAddress: string
 }

backend/core/src/migration/1644365733034-add-custom-email-from.ts
@@ -1,0 +1,34 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+import { CountyCode } from "../shared/types/county-code"
+
+export class addCustomEmailFrom1644365733034 implements MigrationInterface {
+  name = "addCustomEmailFrom1644365733034"
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "jurisdictions" ADD "email_from_address" text`)
+    const jurisdictions: Array<{
+      id: string
+      name: string
+      public_url: string
+    }> = await queryRunner.query(`SELECT id, name, public_url from jurisdictions`)
+
+    const setEmailFromAddress = async (emailFromAddress: string, countyCode: CountyCode) => {
+      const jurisdiction = jurisdictions.find((j) => j.name === countyCode)
+      if (jurisdiction) {
+        await queryRunner.query(`UPDATE jurisdictions SET email_from_address = $1 WHERE id = $2`, [
+          emailFromAddress,
+          jurisdiction.id,
+        ])
+      }
+    }
+
+    setEmailFromAddress("Alameda: Housing Bay Area <bloom-no-reply@exygy.dev>", CountyCode.alameda)
+    setEmailFromAddress("SJ: HousingBayArea.org <bloom-no-reply@exygy.dev>", CountyCode.san_jose)
+    setEmailFromAddress("SMC: HousingBayArea.org <bloom-no-reply@exygy.dev>", CountyCode.san_mateo)
+    setEmailFromAddress("Detroit Housing <bloom-no-reply@exygy.dev>", CountyCode.detroit)
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "jurisdictions" DROP COLUMN "email_from_address"`)
+  }
+}

backend/core/src/seeder/seeds/jurisdictions.ts
@@ -4,10 +4,38 @@ import { Language } from "../../shared/types/language-enum"
 import { JurisdictionsService } from "../../jurisdictions/services/jurisdictions.service"
 
 export const defaultJurisdictions: JurisdictionCreateDto[] = [
-  { name: "Alameda", preferences: [], languages: [Language.en], programs: [], publicUrl: "" },
-  { name: "San Jose", preferences: [], languages: [Language.en], programs: [], publicUrl: "" },
-  { name: "San Mateo", preferences: [], languages: [Language.en], programs: [], publicUrl: "" },
-  { name: "Detroit", preferences: [], languages: [Language.en], programs: [], publicUrl: "" },
+  {
+    name: "Alameda",
+    preferences: [],
+    languages: [Language.en],
+    programs: [],
+    publicUrl: "",
+    emailFromAddress: "Alameda: Housing Bay Area",
+  },
+  {
+    name: "San Jose",
+    preferences: [],
+    languages: [Language.en],
+    programs: [],
+    publicUrl: "",
+    emailFromAddress: "SJ: HousingBayArea.org",
+  },
+  {
+    name: "San Mateo",
+    preferences: [],
+    languages: [Language.en],
+    programs: [],
+    publicUrl: "",
+    emailFromAddress: "SMC: HousingBayArea.org",
+  },
+  {
+    name: "Detroit",
+    preferences: [],
+    languages: [Language.en],
+    programs: [],
+    publicUrl: "",
+    emailFromAddress: "Detroit Housing",
+  },
 ]
 
 export async function createJurisdictions(app: INestApplicationContext) {

commit 04bd6cb1b749b53f7eb92b2156421068d459ca21

fix: email service bug introduced by #2485)

backend/core/src/auth/entities/user.entity.ts
@@ -138,10 +138,10 @@ export class User {
   @JoinTable()
   jurisdictions: Jurisdiction[]
 
-  @Column({ type: "bool" })
+  @Column({ type: "bool", default: false })
   @Expose()
   @IsBoolean({ groups: [ValidationsGroupsEnum.default] })
-  mfaEnabled: boolean
+  mfaEnabled?: boolean
 
   @Column("varchar", { nullable: true })
   @Expose()

backend/core/src/email/email.service.ts
@@ -15,6 +15,7 @@ import { Application } from "../applications/entities/application.entity"
 import { ListingReviewOrder } from "../listings/types/listing-review-order-enum"
 import { Jurisdiction } from "../jurisdictions/entities/jurisdiction.entity"
 import { Language } from "../shared/types/language-enum"
+import { JurisdictionsService } from "../jurisdictions/services/jurisdictions.service"
 
 @Injectable({ scope: Scope.REQUEST })
 export class EmailService {
@@ -24,7 +25,8 @@ export class EmailService {
     private readonly sendGrid: SendGridService,
     private readonly configService: ConfigService,
     private readonly translationService: TranslationsService,
-    private readonly jurisdictionResolverService: JurisdictionResolverService
+    private readonly jurisdictionResolverService: JurisdictionResolverService,
+    private readonly jurisdictionService: JurisdictionsService
   ) {
     this.polyglot = new Polyglot({
       phrases: {},
@@ -41,7 +43,7 @@ export class EmailService {
   }
 
   public async welcome(user: User, appUrl: string, confirmationUrl: string) {
-    const jurisdiction = await this.jurisdictionResolverService.getJurisdiction()
+    const jurisdiction = await this.getUserJurisdiction(user)
     await this.loadTranslationsForUser(user)
     if (this.configService.get<string>("NODE_ENV") === "production") {
       Logger.log(
@@ -50,6 +52,7 @@ export class EmailService {
     }
     await this.send(
       user.email,
+      jurisdiction.emailFromAddress,
       this.polyglot.t("register.welcome"),
       this.template("register-email")({
         user: user,
@@ -59,16 +62,37 @@ export class EmailService {
     )
   }
 
+  private async getUserJurisdiction(user?: User) {
+    let jurisdiction = await this.jurisdictionResolverService.getJurisdiction()
+    if (!jurisdiction && user?.jurisdictions) {
+      jurisdiction = await this.jurisdictionService.findOne({
+        where: { id: user.jurisdictions[0].id },
+      })
+    }
+    return jurisdiction
+  }
+
+  private async getListingJurisdiction(listing?: Listing) {
+    let jurisdiction = await this.jurisdictionResolverService.getJurisdiction()
+    if (!jurisdiction && listing?.jurisdiction) {
+      jurisdiction = listing.jurisdiction
+    }
+
+    return jurisdiction
+  }
+
   private async loadTranslationsForUser(user: User) {
     const language = user.language || Language.en
-    const jurisdiction = await this.jurisdictionResolverService.getJurisdiction()
+    const jurisdiction = await this.getUserJurisdiction(user)
     void (await this.loadTranslations(jurisdiction, language))
   }
 
   public async changeEmail(user: User, appUrl: string, confirmationUrl: string, newEmail: string) {
+    const jurisdiction = await this.getUserJurisdiction(user)
     await this.loadTranslationsForUser(user)
     await this.send(
       newEmail,
+      jurisdiction.emailFromAddress,
       "Bloom email change request",
       this.template("change-email")({
         user: user,
@@ -79,9 +103,11 @@ export class EmailService {
   }
 
   public async sendMfaCode(user: User, email: string, mfaCode: string) {
+    const jurisdiction = await this.getUserJurisdiction(user)
     await this.loadTranslationsForUser(user)
     await this.send(
       email,
+      jurisdiction.emailFromAddress,
       "Bloom account access token",
       this.template("mfa-code")({
         user: user,
@@ -91,7 +117,7 @@ export class EmailService {
   }
 
   public async confirmation(listing: Listing, application: Application, appUrl: string) {
-    const jurisdiction = await this.jurisdictionResolverService.getJurisdiction()
+    const jurisdiction = await this.getListingJurisdiction(listing)
     void (await this.loadTranslations(jurisdiction, application.language || Language.en))
     let whatToExpectText
     const listingUrl = `${appUrl}/listing/${listing.id}`
@@ -123,6 +149,7 @@ export class EmailService {
     }
     await this.send(
       application.applicant.emailAddress,
+      jurisdiction.emailFromAddress,
       this.polyglot.t("confirmation.subject"),
       compiledTemplate({
         listing: listing,
@@ -135,7 +162,7 @@ export class EmailService {
   }
 
   public async forgotPassword(user: User, appUrl: string) {
-    const jurisdiction = await this.jurisdictionResolverService.getJurisdiction()
+    const jurisdiction = await this.getUserJurisdiction(user)
     void (await this.loadTranslations(jurisdiction, user.language))
     const compiledTemplate = this.template("forgot-password")
     const resetUrl = `${appUrl}/reset-password?token=${user.resetToken}`
@@ -148,6 +175,7 @@ export class EmailService {
 
     await this.send(
       user.email,
+      jurisdiction.emailFromAddress,
       this.polyglot.t("forgotPassword.subject"),
       compiledTemplate({
         resetUrl: resetUrl,
@@ -203,12 +231,11 @@ export class EmailService {
     return partials
   }
 
-  private async send(to: string, subject: string, body: string, retry = 3) {
-    const jurisdiction = await this.jurisdictionResolverService.getJurisdiction()
+  private async send(to: string, from: string, subject: string, body: string, retry = 3) {
     await this.sendGrid.send(
       {
         to: to,
-        from: jurisdiction.emailFromAddress,
+        from,
         subject: subject,
         html: body,
       },
@@ -219,7 +246,7 @@ export class EmailService {
           const { body: errBody } = response
           console.error(`Error sending email to: ${to}! Error body: ${errBody}`)
           if (retry > 0) {
-            void this.send(to, subject, body, retry - 1)
+            void this.send(to, from, subject, body, retry - 1)
           }
         }
       }
@@ -231,8 +258,10 @@ export class EmailService {
       user.jurisdictions?.length === 1 ? user.jurisdictions[0] : null,
       user.language || Language.en
     ))
+    const jurisdiction = await this.getUserJurisdiction(user)
     await this.send(
       user.email,
+      jurisdiction.emailFromAddress,
       this.polyglot.t("invite.hello"),
       this.template("invite")({
         user: user,

backend/core/src/jurisdictions/jurisdictions.module.ts
@@ -10,6 +10,6 @@ import { JurisdictionResolverService } from "./services/jurisdiction-resolver.se
   imports: [TypeOrmModule.forFeature([Jurisdiction]), forwardRef(() => AuthModule)],
   controllers: [JurisdictionsController],
   providers: [JurisdictionsService, JurisdictionResolverService],
-  exports: [JurisdictionResolverService],
+  exports: [JurisdictionsService, JurisdictionResolverService],
 })
 export class JurisdictionsModule {}

backend/core/types/src/backend-swagger.ts
@@ -4010,7 +4010,7 @@ export interface User {
   updatedAt: Date
 
   /**  */
-  mfaEnabled: boolean
+  mfaEnabled?: boolean
 
   /**  */
   lastLoginAt?: Date
@@ -4116,7 +4116,7 @@ export interface UserBasic {
   updatedAt: Date
 
   /**  */
-  mfaEnabled: boolean
+  mfaEnabled?: boolean
 
   /**  */
   lastLoginAt?: Date