a05e13bd3d45643c196d9da6ca13d39c9b5024f3
Michal Plebanski <michalp@airnauts.com>
2022-02-15 17:32:09 +0100

    fix(backend): mfa_enabled migration fix

--- backend/core/src/migration/1639051722043-add-mfa-related-columns-to-user-entity.ts
+++ backend/core/src/migration/1639051722043-add-mfa-related-columns-to-user-entity.ts
@@ -4,11 +4,20 @@ export class addMfaRelatedColumnsToUserEntity1639051722043 implements MigrationI
   name = "addMfaRelatedColumnsToUserEntity1639051722043"
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "user_accounts" ADD "mfa_enabled" boolean NOT NULL`)
+    await queryRunner.query(`ALTER TABLE "user_accounts" ADD "mfa_enabled" boolean NOT NULL DEFAULT FALSE`)
     await queryRunner.query(`ALTER TABLE "user_accounts" ADD "mfa_code" character varying`)
+    await  queryRunner.query(`
+        UPDATE user_accounts
+        SET mfa_enabled = true
+        WHERE id IN
+            (SELECT id
+             FROM user_accounts
+             LEFT JOIN user_roles on user_accounts.id = user_roles.user_id WHERE user_roles.is_partner = true OR user_roles.is_admin = true)`)
+
     await queryRunner.query(
       `ALTER TABLE "user_accounts" ADD "mfa_code_updated_at" TIMESTAMP WITH TIME ZONE`
     )
+
     const mfaCodeEmail = {
       message: "Access token for your account has been requested.",
       mfaCode: "Your access token is: %{mfaCode}",
