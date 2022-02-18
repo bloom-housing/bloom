98b87c11aa75e36a16ef8857d2391ddcebe4679b
Jared White <jared@jaredwhite.com>
2022-02-17 21:17:29 -0800

    fix: more style tweaks for AlertBox

--- sites/public/pages/preview/listings/[id].tsx
+++ sites/public/pages/preview/listings/[id].tsx
@@ -29,7 +29,7 @@ export default function ListingPage(props: ListingProps) {
       </Head>
       <MetaTags title={listing.name} image={metaImage} description={metaDescription} />
       <AlertBox
-        className="pt-5 pb-8 bg-red-700 font-bold text-sm"
+        className="pt-6 pb-4 bg-red-700 font-bold text-sm"
         type="alert"
         boundToLayoutWidth
         inverted
