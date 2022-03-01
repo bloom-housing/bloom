f68b79d7bc8d7bb56b804014fa518e37deb39305
Sean Albert <smabert@gmail.com>
2022-03-01 09:35:49 -0700

    fix: lint and cypress errors

--- sites/partners/src/users/FormUserManage.tsx
+++ sites/partners/src/users/FormUserManage.tsx
@@ -117,7 +117,7 @@ const FormUserManage = ({ mode, user, listings, onDrawerClose }: FormUserManageP
       }
 
       return []
-    })() as string[]
+    })()
 
     const validation = await trigger()
 
@@ -356,7 +356,7 @@ const FormUserManage = ({ mode, user, listings, onDrawerClose }: FormUserManageP
                   register={register}
                   type="checkbox"
                   inputProps={{
-                    onChange: (e) => updateAllCheckboxes(e),
+                    onChange: (e: React.ChangeEvent<HTMLInputElement>) => updateAllCheckboxes(e),
                   }}
                 />
 
