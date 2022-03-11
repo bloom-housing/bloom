bac51af07204d8bc5ae09cd405af3f5c0d2df5ec
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2022-03-10 17:45:06 -0700

    fix: styling issues in features section (#1039)

--- sites/public/src/ListingView.tsx
+++ sites/public/src/ListingView.tsx
@@ -487,6 +487,7 @@ export const ListingView = (props: ListingProps) => {
                 depositMax={listing.depositMax}
                 applicationFee={listing.applicationFee}
                 costsNotIncluded={listing.costsNotIncluded}
+                containerClass={"mt-4"}
               />
             </div>
           )}
--- ui-components/src/page_components/listing/AdditionalFees.tsx
+++ ui-components/src/page_components/listing/AdditionalFees.tsx
@@ -7,6 +7,7 @@ export interface AdditionalFeesProps {
   applicationFee?: string
   costsNotIncluded?: string
   depositHelperText?: string
+  containerClass?: string
 }
 
 const AdditionalFees = (props: AdditionalFeesProps) => {
@@ -23,7 +24,7 @@ const AdditionalFees = (props: AdditionalFeesProps) => {
     else return `$${max}`
   }
   return (
-    <div className="info-card bg-gray-100 border-0">
+    <div className={`info-card bg-gray-100 border-0 ${props.containerClass}`}>
       <p className="info-card__title">{t("listings.sections.additionalFees")}</p>
       <div className="info-card__columns text-sm">
         {props.applicationFee && (
