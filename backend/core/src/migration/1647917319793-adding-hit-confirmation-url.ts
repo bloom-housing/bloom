8fdb1a30814b669a5f08cf2bd76acc62c5072e9e
Lint Action <lint-action@samuelmeuli.com>
2022-03-28 22:30:27 +0000

    Fix code style issues with Prettier

--- backend/core/src/migration/1647917319793-adding-hit-confirmation-url.ts
+++ backend/core/src/migration/1647917319793-adding-hit-confirmation-url.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm"
 
 export class addingHitConfirmationUrl1647917319793 implements MigrationInterface {
-    name = 'addingHitConfirmationUrl1647917319793'
+  name = "addingHitConfirmationUrl1647917319793"
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user_accounts" ADD "hit_confirmation_url" TIMESTAMP WITH TIME ZONE`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user_accounts" ADD "hit_confirmation_url" TIMESTAMP WITH TIME ZONE`
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user_accounts" DROP COLUMN "hit_confirmation_url"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user_accounts" DROP COLUMN "hit_confirmation_url"`)
+  }
 }
