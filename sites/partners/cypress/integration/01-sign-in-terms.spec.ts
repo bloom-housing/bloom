4e1d42b02712da694f747d7227da258397205cae
Dominik Barcikowski <dominik@airnauts.com>
2022-03-30 11:24:26 +0200

    feat: Update tests
    Closes #2537

--- sites/partners/cypress/integration/01-sign-in-terms.spec.ts
+++ sites/partners/cypress/integration/01-sign-in-terms.spec.ts
@@ -1,13 +1,6 @@
-describe("Log in using MFA Tests", () => {
-  it("should log in using mfa pathway", () => {
-    cy.intercept("POST", "/auth/request-mfa-code", {
-      statusCode: 201,
-      body: {
-        email: "mfauser@bloom.com",
-        yazeedTest: "yest",
-      },
-    })
-    cy.loginWithMfa()
+describe("Log in and accept terms", () => {
+  it("should log in", () => {
+    cy.loginAndAcceptTerms()
     cy.signOut()
   })
 })
