2b795cb6c47c084937e996332d1583e9e5bcbc54
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2022-04-04 16:02:34 -0600

    feat: listing card title redesign, multiple tags possible (#2531)

--- ui-components/src/headers/Heading.stories.tsx
+++ ui-components/src/headers/Heading.stories.tsx
@@ -1,12 +1,22 @@
 import * as React from "react"
-
 import { Heading } from "./Heading"
 
 export default {
   title: "Headers/Heading",
 }
-export const base = () => <Heading>Test Header</Heading>
-
-export const underlined = () => <Heading underline>Test Header</Heading>
-
-export const underlinedWithH6 = () => <Heading priority={6}>Test Header</Heading>
+export const base1 = () => <Heading>Test Header</Heading>
+export const base2 = () => <Heading priority={2}>Test Header</Heading>
+export const base3 = () => <Heading priority={3}>Test Header</Heading>
+export const base4 = () => <Heading priority={4}>Test Header</Heading>
+export const base5 = () => <Heading priority={5}>Test Header</Heading>
+export const base6 = () => <Heading priority={6}>Test Header</Heading>
+export const cardHeader = () => <Heading style={"cardHeader"}>Card Header</Heading>
+export const cardSubheader = () => <Heading style={"cardSubheader"}>Card Subheader</Heading>
+export const tableHeader = () => <Heading style={"tableHeader"}>Table Header</Heading>
+export const tableSubheader = () => <Heading style={"tableSubheader"}>Table Subheader</Heading>
+export const sidebarHeader = () => <Heading style={"sidebarHeader"}>Sidebar Header</Heading>
+export const customPriority = () => (
+  <Heading style={"cardSubheader"} priority={6}>
+    Custom Priority
+  </Heading>
+)
