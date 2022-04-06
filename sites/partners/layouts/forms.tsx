9ad2135dcfc6e701cbabd33276ac894c7f4eb0f5
Dominik Barcikowski <dominik@airnauts.com>
2022-04-06 10:22:26 +0200

    feat: Update form width
    Closes #2537

--- sites/partners/layouts/forms.tsx
+++ sites/partners/layouts/forms.tsx
@@ -2,31 +2,15 @@ import Layout from "."
 
 type FormsProps = {
   children: React.ReactNode
-  wide?: boolean
 }
 
-const Forms = (props: FormsProps) => {
-  const innerClasses = []
-
-  if (props.wide) {
-    innerClasses.push("max-w-5xl")
-  } else {
-    innerClasses.push("max-w-lg")
-  }
-
-  return (
-    <Layout>
-      <section className="p-px bg-gray-300">
-        <div
-          className={`md:mb-20 md:mt-12 mx-auto print:my-0 print:max-w-full ${innerClasses.join(
-            " "
-          )}`}
-        >
-          {props.children}
-        </div>
-      </section>
-    </Layout>
-  )
-}
-
+const Forms = (props: FormsProps) => (
+  <Layout>
+    <section className="p-px bg-gray-300">
+      <div className="md:mb-20 md:mt-12 mx-auto print:my-0 print:max-w-full max-w-lg">
+        {props.children}
+      </div>
+    </section>
+  </Layout>
+)
 export default Forms
