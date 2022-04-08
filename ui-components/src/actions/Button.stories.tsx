db1fb6916405474bb4df95b44f4277c49faa7fd3
Emily Jablonski <emily.jablonski@exygy.com>
2022-04-08 14:55:08 -0600

    test: purposeful visual test failure

--- ui-components/src/actions/Button.stories.tsx
+++ ui-components/src/actions/Button.stories.tsx
@@ -53,7 +53,7 @@ export const standard = () => {
 
 export const small = () => (
   <Button size={AppearanceSizeType.small} onClick={handleClick}>
-    Small Button
+    Purposeful Visual Test Failure
   </Button>
 )
 
