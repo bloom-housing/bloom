9c49fb2cd35b4b7c5075a06af265ce3025c26857
Adrien Young <60076088+adriencyberspace@users.noreply.github.com>
2022-04-11 09:00:49 -0500

    fix: about page content edit and margin top and bottom fix (#1133)
    * fix: about page content and margin top and bottom
    
    * fix: add partners list and remove copy from non-english json files
    
    * fix: remove json keys entirely

--- sites/public/pages/about.tsx
+++ sites/public/pages/about.tsx
@@ -10,74 +10,9 @@ export default function About() {
         <p className="my-8">{t("about.body1")}</p>
         <p className="my-8">{t("about.body2")}</p>
         <p>{t("about.moreInfoContact")}</p>
+        <p className="my-8">{t("about.thankYouPartners")}</p>
+        <p className="my-8">{t("about.partnersList")}</p>
       </div>
-      <MarkdownSection fullwidth>
-        <GridSection title={t("about.thankYouPartners")}>
-          <GridCell>
-            <img src={"images/about/cass.png"} alt="Cass Community Social Services logo" />
-          </GridCell>
-          <GridCell>
-            <img src={"images/about/google.org.png"} alt="Google.org logo" />
-          </GridCell>
-          <GridCell>
-            <img src={"images/about/cots.png"} alt="COTS logo" />
-          </GridCell>
-          <GridCell>
-            <img
-              src={"images/about/department-of-neighborhoods.jpg"}
-              alt="Department of Neighborhoods logo"
-            />
-          </GridCell>
-          <GridCell>
-            <img
-              src={"images/about/neighborhood-legal-services.jpg"}
-              alt="Wayne County Neighborhood Legal Services logo"
-            />
-          </GridCell>
-          <GridCell>
-            <img
-              src={"images/about/united-community-housing-coalition.png"}
-              alt="United Community Housing Coalition logo"
-            />
-          </GridCell>
-          <GridCell>
-            <img
-              src={"images/about/central-detroit-christian.png"}
-              alt="Central Detroit Christian Community Development logo"
-            />
-          </GridCell>
-          <GridCell>
-            <img src={"images/about/exygy.png"} alt="Exygy logo" />
-          </GridCell>
-          <GridCell>
-            <img src={"images/about/matrix-human-services.png"} alt="Matrix Human Services logo" />
-          </GridCell>
-          <GridCell>
-            <img
-              src={"images/about/detroit-disability-power.png"}
-              alt="Detroit Disability Power logo"
-            />
-          </GridCell>
-          <GridCell>
-            <img
-              src={"images/about/michigan-housing-council.png"}
-              alt="Michigan Housing Council logo"
-            />
-          </GridCell>
-          <GridCell>
-            <img src={"images/about/u-snap-bac.png"} alt="U-SNAP-BAC logo" />
-          </GridCell>
-          <GridCell>
-            <img
-              src={"images/about/north-corktown-council.png"}
-              alt="North Corktown Neighborhood Council logo"
-            />
-          </GridCell>
-          <GridCell>
-            <img src={"images/about/wayne-metro.jpg"} alt="Wayne Metro Community Corps logo" />
-          </GridCell>
-        </GridSection>
-      </MarkdownSection>
     </Layout>
   )
 }
