05e362101cb4fa4dd7e588bf7efd6c12a586069f
Dominik Barcikowski <dominik@airnauts.com>
2022-04-12 11:58:38 +0200

    feat: Export resend confirmation modal
    Closes #2481

---
+++ ui-components/src/page_components/sign-in/ResendConfirmationModal.tsx
@@ -0,0 +1,110 @@
+import {
+  AppearanceStyleType,
+  Button,
+  Modal,
+  t,
+  Form,
+  Field,
+  emailRegex,
+  NavigationContext,
+} from "@bloom-housing/ui-components"
+import React, { useEffect, useMemo, useContext } from "react"
+import { useForm } from "react-hook-form"
+
+export type ResendConfirmationModalProps = {
+  isOpen: boolean
+  initialEmailValue: string
+  onClose: () => void
+  onSubmit: (email: string) => void
+  loading: boolean
+}
+
+export type ResendConfirmationModalForm = {
+  onSubmit: (email: string) => void
+}
+
+const ResendConfirmationModal = ({
+  isOpen,
+  initialEmailValue,
+  loading,
+  onClose,
+  onSubmit,
+}: ResendConfirmationModalProps) => {
+  const { router } = useContext(NavigationContext)
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  const { register, errors, reset, getValues, trigger } = useForm({
+    defaultValues: useMemo(() => {
+      return {
+        emailResend: initialEmailValue,
+      }
+    }, [initialEmailValue]),
+  })
+
+  useEffect(() => {
+    reset({
+      emailResend: initialEmailValue,
+    })
+  }, [initialEmailValue, reset])
+
+  const onFormSubmit = async () => {
+    const isValid = await trigger()
+    if (!isValid) return
+
+    const { emailResend } = getValues()
+    onSubmit(emailResend)
+  }
+
+  return (
+    <Modal
+      open={isOpen}
+      title={t("authentication.signIn.yourAccountIsNotConfirmed")}
+      ariaDescription={t("authentication.createAccount.linkExpired")}
+      onClose={() => {
+        void router.push("/")
+        onClose()
+        window.scrollTo(0, 0)
+      }}
+      actions={[
+        <Button
+          type="button"
+          styleType={AppearanceStyleType.primary}
+          onClick={() => onFormSubmit()}
+          loading={loading}
+        >
+          {t("authentication.createAccount.resendTheEmail")}
+        </Button>,
+        <Button
+          type="button"
+          styleType={AppearanceStyleType.alert}
+          onClick={() => {
+            void router.push("/")
+            onClose()
+            window.scrollTo(0, 0)
+          }}
+        >
+          {t("t.cancel")}
+        </Button>,
+      ]}
+    >
+      <>
+        <Form>
+          <Field
+            caps={true}
+            type="email"
+            name="emailResend"
+            label={t("authentication.createAccount.resendAnEmailTo")}
+            placeholder="example@web.com"
+            validation={{ required: true, pattern: emailRegex }}
+            error={!!errors.emailResend}
+            errorMessage={t("authentication.signIn.loginError")}
+            register={register}
+          />
+        </Form>
+
+        <p className="pt-4">{t("authentication.createAccount.resendEmailInfo")}</p>
+      </>
+    </Modal>
+  )
+}
+
+export { ResendConfirmationModal as default, ResendConfirmationModal }
