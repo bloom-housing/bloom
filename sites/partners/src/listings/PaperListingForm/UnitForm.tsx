aa101ff9d7367cd9c37a64bf5ff7637c779ffd0c
Adrien Young <adrien.elizabeth@gmail.com>
2022-04-21 09:20:23 -0500

    feat: trigger min and max occupancy validation on change for both fields

--- sites/partners/src/listings/PaperListingForm/UnitForm.tsx
+++ sites/partners/src/listings/PaperListingForm/UnitForm.tsx
@@ -88,7 +88,7 @@ const UnitForm = ({ onSubmit, onClose, defaultUnit, nextId, draft }: UnitFormPro
   const maxOccupancy: number = useWatch({
     control,
     name: "maxOccupancy",
-  })
+  }) || numberOccupancyOptions
 
   const maxAmiHouseholdSize = 8
 
@@ -455,10 +455,11 @@ const UnitForm = ({ onSubmit, onClose, defaultUnit, nextId, draft }: UnitFormPro
                 options={numberOptions(numberOccupancyOptions)}
                 error={fieldHasError(errors?.minOccupancy)}
                 errorMessage={t("errors.minGreaterThanMaxOccupancyError")}
-                validation={{ max: maxOccupancy || numberOccupancyOptions }}
+                validation={{ max: maxOccupancy }}
                 inputProps={{
                   onChange: () => {
                     trigger("minOccupancy")
+                    trigger("maxOccupancy")
                   },
                 }}
               />
@@ -480,6 +481,7 @@ const UnitForm = ({ onSubmit, onClose, defaultUnit, nextId, draft }: UnitFormPro
                 validation={{ min: minOccupancy }}
                 inputProps={{
                   onChange: () => {
+                    trigger("minOccupancy")
                     trigger("maxOccupancy")
                   },
                 }}
