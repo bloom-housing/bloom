88e30c9b7ec791f98ae2c6714bacf1dc36f75023
Emily Jablonski <emily.jablonski@exygy.com>
2022-04-25 10:50:16 -0600

    fix: pr review

--- sites/public/src/ListingView.tsx
+++ sites/public/src/ListingView.tsx
@@ -392,7 +392,7 @@ export const ListingView = (props: ListingProps) => {
               : undefined
           }
         />
-        <div className="py-3 mx-3 flex flex-col items-center md:items-start">
+        <div className="py-3 mx-3 flex flex-col items-center md:items-start text-center md:text-left">
           <Heading priority={1} style={"cardHeader"} className={"text-black"}>
             {listing.name}
           </Heading>
