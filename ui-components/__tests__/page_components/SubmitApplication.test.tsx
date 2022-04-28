37684d7d766cd5c757d3de9c04d44dae5c2bfacf
Adrien Young <adrien.elizabeth@gmail.com>
2022-04-28 09:57:49 -0500

    style: one more jest test

--- ui-components/__tests__/page_components/SubmitApplication.test.tsx
+++ ui-components/__tests__/page_components/SubmitApplication.test.tsx
@@ -78,7 +78,7 @@ describe("<ApplicationAddresses>", () => {
     const { getByText, queryByText } = render(<MailingNoPostmarksYesDueDate />)
     expect(getByText("Submit a Paper Application")).toBeTruthy()
     expect(getByText("Send Application by US Mail")).toBeTruthy()
-    expect(getByText("Mailing Address Street")).toBeTruthy()
+    expect(getByText("Mailing Address Street", { exact: false })).toBeTruthy()
     expect(
       getByText(
         "Applications must be received by the deadline. If sending by U.S. Mail, the application must be postmarked by November 29th, 2021. Developer is not responsible for lost or delayed mail.",
