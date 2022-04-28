6ad1005b88716398fa0ad7c168f33e8d5201ae0d
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2022-04-28 09:53:15 -0600

    refactor: remove business logic, strings from application fee component (#2691)

--- shared-helpers/src/stringFormatting.ts
+++ shared-helpers/src/stringFormatting.ts
@@ -5,3 +5,10 @@ export const getTimeRangeString = (start: Date, end: Date) => {
   const endTime = dayjs(end).format("hh:mma")
   return startTime === endTime ? startTime : `${startTime} - ${endTime}`
 }
+
+export const getCurrencyRange = (min: number | null, max: number | null) => {
+  if (min && max && min !== max) {
+    return `$${min} – $${max}`
+  }
+  return min || max ? `$${min ?? max}` : ""
+}
