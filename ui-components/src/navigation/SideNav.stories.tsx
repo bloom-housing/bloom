4ae5e5c159c7150a808df85b2aa47e443256eb5f
Mark Buckner <markbuckner@Marks-MacBook-Air.local>
2022-06-03 14:20:42 -0600

    feat(sidenav component): update Prototype SideNav Component for Production Usage
    Modify prototype SideNav component to use a nav item list array. Add unit tests for SideNav
    Component. Update Styling of SideNav component to utilize updated designs. Migrate Sidenav component
    files from /prototype to /navigation.
    
    Issue #698

---
+++ ui-components/src/navigation/SideNav.stories.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { SideNav } from "./SideNav"
+
+export default {
+  title: "Navigation/SideNav",
+  decorators: [(storyFn: any) => <div style={{ padding: "1rem" }}>{storyFn()}</div>],
+}
+
+export const Default = () => (
+  <SideNav
+    navItems={[
+      { label: "Nav Item 1", url: "#", current: false },
+      { label: "Nav Item 2", url: "#", current: false },
+      { label: "Nav Item 3", url: "#" },
+    ]}
+  />
+)
+
+export const Current = () => (
+  <SideNav
+    navItems={[
+      { label: "Nav Item 1", url: "#", current: false },
+      { label: "Nav Item 2", url: "#", current: true },
+      { label: "Nav Item 3", url: "#", current: false },
+    ]}
+  />
+)
---
+++ ui-components/src/navigation/SideNav.tsx
@@ -0,0 +1,39 @@
+import * as React from "react"
+import { NavigationContext } from "../config/NavigationContext"
+import "./SideNav.scss"
+
+export interface SideNavItemProps {
+  current?: boolean
+  url: string
+  label: string
+}
+
+export interface SideNavProps {
+  navItems?: SideNavItemProps[]
+}
+
+const SideNav = (props: SideNavProps) => {
+  const { LinkComponent } = React.useContext(NavigationContext)
+
+  return (
+    <nav className="side-nav" aria-label="Secondary navigation">
+      <ul>
+        {props.navItems?.map((navItem: SideNavItemProps, index: number) => {
+          if (navItem.current) {
+            return (
+              <li className="is-current" key={index}>
+                {navItem.label}
+              </li>
+            )
+          }
+          return (
+            <li key={index}>
+              <LinkComponent href={navItem.url}>{navItem.label}</LinkComponent>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+  )
+}
+export { SideNav as default, SideNav }
