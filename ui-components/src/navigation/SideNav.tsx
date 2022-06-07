bed1b23d63af134542f6760aa70d1ccac87940fa
Mark Buckner <markbuckner@Marks-MacBook-Air.local>
2022-06-07 11:51:25 -0600

    fix: changed aria-selected to aria-current issue #698

--- ui-components/src/navigation/SideNav.tsx
+++ ui-components/src/navigation/SideNav.tsx
@@ -21,7 +21,7 @@ const SideNav = (props: SideNavProps) => {
         {props.navItems?.map((navItem: SideNavItemProps, index: number) => {
           if (navItem.current) {
             return (
-              <li className="is-current" key={index} aria-selected>
+              <li className="is-current" key={index} aria-current="page">
                 {navItem.label}
               </li>
             )
