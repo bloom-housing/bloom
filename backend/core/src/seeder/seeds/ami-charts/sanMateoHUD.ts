865442159ba8444a4a35f339ce7d4fcff5c6b940
Yazeed Loonat <yazeedloonat@gmail.com>
2022-06-14 11:39:57 -0700

    feat: creating san mateo ami charts

---
+++ backend/core/src/seeder/seeds/ami-charts/sanMateoHUD.ts
@@ -0,0 +1,209 @@
+import { AmiChartCreateDto } from "../../../ami-charts/dto/ami-chart.dto"
+import { BaseEntity } from "typeorm"
+
+// THIS FILE WAS AUTOMATICALLY GENERATED FROM a.txt.
+export const sanMateoHUD: Omit<AmiChartCreateDto, keyof BaseEntity | "jurisdiction"> = {
+  name: "San Mateo County HUD 2022",
+  items: [
+    {
+      percentOfAmi: 30,
+      householdSize: 1,
+      income: 39150,
+    },
+    {
+      percentOfAmi: 30,
+      householdSize: 2,
+      income: 44750,
+    },
+    {
+      percentOfAmi: 30,
+      householdSize: 3,
+      income: 50350,
+    },
+    {
+      percentOfAmi: 30,
+      householdSize: 4,
+      income: 55900,
+    },
+    {
+      percentOfAmi: 30,
+      householdSize: 5,
+      income: 60400,
+    },
+    {
+      percentOfAmi: 30,
+      householdSize: 6,
+      income: 64850,
+    },
+    {
+      percentOfAmi: 30,
+      householdSize: 7,
+      income: 69350,
+    },
+    {
+      percentOfAmi: 30,
+      householdSize: 8,
+      income: 73800,
+    },
+    {
+      percentOfAmi: 50,
+      householdSize: 1,
+      income: 65250,
+    },
+    {
+      percentOfAmi: 50,
+      householdSize: 2,
+      income: 74600,
+    },
+    {
+      percentOfAmi: 50,
+      householdSize: 3,
+      income: 83900,
+    },
+    {
+      percentOfAmi: 50,
+      householdSize: 4,
+      income: 93200,
+    },
+    {
+      percentOfAmi: 50,
+      householdSize: 5,
+      income: 100700,
+    },
+    {
+      percentOfAmi: 50,
+      householdSize: 6,
+      income: 108150,
+    },
+    {
+      percentOfAmi: 50,
+      householdSize: 7,
+      income: 115600,
+    },
+    {
+      percentOfAmi: 50,
+      householdSize: 8,
+      income: 123050,
+    },
+    {
+      percentOfAmi: 80,
+      householdSize: 1,
+      income: 104400,
+    },
+    {
+      percentOfAmi: 80,
+      householdSize: 2,
+      income: 119300,
+    },
+    {
+      percentOfAmi: 80,
+      householdSize: 3,
+      income: 134200,
+    },
+    {
+      percentOfAmi: 80,
+      householdSize: 4,
+      income: 149100,
+    },
+    {
+      percentOfAmi: 80,
+      householdSize: 5,
+      income: 161050,
+    },
+    {
+      percentOfAmi: 80,
+      householdSize: 6,
+      income: 173000,
+    },
+    {
+      percentOfAmi: 80,
+      householdSize: 7,
+      income: 184900,
+    },
+    {
+      percentOfAmi: 80,
+      householdSize: 8,
+      income: 196850,
+    },
+    {
+      percentOfAmi: 100,
+      householdSize: 1,
+      income: 116200,
+    },
+    {
+      percentOfAmi: 100,
+      householdSize: 2,
+      income: 132800,
+    },
+    {
+      percentOfAmi: 100,
+      householdSize: 3,
+      income: 149400,
+    },
+    {
+      percentOfAmi: 100,
+      householdSize: 4,
+      income: 166000,
+    },
+    {
+      percentOfAmi: 100,
+      householdSize: 5,
+      income: 179300,
+    },
+    {
+      percentOfAmi: 100,
+      householdSize: 6,
+      income: 192550,
+    },
+    {
+      percentOfAmi: 100,
+      householdSize: 7,
+      income: 205850,
+    },
+    {
+      percentOfAmi: 100,
+      householdSize: 8,
+      income: 219100,
+    },
+    {
+      percentOfAmi: 120,
+      householdSize: 1,
+      income: 139450,
+    },
+    {
+      percentOfAmi: 120,
+      householdSize: 2,
+      income: 159350,
+    },
+    {
+      percentOfAmi: 120,
+      householdSize: 3,
+      income: 179300,
+    },
+    {
+      percentOfAmi: 120,
+      householdSize: 4,
+      income: 199200,
+    },
+    {
+      percentOfAmi: 120,
+      householdSize: 5,
+      income: 215150,
+    },
+    {
+      percentOfAmi: 120,
+      householdSize: 6,
+      income: 231050,
+    },
+    {
+      percentOfAmi: 120,
+      householdSize: 7,
+      income: 247000,
+    },
+    {
+      percentOfAmi: 120,
+      householdSize: 8,
+      income: 262950,
+    },
+  ],
+}
