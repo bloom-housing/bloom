fbd0783bd0e3bdc0f0dfa917caa5e241747b7ab7
Emily Jablonski <emily.jablonski@exygy.com>
2022-06-17 18:41:44 -0600

    feat: fetch jurisdictional preferences on settings page

--- sites/partners/pages/settings/index.tsx
+++ sites/partners/pages/settings/index.tsx
@@ -1,9 +1,90 @@
-import React from "react"
+import React, { useContext } from "react"
 import Head from "next/head"
-import { NavigationHeader, t, SiteAlert } from "@bloom-housing/ui-components"
+import {
+  AppearanceSizeType,
+  Button,
+  Icon,
+  IconFillColors,
+  LoadingOverlay,
+  MinimalTable,
+  NavigationHeader
+  PageHeader,
+  SiteAlert,
+  StandardCard,
+  t,
+} from "@bloom-housing/ui-components"
+import dayjs from "dayjs"
+import { AuthContext } from "@bloom-housing/shared-helpers"
+import { faClone, faPenToSquare, faTrashCan } from "@fortawesome/free-regular-svg-icons"
 import Layout from "../../layouts"
+import { useJurisdictionalPreferenceList } from "../../lib/hooks"
 
 const Settings = () => {
+  const { profile } = useContext(AuthContext)
+
+  const { data, loading } = useJurisdictionalPreferenceList(
+    profile?.jurisdictions?.reduce((acc, curr) => {
+      return `${acc}${","}${curr.id}`
+    }, "")
+  )
+
+  const iconContent = () => {
+    return (
+      <div className={"flex justify-end"}>
+        <div className={"w-max"}>
+          <span onClick={() => alert("edit")} className={"cursor-pointer"}>
+            <Icon
+              symbol={faPenToSquare}
+              size={"medium"}
+              fill={IconFillColors.primary}
+              className={"mr-5"}
+            />
+          </span>
+          <span onClick={() => alert("copy")} className={"cursor-pointer"}>
+            <Icon
+              symbol={faClone}
+              size={"medium"}
+              fill={IconFillColors.primary}
+              className={"mr-5"}
+            />
+          </span>
+          <span onClick={() => alert("trash")} className={"cursor-pointer"}>
+            <Icon symbol={faTrashCan} size={"medium"} fill={IconFillColors.alert} />
+          </span>
+        </div>
+      </div>
+    )
+  }
+
+  const getCardContent = () => {
+    if (!loading && data?.length === 0) return null
+    return (
+      <>
+        {data?.length ? (
+          <MinimalTable
+            headers={{
+              name: { name: "t.name" },
+              updated: { name: "t.updated" },
+              icons: { name: "" },
+            }}
+            cellClassName={"px-5 py-3"}
+            data={data?.map((preference) => {
+              return {
+                name: { content: preference?.title },
+                updated: {
+                  content: dayjs(preference?.updatedAt).format("MM/DD/YYYY"),
+                },
+                icons: { content: iconContent() },
+              }
+            })}
+          />
+        ) : (
+          <div className={"ml-5 mb-5"}>{t("t.none")}</div>
+        )}
+      </>
+    )
+  }
+
   return (
     <Layout>
       <Head>
@@ -18,7 +99,17 @@ const Settings = () => {
       </NavigationHeader>
 
       <section>
-        <article className="flex-row flex-wrap relative max-w-screen-xl mx-auto py-8 px-4"></article>
+        <article className="flex-row flex-wrap relative max-w-screen-xl mx-auto py-8 px-4">
+          <LoadingOverlay isLoading={loading}>
+            <StandardCard
+              title={t("t.preferences")}
+              emptyStateMessage={t("t.none")}
+              footer={<Button size={AppearanceSizeType.small}>{t("t.addItem")}</Button>}
+            >
+              {getCardContent()}
+            </StandardCard>
+          </LoadingOverlay>
+        </article>
       </section>
     </Layout>
   )
