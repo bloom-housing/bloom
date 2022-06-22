65214dfa406a51a14d0227ce9a39467761fbedf1
Sean Albert <smabert@gmail.com>
2022-06-21 19:13:31 -0700

    test: listing tests

--- backend/core/test/listings/listings.e2e-spec.ts
+++ backend/core/test/listings/listings.e2e-spec.ts
@@ -83,13 +83,23 @@ describe("Listings", () => {
   it("should return the last page of paginated listings", async () => {
     // Make the limit 1 less than the full number of listings, so that the second page contains
     // only one listing.
-    const queryParams = {
-      limit: 15,
+    // query to get max number of listings
+    let queryParams = {
+      limit: 1,
+      page: 1,
+      view: "base",
+    }
+    let query = qs.stringify(queryParams)
+    let res = await supertest(app.getHttpServer()).get(`/listings?${query}`).expect(200)
+    const totalItems = res.body.meta.totalItems
+
+    queryParams = {
+      limit: totalItems - 1,
       page: 2,
       view: "base",
     }
-    const query = qs.stringify(queryParams)
-    const res = await supertest(app.getHttpServer()).get(`/listings?${query}`).expect(200)
+    query = qs.stringify(queryParams)
+    res = await supertest(app.getHttpServer()).get(`/listings?${query}`).expect(200)
     expect(res.body.items.length).toEqual(1)
   })
 
@@ -412,6 +422,8 @@ describe("Listings", () => {
     let listingsSearchResponse = await supertest(app.getHttpServer())
       .get(`/listings?search=random`)
       .expect(200)
+
+    console.log({ listingsSearchResponse })
 
     expect(listingsSearchResponse.body.items.length).toBe(0)
 
