2a8f1cbd28c4301f6438c1663c881258173ffcd6
ColinBuyck <colin.buyck@gmail.com>
2022-06-27 18:02:28 -0700

    fix: added ability to update jurisdiction seed files

--- backend/core/src/seeder/seeds/jurisdictions.ts
+++ backend/core/src/seeder/seeds/jurisdictions.ts
@@ -1,9 +1,10 @@
 import { INestApplicationContext } from "@nestjs/common"
 import { JurisdictionCreateDto } from "../../jurisdictions/dto/jurisdiction-create.dto"
+import { JurisdictionUpdateDto } from "../../jurisdictions/dto/jurisdiction-update.dto"
 import { Language } from "../../shared/types/language-enum"
 import { JurisdictionsService } from "../../jurisdictions/services/jurisdictions.service"
 
-export const defaultJurisdictions: JurisdictionCreateDto[] = [
+export const defaultJurisdictions: (JurisdictionCreateDto & JurisdictionUpdateDto)[] = [
   {
     name: "Alameda",
     preferences: [],
@@ -27,7 +28,7 @@ export const defaultJurisdictions: JurisdictionCreateDto[] = [
     rentalAssistanceDefault:
       "Housing Choice Vouchers, Section 8 and other valid rental assistance programs will be considered for this property. In the case of a valid rental subsidy, the required minimum income will be based on the portion of the rent that the tenant pays after use of the subsidy.",
     enablePartnerSettings: null,
-    enableAccessibilityFeatures: null,
+    enableAccessibilityFeatures: false,
     enableUtilitiesIncluded: true,
   },
   {
@@ -61,14 +62,44 @@ export const defaultJurisdictions: JurisdictionCreateDto[] = [
 export async function createJurisdictions(app: INestApplicationContext) {
   const jurisdictionService = await app.resolve<JurisdictionsService>(JurisdictionsService)
   // some jurisdictions are added via previous migrations
-  const jurisdictions = await jurisdictionService.list()
-  console.log({ jurisdictions })
-  const toInsert = defaultJurisdictions.filter(
-    (rec) => jurisdictions.findIndex((item) => item.name === rec.name) === -1
+  const initialJurisdictions = await jurisdictionService.list()
+  // console.log("JKNFJNDKNJNDJDN")
+  const toUpdate = []
+  const toInsert = []
+
+  defaultJurisdictions.forEach((defaultJuris) => {
+    const location = initialJurisdictions.findIndex((item) => item.name === defaultJuris.name)
+    if (location === -1) {
+      toInsert.push(defaultJuris)
+    } else {
+      const jurisdictionKeys = Object.keys(defaultJuris)
+      let keyIdx = 0
+      let updateNeeded = false
+      while (!updateNeeded && keyIdx < jurisdictionKeys.length) {
+        const currentKey = jurisdictionKeys[keyIdx]
+        if (defaultJuris[currentKey] !== initialJurisdictions[location][currentKey]) {
+          toUpdate.push(initialJurisdictions[location])
+          updateNeeded = true
+        } else {
+          keyIdx++
+        }
+      }
+    }
+  })
+
+  await Promise.all(
+    toUpdate.map(async (jurisdiction) => {
+      const jurisdictionUpdated = { ...jurisdiction, enableUtilitiesIncluded: true }
+      return await jurisdictionService.update(jurisdictionUpdated)
+    })
   )
+  const updatedJurisdictions = await jurisdictionService.list()
+
   const inserted = await Promise.all(
-    toInsert.map(async (jurisdiction) => await jurisdictionService.create(jurisdiction))
+    toInsert.map(async (jurisdiction) => {
+      return await jurisdictionService.create(jurisdiction)
+    })
   )
   // names are unique
-  return jurisdictions.concat(inserted).sort((a, b) => (a.name < b.name ? -1 : 1))
+  return updatedJurisdictions.concat(inserted).sort((a, b) => (a.name < b.name ? -1 : 1))
 }
