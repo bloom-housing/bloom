496581c45e83d2280bf56aa58617bb8c65d01730
Michal Plebanski <michalp@airnauts.com>
2022-07-01 12:24:12 +0200

    feat(backend): refactor listings query building

--- backend/core/src/listings/db/listing.repository.ts
+++ backend/core/src/listings/db/listing.repository.ts
@@ -1,19 +1,25 @@
 import Listing from "../entities/listing.entity"
-import { EntityRepository, Repository } from "typeorm"
+import { EntityRepository } from "typeorm"
+import { ListingsQueryBuilder } from "./listing-query-builder"
+import { GenericRepository } from "../../shared/db/generic.repository"
 
 @EntityRepository(Listing)
-export class ListingRepository extends Repository<Listing> {
+export class ListingRepository extends GenericRepository<Listing> {
   public async getJurisdictionIdByListingId(listingId: string | null): Promise<string | null> {
     if (!listingId) {
       return null
     }
 
     const listing = await this.createQueryBuilder("listings")
-      .where("listings.id = :listingId", { listingId })
+      .where(`listings.id = :listingId`, { listingId })
       .leftJoin("listings.jurisdiction", "jurisdiction")
       .select(["listings.id", "jurisdiction.id"])
       .getOne()
 
     return listing.jurisdiction.id
+  }
+
+  public createQueryBuilder(alias: string): ListingsQueryBuilder {
+    return new ListingsQueryBuilder(super.createQueryBuilder(alias))
   }
 }
