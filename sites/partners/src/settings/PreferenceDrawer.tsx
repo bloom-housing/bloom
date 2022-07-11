70cee95bec2d48db3fb0821ee59744f15182dc28
Emily Jablonski <emily.jablonski@exygy.com>
2022-07-11 14:54:43 -0600

    fix: formatting for the option drawer

--- sites/partners/src/settings/PreferenceDrawer.tsx
+++ sites/partners/src/settings/PreferenceDrawer.tsx
@@ -1,21 +1,45 @@
-import React, { useState } from "react"
+import React, { useState, useContext } from "react"
 import {
-  t,
-  Button,
   AppearanceSizeType,
-  Drawer,
   AppearanceStyleType,
-  GridSection,
-  GridCell,
-  ViewItem,
+  Button,
+  Drawer,
   Field,
-  Textarea,
   FieldGroup,
+  GridCell,
+  GridSection,
+  Select,
+  SelectOption,
+  Textarea,
+  ViewItem,
+  t,
 } from "@bloom-housing/ui-components"
-import { useForm, useFormContext } from "react-hook-form"
+import { AuthContext } from "@bloom-housing/shared-helpers"
+import { useForm } from "react-hook-form"
 import { YesNoAnswer } from "../applications/PaperApplicationForm/FormTypes"
+import { Jurisdiction } from "@bloom-housing/backend-core"
+
+type PreferenceOption = {
+  title: string
+  description: string
+  URL?: string
+  linkTitle?: string
+  collectAddress?: boolean
+  exclusive: boolean
+}
+
+type PreferenceForm = {
+  title: string
+  description: string
+  options: PreferenceOption[]
+  optOut: boolean
+  showOnListing: boolean
+  optOutLabel?: string
+  jurisdiction: Jurisdiction
+}
 
 type PreferenceDrawerProps = {
+  defaultValues: PreferenceForm
   drawerOpen: boolean
   setDrawerOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
@@ -23,18 +47,27 @@ type PreferenceDrawerProps = {
 const PreferenceDrawer = ({ drawerOpen, setDrawerOpen }: PreferenceDrawerProps) => {
   const [optionDrawerOpen, setOptionDrawerOpen] = useState<boolean | null>(null)
 
-  const formMethods = useFormContext()
-  // eslint-disable-next-line @typescript-eslint/unbound-method
-  const { register, getValues, watch, setValue } = useForm()
+  const { profile } = useContext(AuthContext)
 
-  const optOutOptions = [
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  const { register } = useForm()
+
+  const jurisdictionOptions: SelectOption[] = [
+    { label: "", value: "" },
+    ...profile.jurisdictions.map((jurisdiction) => ({
+      label: jurisdiction.name,
+      value: jurisdiction.id,
+    })),
+  ]
+
+  const yesNoOptions = [
     {
-      id: "sameAddressYes",
+      id: YesNoAnswer.Yes,
       label: t("t.yes"),
       value: YesNoAnswer.Yes,
     },
     {
-      id: "sameAddressNo",
+      id: YesNoAnswer.No,
       label: t("t.no"),
       value: YesNoAnswer.No,
     },
@@ -44,8 +77,8 @@ const PreferenceDrawer = ({ drawerOpen, setDrawerOpen }: PreferenceDrawerProps) 
     <>
       <Drawer
         open={!!drawerOpen}
-        title={"Add / Edit"}
-        ariaDescription={"Add / Edit"}
+        title={t("settings.preferenceEdit")}
+        ariaDescription={t("settings.preferenceEdit")}
         onClose={() => {
           if (!optionDrawerOpen) {
             setDrawerOpen(null)
@@ -53,52 +86,102 @@ const PreferenceDrawer = ({ drawerOpen, setDrawerOpen }: PreferenceDrawerProps) 
         }}
       >
         <div className="border rounded-md p-8 bg-white">
-          <GridSection title={"Preference"} columns={4}>
+          <GridSection title={t("settings.preference")} columns={3}>
             <GridCell span={2}>
-              <ViewItem label={"Title"}>
+              <ViewItem label={t("t.title")}>
                 <Field
                   id="title"
                   name="title"
-                  label={"Title"}
-                  placeholder={"Title"}
+                  label={t("t.title")}
+                  placeholder={t("t.title")}
                   register={register}
                   type="text"
                   readerOnly
+                  dataTestId={"preference-title"}
                 />
               </ViewItem>
             </GridCell>
           </GridSection>
-          <GridSection columns={4}>
+          <GridSection columns={3} className={"mb-4"}>
             <GridCell span={2}>
               <Textarea
-                label={"Description"}
+                label={t("t.descriptionTitle")}
                 name={"description"}
                 id={"description"}
                 fullWidth={true}
+                placeholder={t("settings.preferenceDescription")}
                 register={register}
+                dataTestId={"preference-description"}
               />
             </GridCell>
           </GridSection>
           <Button
             type="button"
-            size={AppearanceSizeType.normal}
+            size={AppearanceSizeType.small}
             onClick={() => {
               setOptionDrawerOpen(true)
             }}
+            dataTestId={"preference-add-option-button"}
           >
-            {"Add Preference Option"}
+            {t("settings.preferenceAddOption")}
           </Button>
-          <GridSection columns={2}>
+          <GridSection columns={3} className={"mt-4"}>
             <GridCell>
-              <ViewItem label={"Can applicants opt out?"} className="mb-1" />
+              <ViewItem label={t("settings.preferenceOptOut")} className="mb-1" />
               <FieldGroup
                 name="optOutQuestion"
                 type="radio"
                 register={register}
-                fields={optOutOptions}
+                fields={yesNoOptions}
                 fieldClassName="m-0"
                 fieldGroupClassName="flex h-12 items-center"
+                dataTestId={"preference-opt-out"}
               />
+            </GridCell>
+            <GridCell>
+              <ViewItem label={t("settings.preferenceOptOutLabel")}>
+                <Field
+                  id="title"
+                  name="title"
+                  label={t("settings.preferenceOptOutLabel")}
+                  placeholder={t("settings.preferenceOptOutLabel")}
+                  register={register}
+                  type="text"
+                  readerOnly
+                  dataTestId={"preference-opt-out-label"}
+                />
+              </ViewItem>
+            </GridCell>
+          </GridSection>
+          <GridSection columns={3} className={"mt-4"}>
+            <GridCell>
+              <ViewItem label={t("settings.preferenceShowOnListing")} className="mb-1" />
+              <FieldGroup
+                name="showOnListingQuestion"
+                type="radio"
+                register={register}
+                fields={yesNoOptions}
+                fieldClassName="m-0"
+                fieldGroupClassName="flex h-12 items-center"
+                dataTestId={"preference-show-on-listing"}
+              />
+            </GridCell>
+          </GridSection>
+          <GridSection columns={3}>
+            <GridCell span={1}>
+              <ViewItem label={t("t.jurisdiction")}>
+                <Select
+                  id={"jurisdiction.id"}
+                  name={"jurisdiction.id"}
+                  label={t("t.jurisdiction")}
+                  labelClassName="sr-only"
+                  register={register}
+                  controlClassName={"control"}
+                  keyPrefix={"jurisdictions"}
+                  options={jurisdictionOptions}
+                  dataTestId={"preference-jurisdiction"}
+                />
+              </ViewItem>
             </GridCell>
           </GridSection>
         </div>
@@ -110,6 +193,7 @@ const PreferenceDrawer = ({ drawerOpen, setDrawerOpen }: PreferenceDrawerProps) 
           onClick={() => {
             setDrawerOpen(null)
           }}
+          dataTestId={"preference-save-button"}
         >
           {t("t.save")}
         </Button>
@@ -117,16 +201,113 @@ const PreferenceDrawer = ({ drawerOpen, setDrawerOpen }: PreferenceDrawerProps) 
 
       <Drawer
         open={!!optionDrawerOpen}
-        title={"Add / Edit Option"}
-        ariaDescription={"Add / Edit Option"}
+        title={t("settings.preferenceOptionEdit")}
+        ariaDescription={t("settings.preferenceOptionEdit")}
         onClose={() => {
           setOptionDrawerOpen(null)
         }}
         className={"w-auto"}
       >
-        <div className="border rounded-md p-8 bg-white">Drawer 2</div>
+        <div className="border rounded-md p-8 bg-white">
+          <GridSection title={t("t.option")} columns={3}>
+            <GridCell span={2}>
+              <ViewItem label={t("t.title")}>
+                <Field
+                  id="optionTitle"
+                  name="optionTitle"
+                  label={t("t.title")}
+                  placeholder={t("t.title")}
+                  register={register}
+                  type="text"
+                  readerOnly
+                  dataTestId={"preference-option-title"}
+                />
+              </ViewItem>
+            </GridCell>
+          </GridSection>
+          <GridSection columns={3} className={"mb-4"}>
+            <GridCell span={2}>
+              <Textarea
+                label={t("t.descriptionTitle")}
+                name={"optionDescription"}
+                id={"optionDescription"}
+                placeholder={t("settings.preferenceOptionDescription")}
+                fullWidth={true}
+                register={register}
+                dataTestId={"preference-option-description"}
+              />
+            </GridCell>
+          </GridSection>
+          <GridSection columns={3} className={"mt-4"}>
+            <GridCell>
+              <ViewItem label={t("t.url")}>
+                <Field
+                  id="optionURL"
+                  name="optionURL"
+                  label={t("t.url")}
+                  placeholder={t("t.url")}
+                  register={register}
+                  type="text"
+                  readerOnly
+                  dataTestId={"preference-option-URL"}
+                />
+              </ViewItem>
+            </GridCell>
+            <GridCell>
+              <ViewItem label={t("settings.preferenceLinkTitle")}>
+                <Field
+                  id="optionLinkTitle"
+                  name="optionLinkTitle"
+                  label={t("settings.preferenceLinkTitle")}
+                  placeholder={t("settings.preferenceLinkTitle")}
+                  register={register}
+                  type="text"
+                  readerOnly
+                  dataTestId={"preference-option-link-title"}
+                />
+              </ViewItem>
+            </GridCell>
+          </GridSection>
+          <GridSection columns={3} className={"mt-8"}>
+            <GridCell>
+              <Field
+                type="checkbox"
+                id="collectAddress"
+                name="collectAddress"
+                label={t("settings.preferenceCollectAddress")}
+                register={register}
+                dataTestId={"preference-option-collect-address"}
+                controlClassName={"font-normal"}
+              />
+            </GridCell>
+          </GridSection>
+          <GridSection columns={3}>
+            <GridCell span={2}>
+              <p className="field-label m-4 ml-0">{t("settings.preferenceExclusiveQuestion")}</p>
+              <FieldGroup
+                name="multiSelectQuestion"
+                type="radio"
+                register={register}
+                fields={[
+                  {
+                    label: t("settings.preferenceMultiSelect"),
+                    value: "multiSelect",
+                    id: "multiSelect",
+                    defaultChecked: false,
+                  },
+                  {
+                    label: t("settings.preferenceExclusive"),
+                    value: "exclusive",
+                    id: "exclusive",
+                    defaultChecked: false,
+                  },
+                ]}
+                dataTestId={"preference-option-multiselect"}
+              />
+            </GridCell>
+          </GridSection>
+        </div>
         <Button
-          id="addPreferenceSaveButton"
           type="button"
           className={"mt-4"}
           styleType={AppearanceStyleType.primary}
@@ -134,6 +315,7 @@ const PreferenceDrawer = ({ drawerOpen, setDrawerOpen }: PreferenceDrawerProps) 
           onClick={() => {
             setOptionDrawerOpen(null)
           }}
+          dataTestId={"preference-option-save"}
         >
           {t("t.save")}
         </Button>
