3efb32cc1818e772cc45a3d15437ef85024e77d0
ColinBuyck <colin.buyck@gmail.com>
2022-07-14 11:46:34 -0700

    fix: media modal functionality and styling

--- ui-components/src/blocks/MediaCard.tsx
+++ ui-components/src/blocks/MediaCard.tsx
@@ -1,8 +1,9 @@
 import * as React from "react"
+import { useState } from "react"
 import "./MediaCard.scss"
 import { Icon } from "../icons/Icon"
 import { faCirclePlay } from "@fortawesome/free-regular-svg-icons"
-import { useState } from "@storybook/addons"
+import { Modal } from "../.."
 
 export interface MediaCardProps {
   title?: string
@@ -19,7 +20,7 @@ export interface MediaCardProps {
  * A component that renders an image with optional tags at top and status bars below it
  */
 const MediaCard = (props: MediaCardProps) => {
-  const [openMediaModal, setOpenMediaModal] = useState<boolean>(false)
+  const [openMediaModal, setOpenMediaModal] = useState(false)
   const wrapperClasses = ["media-card"]
   if (props.className) {
     wrapperClasses.push(props.className)
@@ -27,24 +28,38 @@ const MediaCard = (props: MediaCardProps) => {
   return (
     <div className={wrapperClasses.join(" ")}>
       <div className="media-block">
-        <div onClick={() => console.log("testing")}>
-          <Icon symbol={faCirclePlay} size="2xl" className="flex justify-center" fill="white" />
+        <div className="flex justify-center">
+          <span onClick={() => setOpenMediaModal(true)} className={"cursor-pointer"}>
+            <Icon symbol={faCirclePlay} size="2xl" fill="white" />
+          </span>
         </div>
       </div>
+
       <div className="media-description">
-        <h3 className="media-card__title">{props.title}</h3>
+        <a className="w-min" onClick={() => setOpenMediaModal(true)}>
+          <h3 className="media-card__title">{props.title}</h3>
+        </a>
         <div className={"media-card__subtitle"}>{props.subtitle}</div>
       </div>
+      {openMediaModal && (
+        <Modal
+          open={openMediaModal}
+          title={props.title ?? ""}
+          onClose={() => setOpenMediaModal(!openMediaModal)}
+          modalClassNames="media-modal"
+          innerClassNames="media-modal-inner"
+        >
+          <iframe
+            src={props.videoURL}
+            frameBorder="0"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title={props.title}
+          />
+        </Modal>
+      )}
     </div>
   )
 }
-
-// ;<iframe
-//   src={props.videoURL}
-//   frameBorder="0"
-//   allow="accelerometer;  clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-//   allowFullScreen
-//   title={props.title}
-// />
 
 export { MediaCard as default, MediaCard }
