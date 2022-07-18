27b77a805979bcb6f7288d013578be242931abeb
ColinBuyck <colin.buyck@gmail.com>
2022-07-18 14:57:43 -0700

    fix: refined Video component props

--- ui-components/src/actions/Video.stories.tsx
+++ ui-components/src/actions/Video.stories.tsx
@@ -7,17 +7,13 @@ export default {
   decorators: [(storyFn: any) => <div style={{ maxWidth: "700px" }}>{storyFn()}</div>],
 }
 export const videoWithoutCC = () => (
-  <Video url="https://www.youtube.com/embed/dw5s6rF7kxU" label="Bloom Housing Presentation" />
+  <Video videoId="dw5s6rF7kxU" label="Bloom Housing Presentation" />
 )
 
 export const videoWithEnglishCC = () => (
-  <Video url="https://www.youtube.com/embed/1Jrk9irbNAE" label="Web Accessibility Information" />
+  <Video videoId="1Jrk9irbNAE" label="Web Accessibility Information" />
 )
 
 export const videoWithTranslatedCC = () => (
-  <Video
-    url="https://www.youtube.com/embed/WvnBAQFsmu0"
-    label="Spanish Words to Know"
-    ccLang="pt"
-  />
+  <Video videoId="WvnBAQFsmu0" label="Spanish Words to Know" ccLang="pt" />
 )
--- ui-components/src/actions/Video.tsx
+++ ui-components/src/actions/Video.tsx
@@ -1,18 +1,26 @@
 import React from "react"
+import { createClassDeclaration } from "typescript"
 import "./Video.scss"
 
+enum VideoSource {
+  youtube = "youtube",
+}
 export interface VideoProps {
-  url: string
+  /** Located within the square brackets in the following example: https://www.youtube.com/watch?v=[dw5s6rF7kxU] */
+  videoId: string
   label: string
+  videoSource?: VideoSource
   ccLang?: string
 }
 
-const Video = (props: VideoProps) => {
-  const fullVideoUrl = `${props.url}?disablekb=1&cc_load_policy=1&cc_lang_pref=${props.ccLang}`
+const Video = ({ videoId, videoSource = VideoSource.youtube, label, ccLang = "" }: VideoProps) => {
+  let fullVideoUrl = ""
+  if (videoSource === VideoSource.youtube)
+    fullVideoUrl = `https://www.youtube.com/embed/${videoId}?disablekb=1&cc_load_policy=1&cc_lang_pref=${ccLang}`
   return (
     <div className="video">
       <iframe
-        title={props.label}
+        title={label}
         src={fullVideoUrl}
         frameBorder="0"
         allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
