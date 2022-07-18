3d76b7491340254a04e85b1cb37f104050ff5798
ColinBuyck <colin.buyck@gmail.com>
2022-07-18 10:30:31 -0700

    fix: improved variable naming

--- ui-components/src/actions/Video.tsx
+++ ui-components/src/actions/Video.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import "./Video.scss"
-import { useRouter } from "next/router"
 
 export interface VideoProps {
   url: string
@@ -9,12 +8,12 @@ export interface VideoProps {
 }
 
 const Video = (props: VideoProps) => {
-  const youtubeA11y = `?disablekb=1&cc_load_policy=1&cc_lang_pref=${props.ccLang}`
+  const fullVideoUrl = `${props.url}?disablekb=1&cc_load_policy=1&cc_lang_pref=${props.ccLang}`
   return (
     <div className="video">
       <iframe
         title={props.label}
-        src={props.url + youtubeA11y}
+        src={fullVideoUrl}
         frameBorder="0"
         allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
         allowFullScreen
