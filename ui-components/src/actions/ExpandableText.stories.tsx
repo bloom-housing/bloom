9fd7007e43f5a97c32d9778d950aa82fffbc1ffe
Lint Action <lint-action@samuelmeuli.com>
2022-08-03 02:43:04 +0000

    Fix code style issues with Prettier

--- ui-components/src/actions/ExpandableText.stories.tsx
+++ ui-components/src/actions/ExpandableText.stories.tsx
@@ -18,16 +18,24 @@ const longText =
 export const standard = () => (
   <ExpandableText strings={{ readMore: "More", readLess: "Less" }}>{longText}</ExpandableText>
 )
-export const expanded = () => <ExpandableText strings={{ readMore: "More", readLess: "Less" }} expand={true}>{longText}</ExpandableText>
-export const noExpansion = () => <ExpandableText strings={{ readMore: "More", readLess: "Less" }} >Short text</ExpandableText>
+export const expanded = () => (
+  <ExpandableText strings={{ readMore: "More", readLess: "Less" }} expand={true}>
+    {longText}
+  </ExpandableText>
+)
+export const noExpansion = () => (
+  <ExpandableText strings={{ readMore: "More", readLess: "Less" }}>Short text</ExpandableText>
+)
 export const html = () => (
-  <ExpandableText strings={{ readMore: "More", readLess: "Less" }} >{`Go to <a href="http://www.google.com" target="_blank">Google</a>. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been \
+  <ExpandableText
+    strings={{ readMore: "More", readLess: "Less" }}
+  >{`Go to <a href="http://www.google.com" target="_blank">Google</a>. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been \
   the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley \
   of type and scrambled it to make a type specimen book. It has survived not only five centuries`}</ExpandableText>
 )
 export const disableRawHtml = () => (
   <ExpandableText
-  strings={{ readMore: "More", readLess: "Less" }}
+    strings={{ readMore: "More", readLess: "Less" }}
     markdownProps={{ disableParsingRawHTML: true }}
   >{`Go to <a href="http://www.google.com" target="_blank">Google</a>. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been \
   the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley \
