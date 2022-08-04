60f06149e69512dbdf478f8165de8673cb2ca705
ColinBuyck <colin.buyck@gmail.com>
2022-08-04 15:10:44 -0700

    fix: aligned spacing & coloring with Figma

--- ui-components/src/headers/StepHeader.tsx
+++ ui-components/src/headers/StepHeader.tsx
@@ -5,19 +5,19 @@ import "./StepHeader.scss"
 export interface StepHeaderProps {
   currentStep: number
   totalSteps: number
-  stepLabel?: string
+  stepTitle?: string
 }
 
 const StepHeader = ({
   currentStep,
   totalSteps,
-  stepLabel = t("stepHeader.defaultLabel"),
+  stepTitle = t("stepHeader.defaultLabel"),
 }: StepHeaderProps) => {
   return (
     <div className="flex">
       <div className="circle-number">{currentStep}</div>
-      <div>{`out of ${totalSteps}  ${stepLabel}`}</div>
-      <div></div>
+      <div>{`out of ${totalSteps}`}</div>
+      <div className="step-label">{stepTitle}</div>
     </div>
   )
 }
