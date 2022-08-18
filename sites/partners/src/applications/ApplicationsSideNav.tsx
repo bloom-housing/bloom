156a4d78d6c18d33e75582c23e187e5e4818fc62
Sean Albert <smabert@gmail.com>
2022-08-18 14:58:50 -0700

    style: childrenItems comment

--- sites/partners/src/applications/ApplicationsSideNav.tsx
+++ sites/partners/src/applications/ApplicationsSideNav.tsx
@@ -21,8 +21,8 @@ const ApplicationsSideNav = ({ className, listingId }: ApplicationsSideNavProps)
       label: t("applications.pendingReview"),
       url: `/listings/${listingId}/applications/pending`,
       count: data?.totalPendingCount || 0,
-      /* childrenItems: [
-        {
+      childrenItems: [
+        /* {
           label: t("applications.namedob"),
           url: `/listings/${listingId}/applications/pending?type=name_dob`,
           count: data?.totalNamePendingCount || 0,
@@ -31,8 +31,8 @@ const ApplicationsSideNav = ({ className, listingId }: ApplicationsSideNavProps)
           label: t("t.email"),
           url: `/listings/${listingId}/applications/pending?type=email`,
           count: data?.totalEmailPendingCount || 0,
-        },
-      ], */
+        }, */
+      ],
     },
     {
       label: t("t.resolved"),
