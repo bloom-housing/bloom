2ca2f2a92aef0d38e865ff86b7f828fdb39f76d5
dominikx96 <dominik@airnauts.com>
2022-08-19 03:25:59 +0200

    2853/duplicates pages (#2892)
    * refactor: create duplicates pages
    
    * refactor: update applications link
    
    * refactor: refactor application routes
    
    * refactor: update active path detection
    
    * fix: export SideNav component and add className
    
    * refactor: cleanup & update table cols
    
    * refactor: refactor application export and pages structure
    
    * feat: create reusable sidenav component
    
    * feat: update header active paths
    
    * feat: integrate side nav
    
    * feat: adding backend for applicaiton sidenav
    
    * feat: changes applicaiton seeeds
    
    * fix: updates per morgan
    
    * fix: update the "all applications" translation
    
    * refactor: create helper file and move shared part
    
    * feat: integrate resolved apps view
    
    * refactor: cleanup
    
    * feat: hides pending subcats and footer with scan
    
    * style: childrenItems comment
    
    * feat: adds includeDemographics param to useApplicationsExport
    
    Co-authored-by: Yazeed Loonat <yazeedloonat@gmail.com>
    Co-authored-by: Sean Albert <smabert@gmail.com>

---
+++ sites/partners/src/applications/ApplicationsSideNav.tsx
@@ -0,0 +1,65 @@
+import React from "react"
+import { useRouter } from "next/router"
+import { t, SideNav } from "@bloom-housing/ui-components"
+import { useFlaggedApplicationsMeta } from "../../lib/hooks"
+
+type ApplicationsSideNavProps = {
+  className?: string
+  listingId: string
+}
+
+const ApplicationsSideNav = ({ className, listingId }: ApplicationsSideNavProps) => {
+  const router = useRouter()
+  const { data } = useFlaggedApplicationsMeta(listingId)
+  const items = [
+    {
+      label: t("applications.allApplications"),
+      url: `/listings/${listingId}/applications`,
+      count: data?.totalCount || 0,
+    },
+    {
+      label: t("applications.pendingReview"),
+      url: `/listings/${listingId}/applications/pending`,
+      count: data?.totalPendingCount || 0,
+      childrenItems: [
+        /* {
+          label: t("applications.namedob"),
+          url: `/listings/${listingId}/applications/pending?type=name_dob`,
+          count: data?.totalNamePendingCount || 0,
+        },
+        {
+          label: t("t.email"),
+          url: `/listings/${listingId}/applications/pending?type=email`,
+          count: data?.totalEmailPendingCount || 0,
+        }, */
+      ],
+    },
+    {
+      label: t("t.resolved"),
+      url: `/listings/${listingId}/applications/resolved`,
+      count: data?.totalResolvedCount || 0,
+    },
+  ].reduce((acc, curr) => {
+    // check which element is currently active
+
+    if (curr.url === router.asPath) {
+      Object.assign(curr, { current: true })
+    }
+
+    if (curr.childrenItems) {
+      curr.childrenItems.forEach((childItem) => {
+        if (childItem.url === router.asPath) {
+          Object.assign(childItem, { current: true })
+        }
+      })
+    }
+
+    acc.push(curr)
+
+    return acc
+  }, [])
+
+  return <SideNav className={className} navItems={items} />
+}
+
+export { ApplicationsSideNav as default, ApplicationsSideNav }
