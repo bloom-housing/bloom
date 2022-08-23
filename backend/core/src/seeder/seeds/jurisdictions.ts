0da5ff34ef9bd2663c69bef77d78f3f32e5d1e66
Sean Albert <smabert@gmail.com>
2022-08-23 16:01:47 -0700

    test: updates enablePartnerSettings seed

--- backend/core/src/seeder/seeds/jurisdictions.ts
+++ backend/core/src/seeder/seeds/jurisdictions.ts
@@ -24,7 +24,7 @@ export const activeJurisdictions: JurisdictionCreateDto[] = [
     emailFromAddress: "SJ: HousingBayArea.org <bloom-no-reply@exygy.dev>",
     rentalAssistanceDefault:
       "Housing Choice Vouchers, Section 8 and other valid rental assistance programs will be considered for this property. In the case of a valid rental subsidy, the required minimum income will be based on the portion of the rent that the tenant pays after use of the subsidy.",
-    enablePartnerSettings: null,
+    enablePartnerSettings: false,
     enableAccessibilityFeatures: false,
     enableUtilitiesIncluded: true,
   },
