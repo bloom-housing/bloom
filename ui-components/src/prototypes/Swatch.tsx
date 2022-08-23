3af74d0ea26f611df3b1fbec009f87c5e2829b8f
Jared White <jared@jaredwhite.com>
2022-08-22 22:02:03 -0700

    2918/add button group (#2958)
    * feat: add ButtonGroup component
    
    * feat: add ButtonGroup component
    
    Also add a title tooltip to swatch in docs
    Update 2nd Generation badge to primary blue instead of red
    
    * Fix code style issues with Prettier
    
    * fix: clean up button group styles, add react key
    
    * test: ButtonGroup, add to exports
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- ui-components/src/prototypes/Swatch.tsx
+++ ui-components/src/prototypes/Swatch.tsx
@@ -3,6 +3,7 @@ import * as React from "react"
 export const Swatch = (props: { colorVar: string }) => (
   <span
     style={{ backgroundColor: `var(${props.colorVar})` }}
+    title={props.colorVar}
     className="w-6 border border-gray-500 inline-block"
   >
     &nbsp;
