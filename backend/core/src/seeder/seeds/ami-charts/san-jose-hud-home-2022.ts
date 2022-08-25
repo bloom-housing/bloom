76eb93cc75c2f6fba5231baf67ca0561677e291a
Sean Albert <smabert@gmail.com>
2022-08-25 10:27:56 -0700

    feat: adds new san jose ami charts (#463)
    * feat: adds new san jose ami charts
    
    * Fix code style issues with Prettier
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

---
+++ backend/core/src/seeder/seeds/ami-charts/san-jose-hud-home-2022.ts
@@ -0,0 +1,89 @@
+import { AmiChartCreateDto } from "../../../ami-charts/dto/ami-chart.dto"
+import { BaseEntity } from "typeorm"
+
+// THIS FILE WAS AUTOMATICALLY GENERATED FROM san-jose-hud-home-2022.txt.
+export const sanJoseHudHome2022: Omit<AmiChartCreateDto, keyof BaseEntity | "jurisdiction"> = {
+  name: "San Jose HUD/HOME 2022",
+  items: [
+    {
+      percentOfAmi: 30,
+      householdSize: 1,
+      income: 35400,
+    },
+    {
+      percentOfAmi: 30,
+      householdSize: 2,
+      income: 40450,
+    },
+    {
+      percentOfAmi: 30,
+      householdSize: 3,
+      income: 45500,
+    },
+    {
+      percentOfAmi: 30,
+      householdSize: 4,
+      income: 50550,
+    },
+    {
+      percentOfAmi: 30,
+      householdSize: 5,
+      income: 54600,
+    },
+    {
+      percentOfAmi: 30,
+      householdSize: 6,
+      income: 58650,
+    },
+    {
+      percentOfAmi: 30,
+      householdSize: 7,
+      income: 62700,
+    },
+    {
+      percentOfAmi: 30,
+      householdSize: 8,
+      income: 66750,
+    },
+    {
+      percentOfAmi: 60,
+      householdSize: 1,
+      income: 70800,
+    },
+    {
+      percentOfAmi: 60,
+      householdSize: 2,
+      income: 80880,
+    },
+    {
+      percentOfAmi: 60,
+      householdSize: 3,
+      income: 91020,
+    },
+    {
+      percentOfAmi: 60,
+      householdSize: 4,
+      income: 101100,
+    },
+    {
+      percentOfAmi: 60,
+      householdSize: 5,
+      income: 109200,
+    },
+    {
+      percentOfAmi: 60,
+      householdSize: 6,
+      income: 117300,
+    },
+    {
+      percentOfAmi: 60,
+      householdSize: 7,
+      income: 125400,
+    },
+    {
+      percentOfAmi: 60,
+      householdSize: 8,
+      income: 133500,
+    },
+  ],
+}
