cc1fb533c143084cf9025b10765d7627f6ef2101
krzysztof ziecina <kziecina@airnauts.com>
2022-08-30 16:58:42 +0200

    test: remove redundant test

--- sites/public/cypress/integration/pages/application/start/choose-language.spec.ts
+++
@@ -1,9 +0,0 @@
-describe("applications/start/choose-language", function () {
-  it("should redirect to create account page", function () {
-    cy.visit("/listings")
-    cy.get(".is-card-link").contains("Test: Coliseum").click()
-    cy.getByTestId("listing-view-apply-button").eq(1).click()
-    cy.getByTestId("app-choose-language-create-account-button").click()
-    cy.location("pathname").should("equals", "/create-account")
-  })
-})
