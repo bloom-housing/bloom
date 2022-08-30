6de5b334d8a1e65b206bdc84a6e92633d5edbf60
Jared White <jared@jaredwhite.com>
2022-08-30 16:54:26 -0700

    feat: Finalize Card stories, Detroit style, responsive padding

--- ui-components/src/blocks/Card.stories.tsx
+++ ui-components/src/blocks/Card.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { AppearanceStyleType, Button, ButtonGroup, Heading, LinkButton } from "../.."
+import { AppearanceStyleType, Button, ButtonGroup, ButtonGroupSpacing, LinkButton } from "../.."
 import Card from "./Card"
 
 export default {
@@ -9,11 +9,14 @@ export default {
 export const Standard = () => (
   <div style={{ maxWidth: "500px" }}>
     <Card>
-      <Card.Header>
-        <Heading style="cardHeader" priority={3}>
-          I'm a heading!
-        </Heading>
-        <Button>Action</Button>
+      <Card.Header
+        suffix={
+          <div>
+            <Button>Click</Button>
+          </div>
+        }
+      >
+        <h3 className="card-header">I'm a heading!</h3>
       </Card.Header>
 
       <Card.Section>
@@ -22,26 +25,19 @@ export const Standard = () => (
 
       <Card.Footer>
         <ButtonGroup
-          columns={[
-            <Button>Previous</Button>,
-            <Button styleType={AppearanceStyleType.primary}>Next</Button>,
-          ]}
+          spacing={ButtonGroupSpacing.even}
+          columns={[<Button styleType={AppearanceStyleType.primary}>Call to Action</Button>]}
         />
-        <Card.Section centered>
-          <LinkButton href="#" unstyled className="m-0">
-            Skip this and show me listings
-          </LinkButton>
-        </Card.Section>
       </Card.Footer>
     </Card>
   </div>
 )
 
-export const OnlyContent = () => (
+export const OnlyTextContent = () => (
   <div style={{ maxWidth: "500px" }}>
     <Card>
       <Card.Header>
-        <Heading priority={3}>I'm a heading!</Heading>
+        <h3>I'm a heading!</h3>
       </Card.Header>
 
       <Card.Section className="markdown">
@@ -61,3 +57,99 @@ export const OnlyContent = () => (
     </Card>
   </div>
 )
+
+export const HeaderFooterOnly = () => (
+  <div style={{ maxWidth: "600px" }}>
+    <Card>
+      <Card.Header>
+        <h2>Household Maximum Income</h2>
+        <p aria-roledescription="subtitle">
+          To determine your eligibility for this property, choose your household size (include
+          yourself in that calculation).
+        </p>
+      </Card.Header>
+
+      <Card.Footer>
+        <ButtonGroup
+          columns={[
+            <Button>Previous</Button>,
+            <Button styleType={AppearanceStyleType.primary}>Next</Button>,
+          ]}
+        />
+        <Card.Section centered={true}>
+          <LinkButton href="#" unstyled className="m-0">
+            Skip this and show me listings
+          </LinkButton>
+        </Card.Section>
+      </Card.Footer>
+    </Card>
+  </div>
+)
+
+export const DetroitStyle = () => {
+  const cssVarsOverride = `
+    .card-overrides {
+      --bloom-font-sans: Montserrat;
+      --bloom-font-alt-sans: var(--bloom-font-sans);
+      --bloom-color-primary: rgb(41,126,115);
+      --bloom-color-primary-dark: rgb(0,68,69);
+
+      --primary-appearance-hover-background-color: white;
+      --primary-appearance-hover-label-color: var(--bloom-color-primary-dark);
+
+      --outlined-appearance-hover-background-color: var(--bloom-color-primary);
+      --outlined-appearance-hover-border-color: var(--bloom-color-primary);
+
+      font-family: var(--bloom-font-sans);
+    }
+
+    .card-overrides h2 {
+      font-family: var(--bloom-font-sans);
+    }
+
+    .card-overrides .button {
+      --normal-rounded: 60px;
+      --normal-padding: 0.5rem 1rem;
+      --normal-font-size: var(--bloom-font-size-base);
+      --label-letter-spacing: normal;
+      --label-transform: none;
+    }
+  `
+
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+        rel="stylesheet"
+      ></link>
+      <div className="card-overrides" style={{ maxWidth: "600px" }}>
+        <Card>
+          <Card.Header>
+            <h2>Household Maximum Income</h2>
+            <p aria-roledescription="subtitle">
+              To determine your eligibility for this property, choose your household size (include
+              yourself in that calculation).
+            </p>
+          </Card.Header>
+
+          <Card.Footer>
+            <ButtonGroup
+              columns={[
+                <Button>Previous</Button>,
+                <Button styleType={AppearanceStyleType.primary}>Next</Button>,
+              ]}
+            />
+            <Card.Section centered={true}>
+              <LinkButton href="#" unstyled className="m-0">
+                Skip this and show me listings
+              </LinkButton>
+            </Card.Section>
+          </Card.Footer>
+        </Card>
+        <style>{cssVarsOverride}</style>
+      </div>
+    </>
+  )
+}
--- ui-components/src/blocks/Card.tsx
+++ ui-components/src/blocks/Card.tsx
@@ -1,11 +1,29 @@
 import React from "react"
 import "./Card.scss"
 
-const CardHeader: React.FunctionComponent = ({ children }) => {
-  return <header className="card__header">{children}</header>
+interface CardHeaderProps {
+  suffix?: React.ReactNode
+  children: React.ReactNode
 }
 
-const CardSection: React.FunctionComponent = (props) => {
+const CardHeader: React.FunctionComponent<CardHeaderProps> = (props: CardHeaderProps) => {
+  const classNames = ["card__header"]
+  if (props.suffix) classNames.push("is-flex")
+  return (
+    <header className={classNames.join(" ")}>
+      <hgroup role="group">{props.children}</hgroup>
+      {props.suffix}
+    </header>
+  )
+}
+
+interface CardSectionProps {
+  centered?: boolean
+  className?: string
+  children: React.ReactNode
+}
+
+const CardSection: React.FunctionComponent<CardSectionProps> = (props: CardSectionProps) => {
   const classNames = ["card__section"]
   if (props.centered) classNames.push("text-center")
   if (props.className) classNames.push(props.className)
