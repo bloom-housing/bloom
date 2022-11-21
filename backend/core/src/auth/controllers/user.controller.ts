320c0f0e2e2ec8b40bcdddc39875456d53e7cca4
Yazeed Loonat <YazeedLoonat@gmail.com>
2022-11-21 08:47:20 -0700

    feat: adding UUID validation (#3172)

--- backend/core/src/auth/controllers/user.controller.ts
+++ backend/core/src/auth/controllers/user.controller.ts
@@ -4,6 +4,7 @@ import {
   Delete,
   Get,
   Param,
+  ParseUUIDPipe,
   Post,
   Put,
   Query,
@@ -154,7 +155,9 @@ export class UserController {
   @Get(`:id`)
   @ApiOperation({ summary: "Get user by id", operationId: "retrieve" })
   @UseGuards(DefaultAuthGuard, AuthzGuard)
-  async retrieve(@Param("id") userId: string): Promise<UserDto> {
+  async retrieve(
+    @Param("id", new ParseUUIDPipe({ version: "4" })) userId: string
+  ): Promise<UserDto> {
     return mapTo(UserDto, await this.userService.findById(userId))
   }
 
