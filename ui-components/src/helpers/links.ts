4b9381b030faa5d0b3884eb4cae191a68170356e
ColinBuyck <colin.buyck@gmail.com>
2022-12-13 12:49:41 -0800

    fix: delete ui-components

--- ui-components/src/helpers/links.ts
+++
@@ -1,7 +0,0 @@
-export const isExternalLink = (href: string) => {
-  return href.startsWith("http://") || href.startsWith("https://")
-}
-
-export const isInternalLink = (href: string) => {
-  return href.startsWith("/") && !href.startsWith("//")
-}
