285595238f3ec5c46b5f3c7db901f773a8d21c88
Jared White <jared@whitefusion.studio>
2022-12-18 21:51:57 -0800

    fix: support tagalog jurisdiction translations (#567)

--- sites/public/src/translations.ts
+++ sites/public/src/translations.ts
@@ -16,7 +16,7 @@ export const translations = {
 export const overrideTranslations = { en: additionalGeneralTranslations } as Record<string, any>
 
 export const jurisdictionTranslations = async () => {
-  const [english, spanish, chinese, vietnamese] = await Promise.allSettled(
+  const [english, spanish, chinese, vietnamese, tagalog] = await Promise.allSettled(
     ["general", "es", "zh", "vi", "tl"].map(async (locale) => {
       return import(
         `../page_content/jurisdiction_overrides/${process.env.jurisdictionName
@@ -31,5 +31,6 @@ export const jurisdictionTranslations = async () => {
     es: spanish.status === "fulfilled" ? spanish.value : null,
     zh: chinese.status === "fulfilled" ? chinese.value : null,
     vi: vietnamese.status === "fulfilled" ? vietnamese.value : null,
+    tl: tagalog.status === "fulfilled" ? tagalog.value : null,
   }
 }
