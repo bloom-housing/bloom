e0c7c0be08bf51a60556b24efb98a0a5aa826d61
Yazeed Loonat <yazeedloonat@gmail.com>
2022-12-21 13:22:29 -0700

    fix: update to work on safari

--- backend/core/src/auth/constants.ts
+++ backend/core/src/auth/constants.ts
@@ -8,19 +8,19 @@ export const ACCESS_TOKEN_AVAILABLE_NAME = "access-token-available"
 export const TOKEN_COOKIE_MAXAGE = 86400000 // 24 hours
 export const AUTH_COOKIE_OPTIONS: CookieOptions = {
   httpOnly: true,
-  secure: true,
+  secure: process.env.NODE_ENV !== "development",
   sameSite: true,
   maxAge: TOKEN_COOKIE_MAXAGE / 24, // access token should last 1 hr
 }
 export const REFRESH_COOKIE_OPTIONS: CookieOptions = {
   httpOnly: true,
-  secure: true,
+  secure: process.env.NODE_ENV !== "development",
   sameSite: true,
   maxAge: TOKEN_COOKIE_MAXAGE,
 }
 export const ACCESS_TOKEN_AVAILABLE_OPTIONS: CookieOptions = {
   httpOnly: false,
-  secure: true,
+  secure: process.env.NODE_ENV !== "development",
   sameSite: true,
-  maxAge: TOKEN_COOKIE_MAXAGE / 24, // access token should last 1 hr
+  maxAge: TOKEN_COOKIE_MAXAGE / 24, // flag should last 1 hr
 }
