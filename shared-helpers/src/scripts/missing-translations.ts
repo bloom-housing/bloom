6d47fa03d5d2625fb1ba9d73312ec1d214201dd7
Emily Jablonski <emily.jablonski@exygy.com>
2023-01-17 12:21:08 -0500

    refactor: add translations

---
+++ shared-helpers/src/scripts/missing-translations.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// Prints out keys/strings that exist in the english file but not in a foreign language translation file
+// example: `ts-node missing-translations > missing-foreign-keys.json`
+const englishTranslations = require("../locales/general.json")
+const spanishTranslations = require("../locales/es.json")
+const chineseTranslations = require("../locales/zh.json")
+const vietnameseTranslations = require("../locales/vi.json")
+const tagalogTranslations = require("../locales/tl.json")
+
+function main() {
+  type TranslationsType = {
+    [key: string]: string
+  }
+
+  const allTranslations = [
+    { translationKeys: spanishTranslations, language: "Spanish" },
+    { translationKeys: chineseTranslations, language: "Chinese" },
+    { translationKeys: vietnameseTranslations, language: "Vietnamese" },
+    { translationKeys: tagalogTranslations, language: "Tagalog" },
+  ]
+
+  const findMissingStrings = (
+    baseTranslations: TranslationsType,
+    checkedTranslations: TranslationsType
+  ) => {
+    const baseKeys = Object.keys(baseTranslations)
+    const checkedKeys = Object.keys(checkedTranslations)
+    const missingKeys: string[] = []
+    baseKeys.forEach((key) => {
+      if (checkedKeys.indexOf(key) < 0) {
+        missingKeys.push(key)
+      }
+    })
+    return missingKeys
+  }
+
+  allTranslations.forEach((foreignKeys) => {
+    console.log("--------------------")
+    console.log(`Missing Public Site ${foreignKeys.language} Translations:`)
+    const missingPublicSiteTranslations = findMissingStrings(
+      englishTranslations,
+      foreignKeys.translationKeys
+    )
+    missingPublicSiteTranslations.forEach((missingKey) =>
+      console.log(`${missingKey}, ${JSON.stringify(englishTranslations[missingKey])}`)
+    )
+  })
+}
+
+void main()
+
+export {}
