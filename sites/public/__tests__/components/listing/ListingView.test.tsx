cdd777ebf902d7e0c4cb613d7ae742d592e1f330
Liann Sun <lianns@google.com>
2023-02-16 00:02:07 +0000

    chore: add unit test skeleton with temporary copypasta testHelpers file

---
+++ sites/public/__tests__/components/listing/ListingView.test.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Listing, UnitsSummarized, Jurisdiction, Unit } from "@bloom-housing/backend-core/types"
+import { render, cleanup } from "@testing-library/react"
+import { ListingView } from "../../../src/components/listing/ListingView"
+import { listing, jurisdiction } from "../../testHelpers"
+
+//backend/core/types/src/archer-listing.ts may be useful
+afterEach(cleanup)
+
+describe("<ListingView>", () => {
+  it("liann tests", () => {
+    const { getByText } = render(<ListingView listing={listing} jurisdiction={jurisdiction} />)
+
+    expect(true).toBeTruthy()
+  })
+})
