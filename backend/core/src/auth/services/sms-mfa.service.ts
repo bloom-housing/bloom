da5fb5e00c16364521dca99a1fed8148d8161be3
Yazeed Loonat <yazeedloonat@gmail.com>
2023-02-21 11:06:05 -0700

    fix: undoing nestjs bump

--- backend/core/src/auth/services/sms-mfa.service.ts
+++ backend/core/src/auth/services/sms-mfa.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from "@nestjs/common"
 import { User } from "../entities/user.entity"
-import { TwilioService } from "nestjs-twilio"
+import { InjectTwilio, TwilioClient } from "nestjs-twilio"
 import { ConfigService } from "@nestjs/config"
 
 @Injectable()
 export class SmsMfaService {
   public constructor(
-    private readonly client: TwilioService,
+    @InjectTwilio() private readonly client: TwilioClient,
     private readonly configService: ConfigService
   ) {}
-  public async sendMfaCode(user: User, phoneNumber: string, mfaCode: string): Promise<unknown> {
-    return await this.client.client.messages.create({
+  public async sendMfaCode(user: User, phoneNumber: string, mfaCode: string) {
+    return await this.client.messages.create({
       body: `Your Partners Portal account access token: ${mfaCode}`,
       from: this.configService.get("TWILIO_PHONE_NUMBER"),
       to: phoneNumber,
