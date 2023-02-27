b183803f28857f692bd538dbebc55813e07190e5
Liann Sun <lianns@google.com>
2023-02-27 20:58:15 +0000

    test: remove afterEach line

--- sites/public/__tests__/components/listing/ListingView.test.tsx
+++ sites/public/__tests__/components/listing/ListingView.test.tsx
@@ -1,11 +1,9 @@
 import React from "react"
 import dayjs from "dayjs"
 import { ApplicationMethodType } from "@bloom-housing/backend-core/types"
-import { render, cleanup } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import { ListingView } from "../../../src/components/listing/ListingView"
 import { listing, jurisdiction } from "@bloom-housing/shared-helpers/__tests__/testHelpers"
-
-afterEach(cleanup)
 
 describe("<ListingView>", () => {
   describe("'Apply Online' button visibility", () => {
