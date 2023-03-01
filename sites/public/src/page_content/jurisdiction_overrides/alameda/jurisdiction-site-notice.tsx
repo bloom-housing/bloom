4d96c638a980500d96485903db98397e96f350a7
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2023-03-01 11:02:45 -0500

    fix: add feedback link in alameda (#593)

--- sites/public/src/page_content/jurisdiction_overrides/alameda/jurisdiction-site-notice.tsx
+++ sites/public/src/page_content/jurisdiction_overrides/alameda/jurisdiction-site-notice.tsx
@@ -3,8 +3,15 @@ import { t } from "@bloom-housing/ui-components"
 
 export const JursidictionSiteNotice = () => {
   return (
-    <a href="/" target="_blank" className={"cursor-pointer"}>
+    <>
       {t("nav.getFeedback")}
-    </a>
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLScr7JuVwiNW8q-ifFUWTFSWqEyV5ndA08jAhJQSlQ4ETrnl9w/viewform?usp=sf_link"
+        target="_blank"
+        className={"cursor-pointer"}
+      >
+        {t("nav.yourFeedback")}
+      </a>
+    </>
   )
 }
