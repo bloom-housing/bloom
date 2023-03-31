ec94007fb009279a769063498a2928dc918a4dd8
Yazeed Loonat <yazeedloonat@gmail.com>
2023-03-31 16:00:24 -0700

    fix: removing unneeded sign out

--- sites/public/cypress/integration/pages/submit-application.spec.ts
+++ sites/public/cypress/integration/pages/submit-application.spec.ts
@@ -3,7 +3,6 @@ import { coliseumApplication, minimalDataApplication } from "../../mockData/appl
 describe("Submit", function () {
   it("should submit an application for the Test: Coliseum listing", function () {
     cy.submitApplication("Test: Coliseum", coliseumApplication, false)
-    cy.signOut()
   })
   it("should submit a minimal application for the Test: Default, No Preferences", function () {
     cy.submitApplication("Test: Default, No Preferences", minimalDataApplication, false)
