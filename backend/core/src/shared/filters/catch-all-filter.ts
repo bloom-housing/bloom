206e9346b210e3e35f0c6b38d3956e04343eaa68
Yazeed Loonat <yazeedloonat@gmail.com>
2023-05-10 09:54:43 -0700

    feat: adding knownError flag

--- backend/core/src/shared/filters/catch-all-filter.ts
+++ backend/core/src/shared/filters/catch-all-filter.ts
@@ -4,14 +4,19 @@ import { BaseExceptionFilter } from "@nestjs/core"
 @Catch()
 export class CatchAllFilter extends BaseExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
-    console.error({ message: exception?.response?.message, stack: exception.stack, exception })
+    if (exception?.response?.knownError) {
+      delete exception.response.knownError
+    } else {
+      console.error({ message: exception?.response?.message, stack: exception.stack, exception })
+    }
+
     if (exception.name === "EntityNotFound") {
       const response = host.switchToHttp().getResponse()
       response.status(404).json({ message: exception.message })
     } else if (exception.message === "tokenExpired") {
       const response = host.switchToHttp().getResponse()
       response.status(404).json({ message: exception.message })
-    } else if (exception.response === "emailInUse") {
+    } else if (exception?.response?.message === "emailInUse") {
       const response = host.switchToHttp().getResponse()
       response.status(409).json({ message: "That email is already in use" })
     } else {
