bc6306d3ff258ca7d52e4292fdecf7779c01e973
Yazeed Loonat <yazeedloonat@gmail.com>
2023-05-15 14:47:34 -0700

    feat: adding app sub verification endpoint

--- sites/public/src/pages/applications/review/summary.tsx
+++ sites/public/src/pages/applications/review/summary.tsx
@@ -2,7 +2,7 @@
 5.2 Summary
 Display a summary of application fields with edit links per section
 */
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import {
   AppearanceStyleType,
   Button,
@@ -11,6 +11,7 @@ import {
   Form,
   ProgressNav,
   Heading,
+  AlertBox,
 } from "@bloom-housing/ui-components"
 import FormsLayout from "../../../layouts/forms"
 import { useForm } from "react-hook-form"
@@ -24,10 +25,11 @@ import {
   listingSectionQuestions,
 } from "@bloom-housing/shared-helpers"
 import { UserStatus } from "../../../lib/constants"
-import { ApplicationSection } from "@bloom-housing/backend-core"
+import { ApplicationReviewStatus, ApplicationSection } from "@bloom-housing/backend-core"
 
 const ApplicationSummary = () => {
-  const { profile } = useContext(AuthContext)
+  const { profile, applicationsService } = useContext(AuthContext)
+  const [validationError, setValidationError] = useState(false)
   const { conductor, application, listing } = useFormConductor("summary")
   let currentPageSection = 4
   if (listingSectionQuestions(listing, ApplicationSection.programs)?.length) currentPageSection += 1
@@ -46,6 +48,29 @@ const ApplicationSummary = () => {
     })
   }, [profile])
 
+  useEffect(() => {
+    applicationsService
+      .submissionValidation({
+        body: {
+          ...application,
+          reviewStatus: ApplicationReviewStatus.pending,
+          listing: {
+            id: listing.id,
+          },
+          appUrl: window.location.origin,
+          ...(profile && {
+            user: {
+              id: profile.id,
+            },
+          }),
+        },
+      })
+      .then(() => {
+        // if we got a response then we passed validation
+      })
+      .catch(() => setValidationError(true))
+  }, [application, applicationsService, listing.id, profile])
+
   return (
     <FormsLayout>
       <FormCard header={<Heading priority={1}>{listing?.name}</Heading>}>
@@ -62,6 +87,11 @@ const ApplicationSummary = () => {
             {t("application.review.takeAMomentToReview")}
           </h2>
         </div>
+        {validationError && (
+          <AlertBox type="alert" inverted>
+            {t("errors.alert.applicationSubmissionVerificationError")}
+          </AlertBox>
+        )}
 
         <FormSummaryDetails
           application={application}
@@ -71,6 +101,7 @@ const ApplicationSummary = () => {
           }
           hidePrograms={listingSectionQuestions(listing, ApplicationSection.programs)?.length === 0}
           editMode
+          validationError={validationError}
         />
 
         <div className="form-card__group">
@@ -82,7 +113,13 @@ const ApplicationSummary = () => {
         <div className="form-card__pager">
           <div className="form-card__pager-row primary">
             <Form onSubmit={handleSubmit(onSubmit)}>
-              <Button styleType={AppearanceStyleType.primary} data-testid={"app-summary-confirm"}>
+              <Button
+                styleType={
+                  validationError ? AppearanceStyleType.closed : AppearanceStyleType.primary
+                }
+                data-testid={"app-summary-confirm"}
+                disabled={validationError}
+              >
                 {t("t.confirm")}
               </Button>
             </Form>
