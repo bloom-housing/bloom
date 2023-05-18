ef5953c17eb8ae5fcde9b5fdb22c047e5f1176d7
ifranch <inesfranch@google.com>
2023-05-18 19:48:38 +0200

    feat: add text when there are no matching listings (#126)
    * feat: add zero listings card component and add it to the listings page
    
    * fix: lint
    
    * feat: fix component width, add new messages with translations
    
    * fix: pass the button as a child to the zero listings card
    
    * fix: update ZeroListingsItem component which fixes rendering issue for narrow screen
    
    * feat: change title color and add bottom border
    
    * chore: CSS naming changes
    
    * fix: fix spacing
    
    * feat: refactor zero listings div
    
    * fix: formatting

---
+++ doorway-ui-components/src/page_components/listings/ZeroListingsItem.tsx
@@ -0,0 +1,27 @@
+import * as React from "react"
+import { Heading } from "../../.."
+import "./ZeroListingsItem.scss"
+import Markdown from "markdown-to-jsx"
+
+export interface ZeroListingsItemProps {
+  title: string
+  description: string
+  children?: React.ReactNode
+  desktopClass?: string
+}
+
+export const ZeroListingsItem = (props: ZeroListingsItemProps) => (
+  <div className="zero-listings">
+    <Heading styleType={"largePrimary"} className="zero-listings-title">
+      {props.title}
+    </Heading>
+    <div className="zero-listings-description">{props.description}</div>
+    {typeof props.children == "string" ? (
+      <div className="markdown info-card__content">
+        <Markdown options={{ disableParsingRawHTML: true }} children={props.children} />
+      </div>
+    ) : (
+      props.children
+    )}
+  </div>
+)
--- sites/public/src/components/listings/ListingsList.tsx
+++ sites/public/src/components/listings/ListingsList.tsx
@@ -1,16 +1,30 @@
 import * as React from "react"
 import { getListings } from "../../lib/helpers"
 import { Listing } from "@bloom-housing/backend-core"
-import { Button, InfoCard, LinkButton, t } from "@bloom-housing/doorway-ui-components"
+import {
+  Button,
+  InfoCard,
+  LinkButton,
+  ZeroListingsItem,
+  t,
+} from "@bloom-housing/doorway-ui-components"
 
 type ListingsListProps = {
   listings: Listing[]
 }
 
 const ListingsList = (props: ListingsListProps) => {
+  const listingsDiv =
+    props.listings.length > 0 ? (
+      <div className="listingsList">{getListings(props.listings)}</div>
+    ) : (
+      <ZeroListingsItem title={t("t.noMatchingListings")} description={t("t.tryRemovingFilters")}>
+        <Button>{t("t.clearAllFilters")}</Button>
+      </ZeroListingsItem>
+    )
   return (
     <div>
-      <div className="listingsList">{getListings(props.listings)}</div>
+      {listingsDiv}
       {/* TODO: once pagination is implemented for listings, the following should only show on the last page. */}
       <InfoCard
         title={t("t.signUpForAlerts")}
