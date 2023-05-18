d56de28d73b94aa26ad52a87eb234f21267c414b
Krzysztof Zięcina <kziecina@airnauts.com>
2023-05-18 13:39:51 +0200

    fix: public multiple photos from detroit (#3390)
    * feat: add multiple photos
    
    * feat: add tests
    
    * feat: add images to seeds
    
    * fix: return array when no image
    
    * fix: shared helpers photos tests
    
    * fix: add class with variables to parent element
    
    * fix: make actions inside content section optional on desktop
    
    * fix: pass modalCloseInContent prop to listing view's ImageCard
    
    * fix: remove redundant image-card class
    
    * chore: upgrade ui-c
    
    * fix: remove redundant order column
    
    * fix: change font for drawer title
    
    * fix: remove default images value when editing

--- shared-helpers/src/utilities/photos.ts
+++ shared-helpers/src/utilities/photos.ts
@@ -1,20 +1,39 @@
 import { Asset, Listing } from "@bloom-housing/backend-core/types"
 
+export const CLOUDINARY_BUILDING_LABEL = "cloudinaryBuilding"
+
 export const cloudinaryUrlFromId = (publicId: string, size = 400) => {
   const cloudName = process.env.cloudinaryCloudName || process.env.CLOUDINARY_CLOUD_NAME
   return `https://res.cloudinary.com/${cloudName}/image/upload/w_${size},c_limit,q_65/${publicId}.jpg`
 }
 
-export const imageUrlFromListing = (listing: Listing, size = 400) => {
-  // Use the new `image` field
+export const getUrlForListingImage = (image: Asset, size = 400) => {
+  if (!image) return null
+
+  if (image.label == CLOUDINARY_BUILDING_LABEL) {
+    return cloudinaryUrlFromId(image.fileId, size)
+  } else {
+    return image.fileId
+  }
+}
+
+export const imageUrlFromListing = (listing: Listing, size = 400): string[] => {
   const imageAssets =
-    listing?.images?.length && listing.images[0].image ? [listing.images[0].image] : listing?.assets
+    listing?.images?.length && listing.images[0].image
+      ? listing.images
+          .sort((imageA, imageB) => (imageA.ordinal ?? 10) - (imageB?.ordinal ?? 10))
+          .map((imageObj) => imageObj.image)
+      : listing?.assets
 
-  // Fallback to `assets`
-  const cloudinaryBuilding = imageAssets?.find(
-    (asset: Asset) => asset.label == "cloudinaryBuilding"
-  )?.fileId
-  if (cloudinaryBuilding) return cloudinaryUrlFromId(cloudinaryBuilding, size)
+  const imageUrls = imageAssets
+    ?.filter(
+      (asset: Asset) => asset.label === CLOUDINARY_BUILDING_LABEL || asset.label === "building"
+    )
+    ?.map((asset: Asset) => {
+      return asset.label === CLOUDINARY_BUILDING_LABEL
+        ? cloudinaryUrlFromId(asset.fileId, size)
+        : asset.fileId
+    })
 
-  return imageAssets?.find((asset: Asset) => asset.label == "building")?.fileId
+  return imageUrls?.length > 0 ? imageUrls : [""]
 }
