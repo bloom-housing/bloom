7bb685a9a907a90a15fb4651ad9710c0698beb04
emilyjablonski <emijablonski@gmail.com>
2023-08-07 23:47:34 -0600

    refactor: adding new enum values

--- backend/core/src/migration/1691377869351-new-status-fields.ts
+++ backend/core/src/migration/1691377869351-new-status-fields.ts
@@ -5,17 +5,9 @@ export class newStatusFields1691377869351 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TYPE "public"."listings_status_enum" RENAME TO "listings_status_enum_old"`
+      `ALTER TYPE "public"."listings_status_enum" ADD VALUE 'changesRequested'`
     )
-    await queryRunner.query(
-      `CREATE TYPE "public"."listings_status_enum" AS ENUM('active', 'changesRequested', 'closed', 'pending', 'pendingReview')`
-    )
-    await queryRunner.query(`ALTER TABLE "listings" ALTER COLUMN "status" DROP DEFAULT`)
-    await queryRunner.query(
-      `ALTER TABLE "listings" ALTER COLUMN "status" TYPE "public"."listings_status_enum" USING "status"::"text"::"public"."listings_status_enum"`
-    )
-    await queryRunner.query(`ALTER TABLE "listings" ALTER COLUMN "status" SET DEFAULT 'pending'`)
-    await queryRunner.query(`DROP TYPE "public"."listings_status_enum_old"`)
+    await queryRunner.query(`ALTER TYPE "public"."listings_status_enum" ADD VALUE 'pendingReview'`)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
