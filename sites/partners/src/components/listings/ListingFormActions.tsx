a10dc61d3836c7d23c691b4f8338a7724fc013b1
emilyjablonski <emijablonski@gmail.com>
2023-08-08 00:39:17 -0600

    refactor: file structure

--- sites/partners/src/components/listings/ListingFormActions.tsx
+++ sites/partners/src/components/listings/ListingFormActions.tsx
@@ -16,21 +16,21 @@ import { ListingContext } from "./ListingContext"
 import { ListingEventType, ListingStatus } from "@bloom-housing/backend-core/types"
 import { StatusAside } from "../shared/StatusAside"
 
-type AsideProps = {
-  type: AsideType
+type ListingFormActionsProps = {
+  type: ListingFormActionsType
   showCloseListingModal?: () => void
   showLotteryResultsDrawer?: () => void
   submitFormWithStatus?: (confirm?: boolean, status?: ListingStatus) => void
 }
 
-type AsideType = "add" | "edit" | "details"
+type ListingFormActionsType = "add" | "edit" | "details"
 
-const Aside = ({
+const ListingFormActions = ({
   type,
   showCloseListingModal,
   showLotteryResultsDrawer,
   submitFormWithStatus,
-}: AsideProps) => {
+}: ListingFormActionsProps) => {
   const listing = useContext(ListingContext)
 
   const listingId = listing?.id
@@ -44,9 +44,7 @@ const Aside = ({
   }, [listing])
 
   const actions = useMemo(() => {
-    const elements = []
-
-    const cancel = (
+    const cancelButton = (
       <GridCell className="flex" key="btn-cancel">
         <LinkButton
           unstyled
@@ -60,178 +58,191 @@ const Aside = ({
       </GridCell>
     )
 
-    if (type === "details") {
-      elements.push(
-        <GridCell key="btn-submitNew">
-          <LocalizedLink href={`/listings/${listingId}/edit`}>
-            <Button
-              styleType={AppearanceStyleType.primary}
-              fullWidth
-              onClick={() => false}
-              type="button"
-              dataTestId="listingEditButton"
-            >
-              {t("t.edit")}
-            </Button>
-          </LocalizedLink>
-        </GridCell>
-      )
-    }
-
-    if (type === "add") {
-      elements.push(
-        <GridCell key="btn-publish">
-          <Button
-            id="publishButton"
-            styleType={AppearanceStyleType.success}
-            type="button"
-            fullWidth
-            onClick={() => {
-              submitFormWithStatus(true, ListingStatus.active)
-            }}
-          >
-            {t("listings.actions.publish")}
-          </Button>
-        </GridCell>,
-        <GridCell key="btn-draft">
-          <Button
-            type="button"
-            fullWidth
-            onClick={() => submitFormWithStatus(false, ListingStatus.pending)}
-          >
-            {t("listings.actions.draft")}
-          </Button>
-        </GridCell>
-      )
-    }
-
-    if (type === "edit") {
-      elements.push(
-        <GridCell key="btn-save">
+    const editFromDetailButton = (
+      <GridCell key="btn-submitNew">
+        <LocalizedLink href={`/listings/${listingId}/edit`}>
           <Button
             styleType={AppearanceStyleType.primary}
-            type="button"
             fullWidth
-            onClick={() => submitFormWithStatus(true, listing.status)}
-            dataTestId={"saveAndExitButton"}
+            onClick={() => false}
+            type="button"
+            dataTestId="listingEditButton"
           >
-            {t("t.saveExit")}
+            {t("t.edit")}
           </Button>
-        </GridCell>
-      )
+        </LocalizedLink>
+      </GridCell>
+    )
 
-      if (listing.status === ListingStatus.pending || listing.status === ListingStatus.closed) {
-        elements.push(
-          <GridCell key="btn-publish">
-            <Button
-              id="publishButton"
-              type="button"
-              styleType={AppearanceStyleType.success}
-              fullWidth
-              onClick={() => {
-                submitFormWithStatus(true, ListingStatus.active)
-              }}
-            >
-              {t("listings.actions.publish")}
-            </Button>
-          </GridCell>
-        )
-      }
+    const publishButton = (
+      <GridCell key="btn-publish">
+        <Button
+          id="publishButton"
+          styleType={AppearanceStyleType.success}
+          type="button"
+          fullWidth
+          onClick={() => {
+            submitFormWithStatus(true, ListingStatus.active)
+          }}
+        >
+          {t("listings.actions.publish")}
+        </Button>
+      </GridCell>
+    )
 
-      if (listing.status === ListingStatus.active) {
-        elements.push(
-          <div className="grid gap-2" key="btn-close-unpublish">
-            <Button
-              type="button"
-              fullWidth
-              onClick={() => showCloseListingModal && showCloseListingModal()}
-            >
-              {t("listings.actions.close")}
-            </Button>
+    const saveDraftButton = (
+      <GridCell key="btn-draft">
+        <Button
+          type="button"
+          fullWidth
+          onClick={() => submitFormWithStatus(false, ListingStatus.pending)}
+        >
+          {t("listings.actions.draft")}
+        </Button>
+      </GridCell>
+    )
 
-            <Button
-              styleType={AppearanceStyleType.alert}
-              fullWidth
-              type="button"
-              onClick={() => submitFormWithStatus(false, ListingStatus.pending)}
-              border={AppearanceBorderType.outlined}
-            >
-              {t("listings.actions.unpublish")}
-            </Button>
-          </div>
-        )
-      }
+    const saveExitButton = (
+      <GridCell key="btn-save">
+        <Button
+          styleType={AppearanceStyleType.primary}
+          type="button"
+          fullWidth
+          onClick={() => submitFormWithStatus(true, listing.status)}
+          dataTestId={"saveAndExitButton"}
+        >
+          {t("t.saveExit")}
+        </Button>
+      </GridCell>
+    )
 
-      const lotteryResults = listing.events.find(
-        (event) => event.type === ListingEventType.lotteryResults
-      )
-      if (lotteryResults) {
-        elements.push(
-          <GridCell className="flex" key="btn-edit-lottery">
-            <Button
-              type="button"
-              unstyled
-              fullWidth
-              className="bg-opacity-0"
-              onClick={() => showLotteryResultsDrawer && showLotteryResultsDrawer()}
-            >
-              {t("listings.actions.resultsPosted")}{" "}
-              {dayjs(lotteryResults?.startTime).format("MMMM DD, YYYY")}
-              <Icon size="medium" symbol="edit" className="ml-2" />
-            </Button>
-          </GridCell>
-        )
-      } else if (listing.status === ListingStatus.closed) {
-        elements.push(
-          <GridCell key="btn-post-results">
-            <Button
-              type="button"
-              styleType={AppearanceStyleType.success}
-              fullWidth
-              onClick={() => showLotteryResultsDrawer && showLotteryResultsDrawer()}
-            >
-              {t("listings.actions.postResults")}
-            </Button>
-          </GridCell>
-        )
-      }
-    }
+    const closeButton = (
+      <GridCell key="btn-close">
+        <Button
+          type="button"
+          fullWidth
+          onClick={() => showCloseListingModal && showCloseListingModal()}
+        >
+          {t("listings.actions.close")}
+        </Button>
+      </GridCell>
+    )
 
+    const unpublishButton = (
+      <GridCell key="btn-unpublish">
+        <Button
+          styleType={AppearanceStyleType.alert}
+          fullWidth
+          type="button"
+          onClick={() => submitFormWithStatus(false, ListingStatus.pending)}
+          border={AppearanceBorderType.outlined}
+        >
+          {t("listings.actions.unpublish")}
+        </Button>
+      </GridCell>
+    )
+
+    const editPostedResultsButton = (lotteryResults) => (
+      <GridCell className="flex" key="btn-edit-lottery">
+        <Button
+          type="button"
+          unstyled
+          fullWidth
+          className="bg-opacity-0"
+          onClick={() => showLotteryResultsDrawer && showLotteryResultsDrawer()}
+        >
+          {t("listings.actions.resultsPosted")}{" "}
+          {dayjs(lotteryResults?.startTime).format("MMMM DD, YYYY")}
+          <Icon size="medium" symbol="edit" className="ml-2" />
+        </Button>
+      </GridCell>
+    )
+
+    const postResultsButton = (
+      <GridCell key="btn-post-results">
+        <Button
+          type="button"
+          styleType={AppearanceStyleType.success}
+          fullWidth
+          onClick={() => showLotteryResultsDrawer && showLotteryResultsDrawer()}
+        >
+          {t("listings.actions.postResults")}
+        </Button>
+      </GridCell>
+    )
+
+    const previewButton = (
+      <GridCell key="btn-preview">
+        <a
+          target="_blank"
+          href={`${listing?.jurisdiction.publicUrl}/preview/listings/${listingId}`}
+        >
+          <Button fullWidth onClick={() => false} type="button">
+            {t("listings.actions.preview")}
+          </Button>
+        </a>
+      </GridCell>
+    )
+
+    const viewPostedResultsButton = (eventUrl: string) => (
+      <GridCell key="btn-preview-results">
+        <a href={eventUrl} target="_blank" className="inline-flex w-full">
+          <Button type="button" unstyled fullWidth>
+            {t("listings.actions.previewLotteryResults")}{" "}
+            <Icon size="medium" symbol="link" className="ml-2" />
+          </Button>
+        </a>
+      </GridCell>
+    )
+
+    const elements = []
+
+    // read-only form
     if (type === "details") {
-      elements.push(
-        <GridCell key="btn-preview">
-          <a
-            target="_blank"
-            href={`${listing?.jurisdiction.publicUrl}/preview/listings/${listingId}`}
-          >
-            <Button fullWidth onClick={() => false} type="button">
-              {t("listings.actions.preview")}
-            </Button>
-          </a>
-        </GridCell>
-      )
+      elements.push(editFromDetailButton)
+      elements.push(previewButton)
 
       if (listing.events.find((event) => event.type === ListingEventType.lotteryResults)) {
         const eventUrl = pdfUrlFromListingEvents(
-          listing.events,
+          listing?.events,
           ListingEventType.lotteryResults,
           process.env.cloudinaryCloudName
         )
-        elements.push(
-          <GridCell className="flex" key="btn-preview-results">
-            <a href={eventUrl} target="_blank" className="inline-flex w-full">
-              <Button type="button" unstyled fullWidth className="bg-opacity-0">
-                {t("listings.actions.previewLotteryResults")}{" "}
-                <Icon size="medium" symbol="link" className="ml-2" />
-              </Button>
-            </a>
-          </GridCell>
-        )
+        elements.push(viewPostedResultsButton(eventUrl))
       }
+
+      elements.push(cancelButton)
     }
 
-    if (type === "add" || type === "edit") {
-      elements.push(cancel)
+    // new unsaved listing
+    if (type === "add") {
+      elements.push(publishButton)
+      elements.push(saveDraftButton)
+      elements.push(cancelButton)
+    }
+
+    // listing saved at least once
+    if (type === "edit") {
+      elements.push(saveExitButton)
+
+      if (listing.status === ListingStatus.pending || listing.status === ListingStatus.closed) {
+        elements.push(publishButton)
+      }
+
+      if (listing.status === ListingStatus.active) {
+        elements.push(closeButton)
+        elements.push(unpublishButton)
+      }
+
+      const lotteryResults = listing?.events?.find(
+        (event) => event.type === ListingEventType.lotteryResults
+      )
+
+      if (lotteryResults) {
+        elements.push(editPostedResultsButton(lotteryResults))
+      } else if (listing.status === ListingStatus.closed) {
+        elements.push(postResultsButton)
+      }
     }
 
     return elements
@@ -253,4 +264,4 @@ const Aside = ({
   )
 }
 
-export default Aside
+export default ListingFormActions
