3ccf99d1b110175ea08325aa4fead4a8af163c33
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2023-08-31 10:53:50 -0600

    test: add listings approval cypress tests (#3612)

--- sites/partners/src/components/listings/helpers.tsx
+++ sites/partners/src/components/listings/helpers.tsx
@@ -7,31 +7,31 @@ export const getListingStatusTag = (listingStatus: ListingStatus) => {
   switch (listingStatus) {
     case ListingStatus.active:
       return (
-        <Tag variant={"success-inverse"} size={"lg"}>
+        <Tag variant={"success-inverse"} size={"lg"} id={"listing-status-active"}>
           {t(`listings.listingStatus.active`)}
         </Tag>
       )
     case ListingStatus.closed:
       return (
-        <Tag variant={"secondary-inverse"} size={"lg"}>
+        <Tag variant={"secondary-inverse"} size={"lg"} id={"listing-status-closed"}>
           {t(`listings.listingStatus.closed`)}
         </Tag>
       )
     case ListingStatus.pendingReview:
       return (
-        <Tag variant={"in-process"} size={"lg"}>
+        <Tag variant={"in-process"} size={"lg"} id={"listing-status-pending-review"}>
           {t(`applications.pendingReview`)}
         </Tag>
       )
     case ListingStatus.changesRequested:
       return (
-        <Tag variant={"highlight-warm"} size={"lg"}>
+        <Tag variant={"highlight-warm"} size={"lg"} id={"listing-status-changes-requested"}>
           {t(`listings.listingStatus.changesRequested`)}
         </Tag>
       )
     default:
       return (
-        <Tag variant={"primary"} size={"lg"}>
+        <Tag variant={"primary"} size={"lg"} id={"listing-status-pending"}>
           {t(`listings.listingStatus.pending`)}
         </Tag>
       )
