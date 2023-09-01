4d2748e7b0e68406267f5f3126e3bfc37881bb29
Yazeed Loonat <YazeedLoonat@gmail.com>
2023-08-31 17:22:23 -0700

    feat: custom exception filter (#3606)

--- backend_new/src/main.ts
+++ backend_new/src/main.ts
@@ -1,9 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './modules/app.module';
+import { CustomExceptionFilter } from './utilities/custom-exception-filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger:
+      process.env.NODE_ENV === 'development'
+        ? ['error', 'warn', 'log']
+        : ['error', 'warn'],
+  });
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new CustomExceptionFilter(httpAdapter));
   const config = new DocumentBuilder()
     .setTitle('Bloom API')
     .setDescription('The API for Bloom')
