5dd9379124cdaee91ca212de278dfda142109d23
Morgan Ludtke <ludtkemorgan@gmail.com>
2023-10-02 13:04:57 -0500

    fix: update rental assistance seed data

--- backend_new/prisma/seed-staging.ts
+++ backend_new/prisma/seed-staging.ts
@@ -142,7 +142,7 @@ export const stagingSeed = async (
         postmarkedApplicationsReceivedByDate: null,
         programRules: null,
         rentalAssistance:
-          'Housing Choice Vouchers, Section 8 and other valid rental assistance programs will be considered for this property. In the case of a',
+          'Housing Choice Vouchers, Section 8 and other valid rental assistance programs will be considered for this property. In the case of a valid rental subsidy, the required minimum income will be based on the portion of the rent that the tenant pays after use of the subsidy.',
         rentalHistory: null,
         requiredDocuments: null,
         specialNotes: null,
@@ -268,7 +268,7 @@ export const stagingSeed = async (
         postmarkedApplicationsReceivedByDate: null,
         programRules: null,
         rentalAssistance:
-          'Housing Choice Vouchers, Section 8 and other valid rental assistance programs will be considered for this property. In the case of a',
+          'Housing Choice Vouchers, Section 8 and other valid rental assistance programs will be considered for this property. In the case of a valid rental subsidy, the required minimum income will be based on the portion of the rent that the tenant pays after use of the subsidy.',
         rentalHistory: null,
         requiredDocuments: null,
         specialNotes: null,
@@ -505,7 +505,7 @@ export const stagingSeed = async (
         postmarkedApplicationsReceivedByDate: null,
         programRules: null,
         rentalAssistance:
-          'Housing Choice Vouchers, Section 8 and other valid rental assistance programs will be considered for this property. In the case of a',
+          'Housing Choice Vouchers, Section 8 and other valid rental assistance programs will be considered for this property. In the case of a valid rental subsidy, the required minimum income will be based on the portion of the rent that the tenant pays after use of the subsidy.',
         rentalHistory: null,
         requiredDocuments: null,
         specialNotes: null,
@@ -610,7 +610,7 @@ export const stagingSeed = async (
         postmarkedApplicationsReceivedByDate: null,
         programRules: null,
         rentalAssistance:
-          'Housing Choice Vouchers, Section 8 and other valid rental assistance programs will be considered for this property. In the case of a',
+          'Housing Choice Vouchers, Section 8 and other valid rental assistance programs will be considered for this property. In the case of a valid rental subsidy, the required minimum income will be based on the portion of the rent that the tenant pays after use of the subsidy.',
         rentalHistory: null,
         requiredDocuments: null,
         specialNotes: null,
@@ -695,7 +695,7 @@ export const stagingSeed = async (
         postmarkedApplicationsReceivedByDate: null,
         programRules: null,
         rentalAssistance:
-          'Housing Choice Vouchers, Section 8 and other valid rental assistance programs will be considered for this property. In the case of a',
+          'Housing Choice Vouchers, Section 8 and other valid rental assistance programs will be considered for this property. In the case of a valid rental subsidy, the required minimum income will be based on the portion of the rent that the tenant pays after use of the subsidy.',
         rentalHistory: null,
         requiredDocuments: 'Please bring proof of income and a recent paystub.',
         specialNotes: null,
