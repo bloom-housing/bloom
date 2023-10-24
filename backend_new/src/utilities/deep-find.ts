dad6125118bbe3e8919c74c54c5ad5b5ae76b5ab
Yazeed Loonat <yazeedloonat@gmail.com>
2023-10-24 09:53:59 -0700

    fix: reworking for loop

--- backend_new/src/utilities/deep-find.ts
+++ backend_new/src/utilities/deep-find.ts
@@ -1,12 +1,11 @@
 export function deepFind(obj: Record<string, any>, path: string) {
   const paths = path.split('.');
   let current = obj;
-
-  paths.forEach((currPath) => {
-    if (current[currPath] == undefined) {
+  for (const currPath of paths) {
+    if (current[currPath] === undefined) {
       return undefined;
     }
     current = current[currPath];
-  });
+  }
   return current;
 }
