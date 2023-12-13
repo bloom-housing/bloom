97edcb2f7fd0126924f45ae69e7af54bf877e507
Cade Wolcott <cade.wolcott@exygy.com>
2023-12-13 15:50:47 -0600

    fix: add feature flag to bloom public next config

--- sites/public/next.config.js
+++ sites/public/next.config.js
@@ -40,6 +40,7 @@ module.exports = withBundleAnalyzer({
     jurisdictionName: process.env.JURISDICTION_NAME,
     cacheRevalidate: process.env.CACHE_REVALIDATE ? Number(process.env.CACHE_REVALIDATE) : 60,
     cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    showMandatedAccounts: process.env.SHOW_MANDATED_ACCOUNTS === "TRUE",
   },
   i18n: {
     locales: process.env.LANGUAGES ? process.env.LANGUAGES.split(",") : ["en"],
