87ddff02f37c77f3aa49a59a25d118aa21c70cf2
Cade Wolcott <cade.wolcott@exygy.com>
2024-01-10 11:07:35 -0600

    feat: add sign-in copy for mandated accounts

--- sites/public/src/pages/sign-in.tsx
+++ sites/public/src/pages/sign-in.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useRef, useState, useCallback } from "react"
 import { useForm } from "react-hook-form"
-import { t, setSiteAlertMessage, useMutate } from "@bloom-housing/ui-components"
+import { t, setSiteAlertMessage, useMutate, FormSignIn } from "@bloom-housing/ui-components"
 import FormsLayout from "../layouts/forms"
 import { useRedirectToPrevPage } from "../lib/hooks"
 import {
@@ -10,11 +10,12 @@ import {
   NetworkStatusType,
   NetworkStatusContent,
   AuthContext,
-  FormSignIn,
   ResendConfirmationModal,
 } from "@bloom-housing/shared-helpers"
 import { UserStatus } from "../lib/constants"
 import { EnumUserErrorExtraModelUserErrorMessages } from "@bloom-housing/backend-core/types"
+import { faStopwatch, faEye, faLock } from "@fortawesome/free-solid-svg-icons"
+import { Icon, HeadingGroup } from "@bloom-housing/ui-seeds"
 
 const SignIn = () => {
   const { login, userService } = useContext(AuthContext)
@@ -126,23 +127,51 @@ const SignIn = () => {
     }
   }, [networkError])
 
+  const iconListItems = [
+    { icon: faStopwatch, text: "Apply faster with saved application details" },
+    { icon: faEye, text: "Check on the status of an application at any time" },
+    { icon: faLock, text: "Simply reset your password if you forget it" },
+  ]
   return (
     <>
       <FormsLayout>
-        <FormSignIn
-          onSubmit={(data) => void onSubmit(data)}
-          control={{ register, errors, handleSubmit, watch }}
-          networkStatus={{
-            content: networkStatusContent,
-            type: networkStatusType,
-            reset: () => {
-              reset()
-              resetNetworkError()
-              setConfirmationStatusMessage(undefined)
-            },
-          }}
-          showRegisterBtn={true}
-        />
+        <div className="flex flex-row w-max">
+          <FormSignIn
+            onSubmit={(data) => void onSubmit(data)}
+            control={{ register, errors, handleSubmit, watch }}
+            networkStatus={{
+              content: networkStatusContent,
+              type: networkStatusType,
+              reset: () => {
+                reset()
+                resetNetworkError()
+                setConfirmationStatusMessage(undefined)
+              },
+            }}
+            showRegisterBtn={true}
+          />
+          <div className="w-min mt-6 ml-6">
+            <HeadingGroup
+              heading={"Sign up quickly and check application status at anytime"}
+              subheading={
+                "Having an account will save you time by using saved application details, and allow you to check the status of an application at anytime."
+              }
+              size="xl"
+            />
+            <ul className="flex flex-col w-min">
+              {iconListItems.map((item) => (
+                <li className="flex flex-row w-max mb-2 items-center">
+                  <Icon
+                    icon={item.icon}
+                    size="lg"
+                    className="border border-white bg-white rounded-full p-1.5 w-max"
+                  />
+                  <p className="ml-2">{item.text}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </FormsLayout>
 
       <ResendConfirmationModal
