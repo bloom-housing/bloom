3f0b24c9e107344e14dd2a0de795daeb62b0c137
cliu02 <32694266+cliu02@users.noreply.github.com>
2024-01-17 15:45:27 -0800

    feat: add signin styling

--- shared-helpers/src/views/sign-in/FormSignIn.tsx
+++ shared-helpers/src/views/sign-in/FormSignIn.tsx
@@ -1,9 +1,10 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { Field, Form, FormCard, Icon, NavigationContext, t } from "@bloom-housing/ui-components"
-import { Button } from "@bloom-housing/ui-seeds"
+import { Button, Heading } from "@bloom-housing/ui-seeds"
 import { FormSignInErrorBox } from "./FormSignInErrorBox"
 import { NetworkStatus } from "../../auth/catchNetworkError"
 import type { UseFormMethods } from "react-hook-form"
+import styles from "../../../../sites/public/styles/sign-in.module.scss"
 
 export type FormSignInProps = {
   control: FormSignInControl
@@ -34,64 +35,148 @@ const FormSignIn = ({
     window.scrollTo(0, 0)
   }
   const { LinkComponent } = useContext(NavigationContext)
-
+  const [passwordShown, setPasswordShown] = useState(false)
   return (
     <FormCard>
-      <div className="form-card__lead text-center">
-        <Icon size="2xl" symbol="profile" />
-        <h1 className="form-card__title">{t(`nav.signIn`)}</h1>
-      </div>
-      <FormSignInErrorBox
-        errors={errors}
-        networkStatus={networkStatus}
-        errorMessageId={"main-sign-in"}
-      />
-      <div className="form-card__group pt-0">
-        <Form id="sign-in" className="mt-10" onSubmit={handleSubmit(onSubmit, onError)}>
-          <Field
-            caps={true}
-            name="email"
-            label={t("t.email")}
-            validation={{ required: true }}
-            error={errors.email}
-            errorMessage={t("authentication.signIn.enterLoginEmail")}
-            register={register}
-            dataTestId="sign-in-email-field"
-          />
-
-          <aside className="float-right text-sm font-semibold">
-            <LinkComponent href="/forgot-password">
-              {t("authentication.signIn.forgotPassword")}
-            </LinkComponent>
-          </aside>
-
-          <Field
-            caps={true}
-            name="password"
-            label={t("authentication.createAccount.password")}
-            validation={{ required: true }}
-            error={errors.password}
-            errorMessage={t("authentication.signIn.enterLoginPassword")}
-            register={register}
-            type="password"
-            dataTestId="sign-in-password-field"
-          />
-
-          <div className="text-center mt-6">
-            <Button type="submit" variant="primary" id="sign-in-button">
-              {t("nav.signIn")}
-            </Button>
+      {process.env.showMandatedAccounts ? (
+        <>
+          <div className="form-card__lead ml-16 space-y-3">
+            <Icon size="2xl" symbol="profile" />
+            <Heading size="3xl" className="font-semibold">
+              {t("nav.signInLowercase")}
+            </Heading>
           </div>
-        </Form>
-      </div>
-      {showRegisterBtn && (
-        <div className="form-card__group text-center border-t">
-          <h2 className="mb-6">{t("authentication.createAccount.noAccount")}</h2>
+          <FormSignInErrorBox
+            errors={errors}
+            networkStatus={networkStatus}
+            errorMessageId={"main-sign-in"}
+          />
+          <div className="form-card__group">
+            <Form id="sign-in" onSubmit={handleSubmit(onSubmit, onError)}>
+              <Field
+                caps={true}
+                name="email"
+                label={t("t.email")}
+                labelClassName="font-semibold"
+                validation={{ required: true }}
+                error={errors.email}
+                errorMessage={t("authentication.signIn.enterLoginEmail")}
+                register={register}
+                dataTestId="sign-in-email-field"
+              />
 
-          <Button variant="primary-outlined" href="/create-account">
-            {t("account.createAccount")}
-          </Button>
-        </div>
+              <aside>
+                <LinkComponent href="/forgot-password" className={styles["forgot-password"]}>
+                  {t("authentication.signIn.forgotPassword")}
+                </LinkComponent>
+              </aside>
+
+              <Field
+                caps={true}
+                name="password"
+                label={t("authentication.createAccount.password")}
+                labelClassName="font-semibold"
+                validation={{ required: true }}
+                error={errors.password}
+                errorMessage={t("authentication.signIn.enterLoginPassword")}
+                register={register}
+                type={passwordShown ? "text" : "password"}
+                dataTestId="sign-in-password-field"
+                inputProps={{
+                  onChange: () => {
+                    setPasswordShown(!passwordShown)
+                  },
+                }}
+              />
+              <Field
+                type="checkbox"
+                name="showPassword"
+                label={t("authentication.signIn.showPassword")}
+                register={register}
+                inputProps={{
+                  onChange: () => {
+                    setPasswordShown(!passwordShown)
+                  },
+                }}
+              />
+              <div className="mt-6">
+                <Button type="submit" variant="primary" id="sign-in-button">
+                  {t("nav.signInLowercase")}
+                </Button>
+              </div>
+            </Form>
+          </div>
+          {showRegisterBtn && (
+            <div className="form-card__group border-t">
+              <Heading size="3xl" className="font-semibold mb-6">
+                {t("authentication.createAccount.noAccount")}
+              </Heading>
+
+              <Button variant="primary-outlined" href="/create-account">
+                {t("account.createAccount")}
+              </Button>
+            </div>
+          )}
+        </>
+      ) : (
+        <>
+          <div className="form-card__lead text-center">
+            <Icon size="2xl" symbol="profile" />
+            <h1 className="form-card__title">{t(`nav.signIn`)}</h1>
+          </div>
+          <FormSignInErrorBox
+            errors={errors}
+            networkStatus={networkStatus}
+            errorMessageId={"main-sign-in"}
+          />
+          <div className="form-card__group pt-0">
+            <Form id="sign-in" className="mt-10" onSubmit={handleSubmit(onSubmit, onError)}>
+              <Field
+                caps={true}
+                name="email"
+                label={t("t.email")}
+                validation={{ required: true }}
+                error={errors.email}
+                errorMessage={t("authentication.signIn.enterLoginEmail")}
+                register={register}
+                dataTestId="sign-in-email-field"
+              />
+
+              <aside className="float-right text-sm font-semibold">
+                <LinkComponent href="/forgot-password">
+                  {t("authentication.signIn.forgotPassword")}
+                </LinkComponent>
+              </aside>
+
+              <Field
+                caps={true}
+                name="password"
+                label={t("authentication.createAccount.password")}
+                validation={{ required: true }}
+                error={errors.password}
+                errorMessage={t("authentication.signIn.enterLoginPassword")}
+                register={register}
+                type="password"
+                dataTestId="sign-in-password-field"
+              />
+
+              <div className="text-center mt-6">
+                <Button type="submit" variant="primary" id="sign-in-button">
+                  {t("nav.signIn")}
+                </Button>
+              </div>
+            </Form>
+          </div>
+          {showRegisterBtn && (
+            <div className="form-card__group text-center border-t">
+              <h2 className="mb-6">{t("authentication.createAccount.noAccount")}</h2>
+
+              <Button variant="primary-outlined" href="/create-account">
+                {t("account.createAccount")}
+              </Button>
+            </div>
+          )}
+        </>
       )}
     </FormCard>
   )
