772efb3a95d64d09a09798bc9e70c7b8cd2b6651
ColinBuyck <colin.buyck@gmail.com>
2024-01-18 07:01:03 -0800

    fix: css matched app view

--- sites/public/src/components/account/StatusItem.tsx
+++ sites/public/src/components/account/StatusItem.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { t } from "@bloom-housing/ui-components"
-import { Button, Card } from "@bloom-housing/ui-seeds"
+import { Button, Card, Tag } from "@bloom-housing/ui-seeds"
 import styles from "./StatusItem.module.scss"
 import accountStyles from "../../pages/account/account.module.scss"
 
@@ -24,14 +24,18 @@ interface StatusItemProps {
 
 const StatusItem = (props: StatusItemProps) => {
   return (
-    <Card.Section className={accountStyles["account-card-section"]}>
+    <Card.Section
+      className={`${accountStyles["account-card-section"]} ${accountStyles["application-card-section"]}`}
+    >
       <article className={styles["status-item"]}>
         <header className={styles["status-item__header"]}>
           <h3 className={styles["status-item__title"]}>{props.listingName}</h3>
           <p className={styles["status-item__status"]}>
-            <span className={styles["status-item__label"]}>
+            <span className={styles["status-item-label"]}>
               {props.strings?.status ?? t("application.status")}:{" "}
-              {props.strings?.submittedStatus ?? t("application.statuses.submitted")}
+              <Tag variant="primary">
+                {props.strings?.submittedStatus ?? t("application.statuses.submitted")}
+              </Tag>
             </span>
           </p>
         </header>
@@ -62,14 +66,12 @@ const StatusItem = (props: StatusItemProps) => {
         </section>
 
         <footer className={styles["status-item__footer"]}>
-          <div className={styles["status-item_links"]}>
-            <Button href={props.applicationURL} size="sm">
-              {props.strings?.viewApplication ?? t("application.viewApplication")}
-            </Button>
-            <Button href={props.listingURL} size="sm">
-              {props.strings?.seeListing ?? t("t.seeListing")}
-            </Button>
-          </div>
+          <Button href={props.applicationURL} variant="primary-outlined" size="sm">
+            {props.strings?.viewApplication ?? t("application.viewYourApplication")}
+          </Button>
+          <Button href={props.listingURL} variant="primary-outlined" size="sm">
+            {props.strings?.seeListing ?? t("t.viewListing")}
+          </Button>
         </footer>
       </article>
     </Card.Section>
