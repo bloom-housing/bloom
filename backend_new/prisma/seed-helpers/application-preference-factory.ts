a848cfaa623b4f11521e4309ca4d6993fa533a90
Morgan Ludtke <ludtkemorgan@gmail.com>
2024-01-18 16:57:32 -0600

    fix: preferences fix

--- backend_new/prisma/seed-helpers/application-preference-factory.ts
+++ backend_new/prisma/seed-helpers/application-preference-factory.ts
@@ -6,32 +6,30 @@ import { InputType } from '../../src/enums/shared/input-type-enum';
 export const preferenceFactory = (
   multiselectQuestions: Partial<MultiselectQuestions>[],
 ): Prisma.InputJsonValue => {
-  return JSON.stringify(
-    multiselectQuestions.map((question) => ({
-      multiselectQuestionId: question.id,
-      key: question.text,
-      claimed: randomBoolean(),
-      options: JSON.parse(JSON.stringify(question.options)).map((option) => {
-        return {
-          key: option.key,
-          checked: randomBoolean(),
-          extraData: option.collectAddress
-            ? [
-                {
-                  key: 'Address',
-                  type: InputType.address,
-                  value: {
-                    city: randomNoun(),
-                    state: randomNoun(),
-                    street: '123 4th St',
-                    street2: 'Apt 5',
-                    zipCode: '67890',
-                  },
+  return multiselectQuestions.map((question) => ({
+    multiselectQuestionId: question.id,
+    key: question.text,
+    claimed: randomBoolean(),
+    options: JSON.parse(JSON.stringify(question.options)).map((option) => {
+      return {
+        key: option.key,
+        checked: randomBoolean(),
+        extraData: option.collectAddress
+          ? [
+              {
+                key: 'Address',
+                type: InputType.address,
+                value: {
+                  city: randomNoun(),
+                  state: randomNoun(),
+                  street: '123 4th St',
+                  street2: 'Apt 5',
+                  zipCode: '67890',
                 },
-              ]
-            : [],
-        };
-      }),
-    })),
-  );
+              },
+            ]
+          : [],
+      };
+    }),
+  }));
 };
