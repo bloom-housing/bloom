b949caf5dbc344fbb63f604e7e2227cbc3a99b6c
Morgan Ludtke <ludtkemorgan@gmail.com>
2024-01-19 20:33:51 -0600

    fix: add initial heap setup

--- sites/partners/src/pages/_app.tsx
+++ sites/partners/src/pages/_app.tsx
@@ -19,6 +19,17 @@ import "../../styles/overrides.scss"
 
 const signInMessage = "Login is required to view this page."
 
+export const heapScript = () => {
+  const heapKey = process.env.heapKey
+  if (heapKey) {
+    const script = document.createElement("script")
+    script.innerHTML = `
+    window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+    heap.load("${heapKey}");`
+    return script
+  } else return null
+}
+
 function BloomApp({ Component, router, pageProps }: AppProps) {
   const { locale } = router
   const skipLoginRoutes = ["/forgot-password", "/reset-password", "/users/confirm", "/users/terms"]
@@ -26,6 +37,10 @@ function BloomApp({ Component, router, pageProps }: AppProps) {
   // fix for rehydration
   const [hasMounted, setHasMounted] = useState(false)
   useEffect(() => {
+    const heapNode = heapScript()
+    if (heapNode) {
+      document.head.append(heapNode)
+    }
     setHasMounted(true)
   }, [])
 
