27c13b6e65b443887b62b2d51b91f1146d6dc254
Morgan Ludtke <42942267+ludtkemorgan@users.noreply.github.com>
2024-01-24 19:47:42 -0600

    fix: add default value for map layer (#3829)
    * fix: add default value for map layer
    
    * fix: remove test clutter

--- backend/core/src/migration/1705637577495-add-feature-collection.ts
+++ backend/core/src/migration/1705637577495-add-feature-collection.ts
@@ -4,7 +4,9 @@ export class addFeatureCollection1705637577495 implements MigrationInterface {
   name = "addFeatureCollection1705637577495"
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "map_layers" ADD "feature_collection" jsonb NOT NULL`)
+    await queryRunner.query(
+      `ALTER TABLE "map_layers" ADD "feature_collection" jsonb NOT NULL default '{}'::jsonb`
+    )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
