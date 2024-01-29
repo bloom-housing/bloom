e50998561c084e9b6e925cbc89150fd16a5c440e
ColinBuyck <53269332+ColinBuyck@users.noreply.github.com>
2024-01-29 14:22:25 -0800

    feat: 422/updated account theming (#3791)
    * fix: rough draft refactoring
    
    * fix: wip seeds card
    
    * fix: wip account updates
    
    * fix: account card styling
    
    * fix: wip status item styling
    
    * fix: edit screen adjustments
    
    * fix: css matched app view
    
    * fix: testing fixes
    
    * fix: getById chnage
    
    * fix: add plural copy fix
    
    * fix: json update
    
    * fix: remove tailwind
    
    * fix: font weight touches
    
    * fix: mantain translations
    
    * fix: reorganizing files
    
    * fix: svg updates
    
    * fix: current pswd translations
    
    * fix: unused css
    
    * fix: width needed
    
    * fix: css and heading updates
    
    * fix: account card updates
    
    * fix: partial new icon approach
    
    * fix: final tweaks
    
    * fix: no border figma matching

---
+++ sites/public/src/components/account/AccountCard.tsx
@@ -0,0 +1,49 @@
+import { Heading, HeadingGroup, Icon } from "@bloom-housing/ui-seeds"
+import Card from "@bloom-housing/ui-seeds/src/blocks/Card"
+import React from "react"
+import styles from "./AccountCard.module.scss"
+import { CustomIconMap, CustomIconType } from "../shared/CustomIconMap"
+
+interface AccountCardProps {
+  iconSymbol: CustomIconType
+  title: string
+  subtitle?: string
+  children: React.ReactElement
+  id?: string
+  divider?: "flush" | "inset"
+  headingPriority?: 1 | 2 | 3 | 4 | 5 | 6
+  className?: string
+}
+
+const AccountCard = (props: AccountCardProps) => {
+  const classNames = [styles["account-card"]]
+  if (props.className) classNames.push(props.className)
+
+  const customIcon = CustomIconMap[props.iconSymbol]
+
+  return (
+    <Card spacing="lg" className={classNames.join(" ")}>
+      <Card.Header divider={props?.divider}>
+        <Icon size="2xl" className={styles["account-card-icon"]}>
+          {customIcon}
+        </Icon>
+        {props.subtitle ? (
+          <HeadingGroup
+            size="2xl"
+            heading={props.title}
+            subheading={props.subtitle}
+            className={styles["account-card-heading-group"]}
+            headingPriority={props.headingPriority}
+          />
+        ) : (
+          <Heading size="2xl" className={styles["account-card-heading"]}>
+            {props.title}
+          </Heading>
+        )}
+      </Card.Header>
+      {props.children}
+    </Card>
+  )
+}
+
+export { AccountCard as default, AccountCard }
