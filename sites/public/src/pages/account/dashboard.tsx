f0d2561502202bdb9ddbb80b6d114c4e8227b870
ColinBuyck <colin.buyck@gmail.com>
2024-01-29 12:51:29 -0800

    fix: final tweaks

--- sites/public/src/pages/account/dashboard.tsx
+++ sites/public/src/pages/account/dashboard.tsx
@@ -53,7 +53,7 @@ function Dashboard(props: DashboardProps) {
           </AlertBox>
         )}
         <section className="bg-gray-300 border-t border-gray-450">
-          <div className="max-w-5xl mx-auto md:py-8">
+          <div className="max-w-5xl mx-auto sm:py-8">
             <SiteAlert type="success" className="md:mb-8" timeout={30000} />
             <h1 className={"sr-only"}>{t("nav.myDashboard")}</h1>
             <Grid spacing="lg" className={styles["account-card-container"]}>
