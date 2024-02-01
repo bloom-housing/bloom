8a6e4275409c5016284475816851b0c2828afc60
Cade Wolcott <cade.wolcott@exygy.com>
2024-02-01 16:19:29 -0600

    refactor: add helper

---
+++ backend/core/src/shared/utils/get-public-email-url.ts
@@ -0,0 +1,26 @@
+/**
+ * Creates a email URL object from passed url applies redirectUrl and listingId query params if they exist
+ * If they do not exist, the value will be return the email url with just the necessary token
+ */
+
+export const getPublicEmailURL = (url: string, token: string): string => {
+  console.log("URL ", url)
+  console.log("token ", token)
+
+  const urlObj = new URL(url)
+
+  const redirectUrl = urlObj.searchParams.get("redirectUrl")
+  const listingId = urlObj.searchParams.get("listingId")
+
+  console.log("Redirect ", redirectUrl)
+
+  console.log("Listing Id ", listingId)
+
+  let emailUrl = `${urlObj.origin}${urlObj.pathname}?token=${token}`
+  if (!!redirectUrl && !!listingId) {
+    emailUrl = emailUrl.concat(`&redirectUrl=${redirectUrl}&listingId=${listingId}`)
+  }
+  console.log("emailUrl ", emailUrl)
+
+  return emailUrl
+}
