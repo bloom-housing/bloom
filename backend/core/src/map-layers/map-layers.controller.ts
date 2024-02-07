c15f0c558e401b4572ca0ccde4008dc8ac4c23c4
Cade Wolcott <cade.wolcott@exygy.com>
2024-02-07 16:11:36 -0600

    Revert "Merge branch 'main' into 3759/ma-creation-styles"
    This reverts commit f51ff4a75c65d491b2679393cc3cb3734e00ecb4, reversing
    changes made to ffce74d371cb735dcb2bf142aed68ff099fe0d81.

--- backend/core/src/map-layers/map-layers.controller.ts
+++ backend/core/src/map-layers/map-layers.controller.ts
@@ -3,17 +3,17 @@ import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger"
 import { MapLayersService } from "./map-layers.service"
 import { ResourceType } from "../auth/decorators/resource-type.decorator"
 import { OptionalAuthGuard } from "../auth/guards/optional-auth.guard"
+import { AuthzGuard } from "../auth/guards/authz.guard"
 import { defaultValidationPipeOptions } from "../shared/default-validation-pipe-options"
 import { mapTo } from "../shared/mapTo"
 import { MapLayerDto } from "./dto/map-layer.dto"
 import { MapLayersQueryParams } from "./dto/map-layers-query-params"
-import { AdminOrJurisdictionalAdminGuard } from "../auth/guards/admin-or-jurisidictional-admin.guard"
 
 @Controller("/mapLayers")
 @ApiTags("mapLayers")
 @ApiBearerAuth()
 @ResourceType("mapLayer")
-@UseGuards(OptionalAuthGuard, AdminOrJurisdictionalAdminGuard)
+@UseGuards(OptionalAuthGuard, AuthzGuard)
 @UsePipes(new ValidationPipe(defaultValidationPipeOptions))
 export class MapLayersController {
   constructor(private readonly mapLayerService: MapLayersService) {}
