f68168b11549c636798704c43423791cc794f23f
cliu02 <32694266+cliu02@users.noreply.github.com>
2024-02-08 15:18:21 -0800

    fix: add env param

--- sites/public/src/lib/hooks.ts
+++ sites/public/src/lib/hooks.ts
@@ -33,7 +33,7 @@ export const useRedirectToPrevPage = (defaultPath = "/") => {
 }
 
 export const useFormConductor = (stepName: string) => {
-  useRequireLoggedInUser("/sign-in", true)
+  useRequireLoggedInUser("/sign-in", !process.env.showMandatedAccounts)
   const context = useContext(AppSubmissionContext)
   const conductor = context.conductor
 
