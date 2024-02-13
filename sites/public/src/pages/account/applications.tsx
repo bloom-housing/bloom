0c1db5bb46c8073385e5e4442572075f5d2ae8f2
ColinBuyck <colin.buyck@gmail.com>
2024-02-12 22:57:53 -0800

    fix: filtering logic

--- sites/public/src/pages/account/applications.tsx
+++ sites/public/src/pages/account/applications.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, Fragment, useContext } from "react"
 import Head from "next/head"
-import { t, LoadingOverlay } from "@bloom-housing/ui-components"
+import { t, LoadingOverlay, SideNav } from "@bloom-housing/ui-components"
 import { Button, Card, Heading } from "@bloom-housing/ui-seeds"
 import { PageView, pushGtmEvent, AuthContext, RequireLogin } from "@bloom-housing/shared-helpers"
 import Layout from "../../layouts/application"
@@ -10,10 +10,22 @@ import { UserStatus } from "../../lib/constants"
 import { AccountCard } from "@bloom-housing/shared-helpers/src/views/accounts/AccountCard"
 
 import styles from "../../pages/account/account.module.scss"
+import dayjs from "dayjs"
+import { ListingsStatusEnum } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
+
+export enum filterEnum {
+  "all",
+  "closed",
+  "open",
+}
 
 const Applications = () => {
   const { applicationsService, listingsService, profile } = useContext(AuthContext)
   const [applications, setApplications] = useState<AppWithListing[]>()
+  const [displayApplications, setDisplayApplications] = useState<AppWithListing[]>()
+  const [openApplications, setOpenApplications] = useState<AppWithListing[]>()
+  const [closedApplications, setClosedApplications] = useState<AppWithListing[]>()
+  const [applicationFilter, setApplicationFilter] = useState<filterEnum>(filterEnum.all)
   const [loading, setLoading] = useState(true)
   const [listLoading, setListLoading] = useState(true)
   const [error, setError] = useState()
@@ -51,6 +63,24 @@ const Applications = () => {
       .then((res) => {
         setListLoading(false)
         setApplications(res)
+        // separate out open and close applications for effecient filtering
+        const openApps: AppWithListing[] = []
+        const closedApps: AppWithListing[] = []
+        res.forEach((application) => {
+          const listing = application.fullListing
+          if (
+            (listing.status === ListingsStatusEnum.active &&
+              (dayjs(new Date()).isBefore(dayjs(listing.applicationDueDate)) ||
+                !listing.applicationDueDate)) ||
+            listing?.waitlistOpenSpots
+          )
+            openApps.push(application)
+          else {
+            closedApps.push(application)
+          }
+        })
+        setOpenApplications(openApps)
+        setClosedApplications(closedApps)
         setLoading(false)
       })
       .catch((err) => {
@@ -59,6 +89,22 @@ const Applications = () => {
         setLoading(false)
       })
   }, [applications, listLoading, listingsService])
+
+  useEffect(() => {
+    if (applications) {
+      switch (applicationFilter) {
+        case filterEnum.all:
+          setDisplayApplications(applications)
+          break
+        case filterEnum.open:
+          setDisplayApplications(openApplications)
+          break
+        case filterEnum.closed:
+          setDisplayApplications(closedApplications)
+          break
+      }
+    }
+  }, [applicationFilter, applications, closedApplications, openApplications])
 
   const noApplicationsSection = () => {
     return (
@@ -80,6 +126,29 @@ const Applications = () => {
       </Card.Section>
     )
   }
+  const items = [
+    {
+      label: t("application.statuses.allApplications"),
+      onClick: () => setApplicationFilter(filterEnum.all),
+      url: "#",
+      count: applications?.length,
+      current: applicationFilter === filterEnum.all,
+    },
+    {
+      label: t("application.statuses.openApplications"),
+      onClick: () => setApplicationFilter(filterEnum.open),
+      url: "#",
+      count: openApplications?.length,
+      current: applicationFilter === filterEnum.open,
+    },
+    {
+      label: t("application.statuses.closedApplications"),
+      onClick: () => setApplicationFilter(filterEnum.closed),
+      url: "#",
+      count: closedApplications?.length,
+      current: applicationFilter === filterEnum.closed,
+    },
+  ]
   return (
     <RequireLogin signInPath="/sign-in" signInMessage={t("t.loginIsRequired")}>
       <Layout>
@@ -88,7 +157,18 @@ const Applications = () => {
         </Head>
         <MetaTags title={t("account.myApplications")} description="" />
         <section className="bg-gray-300 border-t border-gray-450">
-          <div className="flex flex-wrap relative max-w-3xl mx-auto md:py-8">
+          <div className="flex flex-row relative max-w-5xl mx-auto md:py-8 px-6">
+            <>
+              <div className={"hidden pr-8 md:block"}>
+                <SideNav navItems={items} />
+              </div>
+              <div className={"block md:hidden mb-4 w-full sm:w-auto"}>
+                <SideNav
+                  className={`w-full md:w-72 side-nav__horizontal focus:outline-none`}
+                  navItems={items}
+                />
+              </div>
+            </>
             <AccountCard
               iconSymbol="application"
               title={t("account.myApplications")}
@@ -100,7 +180,7 @@ const Applications = () => {
               <>
                 <LoadingOverlay isLoading={loading}>
                   <Fragment>
-                    {applications?.map((application, index) => {
+                    {displayApplications?.map((application, index) => {
                       return <StatusItemWrapper key={index} application={application} />
                     })}
                   </Fragment>
