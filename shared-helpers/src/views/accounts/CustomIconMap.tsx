42a7df16f91302b7450f79741506c7352e9401ec
cliu02 <32694266+cliu02@users.noreply.github.com>
2024-02-23 15:48:31 -0800

    feat: partners cog icon

--- shared-helpers/src/views/accounts/CustomIconMap.tsx
+++ shared-helpers/src/views/accounts/CustomIconMap.tsx
@@ -1,8 +1,9 @@
-import { Application, Profile } from "./CustomIcons"
+import { Application, Profile, Cog } from "./CustomIcons"
 
 export const CustomIconMap = {
   application: Application,
   profile: Profile,
+  cog: Cog,
 }
 
 export type CustomIconType = keyof typeof CustomIconMap
