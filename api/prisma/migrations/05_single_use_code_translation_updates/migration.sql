689689bcc880bd801c0899f674fbe5b7b4af548d
Yazeed Loonat <yazeedloonat@gmail.com>
2024-03-05 15:33:57 -0700

    fix: updates after convo with eric

--- api/prisma/migrations/05_single_use_code_translation_updates/migration.sql
+++ api/prisma/migrations/05_single_use_code_translation_updates/migration.sql
@@ -4,3 +4,7 @@ UPDATE translations
 SET translations = jsonb_set(translations, '{singleUseCodeEmail}', '{"greeting": "Hi","message": "Use the following code to sign in to your %{jurisdictionName} account. This code will be valid for 5 minutes. Never share this code.","singleUseCode": "%{singleUseCode}"}')
 WHERE jurisdiction_id IS NULL
     and language = 'en';
+
+UPDATE translations
+    SET translations = jsonb_set(translations, '{mfaCodeEmail, mfaCode}', '{"mfaCode": "Your access code is: %{singleUseCode}"}')
+WHERE language = 'en';
