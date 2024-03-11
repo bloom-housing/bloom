13cf2b98dbefcaee2eaeb3c2f5e3a128f662949d
emilyjablonski <emijablonski@gmail.com>
2024-03-11 16:51:46 -0600

    feat: passwordless frontend

---
+++ shared-helpers/src/views/sign-in/FormSignInPwdless.tsx
@@ -0,0 +1,91 @@
+import React, { useContext, useState } from "react"
+import { useRouter } from "next/router"
+import type { UseFormMethods } from "react-hook-form"
+import { Field, Form, NavigationContext, t } from "@bloom-housing/ui-components"
+import { Button } from "@bloom-housing/ui-seeds"
+import { getListingRedirectUrl } from "../../utilities/getListingRedirectUrl"
+import styles from "./FormSignIn.module.scss"
+
+export type FormSignInPwdlessProps = {
+  control: FormSignInPwdlessControl
+  onSubmit: (data: FormSignInPwdlessValues) => void
+}
+
+export type FormSignInPwdlessValues = {
+  email: string
+  password: string
+}
+
+export type FormSignInPwdlessControl = {
+  errors: UseFormMethods["errors"]
+  handleSubmit: UseFormMethods["handleSubmit"]
+  register: UseFormMethods["register"]
+}
+
+const FormSignInPwdless = ({
+  onSubmit,
+  control: { errors, register, handleSubmit },
+}: FormSignInPwdlessProps) => {
+  const onError = () => {
+    window.scrollTo(0, 0)
+  }
+  const { LinkComponent } = useContext(NavigationContext)
+  const router = useRouter()
+  const listingIdRedirect = router.query?.listingId as string
+  const forgetPasswordURL = getListingRedirectUrl(listingIdRedirect, "/forgot-password")
+
+  const [useCode, setUseCode] = useState(true)
+
+  return (
+    <Form id="sign-in" onSubmit={handleSubmit(onSubmit, onError)}>
+      <Field
+        className="mb-6"
+        name="email"
+        label={t("t.email")}
+        labelClassName="text__caps-spaced pb-0"
+        validation={{ required: true }}
+        error={errors.email}
+        errorMessage={t("authentication.signIn.enterLoginEmail")}
+        register={register}
+        dataTestId="sign-in-email-field"
+        note={useCode ? t("authentication.signIn.pwdless.emailHelperText") : ""}
+      />
+
+      {!useCode && (
+        <>
+          <aside>
+            <LinkComponent href={forgetPasswordURL} className={styles["forgot-password"]}>
+              {t("authentication.signIn.forgotPassword")}
+            </LinkComponent>
+          </aside>
+          <Field
+            className="mb-3"
+            name="password"
+            label={t("authentication.createAccount.password")}
+            labelClassName="text__caps-spaced"
+            validation={{ required: !useCode }}
+            error={errors.password}
+            errorMessage={t("authentication.signIn.enterLoginPassword")}
+            register={register}
+            type={"password"}
+            dataTestId="sign-in-password-field"
+          />
+        </>
+      )}
+      <div className="mt-6">
+        <Button type="submit" variant="primary" id="sign-in-button">
+          {useCode ? t("authentication.signIn.pwdless.getCode") : t("nav.signIn")}
+        </Button>
+      </div>
+      <div className="mt-6">
+        <Button variant={"text"} onClick={() => setUseCode(!useCode)}>
+          {useCode
+            ? t("authentication.signIn.pwdless.usePassword")
+            : t("authentication.signIn.pwdless.useCode")}
+        </Button>
+      </div>
+    </Form>
+  )
+}
+
+export { FormSignInPwdless as default, FormSignInPwdless }
