18df523fd2197c46fe791bc62451ec9dc38ece6e
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2024-03-14 15:06:28 -0600

    fix: update seeds + fix listing delete (#3940)

--- api/test/integration/permission-tests/permission-as-juris-admin-correct-juris.e2e-spec.ts
+++ api/test/integration/permission-tests/permission-as-juris-admin-correct-juris.e2e-spec.ts
@@ -1021,7 +1021,9 @@ describe('Testing Permissioning of endpoints as Jurisdictional Admin in the corr
     });
 
     it('should succeed for delete endpoint & create an activity log entry', async () => {
-      const listingData = await listingFactory(jurisId, prisma);
+      const listingData = await listingFactory(jurisId, prisma, {
+        noImage: true,
+      });
       const listing = await prisma.listings.create({
         data: listingData,
       });
