3877cc16429de0d851618d0775e08d5d00bc66aa
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2024-03-19 12:40:42 -0600

    feat: pwdless sign in flow (#3960)

---
+++ shared-helpers/src/views/sign-in/FormSignInDefault.tsx
@@ -0,0 +1,76 @@
+import React, { useContext } from "react"
+import { useRouter } from "next/router"
+import type { UseFormMethods } from "react-hook-form"
+import { Field, Form, NavigationContext, t } from "@bloom-housing/ui-components"
+import { Button } from "@bloom-housing/ui-seeds"
+import { getListingRedirectUrl } from "../../utilities/getListingRedirectUrl"
+import styles from "./FormSignIn.module.scss"
+
+export type FormSignInDefaultProps = {
+  control: FormSignInDefaultControl
+  onSubmit: (data: FormSignInDefaultValues) => void
+}
+
+export type FormSignInDefaultValues = {
+  email: string
+  password: string
+}
+
+export type FormSignInDefaultControl = {
+  errors: UseFormMethods["errors"]
+  handleSubmit: UseFormMethods["handleSubmit"]
+  register: UseFormMethods["register"]
+}
+
+const FormSignInDefault = ({
+  onSubmit,
+  control: { errors, register, handleSubmit },
+}: FormSignInDefaultProps) => {
+  const onError = () => {
+    window.scrollTo(0, 0)
+  }
+  const { LinkComponent } = useContext(NavigationContext)
+  const router = useRouter()
+  const listingIdRedirect = router.query?.listingId as string
+  const forgetPasswordURL = getListingRedirectUrl(listingIdRedirect, "/forgot-password")
+
+  return (
+    <Form id="sign-in" onSubmit={handleSubmit(onSubmit, onError)}>
+      <Field
+        className={styles["sign-in-email-input"]}
+        name="email"
+        label={t("t.email")}
+        labelClassName="text__caps-spaced"
+        validation={{ required: true }}
+        error={errors.email}
+        errorMessage={t("authentication.signIn.enterLoginEmail")}
+        register={register}
+        dataTestId="sign-in-email-field"
+      />
+      <aside>
+        <LinkComponent href={forgetPasswordURL} className={styles["forgot-password"]}>
+          {t("authentication.signIn.forgotPassword")}
+        </LinkComponent>
+      </aside>
+      <Field
+        className={styles["sign-in-password-input"]}
+        name="password"
+        label={t("authentication.createAccount.password")}
+        labelClassName="text__caps-spaced"
+        validation={{ required: true }}
+        error={errors.password}
+        errorMessage={t("authentication.signIn.enterLoginPassword")}
+        register={register}
+        type={"password"}
+        dataTestId="sign-in-password-field"
+      />
+      <div className={styles["sign-in-action"]}>
+        <Button type="submit" variant="primary" id="sign-in-button">
+          {t("nav.signIn")}
+        </Button>
+      </div>
+    </Form>
+  )
+}
+
+export { FormSignInDefault as default, FormSignInDefault }
