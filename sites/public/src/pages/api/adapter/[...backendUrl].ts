e0d6a833f8cfe8800c921ac0b9ea551c6613b404
Yazeed Loonat <yazeedloonat@gmail.com>
2024-03-20 14:54:35 -0700

    feat: modifying adapters to forward ip properly

--- sites/public/src/pages/api/adapter/[...backendUrl].ts
+++ sites/public/src/pages/api/adapter/[...backendUrl].ts
@@ -13,6 +13,7 @@ import { maskAxiosResponse } from "@bloom-housing/shared-helpers"
 */
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("16:", req)
   const jar = new CookieJar()
   const axios = wrapper(
     axiosStatic.create({
