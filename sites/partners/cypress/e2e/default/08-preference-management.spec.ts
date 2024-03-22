63af90a05d4a3e5a45a306c5ded0fb0c84625d5c
Jared White <jared@jaredwhite.com>
2024-03-21 21:22:29 -0700

    test: fix tests due to new Toasts

--- sites/partners/cypress/e2e/default/08-preference-management.spec.ts
+++ sites/partners/cypress/e2e/default/08-preference-management.spec.ts
@@ -45,7 +45,7 @@ describe("Preference Management Tests", () => {
     cy.getByTestId("preference-opt-out-label").type("Preference Opt Out Label")
     cy.getByTestId("preference-jurisdiction").select("Bloomington")
     cy.getByID("preference-save-button").click()
-    cy.getByTestId("alert-box")
+    cy.getByTestId("toast-alert")
       .contains("Preference Created")
       .should("have.text", "Preference Created")
 
