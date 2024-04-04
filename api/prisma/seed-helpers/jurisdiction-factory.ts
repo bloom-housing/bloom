7bd89d75d3fe91267ccce4ae7e2d3c6968ab64f8
krzysztof ziecina <kziecina@airnauts.com>
2024-04-04 14:28:20 +0200

    fix: geocoding tests

--- api/prisma/seed-helpers/jurisdiction-factory.ts
+++ api/prisma/seed-helpers/jurisdiction-factory.ts
@@ -17,6 +17,6 @@ export const jurisdictionFactory = (
   enableAccessibilityFeatures: true,
   enableUtilitiesIncluded: true,
   enableGeocodingPreferences: true,
-  enableGeocodingRadiusMethod: false,
+  enableGeocodingRadiusMethod: true,
   listingApprovalPermissions: listingApprovalPermissions || [],
 });
