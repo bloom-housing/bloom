959e82694ba6c0b7ac72f74561e1fb8e40738382
Eric McGarry <eric@eric.mcgarry.name>
2024-04-19 14:04:34 -0400

    fix: close mock after tests

--- api/test/unit/utilities/generate-single-use-code.spec.ts
+++ api/test/unit/utilities/generate-single-use-code.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 import { generateSingleUseCode } from '../../../src/utilities/generate-single-use-code';
 import { AppModule } from '../../../src/modules/app.module';
 
@@ -12,8 +13,14 @@ describe('generateSingleUseCode', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.use(cookieParser());
     await app.init();
   });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
   it('should generate mfa code of the specified length', () => {
     expect(generateSingleUseCode().length).toEqual(
       Number(process.env.MFA_CODE_LENGTH),
