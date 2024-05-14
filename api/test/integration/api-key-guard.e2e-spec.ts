7f259d11b1fe6463a8371ba0955f684bd970ff45
Yazeed Loonat <yazeedloonat@gmail.com>
2024-05-13 22:52:57 -0700

    fix: update to test

--- api/test/integration/api-key-guard.e2e-spec.ts
+++ api/test/integration/api-key-guard.e2e-spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
+import cookieParser from 'cookie-parser';
 import { AppModule } from '../../src/modules/app.module';
 import { PrismaService } from '../../src/services/prisma.service';
+import { Login } from '../../src/dtos/auth/login.dto';
+import { userFactory } from '../../prisma/seed-helpers/user-factory';
 
 describe('API Key Guard Tests', () => {
   let app: INestApplication;
   let prisma: PrismaService;
+  let cookies = '';
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,7 +19,26 @@ describe('API Key Guard Tests', () => {
 
     app = moduleFixture.createNestApplication();
     prisma = moduleFixture.get<PrismaService>(PrismaService);
+    app.use(cookieParser());
     await app.init();
+
+    const storedUser = await prisma.userAccounts.create({
+      data: await userFactory({
+        roles: { isAdmin: true },
+        mfaEnabled: false,
+        confirmedAt: new Date(),
+      }),
+    });
+    const resLogIn = await request(app.getHttpServer())
+      .post('/auth/login')
+      .set({ passkey: process.env.API_PASS_KEY || '' })
+      .send({
+        email: storedUser.email,
+        password: 'abcdef',
+      } as Login)
+      .expect(201);
+
+    cookies = resLogIn.headers['set-cookie'];
   });
 
   afterAll(async () => {
@@ -23,17 +46,19 @@ describe('API Key Guard Tests', () => {
     await app.close();
   });
 
-  it('should error but because of unauthorized (got passed the pass key)', async () => {
+  it('should succeed when correct header is present', async () => {
     await request(app.getHttpServer())
       .get('/reservedCommunityTypes')
+      .set('Cookie', cookies)
       .set({ passkey: process.env.API_PASS_KEY || '' })
-      .expect(401);
+      .expect(200);
   });
 
   it('should error when incorrect header is present', async () => {
     const res = await request(app.getHttpServer())
       .get('/reservedCommunityTypes')
       .set({ passkey: 'the wrong key' })
+      .set('Cookie', cookies)
       .expect(401);
     expect(res.body.message).toBe('Traffic not from a known source');
   });
@@ -41,6 +66,7 @@ describe('API Key Guard Tests', () => {
   it('should error when no header is present', async () => {
     const res = await request(app.getHttpServer())
       .get('/reservedCommunityTypes')
+      .set('Cookie', cookies)
       .expect(401);
     expect(res.body.message).toBe('Traffic not from a known source');
   });
