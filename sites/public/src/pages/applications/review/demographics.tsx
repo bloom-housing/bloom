1b4baa708e5b6e07355954b7d3b2723218adf37c
Eric McGarry <46828798+mcgarrye@users.noreply.github.com>
2024-05-31 12:29:24 -0400

    release: Demographics Race Field Render Issue (#657)

--- sites/public/src/pages/applications/review/demographics.tsx
+++ sites/public/src/pages/applications/review/demographics.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useMemo } from "react"
 import { useForm } from "react-hook-form"
 
 import { Field, FieldGroup, Form, Select, t } from "@bloom-housing/ui-components"
@@ -7,6 +7,8 @@ import { MultiselectQuestionsApplicationSectionEnum } from "@bloom-housing/share
 import {
   raceKeys,
   spokenLanguageKeys,
+  isKeyIncluded,
+  getCustomValue,
   howDidYouHear,
   fieldGroupObjectToArray,
   OnClientSide,
@@ -65,6 +67,25 @@ const ApplicationDemographics = () => {
     conductor.routeToNextOrReturnUrl()
   }
 
+  const raceOptions = useMemo(() => {
+    return Object.keys(raceKeys).map((rootKey) => ({
+      id: rootKey,
+      label: t(`application.review.demographics.raceOptions.${rootKey}`),
+      value: rootKey,
+      additionalText: rootKey.indexOf("other") >= 0,
+      defaultChecked: isKeyIncluded(rootKey, application.demographics?.race),
+      defaultText: getCustomValue(rootKey, application.demographics?.race),
+      subFields: raceKeys[rootKey].map((subKey) => ({
+        id: subKey,
+        label: t(`application.review.demographics.raceOptions.${subKey}`),
+        value: subKey,
+        defaultChecked: isKeyIncluded(subKey, application.demographics?.race),
+        additionalText: subKey.indexOf("other") >= 0,
+        defaultText: getCustomValue(subKey, application.demographics?.race),
+      })),
+    }))
+  }, [register])
+
   useEffect(() => {
     pushGtmEvent<PageView>({
       event: "pageView",
@@ -98,26 +119,13 @@ const ApplicationDemographics = () => {
               </legend>
               <FieldGroup
                 name="race"
-                fields={Object.keys(raceKeys).map((rootKey) => ({
-                  id: rootKey,
-                  label: t(`application.review.demographics.raceOptions.${rootKey}`),
-                  value: rootKey,
-                  additionalText: rootKey.indexOf("other") >= 0,
-                  defaultChecked: application.demographics.race?.includes(rootKey),
-                  subFields: raceKeys[rootKey].map((subKey) => ({
-                    id: subKey,
-                    label: t(`application.review.demographics.raceOptions.${subKey}`),
-                    value: subKey,
-                    defaultChecked: application.demographics.race?.includes(subKey),
-                    additionalText: subKey.indexOf("other") >= 0,
-                  })),
-                }))}
+                fields={raceOptions}
+                type="checkbox"
+                register={register}
                 strings={{
                   description: "",
                 }}
-                type="checkbox"
                 dataTestId={"app-demographics-race"}
-                register={register}
               />
             </fieldset>
             <div className={"pt-8"}>
