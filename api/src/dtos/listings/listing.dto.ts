869697adc070b8ccc3c21bf29b0f5b1fbe1dc690
ColinBuyck <colin.buyck@gmail.com>
2024-06-11 15:43:28 -0700

    fix: context for new field

--- api/src/dtos/listings/listing.dto.ts
+++ api/src/dtos/listings/listing.dto.ts
@@ -380,6 +380,8 @@ class Listing extends AbstractDTO {
   @ApiPropertyOptional()
   customMapPin?: boolean;
 
+  //Used to refresh translations and communicate recent changes to admin users
+  //should be revisited after translations refactoring to see if its still useful
   @Expose()
   @IsDate({ groups: [ValidationsGroupsEnum.default] })
   @Type(() => Date)
