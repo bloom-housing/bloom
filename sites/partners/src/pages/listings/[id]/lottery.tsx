95f9da424cd9777a92cdeb055430a2fb8730b773
emilyjablonski <emijablonski@gmail.com>
2024-06-27 13:59:08 -0600

    feat: rerun and release lottery modals

--- sites/partners/src/pages/listings/[id]/lottery.tsx
+++ sites/partners/src/pages/listings/[id]/lottery.tsx
@@ -1,9 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import Head from "next/head"
 import axios from "axios"
-import { faTicket } from "@fortawesome/free-solid-svg-icons"
+import Ticket from "@heroicons/react/24/solid/TicketIcon"
+import Download from "@heroicons/react/24/solid/ArrowDownTrayIcon"
 import { t, Breadcrumbs, BreadcrumbLink } from "@bloom-housing/ui-components"
-import { Button, Card, Heading, Icon } from "@bloom-housing/ui-seeds"
+import { Button, Card, Heading, Icon, Dialog } from "@bloom-housing/ui-seeds"
 import { CardHeader, CardSection } from "@bloom-housing/ui-seeds/src/blocks/Card"
 import {
   Listing,
@@ -25,6 +26,9 @@ const Lottery = (props: { listing: Listing }) => {
 
   const { listing } = props
 
+  const [reRunModal, setReRunModal] = useState(false)
+  const [releaseModal, setReleaseModal] = useState(false)
+
   if (!listing) return false
 
   const getHistoryItem = (dateString: string, event: string, user: string) => {
@@ -37,89 +41,206 @@ const Lottery = (props: { listing: Listing }) => {
     )
   }
 
+  const getMainContent = () => {
+    if (listing.lotteryLastRunAt) {
+      return (
+        <CardSection>
+          <Icon size="xl">
+            <Download />
+          </Icon>
+          <Heading priority={2} size={"2xl"}>
+            {"Export lottery data"}
+          </Heading>
+          <div className={styles["card-description"]}>
+            {"File includes randomized general pool and preference data."}
+          </div>
+          <div>
+            <Button>{t("t.export")}</Button>
+          </div>
+        </CardSection>
+      )
+    } else {
+      return (
+        <CardSection>
+          <Icon size="xl">
+            <Ticket />
+          </Icon>
+          <Heading priority={2} size={"2xl"}>
+            {t("listings.lottery.noData")}
+          </Heading>
+          <div className={styles["card-description"]}>
+            {t("listings.lottery.noDataDescription")}
+          </div>
+          <div>
+            <Button>{t("listings.lottery.runLottery")}</Button>
+          </div>
+        </CardSection>
+      )
+    }
+  }
+
+  const getActions = () => {
+    if (listing.lotteryLastRunAt) {
+      return (
+        <div className={styles["actions-container"]}>
+          <Button className={styles["action"]} onClick={() => setReRunModal(true)}>
+            {t("listings.lottery.reRun")}
+          </Button>
+          <Button className={styles["action"]} onClick={() => setReleaseModal(true)}>
+            {t("listings.lottery.release")}
+          </Button>
+        </div>
+      )
+    }
+  }
+
   return (
     <ListingContext.Provider value={listing}>
       <ListingGuard>
-        <Layout>
-          <Head>
-            <title>{t("nav.siteTitlePartners")}</title>
-          </Head>
+        <>
+          <Layout>
+            <Head>
+              <title>{t("nav.siteTitlePartners")}</title>
+            </Head>
 
-          <MetaTags
-            title={t("nav.siteTitlePartners")}
-            image={metaImage}
-            description={metaDescription}
-          />
+            <MetaTags
+              title={t("nav.siteTitlePartners")}
+              image={metaImage}
+              description={metaDescription}
+            />
 
-          <NavigationHeader
-            title={listing.name}
-            listingId={listing.id}
-            tabs={{
-              show: true,
-              listingLabel: t("t.listingSingle"),
-              applicationsLabel: t("nav.applications"),
-              lotteryLabel:
-                listing.status === ListingsStatusEnum.closed &&
-                listing.reviewOrderType === ReviewOrderTypeEnum.lottery
-                  ? t("listings.lotteryTitle")
-                  : undefined,
-            }}
-            breadcrumbs={
-              <Breadcrumbs>
-                <BreadcrumbLink href="/">{t("t.listing")}</BreadcrumbLink>
-                <BreadcrumbLink href={`/listings/${listing.id}`}>{listing.name}</BreadcrumbLink>
-                <BreadcrumbLink href={`/listings/${listing.id}/lottery`} current>
-                  {t("listings.lotteryTitle")}
-                </BreadcrumbLink>
-              </Breadcrumbs>
-            }
-          />
+            <NavigationHeader
+              title={listing.name}
+              listingId={listing.id}
+              tabs={{
+                show: true,
+                listingLabel: t("t.listingSingle"),
+                applicationsLabel: t("nav.applications"),
+                lotteryLabel:
+                  listing.status === ListingsStatusEnum.closed &&
+                  listing.reviewOrderType === ReviewOrderTypeEnum.lottery
+                    ? t("listings.lotteryTitle")
+                    : undefined,
+              }}
+              breadcrumbs={
+                <Breadcrumbs>
+                  <BreadcrumbLink href="/">{t("t.listing")}</BreadcrumbLink>
+                  <BreadcrumbLink href={`/listings/${listing.id}`}>{listing.name}</BreadcrumbLink>
+                  <BreadcrumbLink href={`/listings/${listing.id}/lottery`} current>
+                    {t("listings.lotteryTitle")}
+                  </BreadcrumbLink>
+                </Breadcrumbs>
+              }
+            />
 
-          <ListingStatusBar status={listing?.status} />
-          <section className={styles["lottery"]}>
-            <div className={styles["parent"]}>
-              <div className={styles["container"]}>
-                <div className={styles["main"]}>
-                  <Card spacing={"lg"}>
-                    <CardSection>
-                      <Icon icon={faTicket} size="2xl" />
-                      <Heading priority={2} size={"2xl"}>
-                        {t("listings.lottery.noData")}
-                      </Heading>
-                      <div className={styles["card-description"]}>
-                        {t("listings.lottery.noDataDescription")}
-                      </div>
-                      <div>
-                        <Button>{t("listings.lottery.runLottery")}</Button>
-                      </div>
-                    </CardSection>
-                  </Card>
+            <ListingStatusBar status={listing?.status} />
+            <section className={styles["lottery"]}>
+              <div className={styles["parent"]}>
+                <div className={styles["container"]}>
+                  <div className={styles["main"]}>
+                    <Card spacing={"lg"}>{getMainContent()}</Card>
+                  </div>
+                  <aside className={styles["side"]}>
+                    <>
+                      {getActions()}
+                      <Card>
+                        <CardHeader>
+                          <Heading priority={2} size={"lg"}>
+                            {t("listings.lottery.history")}
+                          </Heading>
+                        </CardHeader>
+                        <CardSection>
+                          {getHistoryItem(
+                            "November 21st, 2023 at 8:30am",
+                            "Listing closed",
+                            "By property"
+                          )}
+                          {getHistoryItem(
+                            "November 21st, 2023 at 8:30am",
+                            "Listing closed",
+                            "By property"
+                          )}
+                        </CardSection>
+                      </Card>
+                    </>
+                  </aside>
                 </div>
-                <aside className={styles["side"]}>
-                  <Card>
-                    <CardHeader>
-                      <Heading priority={2} size={"lg"}>
-                        {t("listings.lottery.history")}
-                      </Heading>
-                    </CardHeader>
-                    <CardSection>
-                      {getHistoryItem(
-                        "November 21st, 2023 at 8:30am",
-                        "Listing closed",
-                        "By property"
-                      )}
-                      {getHistoryItem(
-                        "November 21st, 2023 at 8:30am",
-                        "Listing closed",
-                        "By property"
-                      )}
-                    </CardSection>
-                  </Card>
-                </aside>
               </div>
-            </div>
-          </section>
-        </Layout>
+            </section>
+          </Layout>
+          <Dialog
+            isOpen={!!reRunModal}
+            ariaLabelledBy="rerun-lottery-modal-header"
+            ariaDescribedBy="rerun-lottery-modal-content"
+            onClose={() => setReRunModal(false)}
+          >
+            <Dialog.Header id="rerun-lottery-modal-header">{t("t.areYouSure")}</Dialog.Header>
+            <Dialog.Content id="rerun-lottery-modal-content">
+              <p>
+                <span>{t("listings.lottery.reRunContent")}</span>{" "}
+                <span className={"font-semibold"}>{t("listings.lottery.reRunCannotBeUndone")}</span>
+              </p>
+              <p>{t("listings.lottery.reRunHistory")}</p>
+              <p>{t("applications.addConfirmModalAddApplicationPostLotteryAreYouSure")}</p>
+            </Dialog.Content>
+            <Dialog.Footer>
+              <Button
+                variant="alert"
+                onClick={() => {
+                  // re-run lottery
+                  setReRunModal(false)
+                }}
+                size="sm"
+              >
+                {t("listings.lottery.reRunButton")}
+              </Button>
+              <Button
+                variant="primary-outlined"
+                onClick={() => {
+                  setReRunModal(false)
+                }}
+                size="sm"
+              >
+                {t("t.cancel")}
+              </Button>
+            </Dialog.Footer>
+          </Dialog>
+          <Dialog
+            isOpen={!!releaseModal}
+            ariaLabelledBy="release-lottery-modal-header"
+            ariaDescribedBy="release-lottery-modal-content"
+            onClose={() => setReleaseModal(false)}
+          >
+            <Dialog.Header id="release-lottery-modal-header">
+              {t("applications.addConfirmModalHeader")}
+            </Dialog.Header>
+            <Dialog.Content id="release-lottery-modal-content">
+              <p>{t("listings.lottery.releaseContent")}</p>
+              <p>{t("applications.addConfirmModalAddApplicationPostLotteryAreYouSure")}</p>
+            </Dialog.Content>
+            <Dialog.Footer>
+              <Button
+                variant="primary"
+                onClick={() => {
+                  // release lottery
+                  setReleaseModal(false)
+                }}
+                size="sm"
+              >
+                {t("listings.lottery.releaseButton")}
+              </Button>
+              <Button
+                variant="primary-outlined"
+                onClick={() => {
+                  setReleaseModal(false)
+                }}
+                size="sm"
+              >
+                {t("t.cancel")}
+              </Button>
+            </Dialog.Footer>
+          </Dialog>
+        </>
       </ListingGuard>
     </ListingContext.Provider>
   )
