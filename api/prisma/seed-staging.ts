026b229cab033a58fa35b559ef5b418b68fc94e4
Jared White <jared@jaredwhite.com>
2024-07-09 14:36:04 -0700

    fix: studio unit types in seed data

--- api/prisma/seed-staging.ts
+++ api/prisma/seed-staging.ts
@@ -940,13 +940,13 @@ export const stagingSeed = async (
           minOccupancy: 1,
           monthlyRent: '1200',
           numBathrooms: 1,
-          numBedrooms: 1,
+          numBedrooms: 0,
           number: '101',
           sqFeet: '750.00',
           amiChart: { connect: { id: amiChart.id } },
           unitTypes: {
             connect: {
-              id: unitTypes[1].id,
+              id: unitTypes[5].id,
             },
           },
         },
