e18661fcf7265b6c47a8ecdf8b659a282f9121ec
Eric McGarry <46828798+mcgarrye@users.noreply.github.com>
2024-08-07 16:17:04 -0400

    fix: remove errant lottery copy (#4239) (#745)
    * fix: remove errant lottery copy
    
    * fix: correct tests

--- sites/partners/__tests__/pages/listings/lottery.test.tsx
+++ sites/partners/__tests__/pages/listings/lottery.test.tsx
@@ -378,7 +378,6 @@ describe("lottery", () => {
     expect(header).toBeInTheDocument()
 
     fireEvent.click(getByText("Run lottery"))
-    expect(getByText("This data will expire on", { exact: false })).toBeInTheDocument()
     expect(await findByText("Confirmation needed")).toBeInTheDocument()
     expect(
       getByText("Make sure to add all paper applications before running the lottery.")
@@ -411,7 +410,6 @@ describe("lottery", () => {
     expect(header).toBeInTheDocument()
 
     fireEvent.click(getByText("Run lottery"))
-    expect(getByText("This data will expire on", { exact: false })).toBeInTheDocument()
     expect(await findByText("Confirmation needed")).toBeInTheDocument()
     expect(getByText("5 unresolved duplicate sets.")).toBeInTheDocument()
     expect(getByText("Run lottery without resolving duplicates")).toBeInTheDocument()
