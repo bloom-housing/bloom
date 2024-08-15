daf10fe64dde9b6991eb88f1b0102614c0782c36
Eric McGarry <46828798+mcgarrye@users.noreply.github.com>
2024-08-15 17:08:52 -0400

    release: require lottery date time fields (#787)

---
+++ api/src/utilities/lottery-date-validator.ts
@@ -0,0 +1,41 @@
+import {
+  ListingEventsTypeEnum,
+  ReviewOrderTypeEnum,
+  YesNoEnum,
+} from '@prisma/client';
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from 'class-validator';
+import { ListingEvent } from '../dtos/listings/listing-event.dto';
+
+/*
+    This is a custom validator to make sure lottery startDate is set when reviewOrder is set to lottery
+  */
+@ValidatorConstraint({ name: 'lotteryDate', async: false })
+export class LotteryDateParamValidator implements ValidatorConstraintInterface {
+  validate(
+    listingEvents: ListingEvent[] | undefined,
+    args: ValidationArguments,
+  ) {
+    const { reviewOrderType, lotteryOptIn } = args.object as {
+      reviewOrderType: string;
+      lotteryOptIn: boolean;
+    };
+    if (reviewOrderType === ReviewOrderTypeEnum.lottery && lotteryOptIn) {
+      return !!listingEvents.find(
+        (event: ListingEvent) =>
+          event.type === ListingEventsTypeEnum.publicLottery &&
+          event.startDate &&
+          event.startTime &&
+          event.endTime,
+      );
+    }
+    return true;
+  }
+
+  defaultMessage() {
+    return 'lotteryDate run date must be set';
+  }
+}
