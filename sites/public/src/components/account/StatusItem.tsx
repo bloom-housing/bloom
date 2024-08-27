1accd3b451578e70e3b5483c56823f1c15f442c5
ColinBuyck <colin.buyck@gmail.com>
2024-08-27 09:17:47 -0700

    fix: in progress ui updates

--- sites/public/src/components/account/StatusItem.tsx
+++ sites/public/src/components/account/StatusItem.tsx
@@ -3,6 +3,7 @@ import { t } from "@bloom-housing/ui-components"
 import { Button, Card, Tag } from "@bloom-housing/ui-seeds"
 import styles from "./StatusItem.module.scss"
 import applicationsViewStyles from "./ApplicationsView.module.scss"
+import { ListingsStatusEnum } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
 
 interface StatusItemProps {
   applicationDueDate?: string
@@ -11,6 +12,8 @@ interface StatusItemProps {
   confirmationNumber?: string
   listingName: string
   listingURL: string
+  listingStatus: ListingsStatusEnum
+  lotteryDate: Date
   lotteryResults?: boolean
   lotteryURL?: string
   strings?: {
@@ -26,16 +29,40 @@ interface StatusItemProps {
 }
 
 const StatusItem = (props: StatusItemProps) => {
+  //set custom visuals based on listing/lottery status
+  let tagText = ""
+  let tagVariant: "primary" | "secondary" | "success"
+
+  let deadlineText = ""
+  let dueDate = ""
+
+  if (props.lotteryResults) {
+    tagText = t("account.lotteryRun")
+    tagVariant = "success"
+
+    deadlineText = t("account.lotteryResultsPosted")
+  } else if (props.listingStatus === ListingsStatusEnum.active) {
+    tagText = t("account.openApplications")
+    tagVariant = "primary"
+
+    deadlineText = t("account.applicationsClose")
+  } else {
+    tagText = t("account.closedApplications")
+    tagVariant = "secondary"
+
+    if (props.lotteryDate) {
+      deadlineText = t("account.lotteryDate")
+    }
+  }
+
   return (
     <Card.Section className={applicationsViewStyles["account-card-applications-section"]}>
       <article className={styles["status-item"]}>
         <header className={styles["status-item__header"]}>
           <h3 className={styles["status-item__title"]}>{props.listingName}</h3>
           <p className={styles["status-item__status"]}>
-            {props.strings?.status ?? t("application.status")}:{" "}
-            <Tag variant="primary">
-              {props.strings?.submittedStatus ?? t("application.statuses.submitted")}
-            </Tag>
+            {props.strings?.status ?? t("application.status")}: {""}
+            <Tag variant={tagVariant}>{tagText}</Tag>
           </p>
         </header>
 
