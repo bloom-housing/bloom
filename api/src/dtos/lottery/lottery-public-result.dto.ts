4f980083aefd30646485f0ea07e35c38c3d2b16a
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2024-09-03 11:15:06 -0600

    feat: public lottery view, rank data (#4275)

---
+++ api/src/dtos/lottery/lottery-public-result.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
+import { IsDefined, IsNumber, IsOptional, IsUUID } from 'class-validator';
+import { ValidationsGroupsEnum } from '../../enums/shared/validation-groups-enum';
+
+export class PublicLotteryResult {
+  @Expose()
+  @IsNumber({}, { groups: [ValidationsGroupsEnum.default] })
+  @ApiProperty()
+  @IsDefined({ groups: [ValidationsGroupsEnum.default] })
+  ordinal: number;
+
+  @Expose()
+  @ApiProperty()
+  @IsOptional({ groups: [ValidationsGroupsEnum.default] })
+  @IsUUID(4, { groups: [ValidationsGroupsEnum.default] })
+  multiselectQuestionId?: string;
+}
