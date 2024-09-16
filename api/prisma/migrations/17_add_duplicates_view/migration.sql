dd24981b17b119a9fd1db5f977dbddab91bd3fa6
Morgan Ludtke <ludtkemorgan@gmail.com>
2024-09-16 09:08:56 -0500

    fix: switch to concat

--- api/prisma/migrations/17_add_duplicates_view/migration.sql
+++ api/prisma/migrations/17_add_duplicates_view/migration.sql
@@ -1,13 +1,23 @@
 CREATE VIEW "application_flagged_set_possibilities" AS (
     SELECT
-        LOWER(a.first_name) || '-' || LOWER(a.last_name) || '-' || a.birth_month || '-' || a.birth_day || '-' || a.birth_year as "key",
+        CONCAT(
+            LOWER(a.first_name),
+            '-',
+            LOWER(a.last_name),
+            '-',
+            a.birth_month,
+            '-',
+            a.birth_day,
+            '-',
+            a.birth_year
+        ) as "key",
         app.listing_id,
         app.id as "application_id",
         'nameAndDOB' as "type"
-    from
+    FROM
         applicant a,
         applications app
-    where
+    WHERE
         a.id = app.applicant_id
         and app.deleted_at is null
 )
@@ -18,10 +28,10 @@ UNION
         app.listing_id,
         app.id as "application_id",
         'email' as "type"
-    from
+    FROM
         applications app,
         applicant a
-    where
+    WHERE
         a.id = app.applicant_id
         and a.email_address is not null
         and app.deleted_at is null
@@ -29,14 +39,24 @@ UNION
 UNION
 (
     SELECT
-        LOWER(hm.first_name) || '-' || LOWER(hm.last_name) || '-' || hm.birth_month || '-' || hm.birth_day || '-' || hm.birth_year as "key",
+        CONCAT(
+            LOWER(hm.first_name),
+            '-',
+            LOWER(hm.last_name),
+            '-',
+            hm.birth_month,
+            '-',
+            hm.birth_day,
+            '-',
+            hm.birth_year
+        ) as "key",
         app.listing_id,
         app.id as "application_id",
         'nameAndDOB' as "type"
-    from
+    FROM
         applications app,
         household_member hm
-    where
+    WHERE
         hm.application_id = app.id
         and app.deleted_at is null
 );
