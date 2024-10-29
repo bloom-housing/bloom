563bc60118043756fa92fb374c04b79a648c042a
Eric McGarry <eric@eric.mcgarry.name>
2024-10-29 16:36:10 -0400

    feat: integration tests

--- api/test/integration/permission-tests/permission-as-admin.e2e-spec.ts
+++ api/test/integration/permission-tests/permission-as-admin.e2e-spec.ts
@@ -1249,6 +1249,14 @@ describe('Testing Permissioning of endpoints as Admin User', () => {
 
       expect(activityLogResult).not.toBeNull();
     });
+
+    it('should succeed for mmapMarkers endpoint', async () => {
+      await request(app.getHttpServer())
+        .get(`/listings/mapMarkers`)
+        .set({ passkey: process.env.API_PASS_KEY || '' })
+        .set('Cookie', cookies)
+        .expect(200);
+    });
   });
 
   describe('Testing application flagged set endpoints', () => {
