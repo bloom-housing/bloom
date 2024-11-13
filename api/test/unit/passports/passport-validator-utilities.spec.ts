5f34199f91085d7d13d664409f2213457d276336
ColinBuyck <colin.buyck@gmail.com>
2024-11-13 10:57:01 -0500

    fix: testing coverage

---
+++ api/test/unit/passports/passport-validator-utilities.spec.ts
@@ -0,0 +1,46 @@
+import { checkFailedLoginCount } from '../../../src/utilities/passport-validator-utilities';
+
+describe('Testing checkFailedLoginCount', () => {
+  it('should return without erroring and set failedLoginAttemptsCount to be 0 if lockout expired', () => {
+    const val = {
+      lastLoginAt: new Date('01/01/2024'),
+      failedLoginAttemptsCount: 5,
+    };
+    const updatedFailedLoginCount = checkFailedLoginCount(
+      val.lastLoginAt,
+      val.failedLoginAttemptsCount,
+      5,
+      10,
+    );
+    expect(updatedFailedLoginCount).toEqual(0);
+  });
+
+  it('should return without erroring and leave failed login count unchanged if user is and was not locked out', () => {
+    const val = {
+      lastLoginAt: new Date('01/01/2024'),
+      failedLoginAttemptsCount: 2,
+    };
+    const updatedFailedLoginCount = checkFailedLoginCount(
+      val.lastLoginAt,
+      val.failedLoginAttemptsCount,
+      5,
+      10,
+    );
+    expect(updatedFailedLoginCount).toEqual(2);
+  });
+
+  it('should error if user is still in lockout period', () => {
+    const val = {
+      lastLoginAt: new Date(),
+      failedLoginAttemptsCount: 5,
+    };
+    expect(
+      checkFailedLoginCount(
+        val.lastLoginAt,
+        val.failedLoginAttemptsCount,
+        5,
+        10,
+      ),
+    ).rejects.toThrowError(`Failed login attempts exceeded.`);
+  });
+});
