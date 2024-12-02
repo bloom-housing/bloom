83d34cced1a7921f0943b5a9f0045f5e7b09923b
ColinBuyck <53269332+ColinBuyck@users.noreply.github.com>
2024-12-02 13:19:35 -0500

    fix: enforce max length (#969)

--- sites/partners/src/components/settings/PreferenceDrawer.tsx
+++ sites/partners/src/components/settings/PreferenceDrawer.tsx
@@ -248,8 +248,12 @@ const PreferenceDrawer = ({
                       type="text"
                       dataTestId={"preference-title"}
                       defaultValue={questionData?.text}
-                      errorMessage={t("errors.requiredFieldError")}
-                      validation={{ required: true }}
+                      errorMessage={
+                        errors.text?.type === "maxLength"
+                          ? t("errors.maxLength", { length: 32 })
+                          : t("errors.requiredFieldError")
+                      }
+                      validation={{ required: true, maxLength: 32 }}
                       error={errors.text}
                       inputProps={{
                         onChange: () => clearErrors("text"),
