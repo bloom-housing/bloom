fe8848218f65f039108c44c0ca07a4432271b099
Emily Jablonski <emijablonski@gmail.com>
2024-12-04 13:56:20 -0700

    fix: cleanup

--- sites/public/src/layouts/max-width.tsx
+++ sites/public/src/layouts/max-width.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import styles from "./max-width.module.scss"
 
-interface FormLayoutProps {
+interface MaxWidthLayoutProps {
   children: React.ReactNode
 }
-const MaxWidthLayout = (props: FormLayoutProps) => {
+
+const MaxWidthLayout = (props: MaxWidthLayoutProps) => {
   return (
     <div className={styles["layout-max-width-container"]}>
       <div className={styles["layout-max-width-content"]}>{props.children}</div>
