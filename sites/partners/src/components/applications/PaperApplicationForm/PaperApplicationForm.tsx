9a0a5dd25f2a4c0bd8b7df30b0b384e01f172c40
ColinBuyck <colin.buyck@gmail.com>
2024-12-22 17:24:58 -0800

    fix: handle null order id

--- sites/partners/src/components/applications/PaperApplicationForm/PaperApplicationForm.tsx
+++ sites/partners/src/components/applications/PaperApplicationForm/PaperApplicationForm.tsx
@@ -77,9 +77,13 @@ const ApplicationForm = ({ listingId, editMode, application }: ApplicationFormPr
 
   useEffect(() => {
     if (application?.householdMember) {
-      const orderedHouseholdMembers = application.householdMember.sort(
-        (a, b) => a.orderId - b.orderId
-      )
+      const householdMemberNum = application.householdMember.length
+      const orderedHouseholdMembers = application.householdMember
+        //reset order ids to show members in order user added them
+        .map((member, idx) => {
+          return { ...member, orderId: householdMemberNum - idx }
+        })
+        .sort((a, b) => a.orderId - b.orderId)
       setHouseholdMembers(orderedHouseholdMembers)
     }
   }, [application, setHouseholdMembers])
