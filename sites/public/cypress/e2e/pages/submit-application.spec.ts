515a384b202e4f03bc0ed463427d625d3905052e
Adrien Young <adrien.elizabeth@gmail.com>
2025-01-22 11:04:33 -0500

    test: run autofill test after regular test (broken)

--- sites/public/cypress/e2e/pages/submit-application.spec.ts
+++ sites/public/cypress/e2e/pages/submit-application.spec.ts
@@ -1,7 +1,7 @@
 import { ElmVillageApplication, minimalDataApplication } from "../../mockData/applicationData"
 
 describe("Submit", function () {
-  it("should submit an application for the Elm Village listing", function () {
+  it.only("should submit an application for the Elm Village listing", function () {
     cy.intercept("GET", "/geocoding/v5/**", { fixture: "address" })
     // Interceptor for the address in the preference
     cy.intercept(
@@ -17,8 +17,10 @@ describe("Submit", function () {
       }
     )
 
-    cy.submitApplication("Elm Village", ElmVillageApplication, false, true)
+    cy.submitApplication("Elm Village", ElmVillageApplication, false, true, false)
+    cy.submitApplication("Blue Sky Apartments", ElmVillageApplication, true, true, true)
   })
+
   it("should submit a minimal application for the Test: Default, No Preferences", function () {
     cy.intercept("GET", "/geocoding/v5/**", { fixture: "address" })
     cy.submitApplication("Blue Sky Apartments", minimalDataApplication, false, false)
