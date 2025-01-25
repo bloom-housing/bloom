2338d832e65228c4fef8e0fcfda33635ca362b0b
ColinBuyck <colin.buyck@gmail.com>
2025-01-24 16:22:40 -0800

    fix: default approach

--- sites/partners/src/lib/listings/UnitsFormatter.ts
+++ sites/partners/src/lib/listings/UnitsFormatter.ts
@@ -25,12 +25,8 @@ export default class UnitsFormatter extends Formatter {
         case "oneBdrm":
           unit.numBedrooms = 1
           break
-        case "studio":
-        case "SRO":
-          unit.numBedrooms = 0
-          break
         default:
-          unit.numBedrooms = null
+          unit.numBedrooms = 0
       }
 
       Object.keys(unit).forEach((key) => {
