4d04cc19daf6a683c762e4123101f901aae73ef9
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2025-01-30 09:40:55 -0700

    refactor: max width layout (#4500)

--- sites/public/src/pages/_error.tsx
+++ sites/public/src/pages/_error.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useContext } from "react"
-import Layout from "../layouts/application"
-import Head from "next/head"
-import { Hero, MarkdownSection, t } from "@bloom-housing/ui-components"
+import { Hero, t } from "@bloom-housing/ui-components"
 import { PageView, pushGtmEvent, AuthContext } from "@bloom-housing/shared-helpers"
 import { Button } from "@bloom-housing/ui-seeds"
 import { UserStatus } from "../lib/constants"
+import MaxWidthLayout from "../layouts/max-width"
 
 const ErrorPage = () => {
   const pageTitle = t("errors.notFound.title")
@@ -20,24 +19,21 @@ const ErrorPage = () => {
   }, [profile])
 
   return (
-    <Layout>
-      <Head>
-        <title>{pageTitle}</title>
-      </Head>
+    <>
       <Hero title={pageTitle} buttonTitle={t("welcome.seeRentalListings")} buttonLink="/listings">
         {t("errors.notFound.message")}
       </Hero>
       <div className="homepage-extra">
-        <MarkdownSection fullwidth={true}>
+        <MaxWidthLayout>
           <>
             <p>{t("welcome.seeMoreOpportunities")}</p>
             <Button variant="primary-outlined" href="/help/housing-help">
               {t("welcome.viewAdditionalHousing")}
             </Button>
           </>
-        </MarkdownSection>
+        </MaxWidthLayout>
       </div>
-    </Layout>
+    </>
   )
 }
 
