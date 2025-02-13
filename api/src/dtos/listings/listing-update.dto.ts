8232f00f2ece4f46d5dc5aef9862005f2e2f7185
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-02-13 18:19:33 +0100

    chore: remove unnecessary field declaration from listing update dto

--- api/src/dtos/listings/listing-update.dto.ts
+++ api/src/dtos/listings/listing-update.dto.ts
@@ -162,9 +162,4 @@ export class ListingUpdate extends OmitType(Listing, [
   @ValidateNested({ groups: [ValidationsGroupsEnum.default] })
   @Type(() => IdDTO)
   requestedChangesUser?: IdDTO;
-
-  @Expose()
-  @ApiPropertyOptional()
-  @IsBoolean({ groups: [ValidationsGroupsEnum.default] })
-  isVerified?: boolean;
 }
