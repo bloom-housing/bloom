3ab5e7e960801e77f4a5c1bb5a5f8db6160944bf
Eric McGarry <46828798+mcgarrye@users.noreply.github.com>
2025-02-17 16:59:54 -0500

    feat: expand listing filters (#4628)
    * feat: expand listing filters
    
    4542
    
    * feat: test for expanded listing filters
    
    4542
    
    * feat: remove unused imports
    
    4542
    
    * feat: add e2e tests and corrections
    
    4542
    
    * feat: add test for search
    
    4542
    
    * feat: correct linting and small changes
    
    4542
    
    * feat: make test more unique
    
    4542
    
    * feat: increase response limit
    
    4542
    
    * feat: always filter by jurisdiction
    
    4542. This will reduce flakyness
    
    * feat: fix swagger file
    
    5442
    
    * feat: separate dtos
    
    4542

--- shared-helpers/src/types/backend-swagger.ts
+++ shared-helpers/src/types/backend-swagger.ts
@@ -241,7 +241,7 @@ export class ListingsService {
   filterableList(
     params: {
       /** requestBody */
-      body?: ListingsQueryParams
+      body?: ListingsQueryBody
     } = {} as any,
     options: IRequestOptions = {}
   ): Promise<PaginatedListing> {
@@ -2703,6 +2703,91 @@ export interface SuccessDTO {
   success: boolean
 }
 
+export interface ListingFilterParams {
+  /**  */
+  $comparison: EnumListingFilterParamsComparison
+
+  /**  */
+  availability?: FilterAvailabilityEnum
+
+  /**  */
+  bathrooms?: number
+
+  /**  */
+  bedrooms?: number
+
+  /**  */
+  city?: string
+
+  /**  */
+  counties?: string[]
+
+  /**  */
+  homeTypes?: HomeTypeEnum[]
+
+  /**  */
+  ids?: string[]
+
+  /**  */
+  isVerified?: boolean
+
+  /**  */
+  jurisdiction?: string
+
+  /**  */
+  leasingAgent?: string
+
+  /**  */
+  listingFeatures?: string[]
+
+  /**  */
+  monthlyRent?: number
+
+  /**  */
+  name?: string
+
+  /**  */
+  neighborhood?: string
+
+  /**  */
+  regions?: RegionEnum[]
+
+  /**  */
+  reservedCommunityTypes?: string[]
+
+  /**  */
+  section8Acceptance?: boolean
+
+  /**  */
+  status?: ListingsStatusEnum
+
+  /**  */
+  zipCode?: string
+}
+
+export interface ListingsQueryBody {
+  /**  */
+  page?: number
+
+  /**  */
+  limit?: number | "all"
+
+  /**  */
+  filter?: ListingFilterParams[]
+
+  /**  */
+  view?: ListingViews
+
+  /**  */
+  orderBy?: ListingOrderByKeys
+
+  /**  */
+  orderDir?: OrderByEnum
+
+  /**  */
+  search?: string
+}
+
 export interface ListingsQueryParams {
   /**  */
   page?: number
@@ -2724,32 +2809,6 @@ export interface ListingsQueryParams {
 
   /**  */
   search?: string
-}
-
-export interface ListingFilterParams {
-  /**  */
-  $comparison: EnumListingFilterParamsComparison
-
-  /**  */
-  name?: string
-
-  /**  */
-  status?: ListingsStatusEnum
-
-  /**  */
-  neighborhood?: string
-
-  /**  */
-  bedrooms?: number
-
-  /**  */
-  zipcode?: string
-
-  /**  */
-  leasingAgents?: string
-
-  /**  */
-  jurisdiction?: string
 }
 
 export interface ListingsRetrieveParams {
@@ -6303,6 +6362,41 @@ export interface PublicLotteryTotal {
   multiselectQuestionId?: string
 }
 
+export enum FilterAvailabilityEnum {
+  "waitlistOpen" = "waitlistOpen",
+  "unitsAvailable" = "unitsAvailable",
+}
+
+export enum HomeTypeEnum {
+  "apartment" = "apartment",
+  "duplex" = "duplex",
+  "house" = "house",
+  "townhome" = "townhome",
+}
+
+export enum RegionEnum {
+  "Greater_Downtown" = "Greater_Downtown",
+  "Eastside" = "Eastside",
+  "Southwest" = "Southwest",
+  "Westside" = "Westside",
+}
+
+export enum ListingsStatusEnum {
+  "active" = "active",
+  "pending" = "pending",
+  "closed" = "closed",
+  "pendingReview" = "pendingReview",
+  "changesRequested" = "changesRequested",
+}
+export enum EnumListingFilterParamsComparison {
+  "=" = "=",
+  "<>" = "<>",
+  "IN" = "IN",
+  ">=" = ">=",
+  "<=" = "<=",
+  "LIKE" = "LIKE",
+  "NA" = "NA",
+}
 export enum ListingViews {
   "fundamentals" = "fundamentals",
   "base" = "base",
@@ -6328,21 +6422,6 @@ export enum OrderByEnum {
   "desc" = "desc",
 }
 
-export enum ListingsStatusEnum {
-  "active" = "active",
-  "pending" = "pending",
-  "closed" = "closed",
-  "pendingReview" = "pendingReview",
-  "changesRequested" = "changesRequested",
-}
-export enum EnumListingFilterParamsComparison {
-  "=" = "=",
-  "<>" = "<>",
-  "IN" = "IN",
-  ">=" = ">=",
-  "<=" = "<=",
-  "NA" = "NA",
-}
 export enum ApplicationAddressTypeEnum {
   "leasingAgent" = "leasingAgent",
 }
@@ -6410,13 +6489,6 @@ export enum UnitTypeEnum {
 export enum UnitRentTypeEnum {
   "fixed" = "fixed",
   "percentageOfIncome" = "percentageOfIncome",
-}
-
-export enum HomeTypeEnum {
-  "apartment" = "apartment",
-  "duplex" = "duplex",
-  "house" = "house",
-  "townhome" = "townhome",
 }
 
 export enum AfsView {
@@ -6505,6 +6577,7 @@ export enum EnumMultiselectQuestionFilterParamsComparison {
   "IN" = "IN",
   ">=" = ">=",
   "<=" = "<=",
+  "LIKE" = "LIKE",
   "NA" = "NA",
 }
 export enum InputType {
