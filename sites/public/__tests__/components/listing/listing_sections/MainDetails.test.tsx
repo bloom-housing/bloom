3e174699bb26e085b4fe061ef8bfc0681f95bcc2
Emily Jablonski <emijablonski@gmail.com>
2025-02-20 13:24:51 -0700

    fix: tests

--- sites/public/__tests__/components/listing/listing_sections/MainDetails.test.tsx
+++ sites/public/__tests__/components/listing/listing_sections/MainDetails.test.tsx
@@ -25,19 +25,6 @@ describe("<MainDetails>", () => {
     expect(getByText(listing.developer)).toBeDefined()
     expect(getByAltText("A picture of the building")).toBeDefined()
   })
-  it("shows no tags", () => {
-    const { queryByTestId } = render(
-      <MainDetails
-        listing={{
-          ...listing,
-          reviewOrderType: ReviewOrderTypeEnum.lottery,
-          reservedCommunityTypes: null,
-        }}
-        dueDateContent={[]}
-      />
-    )
-    expect(queryByTestId("listing-tags")).toBeNull()
-  })
   it("shows reserved tag", () => {
     const { getByTestId, getAllByText } = render(
       <MainDetails
@@ -51,6 +38,19 @@ describe("<MainDetails>", () => {
     )
     expect(getByTestId("listing-tags")).toBeDefined()
     expect(getAllByText("Veteran").length).toBeGreaterThan(0)
+  })
+  it("shows units available tag for lottery", () => {
+    const { getByTestId, getByText } = render(
+      <MainDetails
+        listing={{
+          ...listing,
+          reviewOrderType: ReviewOrderTypeEnum.lottery,
+        }}
+        dueDateContent={[]}
+      />
+    )
+    expect(getByTestId("listing-tags")).toBeDefined()
+    expect(getByText("Available Units")).toBeDefined()
   })
   it("shows units available tag", () => {
     const { getByTestId, getByText } = render(
