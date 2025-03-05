c0b0c862c9d2a43e77c558e228c427c0bb9885fa
ColinBuyck <colin.buyck@gmail.com>
2025-03-05 14:07:32 -0800

    fix: support content page toggling

---
+++ sites/public/src/components/content-pages/PrivacySeeds.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useContext } from "react"
+import { t } from "@bloom-housing/ui-components"
+import Markdown from "markdown-to-jsx"
+import { PageView, pushGtmEvent, AuthContext } from "@bloom-housing/shared-helpers"
+import { UserStatus } from "../../lib/constants"
+import Layout from "../../layouts/application"
+import pageContent from "../../md_content/privacy_policy.md"
+import { PageHeaderLayout } from "../../patterns/PageHeaderLayout"
+import styles from "../../patterns/PageHeaderLayout.module.scss"
+
+const Privacy = () => {
+  const { profile } = useContext(AuthContext)
+
+  useEffect(() => {
+    pushGtmEvent<PageView>({
+      event: "pageView",
+      pageTitle: "Privacy",
+      status: profile ? UserStatus.LoggedIn : UserStatus.NotLoggedIn,
+    })
+  }, [profile])
+
+  return (
+    <Layout>
+      <PageHeaderLayout heading={t("pageTitle.privacy")} inverse>
+        <Markdown className={styles["markdown"]}>{pageContent.toString()}</Markdown>
+      </PageHeaderLayout>
+    </Layout>
+  )
+}
+
+export default Privacy
