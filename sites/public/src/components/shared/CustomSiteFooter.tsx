39ed56afdb19c70d18b4dba33e05d6558c7f45b1
ColinBuyck <53269332+ColinBuyck@users.noreply.github.com>
2025-03-04 16:02:42 -0800

    fix: add content + minmize changes (#12)

--- sites/public/src/components/shared/CustomSiteFooter.tsx
+++ sites/public/src/components/shared/CustomSiteFooter.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import Link from "next/link"
 import { t } from "@bloom-housing/ui-components"
 import styles from "./CustomSiteFooter.module.scss"
+import Markdown from "markdown-to-jsx"
 
 const CustomSiteFooter = () => {
   return (
@@ -9,22 +10,25 @@ const CustomSiteFooter = () => {
       <div className={styles["footer-content-container"]}>
         <div className={styles["footer"]}>
           <div className={styles["icon-container"]}>
-            <a href={"/"} className={styles["jurisdiction-icon"]}>
-              <img src="/images/default-housing-logo.svg" alt={"Jurisdiction Logo"} />
-            </a>
+            <img
+              className={styles["jurisdiction-icon"]}
+              src="/images/detroit-logo-white.png"
+              alt={t("footer.logoAlt")}
+            />
           </div>
           <div className={styles["text-container"]}>
-            <span>{t("footer.content.projectOf")}</span>
-            <Link href={"/"}>Mayor's Office of Housing Development</Link>
+            <p>{t("footer.description")}</p>
+          </div>
+
+          <div className={styles["text-container"]}>
+            <p>{t("footer.forListingQuestions")}</p>
+            <p>{t("footer.pleaseContact")}</p>
           </div>
           <div className={styles["text-container"]}>
-            <span>{t("footer.content.partnership")}</span>
-            <Link href={"/"}>Bloomington Department of Technology</Link>
-            <Link href={"/"}>Mayor's Office of Civic Innovation</Link>
-          </div>
-          <div className={styles["text-container"]}>
-            <p>{t("footer.content.applicationQuestions")}</p>
-            <p>{t("footer.content.programQuestions")}</p>
+            <p>{t("footer.forGeneralInquiries")}</p>
+            <p>
+              <Markdown>{t("footer.contactInfo")}</Markdown>
+            </p>
           </div>
         </div>
       </div>
@@ -35,7 +39,6 @@ const CustomSiteFooter = () => {
           <div className={styles["copyright-text"]}>{t("footer.copyright")}</div>
           <div className={styles.links}>
             <Link href="/feedback">{t("footer.giveFeedback")}</Link>
-            <Link href="/">{t("footer.contact")}</Link>
             <Link href="/privacy">{t("pageTitle.privacy")}</Link>
             <Link href="/disclaimer">{t("pageTitle.disclaimer")}</Link>
             <Link href="/accessibility">{t("pageTitle.accessibilityStatement")}</Link>
