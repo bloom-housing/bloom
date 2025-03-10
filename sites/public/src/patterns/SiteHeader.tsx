dd9119aa8a520769b840eb8321a1255d49bd6081
Emily Jablonski <emijablonski@gmail.com>
2025-03-10 13:01:47 -0600

    feat: functional links

--- sites/public/src/patterns/SiteHeader.tsx
+++ sites/public/src/patterns/SiteHeader.tsx
@@ -1,29 +1,98 @@
 import React from "react"
+import ChevronDown from "@heroicons/react/20/solid/ChevronDownIcon"
 import { Heading, Icon } from "@bloom-housing/ui-seeds"
-import HomeIcon from "@heroicons/react/24/solid/HomeModernIcon"
 import styles from "./SiteHeader.module.scss"
 
-interface LinkButtonProps {
+export interface HeaderLink {
   label: string
-  onClicK: () => void
+  href?: string
+  onClick?: () => void
+  subMenuLinks?: HeaderLink[]
 }
 
-const LinkButton = (props: LinkButtonProps) => {
-  return (
-    <button className={styles["link"]} onClick={props.onClicK}>
-      {props.label}
-    </button>
-  )
+interface HeaderLinkProps {
+  link: HeaderLink
+}
+
+// export interface MenuLink {
+//   className?: string
+//   href?: string
+//   iconClassName?: string
+//   iconElement?: React.ReactNode
+//   iconSrc?: string
+//   onClick?: () => void
+//   subMenuLinks?: MenuLink[]
+//   title: string
+// }
+
+const HeaderLink = (props: HeaderLinkProps) => {
+  if (props.link.subMenuLinks?.length) {
+    // Dropdown
+    return (
+      <li className={styles["dropdown-link-container"]}>
+        <button className={styles["link"]} onClick={props.link.onClick}>
+          {props.link.label}
+          <Icon size={"md"} className={styles["dropdown-icon"]}>
+            <ChevronDown />
+          </Icon>
+        </button>
+        <ul className={styles["submenu-container"]}>
+          {props.link.subMenuLinks.map((subMenuLink, index) => {
+            if (subMenuLink.href) {
+              return (
+                <li className={styles["submenu-item"]} key={index}>
+                  <a className={styles["submenu-link"]} href={subMenuLink.href}>
+                    {subMenuLink.label}
+                  </a>
+                </li>
+              )
+            } else {
+              return (
+                <li className={styles["submenu-item"]} key={index}>
+                  <button className={styles["submenu-link"]} onClick={props.link.onClick}>
+                    {props.link.label}
+                  </button>
+                </li>
+              )
+            }
+          })}
+        </ul>
+      </li>
+    )
+  } else {
+    // Single link
+    if (props.link.href) {
+      return (
+        <li>
+          <a className={styles["link"]} href={props.link.href}>
+            {props.link.label}
+          </a>
+        </li>
+      )
+    } else {
+      return (
+        <li>
+          <button className={styles["link"]} onClick={props.link.onClick}>
+            {props.link.label}
+          </button>
+        </li>
+      )
+    }
+  }
 }
 
 interface LanguageButtonProps {
   label: string
-  onClicK: () => void
+  active: boolean
+  onClick: () => void
 }
 
-const LanguageButton = (props: LinkButtonProps) => {
+const LanguageButton = (props: LanguageButtonProps) => {
   return (
-    <button className={styles["language"]} onClick={props.onClicK}>
+    <button
+      className={`${styles["language"]} ${props.active ? styles["active-language"] : ""}`}
+      onClick={props.onClick}
+    >
       {props.label}
     </button>
   )
@@ -53,40 +122,54 @@ export type Language = {
 interface SiteHeaderProps {
   title: string
   languages: Language[]
+  links: HeaderLink[]
+  message?: React.ReactNode
+  titleLink: string
+  logo?: React.ReactNode
+  logoClassName?: string
 }
 
 export const SiteHeader = (props: SiteHeaderProps) => {
   return (
-    <div className={styles["site-header-container"]}>
+    <nav className={styles["site-header-container"]} aria-label={"Main"}>
       <HeadingWrapper className={styles["language-wrapper"]}>
         <div className={styles["language-container"]}>
-          <LanguageButton label={"LanguageA"} onClicK={() => alert("LanguageA")} />
-          <LanguageButton label={"LanguageB"} onClicK={() => alert("LanguageB")} />
-          <LanguageButton label={"LanguageC"} onClicK={() => alert("LanguageC")} />
+          {props.languages?.map((language, index) => {
+            return (
+              <LanguageButton
+                label={language.label}
+                onClick={language.onClick}
+                active={language.active}
+                key={index}
+              />
+            )
+          })}
         </div>
       </HeadingWrapper>
       <HeadingWrapper className={styles["message-wrapper"]}>
-        <div className={styles["message-container"]}>Message</div>
+        <div className={styles["message-container"]}>{props.message ?? ""}</div>
       </HeadingWrapper>
       <HeadingWrapper className={styles["navigation-wrapper"]}>
         <div className={styles["navigation-container"]}>
-          <div className={styles["title-container"]}>
-            <div className={styles["icon"]}>
-              <Icon size={"xl"}>
-                <HomeIcon />
-              </Icon>
-            </div>
+          <a className={styles["title-container"]} href={props.titleLink}>
+            {props.logo && (
+              <div
+                className={`${styles["logo"]} ${props.logoClassName ? props.logoClassName : ""}`}
+              >
+                {props.logo}
+              </div>
+            )}
             <div className={styles["title"]}>
               <Heading size={"xl"}>{props.title}</Heading>
             </div>
-          </div>
-          <div className={styles["links-container"]}>
-            <LinkButton label={"LinkA"} onClicK={() => alert("LinkA")} />
-            <LinkButton label={"LinkB"} onClicK={() => alert("LinkB")} />
-            <LinkButton label={"LinkC"} onClicK={() => alert("LinkC")} />
-          </div>
+          </a>
+          <ul className={styles["links-container"]}>
+            {props.links?.map((link, index) => {
+              return <HeaderLink link={link} key={index} />
+            })}
+          </ul>
         </div>
       </HeadingWrapper>
-    </div>
+    </nav>
   )
 }
