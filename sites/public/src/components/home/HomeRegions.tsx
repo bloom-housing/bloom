cfdf95ff75231c4d870d9d4735ed02a89f7d8e96
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-03-27 11:58:08 +0100

    chore: update home page region section styles

--- sites/public/src/components/home/HomeRegions.tsx
+++ sites/public/src/components/home/HomeRegions.tsx
@@ -5,18 +5,42 @@ import { Grid, Heading } from "@bloom-housing/ui-seeds"
 import { GridRow } from "@bloom-housing/ui-seeds/src/layout/Grid"
 import styles from "./HomeRegions.module.scss"
 
+export const regionImageUrls: Map<RegionEnum, string> = new Map([
+  [
+    RegionEnum.Greater_Downtown,
+    "https://pbs.twimg.com/media/DSzZwQKVAAASkw_?format=jpg&name=large",
+  ],
+  [
+    RegionEnum.Eastside,
+    "https://res.cloudinary.com/exygy/image/upload/v1740007497/detroit/Detroit-Eastside_qzxx6n.jpg",
+  ],
+  [
+    RegionEnum.Southwest,
+    "https://res.cloudinary.com/exygy/image/upload/v1740007258/detroit/Detroit-Southwest_wbx3nu.jpg",
+  ],
+  [
+    RegionEnum.Westside,
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Atkinson_avenue_historic_district.JPG/1920px-Atkinson_avenue_historic_district.JPG",
+  ],
+])
+
 export const HomeRegions = () => {
-  const availableRegions = Object.values(RegionEnum).map((region) => region.replace("_", " "))
+  const availableRegions = Object.values(RegionEnum).map((region) => ({
+    title: region.replace("_", " "),
+    enum: region,
+  }))
 
   return (
     <Grid>
       <GridRow columns={4}>
         {availableRegions.map((region) => (
-          <GridCell key={region}>
+          <GridCell className={styles["region-entry"]} key={region.enum}>
             <ClickableCard className={styles["region-card"]}>
-              <div className="" role="img" />
+              <div className={styles["region-card-image"]}>
+                <img src={regionImageUrls.get(region.enum)} alt={region.title} />
+              </div>
               <Heading priority={5} size="lg" className={styles["region-card-name"]}>
-                {region}
+                {region.title}
               </Heading>
             </ClickableCard>
           </GridCell>
