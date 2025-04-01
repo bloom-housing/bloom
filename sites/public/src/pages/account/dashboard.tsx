cbc2ca33521a7a3be59803a6b8eeecdd12b9cc4f
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2025-04-01 14:25:22 -0600

    feat: seeds siteheader (#4693)

--- sites/public/src/pages/account/dashboard.tsx
+++ sites/public/src/pages/account/dashboard.tsx
@@ -9,11 +9,11 @@ import {
   RequireLogin,
   BloomCard,
 } from "@bloom-housing/shared-helpers"
+import { Button, Card, Grid } from "@bloom-housing/ui-seeds"
 import Layout from "../../layouts/application"
 import { MetaTags } from "../../components/shared/MetaTags"
 import { UserStatus } from "../../lib/constants"
-import { Button, Card, Grid } from "@bloom-housing/ui-seeds"
-
+import MaxWidthLayout from "../../layouts/max-width"
 import styles from "./account.module.scss"
 
 interface DashboardProps {
@@ -58,7 +58,7 @@ function Dashboard(props: DashboardProps) {
           </AlertBox>
         )}
         <section className="bg-gray-300 border-t border-gray-450">
-          <div className="max-w-5xl mx-auto sm:py-8">
+          <MaxWidthLayout className={styles["dashboard-max-width-layout"]}>
             <h1 className={"sr-only"}>{t("nav.myDashboard")}</h1>
             <Grid spacing="lg" className={styles["account-card-container"]}>
               <Grid.Row columns={2}>
@@ -105,7 +105,7 @@ function Dashboard(props: DashboardProps) {
                 </Grid.Cell>
               </Grid.Row>
             </Grid>
-          </div>
+          </MaxWidthLayout>
         </section>
       </Layout>
     </RequireLogin>
