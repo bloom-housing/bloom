4755549cc16c50ecaeb346980bc846d71c3d22aa
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-04-03 15:59:59 +0200

    chore: move helper formatting functions to a separate file

--- sites/partners/src/components/listings/PaperListingForm/sections/Units.tsx
+++ sites/partners/src/components/listings/PaperListingForm/sections/Units.tsx
@@ -22,6 +22,7 @@ import { useFormContext, useWatch } from "react-hook-form"
 import { TempUnit, TempUnitGroup } from "../../../../lib/listings/formTypes"
 import { fieldHasError, fieldMessage } from "../../../../lib/helpers"
 import SectionWithGrid from "../../../shared/SectionWithGrid"
+import { formatRange, formatRentRange, minMaxFinder } from "../../helpers"
 import UnitGroupForm from "../UnitGroupForm"
 
 type UnitProps = {
@@ -31,46 +32,6 @@ type UnitProps = {
   setUnitGroups: (unitGroups: TempUnitGroup[]) => void
   disableUnitsAccordion: boolean
   featureFlags?: FeatureFlag[]
-}
-
-function minMaxFinder(range: MinMax, value: number): MinMax {
-  if (range === undefined) {
-    return {
-      min: value,
-      max: value,
-    }
-  } else {
-    range.min = Math.min(range.min, value)
-    range.max = Math.max(range.max, value)
-
-    return range
-  }
-}
-
-const formatRange = (
-  min: string | number,
-  max: string | number,
-  prefix = "",
-  postfix = ""
-): string => {
-  if (!min && !max) return ""
-  if (min == max || !max) return `${prefix}${min}${postfix}`
-  if (!min) return `${prefix}${max}${postfix}`
-  return `${prefix}${min}${postfix} - ${prefix}${max}${postfix}`
-}
-
-function formatRentRange(rent: MinMax, percent: MinMax): string {
-  let toReturn = ""
-  if (rent) {
-    toReturn += formatRange(rent.min, rent.max)
-  }
-  if (rent && percent) {
-    toReturn += ", "
-  }
-  if (percent) {
-    toReturn += formatRange(percent.min, percent.max, "", "%")
-  }
-  return toReturn
 }
 
 const FormUnits = ({
