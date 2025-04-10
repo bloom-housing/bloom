64a5b7d976aaf0dd7a359be5017557d0be319a1a
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-04-10 12:53:05 +0200

    fix: replace current images with a placeholder

--- sites/public/src/components/home/HomeRegions.tsx
+++ sites/public/src/components/home/HomeRegions.tsx
@@ -5,16 +5,6 @@ import { Grid, Link } from "@bloom-housing/ui-seeds"
 import { GridRow } from "@bloom-housing/ui-seeds/src/layout/Grid"
 import styles from "./HomeRegions.module.scss"
 
-export const regionImageUrls: Map<RegionEnum, string> = new Map([
-  [
-    RegionEnum.Greater_Downtown,
-    "https://res.cloudinary.com/exygy/image/upload/dev/greater_downtown_cpf0r9.jpg",
-  ],
-  [RegionEnum.Eastside, "https://res.cloudinary.com/exygy/image/upload/dev/eastside_cuaa5k.jpg"],
-  [RegionEnum.Southwest, "https://res.cloudinary.com/exygy/image/upload/dev/southwest_mbtr4g.jpg"],
-  [RegionEnum.Westside, "https://res.cloudinary.com/exygy/image/upload/dev/westside_qbysre.jpg"],
-])
-
 export const HomeRegions = () => {
   const availableRegions = Object.values(RegionEnum).map((region) => ({
     title: region.replace("_", " "),
@@ -28,10 +18,7 @@ export const HomeRegions = () => {
           <GridCell className={styles["region-entry"]} key={region.enum}>
             <ClickableCard className={styles["region-card"]}>
               <div className={styles["region-card-image"]}>
-                <img
-                  src={regionImageUrls.get(region.enum) || "/images/listing-fallback.png"}
-                  alt={region.title}
-                />
+                <img src={"/images/listing-fallback.png"} alt={region.title} />
               </div>
               <Link href={"/listings"} className={styles["region-card-name"]}>
                 {region.title}
