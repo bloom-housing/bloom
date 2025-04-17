3f02518641619f7fd8caa72b9f67cc23711ecee2
Morgan Ludtke <ludtkemorgan@gmail.com>
2025-04-16 19:58:56 -0500

    fix: allow duplication to not copy unit groups

--- api/src/services/listing.service.ts
+++ api/src/services/listing.service.ts
@@ -1317,9 +1317,11 @@ export class ListingService implements OnModuleInit {
                       level.monthlyRentDeterminationType,
                     percentageOfIncomeValue: level.percentageOfIncomeValue,
                     flatRentValue: level.flatRentValue,
-                    amiChart: {
-                      connect: { id: level.amiChart.id },
-                    },
+                    amiChart: level.amiChart?.id
+                      ? {
+                          connect: { id: level.amiChart.id },
+                        }
+                      : undefined,
                   })),
                 },
                 unitAccessibilityPriorityTypes:
@@ -1499,6 +1501,7 @@ export class ListingService implements OnModuleInit {
 
     if (!dto.includeUnits) {
       delete mappedListing['units'];
+      delete mappedListing['unitGroups'];
     }
 
     const newListingData: ListingCreate = {
