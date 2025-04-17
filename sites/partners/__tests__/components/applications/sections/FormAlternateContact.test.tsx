eab6a3cbcc9a3e0f4995793bcdedf3fcc8eabcb8
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-04-17 10:17:35 +0200

    chore: add test for other relationship input rendering

--- sites/partners/__tests__/components/applications/sections/FormAlternateContact.test.tsx
+++ sites/partners/__tests__/components/applications/sections/FormAlternateContact.test.tsx
@@ -1,7 +1,8 @@
-import { render, screen } from "@testing-library/react"
+import { act, render, screen } from "@testing-library/react"
 import React from "react"
 import { FormProviderWrapper } from "./helpers"
 import { FormAlternateContact } from "../../../../src/components/applications/PaperApplicationForm/sections/FormAlternateContact"
+import userEvent from "@testing-library/user-event"
 
 describe("<FormAlternateContact>", () => {
   it("renders the form with alternate contact fields", () => {
@@ -25,5 +26,26 @@ describe("<FormAlternateContact>", () => {
     expect(screen.getByLabelText(/city/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/state/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/zip code/i)).toBeInTheDocument()
+
+    expect(screen.queryByLabelText(/other relationship/i)).not.toBeInTheDocument()
+  })
+
+  it("renders other relationship input field when the 'other' option is selected", async () => {
+    render(
+      <FormProviderWrapper>
+        <FormAlternateContact />
+      </FormProviderWrapper>
+    )
+
+    const relationshipSelect = screen.getByLabelText(/relationship/i)
+    expect(relationshipSelect).toBeInTheDocument()
+
+    expect(screen.queryByLabelText(/other relationship/i)).not.toBeInTheDocument()
+    await act(() => userEvent.selectOptions(relationshipSelect, "friend"))
+    expect(relationshipSelect).toHaveValue("friend")
+    expect(screen.queryByLabelText(/other relationship/i)).not.toBeInTheDocument()
+    await act(() => userEvent.selectOptions(relationshipSelect, "other"))
+    expect(relationshipSelect).toHaveValue("other")
+    expect(await screen.findByLabelText(/other relationship/i)).toBeInTheDocument()
   })
 })
