375e62e74ec2992f93c0a0cd58fa663fcc40b589
Jared White <jared@whitefusion.studio>
2025-05-06 16:53:51 +0000

    fix: use newer CSS for Accessibility page Markdown

--- sites/public/src/pages/accessibility.tsx
+++ sites/public/src/pages/accessibility.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useContext } from "react"
-import { MarkdownSection, t } from "@bloom-housing/ui-components"
+import { t } from "@bloom-housing/ui-components"
 import Markdown from "markdown-to-jsx"
 import { PageView, pushGtmEvent, AuthContext } from "@bloom-housing/shared-helpers"
 import { UserStatus } from "../lib/constants"
 import Layout from "../layouts/application"
 import { PageHeaderLayout } from "../patterns/PageHeaderLayout"
+import styles from "../patterns/PageHeaderLayout.module.scss"
 import pageContent from "../md_content/accessibility.md"
 import { RenderIf } from "../lib/helpers"
 
@@ -24,17 +25,16 @@ const Accessibility = () => {
   return (
     <Layout>
       <PageHeaderLayout heading={pageTitle} inverse>
-        <MarkdownSection>
-          <Markdown
-            options={{
-              overrides: {
-                RenderIf,
-              },
-            }}
-          >
-            {pageContent.toString()}
-          </Markdown>
-        </MarkdownSection>
+        <Markdown
+          options={{
+            overrides: {
+              RenderIf,
+            },
+          }}
+          className={styles["markdown"]}
+        >
+          {pageContent.toString()}
+        </Markdown>
       </PageHeaderLayout>
     </Layout>
   )
