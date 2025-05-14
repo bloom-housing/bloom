8259d227e3686b6159e824e469f8b55f429e79d9
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-05-14 15:11:49 +0200

    chore: create a finder mutliselect question component

---
+++ sites/public/src/components/finder/FinderMultiselectQuestion.tsx
@@ -0,0 +1,40 @@
+import { Field } from "@bloom-housing/ui-components";
+import styles from './FinderMultiselectQuestion.module.scss'
+import { useFormContext } from "react-hook-form";
+
+export type FinderQuestion = {
+  label: string;
+  value: string | boolean;
+}
+
+type FinderMultiselectQuestionProps = {
+  legend: string
+  fieldGroupName: string;
+  options: FinderQuestion[]
+}
+
+export default function FinderMultiselectQuestion(props: FinderMultiselectQuestionProps) {
+  const { register } = useFormContext()
+
+  return (
+    <fieldset className={styles['fieldset']}>
+      <legend className={styles['fieldset-legend']}>{props.legend}</legend>
+      {props.options.map((option) =>
+        <Field
+          type="checkbox"
+          name={props.fieldGroupName}
+          id={option.label}
+          key={option.label}
+          label={option.label}
+          register={register}
+          className={styles['question-checkbox']}
+          labelClassName={styles['question-label']}
+          inputProps={{
+            value: option.value,
+          }}
+          bordered
+        />
+      )}
+    </fieldset>
+  )
+}
