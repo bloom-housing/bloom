7069f3a65bd1f3fa2ea7907342f7a18c24fdf801
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-05-14 16:07:23 +0200

    fix: update icon name after conflict resolve

--- sites/public/src/components/assistance/Assistance.tsx
+++ sites/public/src/components/assistance/Assistance.tsx
@@ -40,7 +40,7 @@ const Assistance = () => {
               title={t("assistance.additionalHousingTitle")}
               subtitle={t("assistance.additionalHousingDescription")}
               headingPriority={2}
-              iconSymbol={"bulletList"}
+              iconSymbol={"listBullet"}
               iconOutlined={true}
               variant={"block"}
               className={styles["item"]}
