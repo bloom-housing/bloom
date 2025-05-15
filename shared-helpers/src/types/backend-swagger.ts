d4c6ae560752d5cb978289fe770a50ba55b68cbe
Bartłomiej Ponikiewski <bpsushi@users.noreply.github.com>
2025-05-15 10:45:42 +0200

    feat: company website field (#4807)
    * feat: add management website field with URL validation to Leasing Agent form
    
    * feat: add setup script for PostgreSQL connection in Docker environment
    
    * feat: add management website field to Listing interface and form defaults
    
    * refactor: rearange managementWebsite field
    
    * feat: add management website field with URL validation to Listing DTO
    
    * feat: add management website field to LeasingAgent component and update related tests
    
    * feat: add contact company information to ListingView component
    
    * feat: enable company website feature flag in ListingView and ListingViewSeeds components
    
    * fix: update test to use consistent link name for website in LeasingAgent component
    
    * feat: add company website feature flag to LeasingAgent component
    
    * feat: add enableCompanyWebsite feature flag and description
    
    * fix: regenerate file using yarn generate:client
    
    * feat: enable company website display in DetailLeasingAgent component
    
    * fix: switch frontend  URL validation from managementWebsite field to backend validation only
    
    * fix: regenerate backend-swagger after merge conflicts
    
    * chore: remove class-validator dependency from package.json
    
    * fix: regenerate backend-swagger after merge
    
    * fix: add newline at end of setup-psql.sh
    
    * fix: organize feature flags alphabetically
    
    * fix: organize feature flags map alphabetically
    
    * fix: remove unused trigger method from formMethods in LeasingAgent component
    
    ---------
    
    Co-authored-by: Eric McGarry <46828798+mcgarrye@users.noreply.github.com>

--- shared-helpers/src/types/backend-swagger.ts
+++ shared-helpers/src/types/backend-swagger.ts
@@ -3939,6 +3939,9 @@ export interface Listing {
   leasingAgentTitle?: string
 
   /**  */
+  managementWebsite?: string
+
+  /**  */
   name: string
 
   /**  */
@@ -4578,6 +4581,9 @@ export interface ListingCreate {
   leasingAgentTitle?: string
 
   /**  */
+  managementWebsite?: string
+
+  /**  */
   name: string
 
   /**  */
@@ -4886,6 +4892,9 @@ export interface ListingUpdate {
 
   /**  */
   leasingAgentTitle?: string
+
+  /**  */
+  managementWebsite?: string
 
   /**  */
   name: string
@@ -7093,13 +7102,14 @@ export enum FeatureFlagEnum {
   "enableGeocodingRadiusMethod" = "enableGeocodingRadiusMethod",
   "enableHomeType" = "enableHomeType",
   "enableIsVerified" = "enableIsVerified",
-  "enableListingFavoriting" = "enableListingFavoriting",
   "enableListingOpportunity" = "enableListingOpportunity",
   "enableListingPagination" = "enableListingPagination",
   "enableMarketingStatus" = "enableMarketingStatus",
   "enableNeighborhoodAmenities" = "enableNeighborhoodAmenities",
   "enablePartnerDemographics" = "enablePartnerDemographics",
   "enablePartnerSettings" = "enablePartnerSettings",
+  "enableListingFavoriting" = "enableListingFavoriting",
+  "enableCompanyWebsite" = "enableCompanyWebsite",
   "enableRegions" = "enableRegions",
   "enableSection8Question" = "enableSection8Question",
   "enableSingleUseCode" = "enableSingleUseCode",
