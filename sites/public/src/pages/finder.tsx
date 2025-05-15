d2935b873ef93ed72ff89f308917126699564b51
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-05-15 14:41:21 +0200

    fix: remove unused imports

--- sites/public/src/pages/finder.tsx
+++ sites/public/src/pages/finder.tsx
@@ -1,7 +1,5 @@
 import RentalsFinder from "../components/finder/RentalsFinder";
 import Layout from "../layouts/application";
-import MaxWidthLayout from "../layouts/max-width";
-import { PageHeaderSection } from "../patterns/PageHeaderLayout";
 
 export default function Finder() {
   return (
