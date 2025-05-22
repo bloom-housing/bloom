3512f7f611aeccbfe13f9813716547faeede2098
Mateusz Zduniuk <137785676+matzduniuk@users.noreply.github.com>
2025-05-22 11:40:35 +0200

    feat: Rental Finder - Initial Page (#4869)
    * chore: setup new translation strings
    * chore: create a finder mutliselect question component
    * chore: create initial rental finder questioner component
    * chore: setup finder component
    * chore: implement initial finder page
    * chore: update rental finder mobile design
    * chore: add ui components nav header elements
    * chore: add new translations for rental finder rent questions
    * chore: setup and add a basic rent range question component
    * chore: update integration tests to include the progress bar and steps header
    * fix: remove unused imports
    * fix: remove unused imports
    * fix: update code formatting
    * fix: disable unbound method ts error
    * fix: update translation strings
    * fix: update RentalsFinder component to use seeds CardSection
    * chore: update styles for helper question components
    * fix: update integration tests to PR fixes
    * fix: update tests code linting
    * fix: add missing EOF new line
    * fix: alphabetize imports
    * fix: remove unnecessary new line

---
+++ sites/public/src/pages/finder.tsx
@@ -0,0 +1,10 @@
+import RentalsFinder from "../components/finder/RentalsFinder"
+import Layout from "../layouts/application"
+
+export default function Finder() {
+  return (
+    <Layout>
+      <RentalsFinder />
+    </Layout>
+  )
+}
