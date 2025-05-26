07bcebada2e1f1d70fdf56e6d3df1db300e5ca1f
Morgan Ludtke <42942267+ludtkemorgan@users.noreply.github.com>
2025-05-25 21:46:54 -0500

    fix: allow listing approval to be turned off for partners (#4803) (#880)

--- api/prisma/seed-staging.ts
+++ api/prisma/seed-staging.ts
@@ -929,7 +929,7 @@ export const stagingSeed = async (
           },
           email: `partner-user-${savedListing.name
             .toLowerCase()
-            .replace(' ', '')}@example.com`,
+            .replaceAll(' ', '-')}@example.com`,
           confirmedAt: new Date(),
           jurisdictionIds: [
             savedListing.jurisdictionId,
