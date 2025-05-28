8ac3eefc8db4fc30f621c918107e6ebeb63e77a4
Emily Jablonski <emijablonski@gmail.com>
2025-05-28 16:16:33 -0600

    test: fix queries

--- sites/public/src/components/home/HomeUnderConstruction.tsx
+++ sites/public/src/components/home/HomeUnderConstruction.tsx
@@ -1,7 +1,5 @@
 import { Jurisdiction, Listing } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
-import { GridCell } from "@bloom-housing/ui-components"
 import { Grid } from "@bloom-housing/ui-seeds"
-import { GridRow } from "@bloom-housing/ui-seeds/src/layout/Grid"
 import { MinimalListingCard } from "./MinimalListingCard"
 
 interface HomeUnderConstructionProps {
@@ -12,13 +10,13 @@ interface HomeUnderConstructionProps {
 export const HomeUnderConstruction = (props: HomeUnderConstructionProps) => {
   return (
     <Grid>
-      <GridRow columns={3}>
+      <Grid.Row columns={3}>
         {props.listings.map((listing, index) => (
-          <GridCell key={index}>
+          <Grid.Cell key={index}>
             <MinimalListingCard listing={listing} jurisdiction={props.jurisdiction} />
-          </GridCell>
+          </Grid.Cell>
         ))}
-      </GridRow>
+      </Grid.Row>
     </Grid>
   )
 }
