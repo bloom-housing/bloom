b673fb3843f5d430382cb1a9d013640ee2ed6dbd
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-05-29 15:51:57 +0200

    chore: add feature flag based finder question toggling

--- sites/public/src/pages/finder.tsx
+++ sites/public/src/pages/finder.tsx
@@ -1,10 +1,30 @@
+import {
+  FeatureFlagEnum,
+  Jurisdiction,
+} from "@bloom-housing/shared-helpers/src/types/backend-swagger"
 import RentalsFinder from "../components/finder/RentalsFinder"
 import Layout from "../layouts/application"
+import { fetchJurisdictionByName } from "../lib/hooks"
 
-export default function Finder() {
+export default function Finder(props) {
+  const activeFeatureFlags: FeatureFlagEnum[] = (
+    props.jurisdiction as Jurisdiction
+  ).featureFlags.reduce((acc, featureFlag) => featureFlag.active && [...acc, featureFlag.name], [])
+
   return (
     <Layout>
-      <RentalsFinder />
+      <RentalsFinder activeFeatureFalgs={activeFeatureFlags} />
     </Layout>
   )
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function getStaticProps(context: { req: any; query: any }) {
+  const jurisdiction = await fetchJurisdictionByName(context.req)
+
+  return {
+    props: {
+      jurisdiction: jurisdiction,
+    },
+  }
 }
