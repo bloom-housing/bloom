a6d17c091f435e25617df7a2ba0cc02e70e5b5ce
ColinBuyck <53269332+ColinBuyck@users.noreply.github.com>
2025-05-28 17:36:51 -0700

    feat: filtering pt 2 + 3 (#4868)
    * feat: seeds filter drawer (#4814)
    
    * fix: starting filter development
    
    * fix: tighter font styling
    
    * fix: basic checkbox styling
    
    * fix: form validation
    
    * refactor: new filter fields approach
    
    * fix: linting cleaned
    
    * fix: validation defeat
    
    * fix: testing toolkit
    
    * fix: filtering without validation
    
    * fix: string clean up
    
    * fix: string clean up
    
    * fix: translations tweak
    
    * fix: test tweaks
    
    * fix: improved form data shape
    
    * fix: tidy up
    
    * fix: add in listing tabs
    
    * fix: partial encoding implementation
    
    * fix: filtering + selection saving
    
    * fix: handle submit approach
    
    * fix: functioning submit
    
    * fix: url support in progress
    
    * fix: encoding + decoding exploration
    
    * fix: lint clean up
    
    * fix: functional encoding/decoding with logs
    
    * fix: clean up naming
    
    * fix: undefined check
    
    * fix: linting
    
    * fix: testing fix
    
    * fix: implement flagging and string booleans
    
    * fix: shift encoding + decoding approach
    
    * fix: testing find
    
    * fix: emily comments
    
    * fix: feature flag cleanup
    
    * fix: remove key zero
    
    * fix: add linting
    
    * fix: add issue commenting
    
    * fix: move to browse folder
    
    * fix: start on testing
    
    * fix: unit type bug
    
    * fix: remove empty file
    
    * fix: update lakeview seed
    
    * fix: tidying and alphabetizing
    
    * fix: test stubs and jsdocs

--- sites/public/src/pages/listings-closed.tsx
+++ sites/public/src/pages/listings-closed.tsx
@@ -2,21 +2,42 @@ import React from "react"
 import { fetchClosedListings, fetchJurisdictionByName } from "../lib/hooks"
 import { ListingBrowse, TabsIndexEnum } from "../components/browse/ListingBrowse"
 import { ListingsProps } from "./listings"
+import {
+  decodeQueryToFilterData,
+  encodeFilterDataToBackendFilters,
+  isFiltered,
+} from "../components/browse/FilterDrawerHelpers"
+import { useRouter } from "next/router"
 
 export default function ListingsPageClosed(props: ListingsProps) {
+  const router = useRouter()
+
   return (
     <ListingBrowse
       listings={props.closedListings}
       tab={TabsIndexEnum.closed}
       jurisdiction={props.jurisdiction}
       paginationData={props.paginationData}
+      key={router.asPath}
     />
   )
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function getServerSideProps(context: { req: any; query: any }) {
-  const closedListings = await fetchClosedListings(context.req, Number(context.query.page) || 1)
+  let closedListings
+
+  if (isFiltered(context.query)) {
+    const filterData = decodeQueryToFilterData(context.query)
+    const filters = encodeFilterDataToBackendFilters(filterData)
+    closedListings = await fetchClosedListings(
+      context.req,
+      Number(context.query.page) || 1,
+      filters
+    )
+  } else {
+    closedListings = await fetchClosedListings(context.req, Number(context.query.page) || 1)
+  }
   const jurisdiction = await fetchJurisdictionByName(context.req)
 
   return {
