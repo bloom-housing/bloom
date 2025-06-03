ef17e2829fe86f38f6fb8531858c1d6af517c4b7
Emily Jablonski <emijablonski@gmail.com>
2025-06-03 14:14:55 -0600

    fix: see all under construction button

--- sites/public/src/components/home/HomeUnderConstruction.tsx
+++ sites/public/src/components/home/HomeUnderConstruction.tsx
@@ -1,5 +1,6 @@
 import { Jurisdiction, Listing } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
-import { Grid } from "@bloom-housing/ui-seeds"
+import { t } from "@bloom-housing/ui-components"
+import { Button, Grid } from "@bloom-housing/ui-seeds"
 import { MinimalListingCard } from "./MinimalListingCard"
 
 interface HomeUnderConstructionProps {
@@ -9,14 +10,21 @@ interface HomeUnderConstructionProps {
 
 export const HomeUnderConstruction = (props: HomeUnderConstructionProps) => {
   return (
-    <Grid>
-      <Grid.Row columns={3}>
-        {props.listings.map((listing, index) => (
-          <Grid.Cell key={index}>
-            <MinimalListingCard listing={listing} jurisdiction={props.jurisdiction} />
-          </Grid.Cell>
-        ))}
-      </Grid.Row>
-    </Grid>
+    <>
+      <Grid>
+        <Grid.Row columns={3}>
+          {props.listings.map((listing, index) => (
+            <Grid.Cell key={index}>
+              <MinimalListingCard listing={listing} jurisdiction={props.jurisdiction} />
+            </Grid.Cell>
+          ))}
+        </Grid.Row>
+      </Grid>
+      <div className={"seeds-m-bs-content"}>
+        <Button size={"sm"} variant="primary-outlined" href={"listings?availabilities=comingSoon"}>
+          {t("welcome.underConstructionButton")}
+        </Button>
+      </div>
+    </>
   )
 }
