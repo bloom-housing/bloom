3060bab8c7444eaed19febd0da1cd4a4b4f307d9
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-06-04 11:49:13 +0200

    fix: fix unit group drawer form data reset

--- sites/partners/src/components/listings/PaperListingForm/UnitGroupForm.tsx
+++ sites/partners/src/components/listings/PaperListingForm/UnitGroupForm.tsx
@@ -16,6 +16,7 @@ import {
   AmiChart,
   EnumUnitGroupAmiLevelMonthlyRentDeterminationType,
   UnitAccessibilityPriorityType,
+  YesNoEnum,
 } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
 import { arrayToFormOptions, fieldHasError } from "../../../lib/helpers"
 import { TempAmiLevel, TempUnitGroup } from "../../../lib/listings/formTypes"
@@ -98,20 +99,6 @@ const UnitGroupForm = ({
     { label: "5", value: "5" },
   ]
 
-  const waitlistStatusOptions = [
-    {
-      id: "true",
-      label: t("listings.listingStatus.active"),
-      value: "true",
-      defaultChecked: true,
-    },
-    {
-      id: "false",
-      label: t("listings.listingStatus.closed"),
-      value: "false",
-    },
-  ]
-
   // sets the options for the ami charts
   useEffect(() => {
     if (amiCharts.length === 0 || amiChartsOptions.length) return
@@ -149,6 +136,7 @@ const UnitGroupForm = ({
 
       reset({
         ...defaultUnitGroup,
+        openWaitlist: defaultUnitGroup.openWaitlist ? YesNoEnum.yes : YesNoEnum.no,
         unitTypes: defaultUnitGroup?.unitTypes?.map((elem) => elem.id ?? elem.toString()),
       })
     }
@@ -531,7 +519,20 @@ const UnitGroupForm = ({
                   <FieldGroup
                     name="openWaitlist"
                     type="radio"
-                    fields={waitlistStatusOptions}
+                    fields={[
+                      {
+                        id: "waitlistStatusOpen",
+                        dataTestId: "waitlistStatusOpen",
+                        label: t("listings.listingStatus.active"),
+                        value: YesNoEnum.yes,
+                      },
+                      {
+                        id: "waitlistStatusClosed",
+                        dataTestId: "waitlistStatusClosed",
+                        label: t("listings.listingStatus.closed"),
+                        value: YesNoEnum.no,
+                      },
+                    ]}
                     register={register}
                     fieldClassName="m-0"
                     fieldGroupClassName="flex h-12 items-center"
