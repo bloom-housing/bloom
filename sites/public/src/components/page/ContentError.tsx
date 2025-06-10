79dcc8ba22bb27668237c73cc8019c97f53e9356
Emily Jablonski <emijablonski@gmail.com>
2025-06-09 19:01:36 -0600

    fix: add error page

--- sites/public/src/components/page/ContentError.tsx
+++ sites/public/src/components/page/ContentError.tsx
@@ -3,12 +3,12 @@ import { t } from "@bloom-housing/ui-components"
 import { Button } from "@bloom-housing/ui-seeds"
 import { Hero } from "../../patterns/Hero"
 
-export const Content404 = () => {
+export const ContentError = () => {
   return (
     <Hero
-      title={t("errors.notFound.title")}
-      subtitle={t("errors.notFound.subtitle")}
-      note={"404"}
+      title={t("errors.somethingWentWrong")}
+      subtitle={t("authentication.signIn.errorGenericMessage")}
+      note={"500"}
       action={
         <Button variant="primary-outlined" href="/">
           {t("errors.backToHome")}
