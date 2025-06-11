ecb1ec5df1816180484890b4f7dd3de5e1091f1a
bpsushi <bp.login1@gmail.com>
2025-06-11 21:23:44 +0200

    chore: emove default timestamps from listings

--- api/prisma/migrations/20250304175121_add_required_listing_fields/migration.sql
+++ api/prisma/migrations/20250304175121_add_required_listing_fields/migration.sql
@@ -1,7 +1,2 @@
 -- AlterTable
 ALTER TABLE "jurisdictions" ADD COLUMN     "required_listing_fields" TEXT[] DEFAULT ARRAY[]::TEXT[];
-
--- AlterTable
-ALTER TABLE "listings" ALTER COLUMN "afs_last_run_at" SET DEFAULT '1970-01-01 00:00:00-07'::timestamp with time zone,
-ALTER COLUMN "last_application_update_at" SET DEFAULT '1970-01-01 00:00:00-07'::timestamp with time zone,
-ALTER COLUMN "requested_changes_date" SET DEFAULT '1970-01-01 00:00:00-07'::timestamp with time zone;
