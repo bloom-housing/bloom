52e270acd4ae9f5d1467a70c3d3a047ad865ee46
Bartłomiej Ponikiewski <bpsushi@users.noreply.github.com>
2025-07-03 01:15:09 +0200

    feat: dynamic required fields for listings (#4942)
    * fix: initial testing
    
    * fix: more changes
    
    * fix: more changes
    
    * feat: add memory-bank directory to .gitignore
    
    * feat: refactor listing service and validation pipe to handle required fields
    
    - Updated ListingService
    - Enhanced ListingCreateUpdateValidationPipe
    - Adjusted integration tests to reflect changes in DTOs and validation logic
    
    * feat: add GitHub Copilot instructions to .gitignore
    
    * refactor: extract units availability calculation into a separate method
    
    * refactor: make calculateUnitsAvailable method public for testing
    
    * feat: partially implement dynamic required fields validation for listings based on jurisdiction
    
    * refactor: update validation pipe usage and replace hardcoded names with random names in tests
    
    * refactor: replace hardcoded jurisdiction names with random names in tests
    
    * chore: regenerate client
    
    * chore: emove default timestamps from listings
    
    * chore: rename migration dirs
    
    * feat: add required listing fields for LakeView in  staging seed data
    
    * chore: remove unused validate-listing-publish decorator file
    
    * chore: remove unused import from listing-update DTO
    
    * chore: remove unused imports from validate-listing-publish decorator
    
    * fix: enhance isPublishing method to handle undefined, null, and empty string statuses
    
    * refactor: update migration.sql comments and ensure index creation only if it doesn't exist
    
    * fix: rollback already applaied migration name and rename new migration
    
    * chore: remove obsolete migration files for user_id index and required_listing_fields
    
    * test: enhance error message validation for required fields in listings
    
    * fix: review comments
    
    * fix: update validation for units
    
    * fix: handle unit vs unit groups better
    
    * fix: test fix
    
    * fix: add suggested comments
    
    ---------
    
    Co-authored-by: Morgan Ludtke <ludtkemorgan@gmail.com>
    Co-authored-by: Morgan Ludtke <42942267+ludtkemorgan@users.noreply.github.com>

--- api/prisma/seed-staging.ts
+++ api/prisma/seed-staging.ts
@@ -66,6 +66,21 @@ export const stagingSeed = async (
         FeatureFlagEnum.enableUtilitiesIncluded,
       ],
       languages: Object.values(LanguagesEnum),
+      requiredListingFields: [
+        'listingsBuildingAddress',
+        'name',
+        'developer',
+        'listingImages',
+        'leasingAgentEmail',
+        'leasingAgentName',
+        'leasingAgentPhone',
+        'jurisdictions',
+        'units',
+        'digitalApplication',
+        'paperApplication',
+        'referralOpportunity',
+        'rentalAssistance',
+      ],
     }),
   });
   // jurisdiction with unit groups enabled
@@ -98,6 +113,7 @@ export const stagingSeed = async (
         FeatureFlagEnum.hideCloseListingButton,
         FeatureFlagEnum.swapCommunityTypeWithPrograms,
       ],
+      requiredListingFields: ['name', 'listingsBuildingAddress'],
     }),
   });
   // Basic configuration jurisdiction
--- api/src/decorators/validate-units-required.decorator.ts
+++ api/src/decorators/validate-units-required.decorator.ts
@@ -2,28 +2,31 @@ import {
   registerDecorator,
   ValidationOptions,
   ValidationArguments,
+  ValidationTypes,
 } from 'class-validator';
 import { PrismaService } from '../services/prisma.service';
-import { ListingPublishedUpdate } from '../dtos/listings/listing-published-update.dto';
+import { ListingUpdate } from '../dtos/listings/listing-update.dto';
 import { FeatureFlagEnum } from '../enums/feature-flags/feature-flags-enum';
+import { ListingsStatusEnum } from '@prisma/client';
 
 /*
-  Validates if validated value is array with at least 1 element 
-  only when the jurisdiction has the feature flag enableUnitGroups enabled.
-  (used for units as they should not be required when unitGroups are used)
+  Validates if jurisdiction can handle either unit or unit group and 
+  fail if the opposite is sent
 */
-export function ValidateUnitsRequired(validationOptions?: ValidationOptions) {
+export function ValidateOnlyUnitsOrUnitGroups(
+  validationOptions?: ValidationOptions,
+) {
   return function (object: object, propertyName: string) {
     registerDecorator({
-      name: 'validateUnitsRequired',
+      name: 'validateOnlyUnitsOrUnitGroups',
       target: object.constructor,
       propertyName: propertyName,
       options: validationOptions,
       async: true,
       validator: {
         async validate(value: unknown[], args: ValidationArguments) {
-          const jurisdictionId = (args.object as ListingPublishedUpdate)
-            .jurisdictions?.id;
+          const object = args.object as ListingUpdate;
+          const jurisdictionId = object.jurisdictions?.id;
           if (!jurisdictionId) return true;
 
           const prisma = new PrismaService();
@@ -37,10 +40,67 @@ export function ValidateUnitsRequired(validationOptions?: ValidationOptions) {
               flag.name === FeatureFlagEnum.enableUnitGroups && flag.active,
           );
 
-          return hasUnitGroupsEnabled || (value && value.length > 0);
+          // Not valid to have units on unit group jurisdiction
+          if (
+            propertyName === 'units' &&
+            hasUnitGroupsEnabled &&
+            value &&
+            value.length > 0
+          ) {
+            return false;
+          }
+
+          // Not valid to have unit groups on a units jurisdiction
+          if (
+            propertyName === 'unitGroups' &&
+            !hasUnitGroupsEnabled &&
+            value &&
+            value.length > 0
+          ) {
+            return false;
+          }
+
+          return true;
         },
         defaultMessage: (args: ValidationArguments) =>
-          `${args.property} must contain at least 1 elements`,
+          `${args.property} cannot exist on this jurisdiction`,
+      },
+    });
+  };
+}
+
+/*
+  Validates if validated value is array with at least 1 element for publishing if in requiredList
+  This validation should pass regardless of value for non-publishing listing
+*/
+export function ValidateAtLeastOneUnit(validationOptions?: ValidationOptions) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: ValidationTypes.IS_DEFINED,
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      async: true,
+      validator: {
+        async validate(value: unknown[], args: ValidationArguments) {
+          const object = args.object as ListingUpdate;
+          // At least 1 unit or unit group is only required if publishing the listing
+          if (
+            !(
+              ListingsStatusEnum.active === object.status ||
+              ListingsStatusEnum.pendingReview === object.status
+            )
+          ) {
+            return true;
+          }
+
+          const hasFieldRequired =
+            object?.requiredFields?.includes(propertyName);
+
+          return !hasFieldRequired || (value && value.length > 0);
+        },
+        defaultMessage: (args: ValidationArguments) =>
+          `${args.property} must contain at least 1 element`,
       },
     });
   };
