5af389575b3aa08c9dbcce48f716bbb998f2f38c
Sarah Keller <sarahkeller@Mac.lan>
2025-07-07 17:46:45 -0400

    fix: add -3 back to Sign Out selector

--- sites/public/cypress/support/commands.js
+++ sites/public/cypress/support/commands.js
@@ -23,7 +23,7 @@ Cypress.Commands.add("signOut", () => {
     // data-testid for SiteHeader in this path is set in ui-components
     // See https://github.com/bloom-housing/ui-components/blob/c35c094554e8199f202d67a405272035189060ec/src/headers/SiteHeader.tsx#L175
     cy.get(`[data-testid="My Account-2"]`).trigger("mouseover")
-    cy.get(`[data-testid="Sign Out"]`).trigger("click")
+    cy.get(`[data-testid="Sign Out-3"]`).trigger("click")
   }
 })
 
