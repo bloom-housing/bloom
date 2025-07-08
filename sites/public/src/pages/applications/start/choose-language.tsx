c7ce0b0791073192439c565ee6e8bc0a17f4c35c
ColinBuyck <53269332+ColinBuyck@users.noreply.github.com>
2025-07-08 10:51:40 -0700

    fix: add null checks (#5055)

--- sites/public/src/pages/applications/start/choose-language.tsx
+++ sites/public/src/pages/applications/start/choose-language.tsx
@@ -191,7 +191,7 @@ const ApplicationChooseLanguage = () => {
           </CardSection>
         )}
 
-        {listing?.applicationConfig.languages.length > 1 && (
+        {listing?.applicationConfig?.languages?.length && (
           <CardSection divider={"flush"}>
             <>
               <Heading priority={2} size={"lg"} className={"mb-10"}>
