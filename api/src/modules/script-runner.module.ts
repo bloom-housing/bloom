d1a8d791ecc5afbb588ff6002d2f8d543f128dab
Eric McGarry <eric@eric.mcgarry.name>
2025-07-10 10:04:24 -0400

    feat: migrate detroit to multiselect questions (#4851)
    * feat: migrate detroit to multiselect questions
    
    * chore: remove unused endpoint
    
    * feat: lint cleanup
    
    * feat: simplify logic and add helper tests
    
    4763
    
    * feat: re-remove unneeded test
    
    * feat: improve for loop

--- api/src/modules/script-runner.module.ts
+++ api/src/modules/script-runner.module.ts
@@ -3,6 +3,7 @@ import { ScriptRunnerController } from '../controllers/script-runner.controller'
 import { ScriptRunnerService } from '../services/script-runner.service';
 import { AmiChartModule } from './ami-chart.module';
 import { FeatureFlagModule } from './feature-flag.module';
+import { MultiselectQuestionModule } from './multiselect-question.module';
 import { PermissionModule } from './permission.module';
 import { PrismaModule } from './prisma.module';
 import { AssetModule } from './asset.module';
@@ -11,6 +12,7 @@ import { AssetModule } from './asset.module';
   imports: [
     AmiChartModule,
     FeatureFlagModule,
+    MultiselectQuestionModule,
     PermissionModule,
     PrismaModule,
     AssetModule,
