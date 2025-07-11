d09af61beecccaba6f5f469e768aeda7ae2c7e23
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-07-11 14:02:04 +0200

    fix: disable multiple any typescript any lint errors

--- shared-helpers/src/utilities/resolveObject.ts
+++ shared-helpers/src/utilities/resolveObject.ts
@@ -1,5 +1,6 @@
-// eslint-disable @typescript-eslint/no-explicit-any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const resolveObject = (path: string, obj: any, separator = ".") => {
   const properties = Array.isArray(path) ? path : path.split(separator)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return properties.reduce((prev, curr) => prev && prev[curr as any], obj)
 }
