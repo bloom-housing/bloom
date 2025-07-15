6701abccd040edfdff0479d7d9faa319d95a89df
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-07-15 12:48:54 +0200

    chore: update resource card to use markdown for content rendering

--- sites/public/src/components/resources/ResourceCard.tsx
+++ sites/public/src/components/resources/ResourceCard.tsx
@@ -1,19 +1,50 @@
-import { Card, Link } from "@bloom-housing/ui-seeds"
+import { Card, Heading, Link } from "@bloom-housing/ui-seeds"
 import styles from "./ResourceCard.module.scss"
+import Markdown from "markdown-to-jsx"
+import { RenderIf } from "../../lib/helpers"
 
 export interface ResourceCardProps {
   title: string
-  href: string
   content: string
+  href?: string
 }
 
 const ResourceCard = ({ title, href, content }: ResourceCardProps) => {
   return (
     <Card className={styles["resource-card"]}>
-      <Link hideExternalLinkIcon={true} href={href} className={styles["resource-card-title"]}>
-        {title}
-      </Link>
-      <div className={styles["resource-card-content"]}>{content}</div>
+      {href ? (
+        <Link hideExternalLinkIcon={true} href={href} className={styles["resource-card-title"]}>
+          {title}
+        </Link>
+      ) : (
+        <Heading priority={3} className={styles["resource-card-title"]}>
+          {title}
+        </Heading>
+      )}
+
+      <Markdown
+        options={{
+          overrides: {
+            RenderIf,
+            a: {
+              component: ({ children, ...props }) => {
+                if (props.href?.startsWith("tel:") || props.href?.startsWith("mailto:")) {
+                  return <a {...props}>{children}</a>
+                } else {
+                  // Make sure standard web URLs will open in a new tab
+                  return (
+                    <a {...props} target="_blank">
+                      {children}
+                    </a>
+                  )
+                }
+              },
+            },
+          },
+        }}
+        className={styles["resource-card-content"]}
+        children={content}
+      />
     </Card>
   )
 }
