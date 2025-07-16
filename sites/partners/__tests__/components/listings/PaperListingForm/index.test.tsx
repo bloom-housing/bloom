8afc5ae8823bd72a0e34a99a491265e4f694159c
Emily Jablonski <emijablonski@gmail.com>
2025-07-16 09:54:15 -0600

    fix: test

--- sites/partners/__tests__/components/listings/PaperListingForm/index.test.tsx
+++ sites/partners/__tests__/components/listings/PaperListingForm/index.test.tsx
@@ -147,7 +147,7 @@ describe("add listing", () => {
         "Applicants will be contacted by the property agent in rank order until vacancies are filled. All of the information that you have provided will be verified and your eligibility confirmed. Your application will be removed from the waitlist if you have made any fraudulent statements. If we cannot verify a housing preference that you have claimed, you will not receive the preference but will not be otherwise penalized. Should your application be chosen, be prepared to fill out a more detailed application and provide required supporting documents."
       )
     ).toBeInTheDocument()
-    expect(screen.getByText("549 / 4000 characters")).toBeInTheDocument()
+    expect(screen.getByText("You have 1451 characters remaining")).toBeInTheDocument()
     expect(screen.getByLabelText("Bold")).toBeInTheDocument()
     expect(screen.getByLabelText("Bullet list")).toBeInTheDocument()
     expect(screen.getByLabelText("Numbered list")).toBeInTheDocument()
