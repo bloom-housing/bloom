ddcad5f57d200b3b8edf41ddcc59431f91e9ce88
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-07-16 13:37:58 +0200

    chore: update assistance page content to match production

--- sites/public/src/components/assistance/Assistance.tsx
+++ sites/public/src/components/assistance/Assistance.tsx
@@ -1,11 +1,15 @@
 import Head from "next/head"
 import Link from "next/link"
+import Markdown from "markdown-to-jsx"
 import { Card, t } from "@bloom-housing/ui-components"
 import { Heading } from "@bloom-housing/ui-seeds"
+import { RenderIf } from "../../lib/helpers"
 import Layout from "../../layouts/application"
 import { PageHeaderLayout } from "../../patterns/PageHeaderLayout"
 import styles from "./Assistance.module.scss"
 import { BloomCard } from "@bloom-housing/shared-helpers"
+
+import contactInfo from "../../md_content/resources/contact_info.md"
 
 const Assistance = () => {
   const pageTitle = t("pageTitle.getAssistance")
@@ -23,24 +27,24 @@ const Assistance = () => {
         <article className={styles["site-content"]}>
           <div className={styles["items-wrapper"]}>
             <BloomCard
-              title={t("assistance.applyToHousingTitle")}
-              subtitle={t("assistance.applyToHousingDescription")}
+              title={t("assistance.housingBasicsTitle")}
+              subtitle={t("assistance.housingBasicsDescription")}
               headingPriority={2}
-              iconSymbol={"home"}
+              iconSymbol={"questionMarkCircle"}
               iconOutlined={true}
               variant={"block"}
               className={styles["item"]}
               iconClass={styles["item-icon"]}
             >
               <Card.Section className={styles["item-link"]}>
-                <Link href={"/housing-basics"}>{t("assistance.applyToHousingLink")}</Link>
+                <Link href={"/housing-basics"}>{t("assistance.housingBasicsLink")}</Link>
               </Card.Section>
             </BloomCard>
             <BloomCard
               title={t("assistance.additionalHousingTitle")}
               subtitle={t("assistance.additionalHousingDescription")}
               headingPriority={2}
-              iconSymbol={"listBullet"}
+              iconSymbol={"home"}
               iconOutlined={true}
               variant={"block"}
               className={styles["item"]}
@@ -57,16 +61,18 @@ const Assistance = () => {
                 <Heading size="xl" priority={2}>
                   {t("resources.contactTitle")}
                 </Heading>
-                <p className={styles["contact-card-description"]}>
-                  {t("resources.contactDescription")}
-                </p>
-              </div>
-              <div className={styles["contact-card-subsection"]}>
                 <p className={styles["contact-card-info"]}>{t("resources.contactInfo")}</p>
-                <Link href={`mailto:${t("resources.contactEmail")}`}>
-                  {t("resources.contactEmail")}
-                </Link>
               </div>
+              <Markdown
+                options={{
+                  overrides: {
+                    RenderIf,
+                  },
+                }}
+                className={styles["contact-card-subsection"]}
+              >
+                {contactInfo.toString()}
+              </Markdown>
             </Card>
           </aside>
         </article>
