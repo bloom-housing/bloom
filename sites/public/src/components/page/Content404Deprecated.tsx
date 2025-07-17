f2b8d776d020ce7c9b2eaf339471377e96b5fb21
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2025-07-17 12:54:59 -0600

    fix: ui in deprecated 404 (#5096)

--- sites/public/src/components/page/Content404Deprecated.tsx
+++ sites/public/src/components/page/Content404Deprecated.tsx
@@ -8,17 +8,19 @@ export const Content404Deprecated = () => {
 
   return (
     <>
-      <Hero title={pageTitle} buttonTitle={t("welcome.seeRentalListings")} buttonLink="/listings">
+      <Hero
+        title={t("errors.notFound.title")}
+        buttonTitle={t("welcome.seeRentalListings")}
+        buttonLink="/listings"
+      >
         {t("errors.notFound.message")}
       </Hero>
       <div className="homepage-extra">
         <MaxWidthLayout className={"seeds-p-b-container"}>
-          <>
-            <p className={"seeds-m-be-header"}>{t("welcome.seeMoreOpportunities")}</p>
-            <Button variant="primary-outlined" href="/help/housing-help">
-              {t("welcome.viewAdditionalHousing")}
-            </Button>
-          </>
+          <p className={"seeds-m-be-header text-center"}>{t("welcome.seeMoreOpportunities")}</p>
+          <Button variant="primary-outlined" href="/additional-resources">
+            {t("welcome.viewAdditionalHousing")}
+          </Button>
         </MaxWidthLayout>
       </div>
     </>
