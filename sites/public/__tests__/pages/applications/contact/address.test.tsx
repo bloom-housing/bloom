bbcc011864150a977963807949d284de18cbf6eb
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-07-22 14:19:04 +0200

    chore: add integration test for the new feature flag

--- sites/public/__tests__/pages/applications/contact/address.test.tsx
+++ sites/public/__tests__/pages/applications/contact/address.test.tsx
@@ -1,8 +1,16 @@
 import React from "react"
 import { setupServer } from "msw/lib/node"
-import { fireEvent } from "@testing-library/react"
+import { fireEvent, screen } from "@testing-library/react"
 import { mockNextRouter, render } from "../../../testUtils"
 import ApplicationAddress from "../../../../src/pages/applications/contact/address"
+import ApplicationConductor from "../../../../src/lib/applications/ApplicationConductor"
+import { listing } from "@bloom-housing/shared-helpers/__tests__/testHelpers"
+import {
+  AppSubmissionContext,
+  retrieveApplicationConfig,
+} from "../../../../src/lib/applications/AppSubmissionContext"
+import { FeatureFlagEnum } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
+import { blankApplication } from "@bloom-housing/shared-helpers"
 
 window.scrollTo = jest.fn()
 
@@ -67,6 +75,59 @@ describe("applications pages", () => {
       fireEvent.click(getByText("I don't have a telephone number"))
       expect((await findByTestId("app-primary-phone-number")).firstChild).toBeDisabled()
       expect(await findByTestId("app-primary-phone-number-type")).toBeDisabled()
+    })
+
+    it("should hide workk in region question when flag enabled", () => {
+      const conductor = new ApplicationConductor({}, listing)
+      const applicationConfig = retrieveApplicationConfig(conductor.listing)
+      conductor.config = {
+        ...applicationConfig,
+        languages: [],
+        featureFlags: [
+          {
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            id: "test_id",
+            name: FeatureFlagEnum.disableWorkInRegion,
+            active: true,
+            description: "",
+            jurisdictions: [],
+          },
+        ],
+      }
+
+      render(
+        <AppSubmissionContext.Provider
+          value={{
+            conductor: conductor,
+            application: JSON.parse(JSON.stringify(blankApplication)),
+            listing: listing,
+            syncApplication: jest.fn(),
+            syncListing: jest.fn(),
+          }}
+        >
+          <ApplicationAddress />
+        </AppSubmissionContext.Provider>
+      )
+
+      expect(
+        screen.getByText("Now we need to know how to contact you about your application.", {
+          exact: false,
+        })
+      ).toBeInTheDocument()
+      expect(screen.getByTestId("app-primary-phone-number")).toBeInTheDocument()
+      expect(screen.getByTestId("app-primary-phone-number-type")).toBeInTheDocument()
+      expect(screen.getByTestId("app-primary-no-phone")).toBeInTheDocument()
+      expect(screen.getByTestId("app-primary-additional-phone")).toBeInTheDocument()
+      expect(screen.getByTestId("app-primary-address-street")).toBeInTheDocument()
+      expect(screen.getByTestId("app-primary-address-street2")).toBeInTheDocument()
+      expect(screen.getByTestId("app-primary-address-city")).toBeInTheDocument()
+      expect(screen.getByTestId("app-primary-address-state")).toBeInTheDocument()
+      expect(screen.getByTestId("app-primary-address-zip")).toBeInTheDocument()
+      expect(screen.getByTestId("app-primary-send-to-mailing")).toBeInTheDocument()
+      expect(screen.getAllByTestId("app-primary-contact-preference")).toHaveLength(4)
+      expect(screen.queryByTestId("app-primary-work-in-region-yes")).not.toBeInTheDocument()
+      expect(screen.queryByTestId("app-primary-work-in-region-no")).not.toBeInTheDocument()
     })
   })
 })
