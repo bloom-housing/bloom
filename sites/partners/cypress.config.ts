37b0e57320508dd011024d2f2909d72e152b462d
Emily Jablonski <emijablonski@gmail.com>
2025-07-23 15:16:21 -0600

    feat: axe developer hub setup

--- sites/partners/cypress.config.ts
+++ sites/partners/cypress.config.ts
@@ -1,24 +1,38 @@
 import { defineConfig } from "cypress"
+import { cypressConfig } from "@axe-core/watcher"
+import dotenv from "dotenv"
+dotenv.config()
 
-export default defineConfig({
-  defaultCommandTimeout: 50000,
-  projectId: "bloom-partners-reference",
-  numTestsKeptInMemory: 0,
-  trashAssetsBeforeRuns: true,
-  env: {
-    codeCoverage: {
-      url: "/api/__coverage__",
+export default defineConfig(
+  cypressConfig({
+    axe: {
+      apiKey: process.env.AXE_DEVELOPER_HUB_API_KEY,
     },
-  },
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      return require("./cypress/plugins/index.js")(on, config)
+    defaultCommandTimeout: 50000,
+    projectId: "bloom-partners-reference",
+    numTestsKeptInMemory: 0,
+    trashAssetsBeforeRuns: true,
+    env: {
+      codeCoverage: {
+        url: "/api/__coverage__",
+      },
     },
-    baseUrl: "http://localhost:3001",
-    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
-    experimentalRunAllSpecs: true,
-  },
-})
+    e2e: {
+      // We've imported your old cypress plugins here.
+      // You may want to clean this up later by importing these.
+      setupNodeEvents(on, config) {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        return require("./cypress/plugins/index.js")(on, config)
+      },
+      baseUrl: "http://localhost:3001",
+      specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+      experimentalRunAllSpecs: true,
+    },
+    component: {
+      devServer: {
+        framework: "next",
+        bundler: "webpack",
+      },
+    },
+  })
+)
--- sites/partners/cypress/e2e/default/02-mfa.spec.ts
+++ sites/partners/cypress/e2e/default/02-mfa.spec.ts
@@ -1,3 +1,7 @@
+afterEach(() => {
+  cy.axeWatcherFlush()
+})
+
 describe("Log in using MFA Tests", () => {
   it("should log in using mfa pathway", () => {
     cy.intercept("POST", "api/adapter/auth/request-mfa-code", {
