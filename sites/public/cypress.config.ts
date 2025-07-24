a6f9fec1b977c80e74f1644f13cbb4258b085eeb
Emily Jablonski <emijablonski@gmail.com>
2025-07-23 19:31:41 -0600

    fix: additional a11y

--- sites/public/cypress.config.ts
+++ sites/public/cypress.config.ts
@@ -3,44 +3,52 @@ import { cypressConfig } from "@axe-core/watcher"
 import dotenv from "dotenv"
 dotenv.config()
 
-export default defineConfig(
-  cypressConfig({
-    axe: {
-      apiKey: process.env.AXE_DEVELOPER_HUB_API_KEY,
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let config: Cypress.ConfigOptions<any> = {
+  defaultCommandTimeout: 100000,
+  projectId: "bloom-public-reference",
+  pageLoadTimeout: 100000,
+  video: true,
+  videoUploadOnPasses: false,
+  numTestsKeptInMemory: 0,
+  viewportHeight: 1500,
+  env: {
+    codeCoverage: {
+      url: "/api/__coverage__",
     },
-    defaultCommandTimeout: 100000,
-    projectId: "bloom-public-reference",
-    pageLoadTimeout: 100000,
-    video: true,
-    videoUploadOnPasses: false,
-    numTestsKeptInMemory: 0,
-    viewportHeight: 1500,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      return require("./cypress/plugins/index.js")(on, config)
+    },
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+    experimentalRunAllSpecs: true,
     env: {
-      codeCoverage: {
-        url: "/api/__coverage__",
-      },
+      showSeedsDesign: process.env.SHOW_NEW_SEEDS_DESIGNS,
     },
+  },
 
-    e2e: {
-      // We've imported your old cypress plugins here.
-      // You may want to clean this up later by importing these.
-      setupNodeEvents(on, config) {
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        return require("./cypress/plugins/index.js")(on, config)
-      },
-      baseUrl: "http://localhost:3000",
-      specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
-      experimentalRunAllSpecs: true,
-      env: {
-        showSeedsDesign: process.env.SHOW_NEW_SEEDS_DESIGNS,
-      },
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
     },
+  },
+}
 
-    component: {
-      devServer: {
-        framework: "next",
-        bundler: "webpack",
-      },
-    },
-  })
-)
+const axeDeveloperHubConfig = {
+  axe: {
+    apiKey: process.env.AXE_DEVELOPER_HUB_API_KEY,
+  },
+}
+
+if (process.env.IN_CI !== "TRUE") {
+  config = { ...axeDeveloperHubConfig, ...config }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default defineConfig(cypressConfig(config as any))
