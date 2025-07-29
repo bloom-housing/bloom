981c4d945ed2dea4f8dfec5887707449b5acffaa
Jared White <jared@whitefusion.studio>
2025-07-29 21:53:23 +0000

    chore: streamline migration file

--- api/prisma/migrations/28_change_marketing_date_to_marketing_year/migration.sql
+++ api/prisma/migrations/28_change_marketing_date_to_marketing_year/migration.sql
@@ -1,9 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to change column `marketing_date` on the `listings` table. All the data in the column will be transformed to year only.
-
-*/
 -- AlterTable
 ALTER TABLE "listings"
     ALTER COLUMN "marketing_date" TYPE INTEGER
