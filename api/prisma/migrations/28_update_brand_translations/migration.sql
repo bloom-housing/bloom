6d30887edb34d0fefc4aaf36b7b06f64bfc5a0c0
Jared White <jared@whitefusion.studio>
2025-07-31 19:39:23 +0000

    fix: slim down migration file

--- api/prisma/migrations/28_update_brand_translations/migration.sql
+++ api/prisma/migrations/28_update_brand_translations/migration.sql
@@ -1,10 +1,6 @@
 -- Updates brand in translations for emails.
 
 UPDATE translations
-SET translations = jsonb_set(translations, '{forgotPassword,resetRequest}', '"A request to reset your Detroit Home Connect website password for %{appUrl} has recently been made."')
-WHERE language = 'en';
-
-UPDATE translations
 SET translations = jsonb_set(translations, '{header}', '{"logoUrl": "https://detroitmi.gov/themes/custom/detroitminew/logo.svg", "logoTitle": "Detroit Home Connect"}')
 WHERE language = 'en';
 
@@ -26,16 +22,4 @@ WHERE language = 'en';
 
 UPDATE translations
 SET translations = jsonb_set(translations, '{invite.inviteManageListings}', '"You will now be able to manage listings and applications that you are a part of from one centralized location."')
-WHERE language = 'en';
-
-UPDATE translations
-SET translations = jsonb_set(translations, '{listingEmail.newListing.hrdLabel}', '"Housing & Revitalization Department of the City of Detroit"')
-WHERE language = 'en';
-
-UPDATE translations
-SET translations = jsonb_set(translations, '{listingEmail.updateListing.hrdLabel}', '"Housing & Revitalization Department of the City of Detroit"')
-WHERE language = 'en';
-
-UPDATE translations
-SET translations = jsonb_set(translations, '{listingEmail.updateListing.dhcProjectLabel}', '"Detroit Home Connect is a project of the"')
 WHERE language = 'en';
