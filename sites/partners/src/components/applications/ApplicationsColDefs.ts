78c1a5690f9030422895d86349340c7c66862a18
Krzysztof Zięcina <kziecina@airnauts.com>
2025-08-01 20:42:33 +0200

    fix: testing bug - closed waitlist on search page (#5127)
    * fix: add closed waitlist
    
    * fix: t.null display on fullTime student
    
    * fix: remove console
    
    * test: fiz
    
    * fix: update copy
    
    * test: fix test
    
    * test: fix
    
    * test: fix
    
    ---------
    
    Co-authored-by: Morgan Ludtke <42942267+ludtkemorgan@users.noreply.github.com>

--- sites/partners/src/components/applications/ApplicationsColDefs.ts
+++ sites/partners/src/components/applications/ApplicationsColDefs.ts
@@ -625,7 +625,7 @@ export function getColDefs(
         width: 90,
         minWidth: 50,
         valueFormatter: ({ value }) => {
-          if (value?.length < householdIndex) return ""
+          if (!value[i]?.fullTimeStudent || value?.length < householdIndex) return ""
           return t(`t.${value[i]?.fullTimeStudent}`)
         },
       })
