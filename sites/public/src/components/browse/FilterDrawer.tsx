e50b21286dfc01aa6159a8647c2d9e9c35727650
Emily Jablonski <emijablonski@gmail.com>
2025-08-21 13:11:31 -0600

    fix: translate programs

--- sites/public/src/components/browse/FilterDrawer.tsx
+++ sites/public/src/components/browse/FilterDrawer.tsx
@@ -148,7 +148,13 @@ const FilterDrawer = (props: FilterDrawerProps) => {
               groupLabel={t("t.community")}
               fields={buildDefaultFilterFields(
                 ListingFilterKeys.multiselectQuestions,
-                props.multiselectData?.map((multi) => multi.text),
+                props.multiselectData?.map((multi) =>
+                  t(
+                    multi.untranslatedText
+                      ? t(`listingFilters.program.${multi.untranslatedText}`)
+                      : t(`listingFilters.program.${multi.text}`)
+                  )
+                ),
                 props.multiselectData?.map((multi) => multi.id),
                 props.filterState
               )}
