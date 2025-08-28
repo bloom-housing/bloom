50e49518ecedd7f8614b919ea6b90a197c8e7042
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2025-08-28 08:52:39 -0600

    fix: resurface availability with unit groups (#5320)

--- sites/public/__tests__/components/listing/listing_sections/Availability.test.tsx
+++ sites/public/__tests__/components/listing/listing_sections/Availability.test.tsx
@@ -3,12 +3,14 @@ import dayjs from "dayjs"
 import { render, cleanup } from "@testing-library/react"
 import { listing, jurisdiction } from "@bloom-housing/shared-helpers/__tests__/testHelpers"
 import {
+  EnumUnitGroupAmiLevelMonthlyRentDeterminationType,
   FeatureFlag,
   FeatureFlagEnum,
   ListingsStatusEnum,
   MarketingSeasonEnum,
   MarketingTypeEnum,
   ReviewOrderTypeEnum,
+  UnitTypeEnum,
 } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
 import { Availability } from "../../../../src/components/listing/listing_sections/Availability"
 
@@ -434,7 +436,7 @@ describe("<Availability>", () => {
         )
       ).toBeNull()
     })
-    it("shows nothing for marketing listings with enableMarketingStatus on and enableUnitGroups on", () => {
+    it("shows nothing for marketing listings with enableMarketingStatus on and enableUnitGroups on with available units and no waitlist values", () => {
       const view = render(
         <Availability
           listing={{
@@ -442,6 +444,8 @@ describe("<Availability>", () => {
             status: ListingsStatusEnum.active,
             reviewOrderType: ReviewOrderTypeEnum.waitlist,
             waitlistOpenSpots: null,
+            waitlistMaxSize: null,
+            waitlistCurrentSize: null,
             applicationDueDate: null,
             marketingType: MarketingTypeEnum.marketing,
           }}
@@ -464,6 +468,232 @@ describe("<Availability>", () => {
       expect(view.queryByText("Applications open")).toBeNull()
       expect(view.queryByText("Submit an application for an open slot on the waitlist.")).toBeNull()
       expect(view.queryAllByText("Under construction").length).toBe(0)
+    })
+    it("shows waitlist section for marketing listings with enableMarketingStatus on and enableUnitGroups on with available units and waitlist values and no additional waitlist fields", () => {
+      const view = render(
+        <Availability
+          listing={{
+            ...listing,
+            status: ListingsStatusEnum.active,
+            reviewOrderType: ReviewOrderTypeEnum.waitlist,
+            waitlistOpenSpots: 90,
+            waitlistMaxSize: 100,
+            waitlistCurrentSize: 10,
+            applicationDueDate: null,
+            marketingType: MarketingTypeEnum.marketing,
+          }}
+          jurisdiction={{
+            ...jurisdiction,
+            featureFlags: [
+              { name: FeatureFlagEnum.enableMarketingStatus, active: true } as FeatureFlag,
+              { name: FeatureFlagEnum.enableUnitGroups, active: true } as FeatureFlag,
+            ],
+          }}
+        />
+      )
+      expect(view.getByText("Availability")).toBeDefined()
+      expect(view.queryByText("10 current waitlist size")).toBeNull()
+      expect(view.getByText("90 open waitlist slots")).toBeDefined()
+      expect(view.queryByText("100 final waitlist size")).toBeNull()
+    })
+    it("shows waitlist section for marketing listings with enableMarketingStatus on and enableUnitGroups on with available units and waitlist values and additional waitlist fields", () => {
+      const view = render(
+        <Availability
+          listing={{
+            ...listing,
+            status: ListingsStatusEnum.active,
+            reviewOrderType: ReviewOrderTypeEnum.waitlist,
+            waitlistOpenSpots: 90,
+            waitlistMaxSize: 100,
+            waitlistCurrentSize: 10,
+            applicationDueDate: null,
+            marketingType: MarketingTypeEnum.marketing,
+          }}
+          jurisdiction={{
+            ...jurisdiction,
+            featureFlags: [
+              { name: FeatureFlagEnum.enableMarketingStatus, active: true } as FeatureFlag,
+              { name: FeatureFlagEnum.enableUnitGroups, active: true } as FeatureFlag,
+              { name: FeatureFlagEnum.enableWaitlistAdditionalFields, active: true } as FeatureFlag,
+            ],
+          }}
+        />
+      )
+      expect(view.getByText("Availability")).toBeDefined()
+      expect(view.getByText("10 current waitlist size")).toBeDefined()
+      expect(view.getByText("90 open waitlist slots")).toBeDefined()
+      expect(view.getByText("100 final waitlist size")).toBeDefined()
+    })
+    it("shows waitlist section for marketing listings with enableUnitGroups on", () => {
+      const view = render(
+        <Availability
+          listing={{
+            ...listing,
+            status: ListingsStatusEnum.active,
+            reviewOrderType: ReviewOrderTypeEnum.firstComeFirstServe,
+            applicationDueDate: null,
+            marketingType: MarketingTypeEnum.marketing,
+            waitlistCurrentSize: 10,
+            waitlistOpenSpots: 90,
+            waitlistMaxSize: 100,
+            reservedCommunityTypes: null,
+
+            unitGroups: [
+              {
+                id: "1d4971f5-b651-430c-9a2f-4655534f1bda",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                maxOccupancy: 4,
+                minOccupancy: 1,
+                floorMin: 1,
+                floorMax: 4,
+                totalCount: 2,
+                totalAvailable: null,
+                bathroomMin: 1,
+                bathroomMax: 3,
+                openWaitlist: true,
+                sqFeetMin: 340,
+                sqFeetMax: 725,
+                unitGroupAmiLevels: [
+                  {
+                    id: "8025f0c3-4103-4321-8261-da536e489572",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    amiPercentage: 20,
+                    monthlyRentDeterminationType:
+                      EnumUnitGroupAmiLevelMonthlyRentDeterminationType.flatRent,
+                    percentageOfIncomeValue: null,
+                    flatRentValue: 1500,
+                    amiChart: {
+                      id: "cf8574bb-599f-40fa-9468-87c1e16be898",
+                      createdAt: new Date(),
+                      updatedAt: new Date(),
+                      items: [],
+                      name: "Divine Orchard",
+                      jurisdictions: {
+                        id: "e674b260-d26f-462a-9090-abaabe939cae",
+                        name: "Bloomington",
+                      },
+                    },
+                  },
+                ],
+                unitTypes: [
+                  {
+                    id: "d20ada5f-3b33-4ec6-86b8-ce9412bb8844",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    name: UnitTypeEnum.studio,
+                    numBedrooms: 0,
+                  },
+                  {
+                    id: "a7195b0a-2311-494c-9765-7304a3637312",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    name: UnitTypeEnum.oneBdrm,
+                    numBedrooms: 1,
+                  },
+                ],
+              },
+            ],
+          }}
+          jurisdiction={{
+            ...jurisdiction,
+            featureFlags: [
+              { name: FeatureFlagEnum.enableMarketingStatus, active: true } as FeatureFlag,
+              { name: FeatureFlagEnum.enableUnitGroups, active: true } as FeatureFlag,
+              { name: FeatureFlagEnum.enableWaitlistAdditionalFields, active: true } as FeatureFlag,
+            ],
+          }}
+        />
+      )
+      expect(view.getByText("Availability")).toBeDefined()
+      expect(view.getByText("10 current waitlist size")).toBeDefined()
+      expect(view.getByText("90 open waitlist slots")).toBeDefined()
+      expect(view.getByText("100 final waitlist size")).toBeDefined()
+    })
+    it("does not show waitlist section for marketing listings with enableUnitGroups on if there are no waitlist numbers", () => {
+      const view = render(
+        <Availability
+          listing={{
+            ...listing,
+            status: ListingsStatusEnum.active,
+            reviewOrderType: ReviewOrderTypeEnum.firstComeFirstServe,
+            applicationDueDate: null,
+            marketingType: MarketingTypeEnum.marketing,
+            waitlistCurrentSize: null,
+            waitlistOpenSpots: null,
+            waitlistMaxSize: null,
+            reservedCommunityTypes: null,
+
+            unitGroups: [
+              {
+                id: "1d4971f5-b651-430c-9a2f-4655534f1bda",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                maxOccupancy: 4,
+                minOccupancy: 1,
+                floorMin: 1,
+                floorMax: 4,
+                totalCount: 2,
+                totalAvailable: null,
+                bathroomMin: 1,
+                bathroomMax: 3,
+                openWaitlist: true,
+                sqFeetMin: 340,
+                sqFeetMax: 725,
+                unitGroupAmiLevels: [
+                  {
+                    id: "8025f0c3-4103-4321-8261-da536e489572",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    amiPercentage: 20,
+                    monthlyRentDeterminationType:
+                      EnumUnitGroupAmiLevelMonthlyRentDeterminationType.flatRent,
+                    percentageOfIncomeValue: null,
+                    flatRentValue: 1500,
+                    amiChart: {
+                      id: "cf8574bb-599f-40fa-9468-87c1e16be898",
+                      createdAt: new Date(),
+                      updatedAt: new Date(),
+                      items: [],
+                      name: "Divine Orchard",
+                      jurisdictions: {
+                        id: "e674b260-d26f-462a-9090-abaabe939cae",
+                        name: "Bloomington",
+                      },
+                    },
+                  },
+                ],
+                unitTypes: [
+                  {
+                    id: "d20ada5f-3b33-4ec6-86b8-ce9412bb8844",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    name: UnitTypeEnum.studio,
+                    numBedrooms: 0,
+                  },
+                  {
+                    id: "a7195b0a-2311-494c-9765-7304a3637312",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    name: UnitTypeEnum.oneBdrm,
+                    numBedrooms: 1,
+                  },
+                ],
+              },
+            ],
+          }}
+          jurisdiction={{
+            ...jurisdiction,
+            featureFlags: [
+              { name: FeatureFlagEnum.enableMarketingStatus, active: true } as FeatureFlag,
+              { name: FeatureFlagEnum.enableUnitGroups, active: true } as FeatureFlag,
+              { name: FeatureFlagEnum.enableWaitlistAdditionalFields, active: true } as FeatureFlag,
+            ],
+          }}
+        />
+      )
+      expect(view.queryByText("Availability")).toBeNull()
     })
   })
 })
