3cc452f76ea49aefdd297cd1b19de8f5ecb7fbfa
juanmillord <juancarlosmillord@gmail.com>
2025-09-04 13:07:57 -0400

    feat: Listing Last Edited By backend (#5308)
    added lastUpdatedByUser field to the Listings table

--- api/src/utilities/map-user.ts
+++ api/src/utilities/map-user.ts
@@ -5,7 +5,7 @@ import { User } from '../dtos/users/user.dto';
     This maps a user that has requested changes on a listing to a limited IdDTO 
     This is used by the partner site front end
   */
-export function requestedChangesUserMapper(user: User): IdDTO {
+export function mapUser(user: User): IdDTO {
   return {
     id: user?.id,
     name:
