b85dba98eaaf899a0aee916f4e86946b7ddc3dae
juan <juancarlosmillord@gmail.com>
2025-09-15 10:51:21 -0400

    using content updated instead of just updatedat

--- sites/partners/src/components/listings/ListingFormActions.tsx
+++ sites/partners/src/components/listings/ListingFormActions.tsx
@@ -89,7 +89,7 @@ const ListingFormActions = ({
   const recordUpdated = useMemo(() => {
     if (!listing) return null
 
-    const dayjsDate = dayjs(listing.updatedAt)
+    const dayjsDate = dayjs(listing.contentUpdatedAt)
 
     return dayjsDate.format("MMMM DD, YYYY, hh:mm A")
   }, [listing])
