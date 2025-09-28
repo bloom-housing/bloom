91fd248e51c7454416c98ccee4bcf2af4b1e2755
juan <juancarlosmillord@gmail.com>
2025-09-28 19:12:58 -0400

    cleaning up code

--- api/src/services/listing.service.ts
+++ api/src/services/listing.service.ts
@@ -275,11 +275,8 @@ export class ListingService implements OnModuleInit {
     const userRolesWhere: Prisma.UserAccountsWhereInput[] = [];
     if (userRoles.includes(UserRoleEnum.admin))
       userRolesWhere.push({ userRoles: { isAdmin: true } });
-    if (userRoles.includes(UserRoleEnum.supportAdmin)) {
-      userRolesWhere.push({
-        userRoles: { isSupportAdmin: true },
-      });
-    }
+    if (userRoles.includes(UserRoleEnum.supportAdmin))
+      userRolesWhere.push({ userRoles: { isSupportAdmin: true } });
     if (userRoles.includes(UserRoleEnum.partner))
       userRolesWhere.push({
         userRoles: { isPartner: true },
