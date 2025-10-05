693b6a47c9295310d3f69d37520c0823182087ae
juan <juancarlosmillord@gmail.com>
2025-10-05 19:28:06 -0400

    reverting change for tabs fix

--- sites/partners/src/components/shared/NavigationHeader.tsx
+++ sites/partners/src/components/shared/NavigationHeader.tsx
@@ -79,21 +79,19 @@ const NavigationHeader = ({
 
   const tabNavItems = useMemo(() => {
     return (
-      <>
-        <TabNav className="relative -bottom-8 md:-bottom-10">
-          {tabNavElements.map((tab) => (
-            <TabNavItem
-              key={tab.path}
-              tagContent={tab?.content}
-              current={tab.activePaths.includes(currentPath)}
-              href={tab.path}
-              tagSize={AppearanceSizeType.small}
-            >
-              {tab.label}
-            </TabNavItem>
-          ))}
-        </TabNav>
-      </>
+      <TabNav className="relative -bottom-8 md:-bottom-10">
+        {tabNavElements.map((tab) => (
+          <TabNavItem
+            key={tab.path}
+            tagContent={tab?.content}
+            current={tab.activePaths.includes(currentPath)}
+            href={tab.path}
+            tagSize={AppearanceSizeType.small}
+          >
+            {tab.label}
+          </TabNavItem>
+        ))}
+      </TabNav>
     )
   }, [currentPath, tabNavElements])
 
