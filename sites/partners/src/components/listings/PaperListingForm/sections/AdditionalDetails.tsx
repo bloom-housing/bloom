16373eeb6638d35321d33ccd49b35d3532446a7a
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-10-06 16:04:23 +0200

    chore: update required documents form fields

--- sites/partners/src/components/listings/PaperListingForm/sections/AdditionalDetails.tsx
+++ sites/partners/src/components/listings/PaperListingForm/sections/AdditionalDetails.tsx
@@ -1,20 +1,34 @@
 import React from "react"
-import { useFormContext } from "react-hook-form"
+import { useFormContext, useWatch } from "react-hook-form"
 import { Grid } from "@bloom-housing/ui-seeds"
-import { t, Textarea } from "@bloom-housing/ui-components"
+import { FieldGroup, t, Textarea } from "@bloom-housing/ui-components"
 import SectionWithGrid from "../../../shared/SectionWithGrid"
 import { defaultFieldProps } from "../../../../lib/helpers"
+import styles from "../ListingForm.module.scss"
 
 type AdditionalDetailsProps = {
   defaultText?: string
   requiredFields: string[]
+}
+enum RequiredDocumentEnum {
+  "socialSecurityCard" = "socialSecurityCard",
+  "currentLandlordReference" = "currentLandlordReference",
+  "birthCertificate" = "birthCertificate",
+  "previousLandlordReference" = "previousLandlordReference",
+  "governmentIssuedId" = "governmentIssuedId",
+  "proofOfAssets" = "proofOfAssets",
+  "proofOfIncome" = "proofOfIncome",
+  "residencyDocuments" = "residencyDocuments",
+  "proofOfCustody" = "proofOfCustody",
 }
 
 const AdditionalDetails = (props: AdditionalDetailsProps) => {
   const formMethods = useFormContext()
 
   // eslint-disable-next-line @typescript-eslint/unbound-method
-  const { register, errors, clearErrors } = formMethods
+  const { register, errors, clearErrors, watch } = formMethods
+
+  const jurisdiction = watch("jurisdictions.id")
 
   return (
     <>
@@ -23,25 +37,28 @@ const AdditionalDetails = (props: AdditionalDetailsProps) => {
         heading={t("listings.sections.additionalDetails")}
         subheading={t("listings.sections.additionalDetailsSubtitle")}
       >
-        <Grid.Row columns={2}>
+        <Grid.Row columns={1}>
           <Grid.Cell>
-            <Textarea
-              label={t("listings.requiredDocuments")}
+            <FieldGroup
               name={"requiredDocuments"}
-              id={"requiredDocuments"}
-              fullWidth={true}
+              groupLabel={
+                <span>
+                  {t("listings.requiredDocuments")}
+                  {jurisdiction && <span className={styles["asterisk"]}>{` ${"*"}`}</span>}
+                </span>
+              }
               register={register}
-              maxLength={2000}
-              placeholder={""}
-              {...defaultFieldProps(
-                "requiredDocuments",
-                t("listings.requiredDocuments"),
-                props.requiredFields,
-                errors,
-                clearErrors
-              )}
+              type="checkbox"
+              fields={Object.keys(RequiredDocumentEnum).map((key) => ({
+                id: `requiredDocuments.${key}`,
+                label: t(`listings.requiredDocuments.${key}`),
+                value: key,
+              }))}
+              fieldGroupClassName="grid grid-cols-2 mt-2"
             />
           </Grid.Cell>
+        </Grid.Row>
+        <Grid.Row columns={2}>
           <Grid.Cell>
             <Textarea
               fullWidth={true}
@@ -57,8 +74,6 @@ const AdditionalDetails = (props: AdditionalDetailsProps) => {
               )}
             />
           </Grid.Cell>
-        </Grid.Row>
-        <Grid.Row columns={2}>
           <Grid.Cell>
             <Textarea
               fullWidth={true}
