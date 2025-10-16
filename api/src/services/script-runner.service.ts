837fe2e038889abe0ead7a8376a397955c015a9f
Eric McGarry <46828798+mcgarrye@users.noreply.github.com>
2025-10-16 16:52:38 -0400

    feat: scripts to migrate msq data to refactor schema (#5428)
    * feat: scripts to migrate msq data to refactor schema
    
    5142
    
    * feat: add descriptions
    
    * feat: simple comment requests
    
    * feat: switch to for loop
    
    * feat: empty
    
    * feat: correct equal signs

--- api/src/services/script-runner.service.ts
+++ api/src/services/script-runner.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
 import {
   LanguagesEnum,
+  ListingsStatusEnum,
   MultiselectQuestionsApplicationSectionEnum,
+  MultiselectQuestionsStatusEnum,
   Prisma,
   ReviewOrderTypeEnum,
 } from '@prisma/client';
@@ -17,11 +19,14 @@ import { User } from '../dtos/users/user.dto';
 import { mapTo } from '../utilities/mapTo';
 import { BulkApplicationResendDTO } from '../dtos/script-runner/bulk-application-resend.dto';
 import { Application } from '../dtos/applications/application.dto';
+import { ApplicationMultiselectQuestion } from '../dtos/applications/application-multiselect-question.dto';
 import { AmiChartImportDTO } from '../dtos/script-runner/ami-chart-import.dto';
 import { AmiChartCreate } from '../dtos/ami-charts/ami-chart-create.dto';
 import { AmiChartUpdate } from '../dtos/ami-charts/ami-chart-update.dto';
 import MultiselectQuestion from '../dtos/multiselect-questions/multiselect-question.dto';
+import { MultiselectOption } from '../dtos/multiselect-questions/multiselect-option.dto';
 import { AmiChartUpdateImportDTO } from '../dtos/script-runner/ami-chart-update-import.dto';
+import { calculateSkip, calculateTake } from '../utilities/pagination-helpers';
 
 /**
   this is the service for running scripts
@@ -646,6 +651,263 @@ export class ScriptRunnerService {
 
     await this.markScriptAsComplete(
       'migrate Detroit to multiselect questions',
+      requestingUser,
+    );
+    return { success: true };
+  }
+
+  /**
+   *
+   * @param req incoming request object
+   * @returns successDTO
+   * @description pulls in all multiselect questions, updates new fields
+   * and moves option data out of JSON format and into MultiselectOptions table
+   */
+  async migrateMultiselectDataToRefactor(
+    req: ExpressRequest,
+  ): Promise<SuccessDTO> {
+    const requestingUser = mapTo(User, req['user']);
+    await this.markScriptAsRunStart(
+      'migrate multiselect data to refactor',
+      requestingUser,
+    );
+
+    const multiselectQuestions =
+      await this.prisma.multiselectQuestions.findMany({
+        include: {
+          listings: {
+            include: {
+              listings: {
+                select: {
+                  status: true,
+                },
+              },
+            },
+          },
+        },
+      });
+
+    for (const msq of multiselectQuestions) {
+      const id = msq.id;
+
+      const hasPublishedListing = msq.listings.some(({ listings }) => {
+        return (
+          listings.status === ListingsStatusEnum.active ||
+          listings.status === ListingsStatusEnum.closed
+        );
+      });
+      const status: MultiselectQuestionsStatusEnum = hasPublishedListing
+        ? MultiselectQuestionsStatusEnum.active
+        : MultiselectQuestionsStatusEnum.visible;
+
+      const options = msq.options as unknown[] as MultiselectOption[];
+
+      const isExclusive = options.some((options) => {
+        return options.exclusive;
+      });
+      await this.prisma.multiselectQuestions.update({
+        data: { isExclusive: isExclusive, status: status },
+        where: { id: id },
+      });
+
+      await this.prisma.multiselectOptions.createMany({
+        data: options.map((option) => {
+          return {
+            description:
+              option.description?.trim() === '' ? null : option.description,
+            isOptOut: false,
+            links: option.links
+              ? (option.links as unknown as Prisma.InputJsonArray)
+              : undefined,
+            mapLayerId: option.mapLayerId,
+            mapPinPosition: option.mapPinPosition,
+            multiselectQuestionId: id,
+            name: option.text,
+            ordinal: option.ordinal,
+            radiusSize: option.radiusSize,
+            shouldCollectAddress: option.collectAddress,
+            shouldCollectName: option.collectName,
+            shouldCollectRelationship: option.collectRelationship,
+            validationMethod: option.validationMethod,
+          };
+        }),
+      });
+
+      if (msq.optOutText) {
+        await this.prisma.multiselectOptions.create({
+          data: {
+            isOptOut: true,
+            multiselectQuestionId: id,
+            name: msq.optOutText,
+            ordinal: options.length + 1,
+          },
+        });
+      }
+    }
+
+    await this.markScriptAsComplete(
+      'migrate multiselect data to refactor',
+      requestingUser,
+    );
+    return { success: true };
+  }
+
+  /**
+   *
+   * @param req incoming request object
+   * @param page which page of the application table to query from
+   * @param pageSize size of the page of the application table being quieried
+   * @returns successDTO
+   * @description pulls all applications within a given range, loops over the
+   * preferences and programs jsons to find the correct MSQ and option, and maps
+   * the data to the ApplicationSelections and ApplicationSelectionOptions tables.
+   */
+  async migrateMultiselectApplicationDataToRefactor(
+    req: ExpressRequest,
+    page?: number,
+    pageSize?: number,
+  ): Promise<SuccessDTO> {
+    const requestingUser = mapTo(User, req['user']);
+    await this.markScriptAsRunStart(
+      `migrate multiselect application data to refactor with page ${
+        page || 1
+      } of size ${pageSize || 5_000}`,
+      requestingUser,
+    );
+
+    const skip = calculateSkip(pageSize || 5_000, page || 1);
+    const take = calculateTake(pageSize || 5_000);
+    console.log(`START OF RUN ${page ? skip : 1}:${skip + take}\n\n\n\n`);
+    const applications = await this.prisma.applications.findMany({
+      include: {
+        listings: {
+          include: {
+            jurisdictions: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+      },
+      skip: skip,
+      take: take,
+      orderBy: { createdAt: 'asc' },
+    });
+
+    for (const { id, listings, preferences, programs } of applications) {
+      const preferencesAndPrograms = (
+        preferences as unknown[] as ApplicationMultiselectQuestion[]
+      ).concat(
+        programs
+          ? (programs as unknown[] as ApplicationMultiselectQuestion[])
+          : [],
+      );
+
+      for (const {
+        key,
+        claimed,
+        options,
+        multiselectQuestionId = null,
+      } of preferencesAndPrograms as unknown[] as ApplicationMultiselectQuestion[]) {
+        if (!claimed) {
+          continue;
+        }
+        let multiselectQuestion;
+        if (multiselectQuestionId) {
+          multiselectQuestion =
+            await this.prisma.multiselectQuestions.findFirst({
+              include: { multiselectOptions: true },
+              where: { id: multiselectQuestionId },
+            });
+        } else {
+          multiselectQuestion =
+            await this.prisma.multiselectQuestions.findFirst({
+              include: { multiselectOptions: true },
+              where: {
+                text: { equals: key, mode: Prisma.QueryMode.insensitive },
+              },
+            });
+        }
+        let hasOptedOut = false;
+        const selectedOptions = [];
+        if (!multiselectQuestion) {
+          console.log(
+            `Could not find MSQ with id: "${multiselectQuestionId}" or key: "${key}" for application with id: "${id}"`,
+          );
+          continue;
+        }
+
+        for (const selected of options.filter(({ checked }) => checked)) {
+          const selectedName = selected.key
+            ?.trim()
+            ?.replaceAll('%{county}', listings.jurisdictions.name)
+            ?.replaceAll('  ', ' ')
+            ?.replaceAll("'", '')
+            ?.replaceAll('.', '')
+            ?.toLowerCase();
+          const multiselectOption = multiselectQuestion.multiselectOptions.find(
+            (option: MultiselectOption) =>
+              option.name
+                ?.trim()
+                ?.replaceAll('  ', ' ')
+                ?.replaceAll("'", '')
+                ?.replaceAll('.', '')
+                ?.toLowerCase() === selectedName,
+          );
+          if (!multiselectOption) {
+            console.log(
+              `Could not match MSQ option with key: "${selected.key}" for MSQ with id: "${multiselectQuestion.id}"`,
+            );
+            continue;
+          }
+
+          if (multiselectOption.isOptOut) {
+            hasOptedOut = true;
+          }
+
+          const selectedOptionBody = {
+            addressHolderAddressId: null,
+            addressHolderName: null,
+            addressHolderRelationship: null,
+            isGeocodingVerified: null,
+            multiselectOptionId: multiselectOption.id,
+          };
+
+          for (const { key, value } of selected.extraData) {
+            if (key === 'addressHolderAddress' || key === 'address') {
+              const address = await this.prisma.address.create({ data: value });
+              selectedOptionBody.addressHolderAddressId = address.id;
+            } else if (key === 'addressHolderName' || key === 'name') {
+              selectedOptionBody.addressHolderName = value;
+            } else if (
+              key === 'addressHolderRelationship' ||
+              key === 'relationship'
+            ) {
+              selectedOptionBody.addressHolderRelationship = value;
+            } else if (key === 'geocodingVerified') {
+              selectedOptionBody.isGeocodingVerified = Boolean(value);
+            }
+          }
+          selectedOptions.push(selectedOptionBody);
+        }
+        const selectedBody = {
+          applicationId: id,
+          hasOptedOut: hasOptedOut,
+          multiselectQuestionId: multiselectQuestion.id,
+          selections: { createMany: { data: selectedOptions } },
+        };
+        await this.prisma.applicationSelections.create({
+          data: selectedBody,
+        });
+      }
+    }
+    console.log(`END OF RUN ${page ? skip : 1}:${skip + take}\n\n\n\n`);
+
+    await this.markScriptAsComplete(
+      `migrate multiselect application data to refactor with page ${
+        page || 1
+      } of size ${pageSize || 5_000}`,
       requestingUser,
     );
     return { success: true };
