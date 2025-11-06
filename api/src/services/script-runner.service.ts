e8cdf94126e805898dfabe5321a02e5d5db7f53d
Eric McGarry <eric@eric.mcgarry.name>
2025-11-06 15:54:04 -0500

    feat: align script runner

--- api/src/services/script-runner.service.ts
+++ api/src/services/script-runner.service.ts
@@ -573,21 +573,24 @@ export class ScriptRunnerService {
         jurisInfo?.length ? jurisInfo[0].name : '',
         translations,
       );
-      await this.multiselectQuestionService.create({
-        text: pref.title,
-        subText: pref.subtitle,
-        description: pref.description,
-        links: pref.links ?? null,
-        hideFromListing: this.resolveHideFromListings(pref),
-        optOutText: optOutText ?? null,
-        options: options,
-        applicationSection:
-          MultiselectQuestionsApplicationSectionEnum.preferences,
-        jurisdictions: jurisInfo.map((juris) => {
-          return { id: juris.id };
-        }),
-        status: 'draft',
-      });
+      await this.multiselectQuestionService.create(
+        {
+          text: pref.title,
+          subText: pref.subtitle,
+          description: pref.description,
+          links: pref.links ?? null,
+          hideFromListing: this.resolveHideFromListings(pref),
+          optOutText: optOutText ?? null,
+          options: options,
+          applicationSection:
+            MultiselectQuestionsApplicationSectionEnum.preferences,
+          jurisdictions: jurisInfo.map((juris) => {
+            return { id: juris.id };
+          }),
+          status: 'draft',
+        },
+        requestingUser,
+      );
     }
 
     // begin migration from programs
@@ -619,21 +622,24 @@ export class ScriptRunnerService {
         `);
 
       const res: MultiselectQuestion =
-        await this.multiselectQuestionService.create({
-          text: prog.title,
-          subText: prog.subtitle,
-          description: prog.description,
-          links: null,
-          hideFromListing: this.resolveHideFromListings(prog),
-          optOutText: null,
-          options: null,
-          applicationSection:
-            MultiselectQuestionsApplicationSectionEnum.programs,
-          jurisdictions: jurisInfo.map((juris) => {
-            return { id: juris.id };
-          }),
-          status: 'draft',
-        });
+        await this.multiselectQuestionService.create(
+          {
+            text: prog.title,
+            subText: prog.subtitle,
+            description: prog.description,
+            links: null,
+            hideFromListing: this.resolveHideFromListings(prog),
+            optOutText: null,
+            options: null,
+            applicationSection:
+              MultiselectQuestionsApplicationSectionEnum.programs,
+            jurisdictions: jurisInfo.map((juris) => {
+              return { id: juris.id };
+            }),
+            status: 'draft',
+          },
+          requestingUser,
+        );
 
       const listingsInfo: { ordinal; listing_id }[] = await this.prisma
         .$queryRawUnsafe(`
