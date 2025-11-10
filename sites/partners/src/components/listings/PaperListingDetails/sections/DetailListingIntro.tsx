edade1b80a5716b3446db0e22521fc78ed456ee0
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-11-10 15:39:24 +0100

    fix: hide new fields on listing preview based on the non-regulated feature flag

--- sites/partners/src/components/listings/PaperListingDetails/sections/DetailListingIntro.tsx
+++ sites/partners/src/components/listings/PaperListingDetails/sections/DetailListingIntro.tsx
@@ -57,22 +57,24 @@ const DetailListingIntro = () => {
           </FieldValue>
         </Grid.Cell>
       </Grid.Row>
-      <Grid.Row>
-        <Grid.Cell>
-          <FieldValue id="listingType" label={t("listings.listingTypeTile")}>
-            {listing.listingType === EnumListingListingType.regulated
-              ? t("listings.regulatedListing")
-              : t("listings.nonRegulatedListing")}
-          </FieldValue>
-        </Grid.Cell>
-        {listing.listingType === EnumListingListingType.nonRegulated && (
+      {enableNonRegulatedListings && (
+        <Grid.Row>
           <Grid.Cell>
-            <FieldValue id="hasHudEbllClearance" label={t("listings.hasEbllClearanceTitle")}>
-              {listing.hasHudEbllClearance ? t("t.yes") : t("t.no")}
+            <FieldValue id="listingType" label={t("listings.listingTypeTile")}>
+              {listing.listingType === EnumListingListingType.regulated
+                ? t("listings.regulatedListing")
+                : t("listings.nonRegulatedListing")}
             </FieldValue>
           </Grid.Cell>
-        )}
-      </Grid.Row>
+          {listing.listingType === EnumListingListingType.nonRegulated && (
+            <Grid.Cell>
+              <FieldValue id="hasHudEbllClearance" label={t("listings.hasEbllClearanceTitle")}>
+                {listing.hasHudEbllClearance ? t("t.yes") : t("t.no")}
+              </FieldValue>
+            </Grid.Cell>
+          )}
+        </Grid.Row>
+      )}
     </SectionWithGrid>
   )
 }
