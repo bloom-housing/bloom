8729149506c6949b9a1e87ff9664da4d89c4ec89
Morgan Ludtke <ludtkemorgan@gmail.com>
2025-11-19 14:44:11 -0600

    fix: add some seeded data

--- api/src/modules/multiselect-question.module.ts
+++ api/src/modules/multiselect-question.module.ts
@@ -4,9 +4,10 @@ import { PermissionModule } from './permission.module';
 import { PrismaModule } from './prisma.module';
 import { MultiselectQuestionController } from '../controllers/multiselect-question.controller';
 import { MultiselectQuestionService } from '../services/multiselect-question.service';
+import { CronJobModule } from './cron-job.module';
 
 @Module({
-  imports: [PrismaModule, PermissionModule],
+  imports: [PrismaModule, PermissionModule, CronJobModule],
   controllers: [MultiselectQuestionController],
   providers: [Logger, MultiselectQuestionService, SchedulerRegistry],
   exports: [MultiselectQuestionService],
