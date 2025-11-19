85e6f987caccb2775830661b2e9b1b9eeb26d799
juan <juancarlosmillord@gmail.com>
2025-11-19 17:25:03 -0400

    reverted change no needed in this branch

--- sites/public/src/components/listing/listing_sections/Availability.tsx
+++ sites/public/src/components/listing/listing_sections/Availability.tsx
@@ -246,7 +246,6 @@ export const Availability = ({ listing, jurisdiction }: AvailabilityProps) => {
         case ReviewOrderTypeEnum.lottery:
           return [lotteryContent]
         case ReviewOrderTypeEnum.waitlist:
-        case ReviewOrderTypeEnum.waitlistLottery:
           return [waitlistContent]
         default:
           return [fcfsContent]
