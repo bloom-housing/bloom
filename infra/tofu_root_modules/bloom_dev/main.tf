ada8fa1e544c35ddf84342618d8dcf25a9961866
Avritt Rohwer <avritt.rohwer@exygy.com>
2025-11-20 18:12:42 -0800

    Add ability to set db settings, add "high_availability" setting

--- infra/tofu_root_modules/bloom_dev/main.tf
+++ infra/tofu_root_modules/bloom_dev/main.tf
@@ -8,15 +8,21 @@ terraform {
   backend "s3" {
     region       = local.aws_region
     profile      = local.sso_profile_id
-    bucket       = "bloom-dev-tofu-state"
+    bucket       = local.state_bucket_name
     key          = "state"
     use_lockfile = true
   }
 }
 
 locals {
-  aws_region          = "us-west-2"
-  sso_profile_id      = "bloom-dev-deployer"
+  sso_profile_id    = "bloom-dev-deployer"
+  state_bucket_name = "bloom-dev-tofu-state"
+
+  aws_account_number = 242477209009
+  aws_region         = "us-west-2"
+
+  domain_name = "core-dev.bloomhousing.dev"
+
   deletion_protection = false
 }
 
@@ -29,7 +35,7 @@ provider "aws" {
 # works for the first apply of the module.
 resource "aws_s3_bucket" "tofu_state" {
   region        = local.aws_region
-  bucket        = "bloom-dev-tofu-state"
+  bucket        = local.state_bucket_name
   force_destroy = !local.deletion_protection
 }
 # https://opentofu.org/docs/v1.11/language/settings/backends/s3/:
@@ -45,7 +51,6 @@ resource "aws_s3_bucket_versioning" "tofu_state" {
 # successfully. See the README.md for more details for how to deploy and validate the certificate
 # before deploying the bloom_deployment module.
 locals {
-  domain_name = "core-dev.bloomhousing.dev"
 }
 resource "aws_acm_certificate" "bloom" {
   region            = local.aws_region
@@ -54,7 +59,6 @@ resource "aws_acm_certificate" "bloom" {
   subject_alternative_names = [
     "partners.${local.domain_name}"
   ]
-
   lifecycle {
     create_before_destroy = true
   }
@@ -77,12 +81,14 @@ module "bloom_deployment" {
   source = "../../tofu_importable_modules/bloom_deployment"
 
   aws_profile        = local.sso_profile_id
-  aws_account_number = 242477209009
+  aws_account_number = local.aws_account_number
   aws_region         = local.aws_region
 
-  env_type            = "dev"
   domain_name         = aws_acm_certificate.bloom.domain_name
   aws_certificate_arn = aws_acm_certificate.bloom.arn
+
+  env_type          = "dev"
+  high_availability = false
 
   bloom_api_image           = "ghcr.io/bloom-housing/bloom/api:gitsha-f642fc1f3f056b9fa53429c4fa81689c5e856e5a"
   bloom_site_partners_image = "ghcr.io/bloom-housing/bloom/partners:gitsha-f642fc1f3f056b9fa53429c4fa81689c5e856e5a"
