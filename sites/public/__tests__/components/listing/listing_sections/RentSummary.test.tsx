c260f35c3b9e87c372a0a6842fb6af20b4d13270
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-11-26 12:30:41 +0100

    chore: add integration tests for non-regulated listing rent table

--- sites/public/__tests__/components/listing/listing_sections/RentSummary.test.tsx
+++ sites/public/__tests__/components/listing/listing_sections/RentSummary.test.tsx
@@ -1,8 +1,12 @@
 import React from "react"
-import { render, cleanup } from "@testing-library/react"
+import { render, cleanup, screen, within } from "@testing-library/react"
 import { RentSummary } from "../../../../src/components/listing/listing_sections/RentSummary"
 import { listing } from "@bloom-housing/shared-helpers/__tests__/testHelpers"
-import { ReviewOrderTypeEnum } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
+import {
+  EnumListingListingType,
+  ReviewOrderTypeEnum,
+  UnitTypeEnum,
+} from "@bloom-housing/shared-helpers/src/types/backend-swagger"
 
 afterEach(cleanup)
 
@@ -79,5 +83,136 @@ describe("<RentSummary>", () => {
       />
     )
     expect(queryByText("Rent")).toBeNull()
+  })
+  it("shows all content for non-regulated listings", () => {
+    render(
+      <RentSummary
+        amiValues={[]}
+        reviewOrderType={ReviewOrderTypeEnum.firstComeFirstServe}
+        unitsSummarized={null}
+        listing={{
+          ...listing,
+          listingType: EnumListingListingType.nonRegulated,
+          unitGroupsSummarized: {
+            unitGroupSummary: [
+              {
+                unitTypes: [
+                  {
+                    id: "unit_type_id_1",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    name: UnitTypeEnum.studio,
+                    numBedrooms: 0,
+                  },
+                ],
+                rentRange: {
+                  min: "$2000",
+                  max: "$2000",
+                },
+                openWaitlist: false,
+                unitVacancies: 5,
+                bathroomRange: {
+                  min: 1,
+                  max: 1,
+                },
+                floorRange: {
+                  min: null,
+                  max: null,
+                },
+                sqFeetRange: {
+                  min: null,
+                  max: null,
+                },
+                amiPercentageRange: null,
+              },
+              {
+                unitTypes: [
+                  {
+                    id: "unit_type_id_2",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    name: UnitTypeEnum.oneBdrm,
+                    numBedrooms: 1,
+                  },
+                  {
+                    id: "unit_type_id_3",
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    name: UnitTypeEnum.twoBdrm,
+                    numBedrooms: 2,
+                  },
+                ],
+                rentRange: {
+                  min: "$200",
+                  max: "$700",
+                },
+                openWaitlist: false,
+                unitVacancies: null,
+                bathroomRange: {
+                  min: null,
+                  max: null,
+                },
+                floorRange: {
+                  min: null,
+                  max: null,
+                },
+                sqFeetRange: {
+                  min: null,
+                  max: null,
+                },
+                amiPercentageRange: null,
+              },
+            ],
+            householdMaxIncomeSummary: {
+              columns: {
+                householdSize: "householdSize",
+                "30": 30,
+              },
+              rows: [
+                {
+                  householdSize: "1",
+                  "30": 36000,
+                },
+                {
+                  householdSize: "2",
+                  "30": 48000,
+                },
+                {
+                  householdSize: "3",
+                  "30": 60000,
+                },
+              ],
+            },
+          },
+        }}
+        section8Acceptance={false}
+      />
+    )
+
+    const rentTable = screen.getByRole("table")
+    expect(rentTable).toBeInTheDocument()
+
+    const [head, body] = within(rentTable).getAllByRole("rowgroup")
+
+    const tableHeaders = within(head).getAllByRole("columnheader")
+    expect(tableHeaders).toHaveLength(3)
+
+    const [unitTypeHeader, rentHeader, availabilityHeader] = tableHeaders
+    expect(unitTypeHeader).toHaveTextContent(/unit type/i)
+    expect(rentHeader).toHaveTextContent(/rent/i)
+    expect(availabilityHeader).toHaveTextContent(/availability/i)
+
+    const rows = within(body).getAllByRole("row")
+    expect(rows).toHaveLength(2)
+
+    const [firstUnitType, firstRent, firstAvailability] = within(rows[0]).getAllByRole("cell")
+    expect(firstUnitType).toHaveTextContent(/studio/i)
+    expect(firstRent).toHaveTextContent(/\$2000/i)
+    expect(firstAvailability).toHaveTextContent(/5 vacant units & closed waitlist/i)
+
+    const [secondUnitType, secondRent, secondAvailability] = within(rows[1]).getAllByRole("cell")
+    expect(secondUnitType).toHaveTextContent(/1 BR, 2 BR/i)
+    expect(secondRent).toHaveTextContent(/\$200.*\$700/i)
+    expect(secondAvailability).toHaveTextContent(/closed waitlist/i)
   })
 })
