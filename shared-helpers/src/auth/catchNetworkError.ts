04641de74ab6df8153bcfa94be266834a3d6055f
juan <juancarlosmillord@gmail.com>
2025-11-28 21:04:59 -0400

    added new text and link

--- shared-helpers/src/auth/catchNetworkError.ts
+++ shared-helpers/src/auth/catchNetworkError.ts
@@ -47,9 +47,9 @@ export const useCatchNetworkError = () => {
     if (message?.includes(NetworkErrorMessage.PasswordOutdated)) {
       setNetworkError({
         title: t("authentication.signIn.passwordOutdated"),
-        description: `${t(
-          "authentication.signIn.changeYourPassword"
-        )} <a href="/forgot-password">${t("t.here")}</a>`,
+        description: `<a href="/forgot-password">${t(
+          "t.authentication.signIn.changeYourPassword"
+        )}</a>`,
         error,
       })
     } else if (message === NetworkErrorMessage.MfaUnauthorized) {
