6855075b4782b9d997b0ada119f5297db77298aa
juan <juancarlosmillord@gmail.com>
2025-12-01 20:15:15 -0400

    ran prettier and fixed tests

--- sites/public/__tests__/components/account/PasswordExpiredModal.test.tsx
+++ sites/public/__tests__/components/account/PasswordExpiredModal.test.tsx
@@ -42,7 +42,7 @@ describe("PasswordExpiredModal", () => {
 
     expect(screen.queryByText("Password Expired")).not.toBeInTheDocument()
     expect(
-      queryByText("The password tied to your account has expired. Please reset it to continue.")
+      screen.queryByText("The password tied to your account has expired. Please reset it to continue.")
     ).not.toBeInTheDocument()
   })
 
