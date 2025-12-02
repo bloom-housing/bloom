101468e3533af359206ded48d229880e9ef89ae2
Jared White <jared@whitefusion.studio>
2025-12-02 02:30:15 +0000

    feat: set up a new page for the Preferences AG table

---
+++ sites/partners/src/pages/settings/preferences.tsx
@@ -0,0 +1,151 @@
+import React, { useContext, useState, useMemo, useEffect } from "react"
+import { AuthContext } from "@bloom-housing/shared-helpers"
+import { MultiselectQuestionsApplicationSectionEnum } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
+import { AgTable, t, useAgTable } from "@bloom-housing/ui-components"
+import { Button, Tag } from "@bloom-housing/ui-seeds"
+import dayjs from "dayjs"
+import Head from "next/head"
+import Layout from "../../layouts"
+import { NavigationHeader } from "../../components/shared/NavigationHeader"
+import { useJurisdictionalMultiselectQuestionList } from "../../lib/hooks"
+import styles from "./preferences.module.scss"
+
+const NewPreferences = () => {
+  const { profile } = useContext(AuthContext)
+  const tableOptions = useAgTable()
+
+  const columns = useMemo(() => {
+    return [
+      {
+        headerName: t("t.preference"),
+        field: "name",
+        flex: 1.5,
+        cellRendererFramework: (params) => {
+          const preference = params.data
+          return (
+            <Button
+              variant="text"
+              onClick={() => alert("edit!")}
+              id={`preference-link-${preference.id}`}
+            >
+              {preference.name}
+            </Button>
+          )
+        },
+      },
+      {
+        headerName: t("application.status"),
+        field: "",
+        cellRendererFramework: ({ data }) => {
+          let variant = null
+          switch (data.status) {
+            case "draft":
+              variant = "primary"
+              break
+            case "active":
+              variant = "success"
+              break
+            case "retiring":
+              variant = "highlight-warm"
+              break
+          }
+          const statusText = data.status.charAt(0).toUpperCase() + data.status.slice(1)
+          return (
+            <Tag variant={variant} className={styles["tag-in-table"]}>
+              {statusText}
+            </Tag>
+          )
+        },
+      },
+      {
+        headerName: t("t.jurisdictions"),
+        field: "jurisdictions",
+        flex: 1.5,
+      },
+      { headerName: t("t.lastUpdated"), field: "updatedAt" },
+    ]
+  }, [])
+
+  const { data, loading, cacheKey } = useJurisdictionalMultiselectQuestionList(
+    profile?.jurisdictions?.map((jurisdiction) => jurisdiction.id).toString(),
+    MultiselectQuestionsApplicationSectionEnum.preferences
+  )
+
+  const items = useMemo(
+    () =>
+      (data || [])
+        .sort((a, b) => {
+          const aChar = a.text.toUpperCase()
+          const bChar = b.text.toUpperCase()
+          if (aChar === bChar)
+            return a.updatedAt > b.updatedAt ? -1 : a.updatedAt < b.updatedAt ? 1 : 0
+          return aChar.localeCompare(bChar)
+        })
+        .map(({ name, status, jurisdictions, updatedAt }) => {
+          return {
+            name,
+            status,
+            jurisdictions: jurisdictions.map((jurisdiction) => jurisdiction.name).join(", "),
+            updatedAt: dayjs(updatedAt).format("MM/DD/YYYY"),
+          }
+        }),
+    [data]
+  )
+
+  if (profile?.userRoles?.isPartner || profile?.userRoles?.isSupportAdmin) {
+    window.location.href = "/unauthorized"
+  }
+
+  return (
+    <Layout>
+      <Head>
+        <title>{`Preferences - ${t("nav.siteTitlePartners")}`}</title>
+      </Head>
+      <NavigationHeader className="relative" title={t("t.preferences")} />
+      <section className={styles["preferences-section"]}>
+        <div className={styles["table-wrapper"]}>
+          <AgTable
+            id="preferences-table"
+            pagination={{
+              perPage: tableOptions.pagination.itemsPerPage,
+              setPerPage: tableOptions.pagination.setItemsPerPage,
+              currentPage: tableOptions.pagination.currentPage,
+              setCurrentPage: tableOptions.pagination.setCurrentPage,
+            }}
+            config={{
+              columns,
+              totalItemsLabel: "items",
+            }}
+            data={{
+              items,
+              loading: loading,
+              totalItems: 2,
+              totalPages: 1,
+            }}
+            search={{
+              showSearch: false,
+              setSearch: tableOptions.filter.setFilterValue, // currently not used
+            }}
+            headerContent={
+              <>
+                <span></span>
+                <span>
+                  <Button
+                    variant="primary"
+                    size="sm"
+                    onClick={() => alert("Howdy!")}
+                    id={"add-user"}
+                  >
+                    {t("t.addItem")}
+                  </Button>
+                </span>
+              </>
+            }
+          />
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default NewPreferences
