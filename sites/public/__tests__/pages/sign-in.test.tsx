c53cb1fe16cd3f1678d5481288ba40bf1ef2c86d
juan <juancarlosmillord@gmail.com>
2025-12-03 14:52:39 -0400

    updated copy test for sign in

--- sites/public/__tests__/pages/sign-in.test.tsx
+++ sites/public/__tests__/pages/sign-in.test.tsx
@@ -586,7 +586,7 @@ describe("User's password is out of date flow", () => {
 
     expect(
       await findByText(
-        "The password tied to your account has expired. Please reset it to continue."
+        "The password associated with your account has expired. You will need to reset it in order to access your account. Click continue to reset your password."
       )
     ).toBeInTheDocument()
     expect(await findByText("Password Expired")).toBeInTheDocument()
