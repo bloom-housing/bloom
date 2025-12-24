a16721b8c13417994186addbc6ce9e819326c633
Yazeed Loonat <yazeedloonat@gmail.com>
2025-12-24 11:06:07 -0700

    feat: changes to listing DTOs for snapshots

--- api/src/dtos/units/unit-create.dto.ts
+++ api/src/dtos/units/unit-create.dto.ts
@@ -1,45 +1,14 @@
 import { ApiPropertyOptional, OmitType } from '@nestjs/swagger';
 import { Expose, Type } from 'class-transformer';
+import { UnitAmiChartOverrideCreate } from './ami-chart-override-create.dto';
+import { UnitUpdate } from './unit-update.dto';
 import { ValidateNested } from 'class-validator';
 import { ValidationsGroupsEnum } from '../../enums/shared/validation-groups-enum';
-import { IdDTO } from '../shared/id.dto';
-import { Unit } from './unit.dto';
-import { UnitAmiChartOverrideCreate } from './ami-chart-override-create.dto';
 
-export class UnitCreate extends OmitType(Unit, [
+export class UnitCreate extends OmitType(UnitUpdate, [
   'id',
-  'createdAt',
-  'updatedAt',
-  'amiChart',
-  'unitTypes',
-  'unitAccessibilityPriorityTypes',
-  'unitRentTypes',
   'unitAmiChartOverrides',
 ]) {
-  @Expose()
-  @Type(() => IdDTO)
-  @ApiPropertyOptional({ type: IdDTO })
-  @ValidateNested({ groups: [ValidationsGroupsEnum.default] })
-  unitTypes?: IdDTO;
-
-  @Expose()
-  @Type(() => IdDTO)
-  @ValidateNested({ groups: [ValidationsGroupsEnum.default] })
-  @ApiPropertyOptional({ type: IdDTO })
-  amiChart?: IdDTO;
-
-  @Expose()
-  @Type(() => IdDTO)
-  @ApiPropertyOptional({ type: IdDTO })
-  @ValidateNested({ groups: [ValidationsGroupsEnum.default] })
-  unitAccessibilityPriorityTypes?: IdDTO;
-
-  @Expose()
-  @Type(() => IdDTO)
-  @ApiPropertyOptional({ type: IdDTO })
-  @ValidateNested({ groups: [ValidationsGroupsEnum.default] })
-  unitRentTypes?: IdDTO;
-
   @Expose()
   @ValidateNested({ groups: [ValidationsGroupsEnum.default] })
   @Type(() => UnitAmiChartOverrideCreate)
