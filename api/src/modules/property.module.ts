1330c7db6bd19f0374b863dc0f94b5ec23b3b670
Mateusz Zduniuk <matzduniuk@gmail.com>
2025-12-29 18:30:54 +0100

    fix: add prisma and permission modules to properties modules

--- api/src/modules/property.module.ts
+++ api/src/modules/property.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { PropertyController } from '../controllers/property.controller';
 import { PropertyService } from '../services/property.service';
 import { PermissionService } from '../services/permission.service';
+import { PermissionModule } from './permission.module';
+import { PrismaModule } from './prisma.module';
 
 @Module({
   controllers: [PropertyController],
-  providers: [PropertyService, PermissionService],
+  providers: [
+    PropertyService,
+    PermissionService,
+    PermissionModule,
+    PrismaModule,
+  ],
 })
 export class PropertyModule {}
