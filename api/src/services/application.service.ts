8dc95018f5d1c5573fd5d643177188b964746cfb
Krzysztof Zięcina <kziecina@airnauts.com>
2026-01-12 15:44:14 +0100

    feat: add status to public account application cards (#5732)
    * feat: add status to public account application cards
    
    * fix: add translations
    
    * test: fix tests
    
    * test: add application status tests

--- api/src/services/application.service.ts
+++ api/src/services/application.service.ts
@@ -428,6 +428,8 @@ export class ApplicationService {
         userId: true,
         confirmationCode: true,
         updatedAt: true,
+        status: true,
+        markedAsDuplicate: true,
         listings: {
           select: {
             id: true,
