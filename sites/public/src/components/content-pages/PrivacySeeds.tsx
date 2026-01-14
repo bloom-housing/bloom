c5da7083466bf4688c8b18b4f2516804fe045896
Emily Jablonski <65367387+emilyjablonski@users.noreply.github.com>
2026-01-14 14:27:00 -0700

    refactor: remove smc content from core (#5762)

--- sites/public/src/components/content-pages/PrivacySeeds.tsx
+++ sites/public/src/components/content-pages/PrivacySeeds.tsx
@@ -4,7 +4,7 @@ import Markdown from "markdown-to-jsx"
 import { PageView, pushGtmEvent, AuthContext } from "@bloom-housing/shared-helpers"
 import { UserStatus } from "../../lib/constants"
 import Layout from "../../layouts/application"
-import pageContent from "../../md_content/privacy_policy.md"
+import pageContent from "../../md_content/generic_content.md"
 import { PageHeaderLayout } from "../../patterns/PageHeaderLayout"
 import styles from "../../patterns/PageHeaderLayout.module.scss"
 
