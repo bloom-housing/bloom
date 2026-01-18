0cf176cb17e93f3cd63d8f9db1bbb16043c5093a
Emily Jablonski <emijablonski@gmail.com>
2026-01-18 12:47:44 -0700

    fix: cleanup

--- sites/partners/src/pages/index.tsx
+++ sites/partners/src/pages/index.tsx
@@ -153,7 +153,7 @@ export default function ListingsList() {
         ),
         header: () => t("t.name"),
         footer: (props) => props.column.id,
-        minSize: 430,
+        minSize: 300,
         meta: {
           plaintextName: t("t.name"),
         },
