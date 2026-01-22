86525a7d222d46e5e96e3f09358250c0ad786f08
Emily Jablonski <emijablonski@gmail.com>
2026-01-21 20:58:42 -0700

    feat: testing out config

---
+++ api/prisma/migrations/45_add_listing_features_configuration_to_jurisdiction/migration.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "jurisdictions" ADD COLUMN     "listing_features_configuration" JSONB;
---
+++ api/src/dtos/jurisdictions/listing-features-config.dto.ts
@@ -0,0 +1,49 @@
+import { IsString, ValidateNested, IsArray, IsBoolean } from 'class-validator';
+import { Type, Expose } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { ValidationsGroupsEnum } from 'src/enums/shared/validation-groups-enum';
+
+export class ListingFeatureField {
+  @Expose()
+  @ApiProperty({
+    example: 'wheelchairRamp',
+  })
+  @IsString()
+  id: string;
+}
+
+export class ListingFeatureCategory {
+  @Expose()
+  @ApiProperty({
+    example: 'mobility',
+  })
+  @IsString()
+  id: string;
+
+  @Expose()
+  @ApiProperty({
+    type: ListingFeatureField,
+    isArray: true,
+  })
+  @ValidateNested({ each: true })
+  @Type(() => ListingFeatureField)
+  @IsArray()
+  fields: ListingFeatureField[];
+
+  @Expose()
+  @IsBoolean({ groups: [ValidationsGroupsEnum.default] })
+  @ApiPropertyOptional()
+  required?: boolean;
+}
+
+export class ListingFeaturesConfiguration {
+  @Expose()
+  @ApiProperty({
+    type: ListingFeatureCategory,
+    isArray: true,
+  })
+  @ValidateNested({ each: true })
+  @Type(() => ListingFeatureCategory)
+  @IsArray()
+  categories: ListingFeatureCategory[];
+}
