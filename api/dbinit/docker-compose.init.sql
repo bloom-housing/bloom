c0f16751c5144f39e6a2f8afb93480ad482fcf72
Avritt Rohwer <avritt.rohwer@exygy.com>
2026-01-27 12:59:56 -0800

    Try set default privileges for readonly another way

--- api/dbinit/docker-compose.init.sql
+++ api/dbinit/docker-compose.init.sql
@@ -1,15 +1,32 @@
--- Create bloom_prosma DB, bloom_api and bloom_readonly db users.
+-- Creates the bloom_prisma database and database users.
+-- Follows examples from https://aws.amazon.com/blogs/database/managing-postgresql-users-and-roles/.
+
 \set ON_ERROR_STOP on
 
 CREATE DATABASE bloom_prisma;
 \c bloom_prisma
 
+-- Revoke public privleges
+REVOKE CREATE ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON DATABASE bloom_prisma FROM PUBLIC;
+
+-- Roles
 -- bloom_api
-CREATE USER bloom_api WITH LOGIN PASSWORD 'bloom_api_pw';
+CREATE USER bloom_api PASSWORD 'bloom_api_pw';
+GRANT CONNECT ON DATABASE bloom_prisma TO bloom_api;
 GRANT ALL PRIVILEGES ON DATABASE bloom_prisma TO bloom_api;
 GRANT ALL PRIVILEGES ON SCHEMA public TO bloom_api;
-
 -- bloom_readonly
-CREATE USER bloom_readonly WITH LOGIN PASSWORD 'bloom_readonly_pw';
-ALTER DEFAULT PRIVILEGES FOR USER bloom_api -- setting the defaults for any tables created by bloom_api.
-IN SCHEMA public GRANT SELECT ON TABLES TO bloom_readonly;
+CREATE USER bloom_readonly PASSWORD 'bloom_readonly_pw';
+GRANT CONNECT ON DATABASE bloom_prisma TO bloom_readonly;
+GRANT USAGE ON SCHEMA public TO bloom_readonly;
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO bloom_readonly;
+
+-- Set local role within transation.
+BEGIN;
+SET LOCAL ROLE bloom_api;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO bloom_readonly;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE ON SEQUENCES TO bloom_readonly;
+COMMIT;
+
