f245e46d9dc74350f8983d6d7de309362fb829d5
Emily Jablonski <emijablonski@gmail.com>
2026-01-29 11:55:55 -0700

    refactor: move types to different file

--- sites/partners/cypress/fixtures/bloomingtonListing.ts
+++ sites/partners/cypress/fixtures/bloomingtonListing.ts
@@ -1,7 +1,5 @@
 import {
   HomeTypeEnum,
-  Listing,
-  Address,
   UnitType,
   UnitAccessibilityPriorityType,
   Unit,
@@ -14,52 +12,7 @@ import {
   ReviewOrderTypeEnum,
   ListingNeighborhoodAmenities,
 } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
-
-export type CypressListingImage = {
-  fixtureName: string
-  altText?: string
-}
-export type CypressListing = Listing & {
-  "jurisdiction.id": string
-  events: CypressListingEvent[]
-  dueDate?: CypressListingDateTime
-  postmarkDate?: CypressListingDateTime
-  editedName: string
-  cypressImages?: CypressListingImage[]
-  listingsBuildingAddress: CypressAddress
-  listingsApplicationMailingAddress: CypressAddress
-  listingsLeasingAgentAddress: CypressAddress
-  cypressUtilities?: CypressListingFeatures[]
-  cypressFeatures?: CypressListingFeatures[]
-}
-
-export type CypressListingDateTime = {
-  day: string
-  month: string
-  year: string
-  startHours: string
-  startMinutes: string
-  endHours: string
-  endMinutes: string
-  period: "am" | "pm"
-}
-
-export type CypressListingEvent = {
-  type: ListingEventsTypeEnum
-  dateTime: CypressListingDateTime
-  note: string
-  label: string
-  url: string
-}
-
-export type CypressListingFeatures = {
-  key: string
-  translation: string
-}
-
-export type CypressAddress = Address & {
-  abbreviatedState?: string
-}
+import { CypressAddress, CypressListing } from "./cypressListingHelpers"
 
 export const bloomingtonListing: CypressListing = {
   id: "1",
