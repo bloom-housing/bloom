ae910c5e13eb425e1bc1142fb07f7c240910e94a
Avritt Rohwer <avritt.rohwer@exygy.com>
2026-02-02 11:22:07 -0800

    Validate readonly user can not insert

--- api/dbinit/docker-compose.check.sql
+++ api/dbinit/docker-compose.check.sql
@@ -1,4 +1,17 @@
 -- Test the bloom_readonly expected permissions.
 \set ON_ERROR_STOP on
 
-SELECT id FROM jurisdictions;
+DO $$
+BEGIN
+  -- Should succeed (PERFORM is like a SELECT but the results are discarded).
+  PERFORM id FROM jurisdictions;
+  RAISE NOTICE 'SELECT: OK';
+
+  -- Should fail
+  BEGIN
+    INSERT INTO applicant DEFAULT VALUES;
+    RAISE EXCEPTION 'INSERT unexpectedly succeeded';
+  EXCEPTION WHEN insufficient_privilege THEN
+      RAISE NOTICE 'INSERT: correctly denied';
+  END;
+END $$;
