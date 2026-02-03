01bbde5a6b7d76fe44a29b5922c36a84e9d9a35d
juan <juancarlosmillord@gmail.com>
2026-02-03 14:58:13 -0400

    changed name

---
+++ sites/public/src/components/listing/listing_sections/DetailsCard.tsx
@@ -0,0 +1,26 @@
+import * as React from "react"
+import { Card, Heading, Link } from "@bloom-housing/ui-seeds"
+import styles from "../ListingViewSeeds.module.scss"
+
+type PropertyDetailsCardProps = {
+  heading: string
+  children: React.ReactNode
+  linkUrl: string
+  linkText: string
+}
+
+export const DetailsCard = ({ heading, linkText, linkUrl, children }: PropertyDetailsCardProps) => {
+  return (
+    <Card className={`${styles["mobile-full-width-card"]} ${styles["mobile-no-bottom-border"]}`}>
+      <Card.Section>
+        <Heading size={"lg"} priority={2} className={"seeds-m-be-header"}>
+          {heading}
+        </Heading>
+        {children}
+        <Link href={linkUrl ?? ""} className={"seeds-m-bs-4"} newWindowTarget={true}>
+          {linkText}
+        </Link>
+      </Card.Section>
+    </Card>
+  )
+}
