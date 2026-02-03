2e4dd4ea5296fa16febc1fbe43663b939eede14f
Yazeed Loonat <yazeedloonat@gmail.com>
2026-02-03 00:39:06 -0800

    fix: relative paths <- absolute paths

--- api/src/dtos/applications/demographic-update.dto.ts
+++ api/src/dtos/applications/demographic-update.dto.ts
@@ -2,7 +2,7 @@ import { ApiPropertyOptional, OmitType } from '@nestjs/swagger';
 import { Demographic } from './demographic.dto';
 import { Expose } from 'class-transformer';
 import { IsString, IsUUID } from 'class-validator';
-import { ValidationsGroupsEnum } from 'src/enums/shared/validation-groups-enum';
+import { ValidationsGroupsEnum } from '../../../src/enums/shared/validation-groups-enum';
 
 export class DemographicUpdate extends OmitType(Demographic, ['id']) {
   @Expose()
