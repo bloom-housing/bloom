e87e1026e0997b65a9687f16179f54909804d609
juan <juancarlosmillord@gmail.com>
2026-02-04 12:59:05 -0400

    added simple tests

--- sites/public/src/components/listing/listing_sections/PropertyDetailsCard.tsx
+++ sites/public/src/components/listing/listing_sections/PropertyDetailsCard.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { Card, Heading } from "@bloom-housing/ui-seeds"
-import styles from "../ListingViewSeeds.module.scss"
 
 type PropertyDetailsCardProps = {
   heading: string
