0b6993d535ea5efe169c538a0f247039105bd3b1
Mateusz Zduniuk <matzduniuk@gmail.com>
2026-02-11 16:38:26 +0100

    chore: remove unused imports

--- api/src/dtos/users/advocate-user-create.dto.ts
+++ api/src/dtos/users/advocate-user-create.dto.ts
@@ -1,12 +1,5 @@
-import { ApiProperty, ApiPropertyOptional, OmitType } from '@nestjs/swagger';
+import { OmitType } from '@nestjs/swagger';
 import { AdvocateUserUpdate } from './advocate-user-update.dto';
-import { IsEmail, IsString, Matches, MaxLength } from 'class-validator';
-import { ValidationsGroupsEnum } from '../../enums/shared/validation-groups-enum';
-import { passwordRegex } from 'src/utilities/password-regex';
-import { Match } from '../../decorators/match-decorator';
-import { EnforceLowerCase } from '../../decorators/enforce-lower-case.decorator';
-import { Expose } from 'class-transformer';
-
 export class AdvocateUserCreate extends OmitType(AdvocateUserUpdate, [
   'id',
   'newEmail',
