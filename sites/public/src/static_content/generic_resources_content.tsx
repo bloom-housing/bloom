45c5841a3e9661ab204c1b17d051e57d4d3a7cbd
Emily Jablonski <emijablonski@gmail.com>
2026-02-17 12:40:01 -0700

    fix: contact top

--- sites/public/src/static_content/generic_resources_content.tsx
+++ sites/public/src/static_content/generic_resources_content.tsx
@@ -3,6 +3,8 @@ import { Card, Grid, Heading, Link } from "@bloom-housing/ui-seeds"
 import { GridRow } from "@bloom-housing/ui-seeds/src/layout/Grid"
 import ResourceSection from "../components/resources/ResourceSection"
 import styles from "../components/resources/Resources.module.scss"
+import sectionStyles from "../components/resources/ResourceSection.module.scss"
+
 import ResourceCard from "../components/resources/ResourceCard"
 
 export const getGenericResourcesContent = (): React.ReactNode => {
@@ -15,6 +17,27 @@ export const getGenericResourcesContent = (): React.ReactNode => {
   )
   return (
     <article className={styles["site-content"]}>
+      <div>
+        <Heading
+          className={`${sectionStyles["resource-section-title"]} seeds-m-be-header`}
+          priority={2}
+        >
+          {t("resources.contactTitle")}
+        </Heading>
+        <Card className={styles["contact-card"]}>
+          <div className={styles["contact-card-subsection"]}>
+            <p className={styles["contact-card-description"]}>
+              {t("resources.contactDescription")}
+            </p>
+          </div>
+          <div className={styles["contact-card-subsection"]}>
+            <p className={styles["contact-card-info"]}>{t("resources.contactInfo")}</p>
+            <Link href={`mailto:${t("resources.contactEmail")}`}>
+              {t("resources.contactEmail")}
+            </Link>
+          </div>
+        </Card>
+      </div>
       <div className={styles["resources-section-wrapper"]}>
         <ResourceSection
           sectionTitle={t("resources.immediateHousingTitle")}
@@ -30,24 +53,6 @@ export const getGenericResourcesContent = (): React.ReactNode => {
           </Grid>
         </ResourceSection>
       </div>
-      <aside className={styles["aside-section"]}>
-        <Card className={styles["contact-card"]}>
-          <div className={styles["contact-card-subsection"]}>
-            <Heading size="xl" priority={2}>
-              {t("resources.contactTitle")}
-            </Heading>
-            <p className={styles["contact-card-description"]}>
-              {t("resources.contactDescription")}
-            </p>
-          </div>
-          <div className={styles["contact-card-subsection"]}>
-            <p className={styles["contact-card-info"]}>{t("resources.contactInfo")}</p>
-            <Link href={`mailto:${t("resources.contactEmail")}`}>
-              {t("resources.contactEmail")}
-            </Link>
-          </div>
-        </Card>
-      </aside>
     </article>
   )
 }
