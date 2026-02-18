6f1526616c8db376840210f40df63f948f002317
Emily Jablonski <emijablonski@gmail.com>
2026-02-17 19:37:19 -0700

    fix: override foreign translations

--- sites/public/src/lib/translations.ts
+++ sites/public/src/lib/translations.ts
@@ -10,6 +10,15 @@ import armenianTranslations from "@bloom-housing/shared-helpers/src/locales/hy.j
 import farsiTranslations from "@bloom-housing/shared-helpers/src/locales/fa.json"
 
 import additionalGeneralTranslations from "../../page_content/locale_overrides/general.json"
+import additionalSpanishTranslations from "../../page_content/locale_overrides/es.json"
+import additionalChineseTranslations from "../../page_content/locale_overrides/zh.json"
+import additionalVietnameseTranslations from "../../page_content/locale_overrides/vi.json"
+import additionalTagalogTranslations from "../../page_content/locale_overrides/tl.json"
+import additionalBengaliTranslations from "../../page_content/locale_overrides/bn.json"
+import additionalArabicTranslations from "../../page_content/locale_overrides/ar.json"
+import additionalKoreanTranslations from "../../page_content/locale_overrides/ko.json"
+import additionalArmenianTranslations from "../../page_content/locale_overrides/hy.json"
+import additionalFarsiTranslations from "../../page_content/locale_overrides/fa.json"
 
 export const translations = {
   general: generalTranslations,
@@ -24,4 +33,15 @@ export const translations = {
   fa: farsiTranslations,
 } as Record<string, any>
 
-export const overrideTranslations = { en: additionalGeneralTranslations } as Record<string, any>
+export const overrideTranslations = {
+  en: additionalGeneralTranslations,
+  es: additionalSpanishTranslations,
+  zh: additionalChineseTranslations,
+  vi: additionalVietnameseTranslations,
+  tl: additionalTagalogTranslations,
+  bn: additionalBengaliTranslations,
+  ar: additionalArabicTranslations,
+  ko: additionalKoreanTranslations,
+  hy: additionalArmenianTranslations,
+  fa: additionalFarsiTranslations,
+} as Record<string, any>
