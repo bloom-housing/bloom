7673ce841c86f6d126740fc627a8755a2225d39f
juan <juancarlosmillord@gmail.com>
2026-02-25 12:45:19 -0400

    Revert "feat(public): copy generic fag as a template"
    This reverts commit 437ced9bc92eab127fc6f492ae97babc563e40ee.

--- sites/public/src/static_content/jurisdiction_faq_content.tsx
+++ sites/public/src/static_content/jurisdiction_faq_content.tsx
@@ -1,49 +1,5 @@
-import Markdown from "markdown-to-jsx"
-import { Link } from "@bloom-housing/ui-seeds"
-import { t } from "@bloom-housing/ui-components"
-import { FaqCategory, FaqContent } from "../patterns/FrequentlyAskedQuestions"
+import { FaqContent } from "../patterns/FrequentlyAskedQuestions"
 
 export const getJurisdictionFaqContent = (): FaqContent => {
-  const faqContentSection: FaqCategory = {
-    title: t("faq.genericHeading"),
-    faqs: [
-      {
-        question: t("faq.howDoesThisWork"),
-        answer: (
-          <Markdown>{`${t("content.genericParagraph")} ${t("content.genericParagraph")} ${t(
-            "content.genericParagraph"
-          )}`}</Markdown>
-        ),
-      },
-      {
-        question: t("faq.whatHappensNext"),
-        answer: (
-          <>
-            {t("content.genericParagraph")}
-            <ul>
-              <li>List item one </li>
-              <li>List item two </li>
-              <li>List item three </li>
-              <li>
-                <Link href={"/"}>Linked list item four </Link>
-              </li>
-              <li>
-                {`List item five demonstrates multi-line list elements: ${t(
-                  "content.genericParagraph"
-                )}`}
-              </li>
-            </ul>
-          </>
-        ),
-      },
-      {
-        question: t("faq.howDoesThisFunction"),
-        answer: t("content.genericParagraph"),
-      },
-    ],
-  }
-
-  return {
-    categories: [faqContentSection, faqContentSection, faqContentSection],
-  }
+  return null
 }
