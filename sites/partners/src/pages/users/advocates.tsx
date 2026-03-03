e8aac2fe22e6f6653722248098359947d7025e01
Mateusz Zduniuk <matzduniuk@gmail.com>
2026-03-03 15:32:21 +0100

    chore: add actions button section and dialogs for accepting and rejecting of advocate users

--- sites/partners/src/pages/users/advocates.tsx
+++ sites/partners/src/pages/users/advocates.tsx
@@ -1,10 +1,14 @@
-import React, { useContext, useMemo } from "react"
+import React, { useContext, useMemo, useState } from "react"
 import Head from "next/head"
 import { useRouter } from "next/router"
 import { AgTable, t, useAgTable } from "@bloom-housing/ui-components"
 import { AuthContext } from "@bloom-housing/shared-helpers"
-import { FeatureFlagEnum } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
-import { Button, Icon } from "@bloom-housing/ui-seeds"
+import {
+  Agency,
+  FeatureFlagEnum,
+  User,
+} from "@bloom-housing/shared-helpers/src/types/backend-swagger"
+import { Button, Dialog, Icon } from "@bloom-housing/ui-seeds"
 import DocumentArrowDownIcon from "@heroicons/react/24/solid/DocumentArrowDownIcon"
 import dayjs from "dayjs"
 import Layout from "../../layouts"
@@ -12,9 +16,14 @@ import { NavigationHeader } from "../../components/shared/NavigationHeader"
 import TabView from "../../layouts/TabView"
 import { getUsersTabs, UsersIndexEnum } from "../../components/users/UsersViewHelpers"
 import { useUserList, useUsersExport } from "../../lib/hooks"
+import CheckCircleIcon from "@heroicons/react/24/solid/CheckCircleIcon"
+import XCircleIcon from "@heroicons/react/24/solid/XCircleIcon"
+import { DialogFooter } from "@bloom-housing/ui-seeds/src/overlays/Dialog"
 
 const Advocates = () => {
   const { profile, doJurisdictionsHaveFeatureFlagOn } = useContext(AuthContext)
+  const [selectedUser, setSelectedUser] = useState<User>(null)
+  const [dialogConfig, setDialogConfig] = useState<"accept" | "reject">(null)
   const router = useRouter()
 
   const enableHousingAdvocate = doJurisdictionsHaveFeatureFlagOn(
@@ -26,6 +35,11 @@ const Advocates = () => {
   //TODO (Advocate): Update this to use the new export function
   const { onExport, csvExportLoading } = useUsersExport()
 
+  const handleCloseDialog = () => {
+    setDialogConfig(null)
+    setSelectedUser(null)
+  }
+
   const columns = useMemo(() => {
     return [
       {
@@ -33,6 +47,7 @@ const Advocates = () => {
         field: "agency",
         flex: 1,
         minWidth: 150,
+        valueFormatter: ({ value }) => (value as Agency)?.name,
       },
       {
         headerName: t("t.email"),
@@ -52,87 +67,162 @@ const Advocates = () => {
       {
         headerName: t("users.action"),
         field: "action",
-        cellRendererFramework: () => {
-          //TODO: Add actions
-          return <></>
+        pinned: "right",
+        maxWidth: 150,
+        cellRendererFramework: ({ data }) => {
+          return (
+            <div className="flex justify-center gap-2">
+              <button
+                className="text-success"
+                onClick={() => {
+                  setDialogConfig("accept")
+                  setSelectedUser(data)
+                }}
+                aria-label={"Accept"}
+              >
+                <Icon size="lg">
+                  <CheckCircleIcon />
+                </Icon>
+              </button>
+              <button
+                className="text-alert"
+                onClick={() => {
+                  setDialogConfig("reject")
+                  setSelectedUser(data)
+                }}
+                aria-label={"Reject"}
+              >
+                <Icon size="lg">
+                  <XCircleIcon />
+                </Icon>
+              </button>
+            </div>
+          )
         },
       },
     ]
   }, [])
 
-  //TODO (Advocate): Switch to useUserListAdvocate or sth like that
   const {
-    data: userList,
+    data: advocateUserList,
     loading,
     error,
   } = useUserList({
     page: tableOptions.pagination.currentPage,
     limit: tableOptions.pagination.itemsPerPage,
     search: tableOptions.filter.filterValue,
+    filter: {
+      isAdvocateUser: true,
+    },
   })
 
   if (error) return <div>{t("t.errorOccurred")}</div>
 
   return (
-    <Layout>
-      <Head>
-        <title>{`Users - ${t("users.tabAdvocatesPublic")} - ${t("nav.siteTitlePartners")}`}</title>
-      </Head>
-      <NavigationHeader className="relative" title={t("nav.users")} />
-      <TabView
-        hideTabs={!enableHousingAdvocate}
-        tabs={getUsersTabs(UsersIndexEnum.advocates, router)}
-      >
-        <section>
-          <article className="flex-row flex-wrap relative max-w-screen-xl mx-auto py-8 px-4">
-            <AgTable
-              id="advocate-users-table"
-              pagination={{
-                perPage: tableOptions.pagination.itemsPerPage,
-                setPerPage: tableOptions.pagination.setItemsPerPage,
-                currentPage: tableOptions.pagination.currentPage,
-                setCurrentPage: tableOptions.pagination.setCurrentPage,
-              }}
-              config={{
-                columns,
-                totalItemsLabel: t("users.totalUsers"),
-              }}
-              data={{
-                items: userList?.items,
-                loading: loading,
-                totalItems: userList?.meta.totalItems,
-                totalPages: userList?.meta.totalPages,
-              }}
-              search={{
-                setSearch: tableOptions.filter.setFilterValue,
-              }}
-              headerContent={
-                <div className="flex gap-2 items-center">
-                  {(profile?.userRoles?.isAdmin || profile?.userRoles?.isJurisdictionalAdmin) && (
-                    <Button
-                      variant="primary-outlined"
-                      size="sm"
-                      leadIcon={
-                        !csvExportLoading ? (
-                          <Icon>
-                            <DocumentArrowDownIcon />
-                          </Icon>
-                        ) : null
-                      }
-                      onClick={() => onExport()}
-                      loadingMessage={csvExportLoading && t("t.formSubmitted")}
-                      id={"export-users"}
-                    >
-                      {t("t.exportToCSV")}
-                    </Button>
-                  )}
-                </div>
-              }
-            />
-          </article>
-        </section>
-      </TabView>
-    </Layout>
+    <>
+      <Layout>
+        <Head>
+          <title>{`Users - ${t("users.tabAdvocatesPublic")} - ${t(
+            "nav.siteTitlePartners"
+          )}`}</title>
+        </Head>
+        <NavigationHeader className="relative" title={t("nav.users")} />
+        <TabView
+          hideTabs={!enableHousingAdvocate}
+          tabs={getUsersTabs(UsersIndexEnum.advocates, router)}
+        >
+          <section>
+            <article className="flex-row flex-wrap relative max-w-screen-xl mx-auto py-8 px-4">
+              <AgTable
+                id="advocate-users-table"
+                pagination={{
+                  perPage: tableOptions.pagination.itemsPerPage,
+                  setPerPage: tableOptions.pagination.setItemsPerPage,
+                  currentPage: tableOptions.pagination.currentPage,
+                  setCurrentPage: tableOptions.pagination.setCurrentPage,
+                }}
+                config={{
+                  columns,
+                  totalItemsLabel: t("users.totalUsers"),
+                }}
+                data={{
+                  items: advocateUserList?.items,
+                  loading: loading,
+                  totalItems: advocateUserList?.meta.totalItems,
+                  totalPages: advocateUserList?.meta.totalPages,
+                }}
+                search={{
+                  setSearch: tableOptions.filter.setFilterValue,
+                }}
+                headerContent={
+                  <div className="flex gap-2 items-center">
+                    {(profile?.userRoles?.isAdmin || profile?.userRoles?.isJurisdictionalAdmin) && (
+                      <Button
+                        variant="primary-outlined"
+                        size="sm"
+                        leadIcon={
+                          !csvExportLoading ? (
+                            <Icon>
+                              <DocumentArrowDownIcon />
+                            </Icon>
+                          ) : null
+                        }
+                        onClick={() => onExport()}
+                        loadingMessage={csvExportLoading && t("t.formSubmitted")}
+                        id={"export-users"}
+                      >
+                        {t("t.exportToCSV")}
+                      </Button>
+                    )}
+                  </div>
+                }
+              />
+            </article>
+          </section>
+        </TabView>
+      </Layout>
+      <Dialog isOpen={!!dialogConfig} onClose={handleCloseDialog}>
+        {dialogConfig === "accept" ? (
+          <>
+            <Dialog.Header>{t("users.advocate.acceptHeader")}</Dialog.Header>
+            <Dialog.Content>
+              {t("users.advocate.acceptContent", {
+                user_name: [
+                  selectedUser?.firstName,
+                  selectedUser?.middleName,
+                  selectedUser?.lastName,
+                ].join(" "),
+              })}
+            </Dialog.Content>
+            <DialogFooter>
+              <Button>{t("t.approve")}</Button>
+              <Button variant="primary-outlined" onClick={handleCloseDialog}>
+                {t("t.cancel")}
+              </Button>
+            </DialogFooter>
+          </>
+        ) : (
+          <>
+            <Dialog.Header>{t("users.advocate.rejectHeader")}</Dialog.Header>
+            <Dialog.Content>
+              {t("users.advocate.rejectContent", {
+                user_name: [
+                  selectedUser?.firstName,
+                  selectedUser?.middleName,
+                  selectedUser?.lastName,
+                ].join(" "),
+              })}
+            </Dialog.Content>
+            <DialogFooter>
+              <Button variant="alert">{t("t.decline")}</Button>
+              <Button variant="primary-outlined" onClick={handleCloseDialog}>
+                {t("t.cancel")}
+              </Button>
+            </DialogFooter>
+          </>
+        )}
+      </Dialog>
+    </>
   )
 }
 
