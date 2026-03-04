8cffa6132cff50eff751a395cfdb969c9957e31c
Mateusz Zduniuk <matzduniuk@gmail.com>
2026-03-04 15:28:10 +0100

    chore: update the advocate user accept DTO to pass the appURL

--- api/src/dtos/users/advocate-user-accept.dto.ts
+++ api/src/dtos/users/advocate-user-accept.dto.ts
@@ -1,7 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IdDTO } from '../shared/id.dto';
 import { Expose, Type } from 'class-transformer';
-import { IsBoolean, ValidateNested } from 'class-validator';
+import {
+  IsBoolean,
+  IsString,
+  MaxLength,
+  ValidateNested,
+} from 'class-validator';
 import { ValidationsGroupsEnum } from 'src/enums/shared/validation-groups-enum';
 
 export class AdvocateUserAccept {
@@ -15,4 +20,10 @@ export class AdvocateUserAccept {
   @IsBoolean({ groups: [ValidationsGroupsEnum.default] })
   @ApiProperty()
   isAccepted: boolean;
+
+  @Expose()
+  @IsString({ groups: [ValidationsGroupsEnum.default] })
+  @MaxLength(256, { groups: [ValidationsGroupsEnum.default] })
+  @ApiProperty()
+  appUrl: string;
 }
