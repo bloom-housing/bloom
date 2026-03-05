db3b40e814ec81d1fdc8ce334f65f264c2c16ffa
Mateusz Zduniuk <matzduniuk@gmail.com>
2026-03-05 10:29:11 +0100

    chore: generate new backend swagger types

--- shared-helpers/src/types/backend-swagger.ts
+++ shared-helpers/src/types/backend-swagger.ts
@@ -2043,6 +2043,28 @@ export class UserService {
     })
   }
   /**
+   * Accept or decline advocate user request
+   */
+  approveAdvocate(
+    params: {
+      /** requestBody */
+      body?: AdvocateUserAccept
+    } = {} as any,
+    options: IRequestOptions = {}
+  ): Promise<SuccessDTO> {
+    return new Promise((resolve, reject) => {
+      let url = basePath + "/user/advocate/approve"
+
+      const configs: IRequestConfig = getConfigs("post", "application/json", url, options)
+
+      let data = params.body
+
+      configs.data = data
+
+      axios(configs, resolve, reject)
+    })
+  }
+  /**
    * Request single use code
    */
   requestSingleUseCode(
@@ -9291,6 +9313,14 @@ export interface AdvocateUserUpdate {
 
   /**  */
   jurisdictions?: IdDTO[]
+}
+
+export interface AdvocateUserAccept {
+  /**  */
+  advocateId: IdDTO
+
+  /**  */
+  isAccepted: boolean
 }
 
 export interface User {
