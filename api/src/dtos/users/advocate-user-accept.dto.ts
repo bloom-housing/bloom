2c20b26c1f4718f1012ced05a76ce2a2d16a3300
Mateusz Zduniuk <matzduniuk@gmail.com>
2026-03-05 10:15:14 +0100

    chore: remove app URL from advocate acceptation DTO

--- api/src/dtos/users/advocate-user-accept.dto.ts
+++ api/src/dtos/users/advocate-user-accept.dto.ts
@@ -1,12 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IdDTO } from '../shared/id.dto';
 import { Expose, Type } from 'class-transformer';
-import {
-  IsBoolean,
-  IsString,
-  MaxLength,
-  ValidateNested,
-} from 'class-validator';
+import { IsBoolean, ValidateNested } from 'class-validator';
 import { ValidationsGroupsEnum } from 'src/enums/shared/validation-groups-enum';
 
 export class AdvocateUserAccept {
@@ -20,10 +15,4 @@ export class AdvocateUserAccept {
   @IsBoolean({ groups: [ValidationsGroupsEnum.default] })
   @ApiProperty()
   isAccepted: boolean;
-
-  @Expose()
-  @IsString({ groups: [ValidationsGroupsEnum.default] })
-  @MaxLength(256, { groups: [ValidationsGroupsEnum.default] })
-  @ApiProperty()
-  appUrl: string;
 }
