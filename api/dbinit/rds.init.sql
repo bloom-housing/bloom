e207dafaaf30d15ed1e3f08d646d88e9efc75434
Avritt Rohwer <avritt.rohwer@exygy.com>
2026-03-05 18:06:36 -0800

    only grant rds_iam if creating the db user

--- api/dbinit/rds.init.sql
+++ api/dbinit/rds.init.sql
@@ -18,8 +18,8 @@ DO $$
 BEGIN
     IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'bloom_api') THEN
         CREATE USER bloom_api;
+        GRANT rds_iam TO bloom_api;
     END IF;
-    GRANT rds_iam TO bloom_api;
     GRANT CONNECT ON DATABASE bloom_prisma TO bloom_api;
     GRANT ALL PRIVILEGES ON DATABASE bloom_prisma TO bloom_api;
     GRANT ALL PRIVILEGES ON SCHEMA public TO bloom_api;
@@ -32,8 +32,8 @@ DO $$
 BEGIN
     IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'bloom_readonly') THEN
         CREATE USER bloom_readonly;
+        GRANT rds_iam TO bloom_readonly;
     END IF;
-    GRANT rds_iam TO bloom_readonly;
     GRANT CONNECT ON DATABASE bloom_prisma TO bloom_readonly;
     GRANT USAGE ON SCHEMA public TO bloom_readonly;
     GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO bloom_readonly;
