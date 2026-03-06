821611f7d88db2bb0a327d35b782c84b22bd4ac8
Avritt Rohwer <avritt.rohwer@exygy.com>
2026-03-05 18:02:06 -0800

    fixes

--- api/dbinit/rds.init.sql
+++ api/dbinit/rds.init.sql
@@ -18,11 +18,11 @@ DO $$
 BEGIN
     IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'bloom_api') THEN
         CREATE USER bloom_api;
-        GRANT rds_iam TO bloom_api;
-        GRANT CONNECT ON DATABASE bloom_prisma TO bloom_api;
-        GRANT ALL PRIVILEGES ON DATABASE bloom_prisma TO bloom_api;
-        GRANT ALL PRIVILEGES ON SCHEMA public TO bloom_api;
     END IF;
+    GRANT rds_iam TO bloom_api;
+    GRANT CONNECT ON DATABASE bloom_prisma TO bloom_api;
+    GRANT ALL PRIVILEGES ON DATABASE bloom_prisma TO bloom_api;
+    GRANT ALL PRIVILEGES ON SCHEMA public TO bloom_api;
 END
 $$;
 
@@ -32,11 +32,11 @@ DO $$
 BEGIN
     IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'bloom_readonly') THEN
         CREATE USER bloom_readonly;
-        GRANT rds_iam TO bloom_readonly;
-        GRANT CONNECT ON DATABASE bloom_prisma TO bloom_readonly;
-        GRANT USAGE ON SCHEMA public TO bloom_readonly;
-        GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO bloom_readonly;
     END IF;
+    GRANT rds_iam TO bloom_readonly;
+    GRANT CONNECT ON DATABASE bloom_prisma TO bloom_readonly;
+    GRANT USAGE ON SCHEMA public TO bloom_readonly;
+    GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO bloom_readonly;
 END
 $$;
 
