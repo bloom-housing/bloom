e062d99f9311256e050acc2d3f3c5038d66720d9
juanmillord <juancarlosmillord@gmail.com>
2026-03-09 10:38:23 -0400

    feat: la faq content (#20)
    * feat(public): found file for changes
    
    * feat(public): added test content
    
    * feat(public): finished testing impl
    
    * feat(public): added md files
    
    * feat(public): added first questions
    
    * feat(public): added more questions
    
    * feat(public): added sentence casing of the question
    
    * feat(public): added sentece
    
    * feat(public): finished first section
    
    * feat(public): added new section
    
    * feat(public): added more questions
    
    * feat(public): added more questions
    
    * feat(public): added one more question
    
    * feat(public): added all english questions
    
    * feat(public): removed code that was not needed
    
    * feat(public): added translations for other langs
    
    * feat(public): fixed conflicts
    
    * feat(public): created a helper func
    
    * Revert "feat(public): added translations for other langs"
    
    This reverts commit 0a64a929533975579c7f857fe422396026c09854.
    
    * feat(public): added first two answer
    
    * feat(public): updated english version
    
    * feat(public): updated other versions with the correct text
    
    * feat(public): added double quotes to affordable
    
    * feat(public): fix affordale quotation
    
    * feat(public): updated bullet points first question
    
    * feat(public): updated bullet text position
    
    * feat(public): updated first question
    
    * feat(public): updated quotation
    
    * feat(public): fix vi bullet points issue
    
    * feat(public): updated english content of question
    
    * feat(public): updated question real value
    
    * feat(public): added last two questions
    
    * feat(public): updated other lang for missing questiosn
    
    * feat(public): fixed ko link
    
    * feat(publi): updated quotation mark for korean

--- sites/public/src/static_content/jurisdiction_faq_content.tsx
+++ sites/public/src/static_content/jurisdiction_faq_content.tsx
@@ -1,5 +1,138 @@
-import { FaqContent } from "../patterns/FrequentlyAskedQuestions"
+import Markdown from "markdown-to-jsx"
+import { FaqCategory, FaqContent } from "../patterns/FrequentlyAskedQuestions"
+import { t } from "@bloom-housing/ui-components"
 
 export const getJurisdictionFaqContent = (): FaqContent => {
-  return null
+  const renderMD = (key: string) => (
+    <Markdown options={{ disableParsingRawHTML: true }}>{t(key)}</Markdown>
+  )
+
+  const howShouldIPrepareForTheProcessSection: FaqCategory = {
+    title: t("faq.howShouldIPrepareForTheProcessHeader"),
+    faqs: [
+      {
+        question: t("faq.whatIdentificationWillINeed"),
+        answer: renderMD("faq.whatIdentificationWillINeedAnswer"),
+      },
+      {
+        question: t("faq.whatOtherPaperwork"),
+        answer: renderMD("faq.whatOtherPaperworkAnswer"),
+      },
+      {
+        question: t("faq.howDoIDetermineMyHouseholdSize"),
+        answer: renderMD("faq.howDoIDetermineMyHouseholdSizeAnswer"),
+      },
+      {
+        question: t("faq.whatSizeUnitShouldIApply"),
+        answer: renderMD("faq.whatSizeUnitShouldIApplyAnswer"),
+      },
+      {
+        question: t("faq.howCanImproveMyChancesOfGettingHousing"),
+        answer: renderMD("faq.howCanImproveMyChancesOfGettingHousingAnswer"),
+      },
+      {
+        question: t("faq.areSomeUnitsSetAsideForParticularCommunities"),
+        answer: renderMD("faq.areSomeUnitsSetAsideForParticularCommunitiesAnswer"),
+      },
+    ],
+  }
+  const afterIApplyWhatNextSection: FaqCategory = {
+    title: t("faq.afterIApplyWhatNextHeader"),
+    faqs: [
+      {
+        question: t("faq.howDoPropertyManagersMakeTheirSelections"),
+        answer: renderMD("faq.howDoPropertyManagersMakeTheirSelectionsAnswer"),
+      },
+      {
+        question: t("faq.howLongDoesTheProcessTake"),
+        answer: renderMD("faq.howLongDoesTheProcessTakeAnswer"),
+      },
+      {
+        question: t("faq.whatHappensIfImSelected"),
+        answer: renderMD("faq.whatHappensIfImSelectedAnswer"),
+      },
+      {
+        question: t("faq.onceIveBeenPlacedInAnAffordableUnitHowDoIRemainEligible"),
+        answer: renderMD("faq.onceIveBeenPlacedInAnAffordableUnitHowDoIRemainEligibleAnswer"),
+      },
+    ],
+  }
+
+  const whatElseShouldIKnowAboutAffordableHousingSection: FaqCategory = {
+    title: t("faq.whatElseShouldIKnowAboutAffordableHousingHeader"),
+    faqs: [
+      {
+        question: t("faq.whatMakesHousingAffordable"),
+        answer: renderMD("faq.whatMakesHousingAffordableAnswer"),
+      },
+      {
+        question: t("faq.howDoesMyIncomeAffectTheRentIPay"),
+        answer: renderMD("faq.howDoesMyIncomeAffectTheRentIPayAnswer"),
+      },
+      {
+        question: t("faq.whatIsTheDifferenceBetweenSection8AndHousingChoiceVouchers"),
+        answer: t("faq.whatIsTheDifferenceBetweenSection8AndHousingChoiceVouchersAnswer"),
+      },
+      {
+        question: t("faq.howCanITellIfImBeingScammed"),
+        answer: renderMD("faq.howCanITellIfImBeingScammedAnswer"),
+      },
+      {
+        question: t("faq.whatAgeRestrictionsAreApplicableToSeniorDevelopments"),
+        answer: t("faq.whatAgeRestrictionsAreApplicableToSeniorDevelopmentsAnswer"),
+      },
+      {
+        question: t("faq.areSomeUnitsSetAsideForPersonsThatHaveMobility"),
+        answer: t("faq.areSomeUnitsSetAsideForPersonsThatHaveMobilityAnswer"),
+      },
+      {
+        question: t("faq.howDoIGetSection8Housing"),
+        answer: renderMD("faq.howDoIGetSection8HousingAnswer"),
+      },
+      {
+        question: t("faq.howDoIKnowIfIHaveADisability"),
+        answer: renderMD("faq.howDoIKnowIfIHaveADisabilityAnswer"),
+      },
+      {
+        question: t("faq.whatQuestionsCanBeAskedToProveADisability"),
+        answer: t("faq.whatQuestionsCanBeAskedToProveADisabilityAnswer"),
+      },
+      {
+        question: t("faq.ifIveBeenDiscriminatedAgainstWhatShouldIDo"),
+        answer: renderMD("faq.ifIveBeenDiscriminatedAgainstWhatShouldIDoAnswer"),
+      },
+      {
+        question: t("faq.whatIfIHaveACommunicationBarrier"),
+        answer: renderMD("faq.whatIfIHaveACommunicationBarrierAnswer"),
+      },
+      {
+        question: t("faq.canApropertyAdoptANoPetPolicy"),
+        answer: t("faq.canApropertyAdoptANoPetPolicyAnswer"),
+      },
+      {
+        question: t("faq.whatIsAReasonableModification"),
+        answer: t("faq.whatIsAReasonableModificationAnswer"),
+      },
+      {
+        question: t("faq.howDoIRequestAReasonableModification"),
+        answer: renderMD("faq.howDoIRequestAReasonableModificationAnswer"),
+      },
+      {
+        question: t("faq.areApplicantsWithADIsabilityTreatedDifferently"),
+        answer: renderMD("faq.areApplicantsWithADIsabilityTreatedDifferentlyAnswer"),
+      },
+      {
+        question: t("faq.imExperiencingHomelessness"),
+        answer: renderMD("faq.imExperiencingHomelessnessAnswer"),
+      },
+    ],
+  }
+
+  return {
+    categories: [
+      howShouldIPrepareForTheProcessSection,
+      afterIApplyWhatNextSection,
+      whatElseShouldIKnowAboutAffordableHousingSection,
+    ],
+  }
 }
