e0c085992b3a791c26bc73a519d006b20a3cc4cd
Jared White <jared@whitefusion.studio>
2026-03-19 17:25:30 +0000

    fix: on application view, retrive and pass jurisdiction for non-logged-in users

--- sites/public/src/pages/applications/view.tsx
+++ sites/public/src/pages/applications/view.tsx
@@ -5,14 +5,24 @@ Optional application summary
 import { t } from "@bloom-housing/ui-components"
 import FormsLayout from "../../layouts/forms"
 import { AppSubmissionContext } from "../../lib/applications/AppSubmissionContext"
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { pushGtmEvent, PageView, AuthContext } from "@bloom-housing/shared-helpers"
 import { UserStatus } from "../../lib/constants"
 import { SubmittedApplicationView } from "../../components/applications/SubmittedApplicationView"
+import { Jurisdiction } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
 
 const ApplicationView = () => {
   const { application, listing } = useContext(AppSubmissionContext)
-  const { profile } = useContext(AuthContext)
+  const { jurisdictionsService, profile } = useContext(AuthContext)
+  const [jurisdiction, setJurisdiction] = useState<Jurisdiction>()
+
+  // We have to load the jurisdiction manually in order to check for feature flags, in case a user isn't
+  // logged in and thus doesn't have a profile
+  useEffect(() => {
+    jurisdictionsService
+      .retrieve({ jurisdictionId: listing.jurisdictions.id })
+      .then((res) => setJurisdiction(res))
+  }, [listing, jurisdictionsService])
 
   useEffect(() => {
     pushGtmEvent<PageView>({
@@ -28,6 +38,7 @@ const ApplicationView = () => {
         application={application}
         listing={listing}
         backHref={"/applications/review/confirmation"}
+        jurisdiction={jurisdiction}
       ></SubmittedApplicationView>
     </FormsLayout>
   )
