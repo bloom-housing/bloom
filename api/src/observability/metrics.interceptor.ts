aded6eb4636fb03e1ffb48ead3041a25ed9a4050
Avritt Rohwer <avritt.rohwer@exygy.com>
2026-03-19 22:46:49 -0700

    lint:fix

--- api/src/observability/metrics.interceptor.ts
+++ api/src/observability/metrics.interceptor.ts
@@ -1,4 +1,9 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Response } from 'express';
@@ -6,35 +11,37 @@ import * as opentelemetry from '@opentelemetry/api';
 
 @Injectable()
 export class MetricsInterceptor implements NestInterceptor {
-  meter: opentelemetry.Meter
-  request_counter: opentelemetry.Counter
-  request_latency: opentelemetry.Histogram
+  meter: opentelemetry.Meter;
+  request_counter: opentelemetry.Counter;
+  request_latency: opentelemetry.Histogram;
 
   constructor() {
-    this.meter = opentelemetry.metrics.getMeter('metrics.interceptor')
-    this.request_counter = this.meter.createCounter('metrics.interceptor.request_counter')
-    this.request_latency = this.meter.createHistogram('metrics.interceptor.request_latency')
+    this.meter = opentelemetry.metrics.getMeter('metrics.interceptor');
+    this.request_counter = this.meter.createCounter(
+      'metrics.interceptor.request_counter',
+    );
+    this.request_latency = this.meter.createHistogram(
+      'metrics.interceptor.request_latency',
+    );
   }
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const start = Date.now();
     const attributes = {
-      "controller": context.getClass().name,
-      "method": context.getHandler().name,
-      "protocol": context.getType()
-    }
-    return next
-      .handle()
-      .pipe(
-        tap(() => {
-          const latency = Date.now() - start
-          const httpContext = context.switchToHttp();
-          attributes["response_code"] = httpContext.getResponse<Response>().statusCode
+      controller: context.getClass().name,
+      method: context.getHandler().name,
+      protocol: context.getType(),
+    };
+    return next.handle().pipe(
+      tap(() => {
+        const latency = Date.now() - start;
+        const httpContext = context.switchToHttp();
+        attributes['response_code'] =
+          httpContext.getResponse<Response>().statusCode;
 
-          this.request_counter.add(1, attributes)
-          this.request_latency.record(latency, attributes)
-        }),
-      );
+        this.request_counter.add(1, attributes);
+        this.request_latency.record(latency, attributes);
+      }),
+    );
   }
 }
-
--- api/src/observability/open-telemetry-init.ts
+++ api/src/observability/open-telemetry-init.ts
@@ -6,13 +6,13 @@ import {
   ATTR_SERVICE_NAME,
   ATTR_SERVICE_VERSION,
 } from '@opentelemetry/semantic-conventions';
-// https://aws-otel.github.io/docs/getting-started/js-sdk/metric-manual-instr
-const { OTLPMetricExporter } = require("@opentelemetry/exporter-metrics-otlp-grpc");
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc';
 
+// https://aws-otel.github.io/docs/getting-started/js-sdk/metric-manual-instr
 const sdk = new NodeSDK({
   resource: resourceFromAttributes({
     [ATTR_SERVICE_NAME]: 'bloom-api',
-    [ATTR_SERVICE_VERSION]: '2.0'
+    [ATTR_SERVICE_VERSION]: '2.0',
   }),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter(),
