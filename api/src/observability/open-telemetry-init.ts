fb35cc11140be4f5d90b9dc83b603155bed2424d
Avritt Rohwer <avritt.rohwer@exygy.com>
2026-03-19 22:39:06 -0700

    grafana working

--- api/src/observability/open-telemetry-init.ts
+++ api/src/observability/open-telemetry-init.ts
@@ -1,15 +1,13 @@
 // https://opentelemetry.io/docs/languages/js/getting-started/nodejs/
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
-import {
-  PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
-} from '@opentelemetry/sdk-metrics';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { resourceFromAttributes } from '@opentelemetry/resources';
 import {
   ATTR_SERVICE_NAME,
   ATTR_SERVICE_VERSION,
 } from '@opentelemetry/semantic-conventions';
+// https://aws-otel.github.io/docs/getting-started/js-sdk/metric-manual-instr
+const { OTLPMetricExporter } = require("@opentelemetry/exporter-metrics-otlp-grpc");
 
 const sdk = new NodeSDK({
   resource: resourceFromAttributes({
@@ -17,8 +15,8 @@ const sdk = new NodeSDK({
     [ATTR_SERVICE_VERSION]: '2.0'
   }),
   metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
-    exportIntervalMillis: 10 * 1000, // 10 seconds
+    exporter: new OTLPMetricExporter(),
+    exportIntervalMillis: 1000,
   }),
 });
 
