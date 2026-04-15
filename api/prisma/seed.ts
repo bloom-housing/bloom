2b987d7521bdd74439125c9b6f19145807bbc5ca
Morgan Ludtke <ludtkemorgan@gmail.com>
2026-04-15 09:27:58 -0500

    refactor: cleanup seed-staging file and add bay bridge

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -11,12 +11,13 @@ const options: { [name: string]: { type: 'string' | 'boolean' } } = {
   environment: { type: 'string' },
   jurisdictionName: { type: 'string' },
   msqV2: { type: 'boolean' },
+  asRegion: { type: 'boolean' },
 };
 
 const prisma = new PrismaService();
 async function main() {
   const {
-    values: { environment, jurisdictionName, msqV2 },
+    values: { environment, jurisdictionName, msqV2, asRegion },
   } = parseArgs({ options });
   const publicSiteBaseURL = env.DBSEED_PUBLIC_SITE_BASE_URL;
 
@@ -34,12 +35,12 @@ async function main() {
     case 'staging':
       // Staging setup should have realistic looking data with a preset list of listings
       // along with all of the required tables (ami, users, etc)
-      stagingSeed(
-        prisma,
-        jurisdictionName as string,
-        publicSiteBaseURL,
-        msqV2 as boolean,
-      );
+      stagingSeed(prisma, {
+        jurisdictionName: jurisdictionName as string,
+        publicSiteBaseURL: publicSiteBaseURL,
+        msqV2: msqV2 as boolean,
+        asRegion: asRegion as boolean,
+      });
       break;
     case 'development':
     default:
