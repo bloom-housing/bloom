c77794091dc7595daa359376df6dae5fddf5e073
Mateusz Zduniuk <matzduniuk@gmail.com>
2026-04-16 14:34:15 +0200

    chore: add collapsible regions field group

--- sites/public/src/components/account/NotificationPreferences.tsx
+++ sites/public/src/components/account/NotificationPreferences.tsx
@@ -1,10 +1,16 @@
 import { BloomCard } from "@bloom-housing/shared-helpers"
 import styles from "./NotificationPreferences.module.scss"
-import { Button, Card } from "@bloom-housing/ui-seeds"
+import { Button, Card, Icon } from "@bloom-housing/ui-seeds"
 import { useForm } from "react-hook-form"
 import { FieldGroup, t } from "@bloom-housing/ui-components"
+import { useState } from "react"
+import PlusIcon from "@heroicons/react/24/solid/PlusIcon"
+import MinusIcon from "@heroicons/react/24/solid/MinusIcon"
+import { RegionEnum } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
 
 export const NotificationPreferences = () => {
+  const [collapsed, setCollapsed] = useState(false)
+
   // eslint-disable-next-line @typescript-eslint/unbound-method
   const { register } = useForm()
 
@@ -22,7 +28,7 @@ export const NotificationPreferences = () => {
         <Card.Section>
           <FieldGroup
             register={register}
-            name="Test Input"
+            name="preferences-field-group"
             type={"checkbox"}
             fieldGroupClassName={styles["notification-preferences-field-group"]}
             fields={[
@@ -58,9 +64,52 @@ export const NotificationPreferences = () => {
               },
             ]}
           />
+          <div className={styles["collapsible-field-group"]}>
+            <div className={styles["button-header"]}>
+              <button
+                onClick={() => setCollapsed(!collapsed)}
+                aria-label={!collapsed ? t("t.collapseSection") : t("t.expandSection")}
+                aria-expanded={!collapsed}
+                className={styles["button"]}
+              >
+                <div className={styles["button-label-container"]}>
+                  <span className={styles["button-label"]}>Regions</span>
+                  <div>
+                    <div className={styles["button-icon"]}>
+                      {collapsed ? (
+                        <Icon size={"md"}>
+                          <PlusIcon />
+                        </Icon>
+                      ) : (
+                        <Icon size={"md"}>
+                          <MinusIcon />
+                        </Icon>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </button>
+              <span className={styles["collapsible-field-group-note"]}>Two regions selected</span>
+            </div>
+            <div>
+              {!collapsed && (
+                <FieldGroup
+                  register={register}
+                  name="regions-field-group"
+                  type={"checkbox"}
+                  fields={Object.values(RegionEnum).map((region) => ({
+                    id: region,
+                    label: region.replace("_", " "),
+                  }))}
+                />
+              )}
+            </div>
+          </div>
         </Card.Section>
         <Card.Footer className={styles["card-footer"]}>
-          <Button variant="primary-outlined">{t("t.save")}</Button>
+          <Button type="submit" size="sm" variant="primary-outlined">
+            {t("t.save")}
+          </Button>
         </Card.Footer>
       </>
     </BloomCard>
