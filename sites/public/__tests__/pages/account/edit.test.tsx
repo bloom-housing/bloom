0cb6b16391f52f71f6970dfdaccfdce4a6491a95
Mateusz Zduniuk <matzduniuk@gmail.com>
2026-04-20 12:21:18 +0200

    chore: update integrations tests for edit account page

--- sites/public/__tests__/pages/account/edit.test.tsx
+++ sites/public/__tests__/pages/account/edit.test.tsx
@@ -1,8 +1,13 @@
 import React from "react"
 import { useRouter } from "next/router"
 import { AuthContext } from "@bloom-housing/shared-helpers"
-import { Agency, User, UserService } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
-import { user } from "@bloom-housing/shared-helpers/__tests__/testHelpers"
+import {
+  Agency,
+  FeatureFlagEnum,
+  User,
+  UserService,
+} from "@bloom-housing/shared-helpers/src/types/backend-swagger"
+import { jurisdiction, user } from "@bloom-housing/shared-helpers/__tests__/testHelpers"
 import Edit from "../../../src/pages/account/edit"
 import { render, screen } from "../../testUtils"
 
@@ -65,12 +70,14 @@ describe("<Edit>", () => {
           userService: mockUserService as unknown as UserService,
         }}
       >
-        <Edit agencies={mockAgencies} />
+        <Edit agencies={mockAgencies} jurisdiction={jurisdiction} />
       </AuthContext.Provider>
     )
 
     expect(screen.getByTestId("edit-advocate-account")).toBeInTheDocument()
     expect(screen.queryByTestId("edit-public-account")).not.toBeInTheDocument()
+    expect(screen.queryByRole("link", { name: /my profile/i })).not.toBeInTheDocument()
+    expect(screen.queryByRole("link", { name: /my notifications/i })).not.toBeInTheDocument()
   })
 
   it("renders public account edit form when user is not an advocate", () => {
@@ -95,9 +102,61 @@ describe("<Edit>", () => {
           userService: mockUserService as unknown as UserService,
         }}
       >
-        <Edit agencies={mockAgencies} />
+        <Edit agencies={mockAgencies} jurisdiction={jurisdiction} />
       </AuthContext.Provider>
     )
+
+    expect(screen.getByTestId("edit-public-account")).toBeInTheDocument()
+    expect(screen.queryByTestId("edit-advocate-account")).not.toBeInTheDocument()
+    expect(screen.queryByRole("link", { name: /my profile/i })).not.toBeInTheDocument()
+    expect(screen.queryByRole("link", { name: /my notifications/i })).not.toBeInTheDocument()
+  })
+
+  it("renders the user profile edit form in tabbed view when the enableCustomListingNotifications feature flag is on", () => {
+    const testUser = {
+      ...user,
+      firstName: "First",
+      middleName: "Middle",
+      lastName: "Last",
+      email: "first.last@bloom.com",
+      isAdvocate: false,
+      isApproved: false,
+      listings: [],
+      jurisdictions: [],
+    }
+    mockUserService.retrieve.mockResolvedValue(testUser)
+    render(
+      <AuthContext.Provider
+        value={{
+          profile: {
+            isAdvocate: false,
+          } as User,
+          userService: mockUserService as unknown as UserService,
+        }}
+      >
+        <Edit
+          agencies={mockAgencies}
+          jurisdiction={{
+            ...jurisdiction,
+            featureFlags: [
+              ...jurisdiction.featureFlags,
+              {
+                id: "feature_flag_id",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                name: FeatureFlagEnum.enableCustomListingNotifications,
+                description: "",
+                active: true,
+                jurisdictions: [],
+              },
+            ],
+          }}
+        />
+      </AuthContext.Provider>
+    )
+
+    expect(screen.getByRole("link", { name: /my profile/i })).toBeInTheDocument()
+    expect(screen.getByRole("link", { name: /my notifications/i })).toBeInTheDocument()
 
     expect(screen.getByTestId("edit-public-account")).toBeInTheDocument()
     expect(screen.queryByTestId("edit-advocate-account")).not.toBeInTheDocument()
