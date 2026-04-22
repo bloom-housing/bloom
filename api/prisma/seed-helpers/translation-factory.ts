0547fa9109caa6f7711de8e83f74619e1faafe73
krzysztof ziecina <kziecina@airnauts.com>
2026-04-22 18:04:04 +0200

    fix: add view listing and apply translations

--- api/prisma/seed-helpers/translation-factory.ts
+++ api/prisma/seed-helpers/translation-factory.ts
@@ -586,6 +586,15 @@ const translations = (
           },
           viewButton: {
             en: 'View listing & apply',
+            es: 'Ver listado y aplicar',
+            zh: '查看列表并申请',
+            vi: 'Xem danh sách và áp dụng',
+            tl: 'Tingnan ang listahan at mag-apply',
+            bn: 'তালিকা দেখুন এবং আবেদন করুন',
+            ar: 'عرض القائمة والتقديم',
+            fa: 'مشاهده لیست و اعمال',
+            hy: 'Դիտեք ցուցակը և կիրառեք',
+            ko: '목록 보기 및 신청',
           },
           footer: {
             accessibleMarketingFlyer: 'Accessible marketing flyer',
