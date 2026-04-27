909bd55dd70bb1ba828978210329430c1210f384
Mateusz Zduniuk <matzduniuk@gmail.com>
2026-04-27 16:32:38 +0200

    chore: add additional integration tests

--- sites/public/__tests__/pages/account/notifications.test.tsx
+++ sites/public/__tests__/pages/account/notifications.test.tsx
@@ -257,4 +257,260 @@ describe("<Notifications>", () => {
     await userEvent.click(regionsCheckboxes[1])
     expect(await within(formWrapper).findByText("2 regions selected")).toBeInTheDocument()
   })
+
+  it("should hide the region checkboxes when button is clicked", async () => {
+    const mockNotificationPreferences: UserNotificationPreferences = {
+      lottery: false,
+      waitlist: false,
+      mobility: false,
+      hearing: false,
+      vision: false,
+      hearingAndVision: false,
+      mobilityAndHearing: false,
+      mobilityAndVision: false,
+      mobilityHearingAndVision: false,
+      wantsRegionNotifs: false,
+      regions: [],
+    }
+
+    const mockRegions: string[] = [
+      "Metro_Area",
+      "South_Bay",
+      "Downtown",
+      "Harbor_Area",
+      "Southwest",
+    ]
+
+    mockUserService.retrieve.mockResolvedValue(user)
+    mockUserService.getNotificationPreferences.mockResolvedValue(mockNotificationPreferences)
+    render(
+      <AuthContext.Provider
+        value={{
+          profile: {
+            isAdvocate: false,
+          } as User,
+          userService: mockUserService as unknown as UserService,
+        }}
+      >
+        <Notifications
+          jurisdiction={{
+            ...jurisdiction,
+            regions: mockRegions as [],
+            featureFlags: [
+              ...jurisdiction.featureFlags,
+              {
+                id: "feature_flag_id",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                name: FeatureFlagEnum.enableCustomListingNotifications,
+                description: "",
+                active: true,
+                jurisdictions: [],
+              },
+            ],
+          }}
+        />
+      </AuthContext.Provider>
+    )
+
+    const regionsFieldGroup = await screen.findByTestId("regions-field-group")
+    expect(regionsFieldGroup).toBeInTheDocument()
+    const regionsButton = within(regionsFieldGroup.parentElement).getByRole("button")
+    expect(regionsButton).toBeInTheDocument()
+
+    expect(regionsButton).toHaveAttribute("aria-expanded", "true")
+    await userEvent.click(regionsButton)
+    expect(regionsButton).toHaveAttribute("aria-expanded", "false")
+  })
+
+  it("does not render regions checkboxes for jurisdiction without", async () => {
+    const mockNotificationPreferences: UserNotificationPreferences = {
+      lottery: false,
+      waitlist: false,
+      mobility: false,
+      hearing: false,
+      vision: false,
+      hearingAndVision: false,
+      mobilityAndHearing: false,
+      mobilityAndVision: false,
+      mobilityHearingAndVision: false,
+      wantsRegionNotifs: false,
+      regions: [],
+    }
+
+    const mockRegions: string[] = []
+
+    mockUserService.retrieve.mockResolvedValue(user)
+    mockUserService.getNotificationPreferences.mockResolvedValue(mockNotificationPreferences)
+    render(
+      <AuthContext.Provider
+        value={{
+          profile: {
+            isAdvocate: false,
+          } as User,
+          userService: mockUserService as unknown as UserService,
+        }}
+      >
+        <Notifications
+          jurisdiction={{
+            ...jurisdiction,
+            regions: mockRegions as [],
+            featureFlags: [
+              ...jurisdiction.featureFlags,
+              {
+                id: "feature_flag_id",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                name: FeatureFlagEnum.enableCustomListingNotifications,
+                description: "",
+                active: true,
+                jurisdictions: [],
+              },
+            ],
+          }}
+        />
+      </AuthContext.Provider>
+    )
+
+    expect(screen.getByRole("link", { name: /my profile/i })).toBeInTheDocument()
+    expect(screen.getByRole("link", { name: /my notifications/i })).toBeInTheDocument()
+
+    const formWrapper = await screen.findByRole("article")
+    expect(formWrapper).toBeInTheDocument()
+
+    expect(within(formWrapper).queryByTestId("regions-field-group")).not.toBeInTheDocument()
+  })
+
+  it("select all option should select and deselect all available options", async () => {
+    const mockNotificationPreferences: UserNotificationPreferences = {
+      lottery: false,
+      waitlist: false,
+      mobility: false,
+      hearing: false,
+      vision: false,
+      hearingAndVision: false,
+      mobilityAndHearing: false,
+      mobilityAndVision: false,
+      mobilityHearingAndVision: false,
+      wantsRegionNotifs: false,
+      regions: [],
+    }
+
+    const mockRegions: string[] = [
+      "Metro_Area",
+      "South_Bay",
+      "Downtown",
+      "Harbor_Area",
+      "Southwest",
+    ]
+
+    mockUserService.retrieve.mockResolvedValue(user)
+    mockUserService.getNotificationPreferences.mockResolvedValue(mockNotificationPreferences)
+    render(
+      <AuthContext.Provider
+        value={{
+          profile: {
+            isAdvocate: false,
+          } as User,
+          userService: mockUserService as unknown as UserService,
+        }}
+      >
+        <Notifications
+          jurisdiction={{
+            ...jurisdiction,
+            regions: mockRegions as [],
+            featureFlags: [
+              ...jurisdiction.featureFlags,
+              {
+                id: "feature_flag_id",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                name: FeatureFlagEnum.enableCustomListingNotifications,
+                description: "",
+                active: true,
+                jurisdictions: [],
+              },
+            ],
+          }}
+        />
+      </AuthContext.Provider>
+    )
+
+    expect(screen.getByRole("link", { name: /my profile/i })).toBeInTheDocument()
+    expect(screen.getByRole("link", { name: /my notifications/i })).toBeInTheDocument()
+
+    const formWrapper = await screen.findByRole("article")
+    expect(formWrapper).toBeInTheDocument()
+
+    const selectAllCheckbox = within(formWrapper).getByRole("checkbox", { name: /select all/i })
+    expect(selectAllCheckbox).toBeInTheDocument()
+    expect(selectAllCheckbox).not.toBeChecked()
+
+    const hearingAndVisionCheckbox = within(formWrapper).getByRole("checkbox", {
+      name: /^hearing\/vision units$/i,
+    })
+    expect(hearingAndVisionCheckbox).toBeInTheDocument()
+    expect(hearingAndVisionCheckbox).not.toBeChecked()
+
+    const mobilityCheckbox = within(formWrapper).getByRole("checkbox", { name: /mobility units/i })
+    expect(mobilityCheckbox).toBeInTheDocument()
+    expect(mobilityCheckbox).not.toBeChecked()
+
+    const mobilityHearingAndVisionCheckbox = within(formWrapper).getByRole("checkbox", {
+      name: /mobility and hearing\/vision units/i,
+    })
+    expect(mobilityHearingAndVisionCheckbox).toBeInTheDocument()
+    expect(mobilityHearingAndVisionCheckbox).not.toBeChecked()
+
+    const newLotteriesCheckbox = within(formWrapper).getByRole("checkbox", {
+      name: /new lotteries/i,
+    })
+    expect(newLotteriesCheckbox).toBeInTheDocument()
+
+    const newWaitlistCheckbox = within(formWrapper).getByRole("checkbox", {
+      name: /new waitlists/i,
+    })
+    expect(newWaitlistCheckbox).toBeInTheDocument()
+    expect(newWaitlistCheckbox).not.toBeChecked()
+
+    const regionsFieldGroup = within(formWrapper).getByTestId("regions-field-group")
+    expect(regionsFieldGroup).toBeInTheDocument()
+
+    const regionsButton = within(regionsFieldGroup).getByText(/regions$/i)
+    expect(regionsButton).toBeInTheDocument()
+
+    expect(within(formWrapper).getByText("No regions selected")).toBeInTheDocument()
+    const regionCheckboxes = Object.values(mockRegions).map((region) => {
+      return within(regionsFieldGroup).getByRole("checkbox", {
+        name: region.replace("_", " "),
+      })
+    })
+
+    regionCheckboxes.forEach((entry) => {
+      expect(entry).toBeInTheDocument()
+      expect(entry).not.toBeChecked()
+    })
+
+    await userEvent.click(selectAllCheckbox)
+
+    expect(selectAllCheckbox).toBeChecked()
+    expect(hearingAndVisionCheckbox).toBeChecked()
+    expect(mobilityCheckbox).toBeChecked()
+    expect(mobilityHearingAndVisionCheckbox).toBeChecked()
+    expect(newWaitlistCheckbox).toBeChecked()
+    regionCheckboxes.forEach((entry) => {
+      expect(entry).toBeChecked()
+    })
+
+    await userEvent.click(selectAllCheckbox)
+
+    expect(selectAllCheckbox).not.toBeChecked()
+    expect(hearingAndVisionCheckbox).not.toBeChecked()
+    expect(mobilityCheckbox).not.toBeChecked()
+    expect(mobilityHearingAndVisionCheckbox).not.toBeChecked()
+    expect(newWaitlistCheckbox).not.toBeChecked()
+    regionCheckboxes.forEach((entry) => {
+      expect(entry).not.toBeChecked()
+    })
+  })
 })
