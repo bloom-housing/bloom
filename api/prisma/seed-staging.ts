266ba18e660189173e7dd6b5fd836bf87496ee29
Mateusz Zduniuk <matzduniuk@gmail.com>
2026-04-27 13:11:50 +0200

    fix: add the enableCustomListingNotifications as default for two seed jurisdictions

--- api/prisma/seed-staging.ts
+++ api/prisma/seed-staging.ts
@@ -207,6 +207,7 @@ export const stagingSeed = async (
         FeatureFlagEnum.enableUtilitiesIncluded,
         FeatureFlagEnum.enableWaitlistLottery,
         FeatureFlagEnum.enableWhatToExpectAdditionalField,
+        FeatureFlagEnum.enableCustomListingNotifications,
       ],
       languages: Object.values(LanguagesEnum),
       requiredListingFields: [
@@ -353,6 +354,7 @@ export const stagingSeed = async (
         FeatureFlagEnum.enableSpokenLanguage,
         FeatureFlagEnum.enableUnitAccessibilityTypeTags,
         FeatureFlagEnum.disableReservedCommunityTypeEdit,
+        FeatureFlagEnum.enableCustomListingNotifications,
       ],
       visibleNeighborhoodAmenities: [
         NeighborhoodAmenitiesEnum.groceryStores,
