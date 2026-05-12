2d02b83b9ac3c1e07d571f7cb351f7f0750f6f52
Morgan Ludtke <ludtkemorgan@gmail.com>
2026-05-12 17:07:04 -0500

    fix: rest of faq content and some resource content

--- sites/public/src/static_content/jurisdiction_faq_content.tsx
+++ sites/public/src/static_content/jurisdiction_faq_content.tsx
@@ -159,16 +159,167 @@ export const getJurisdictionFaqContent = (): FaqContent => {
           </>
         ),
       },
+      {
+        question: t("help.faq.longProcess"),
+        answer: (
+          <>
+            {t("help.faq.longProcessResp1")}
+            <br />
+            <br />
+            {t("help.faq.longProcessResp2")}
+            <ul>
+              <li>{t("help.faq.longProcessResp3")}</li>
+              <li>{t("help.faq.longProcessResp4")}</li>
+              <li>{t("help.faq.longProcessResp5")}</li>
+            </ul>
+            <br />
+            {t("help.faq.longProcessResp6")}
+            <br />
+            <br />
+            {t("help.faq.longProcessResp7")}
+          </>
+        ),
+      },
+      {
+        question: t("help.faq.selected"),
+        answer: (
+          <>
+            {t("help.faq.selectedResp1")}
+            <ul>
+              <li>{t("help.faq.selectedResp2")}</li>
+              <li>{t("help.faq.selectedResp3")}</li>
+            </ul>
+          </>
+        ),
+      },
+      {
+        question: t("help.faq.remainEligible"),
+        answer: (
+          <>
+            <ul>
+              <li>{t("help.faq.remainEligibleResp1")}</li>
+              <li>{t("help.faq.remainEligibleResp2")}</li>
+            </ul>
+          </>
+        ),
+      },
     ],
   }
 
   const whatElseToKnow: FaqCategory = {
     title: t("help.faq.whatElseToKnowHeader"),
-    faqs: [],
+    faqs: [
+      {
+        question: t("help.faq.whatMakesAffordable"),
+        answer: (
+          <>
+            {t("help.faq.whatMakesAffordableResp")}
+            <ul>
+              <li>
+                <b>{t("help.faq.whatMakesAffordableResp1")}</b>
+                {t("help.faq.whatMakesAffordableResp1b")}
+              </li>
+              <li>
+                <b>{t("help.faq.whatMakesAffordableResp2")}</b>
+                {t("help.faq.whatMakesAffordableResp2b")}
+              </li>
+            </ul>
+            <br />
+            {t("help.faq.whatMakesAffordableResp3")}
+            <br />
+            <br />
+            {t("help.faq.whatMakesAffordableResp4")}
+            <ul>
+              <li>{t("help.faq.whatMakesAffordableResp5")}</li>
+              <li>{t("help.faq.whatMakesAffordableResp6")}</li>
+              <li>{t("help.faq.whatMakesAffordableResp7")}</li>
+              <li>{t("help.faq.whatMakesAffordableResp8")}</li>
+              <li>{t("help.faq.whatMakesAffordableResp9")}</li>
+            </ul>
+          </>
+        ),
+      },
+      {
+        question: t("help.faq.incomeAffect"),
+        answer: (
+          <>
+            {t("help.faq.incomeAffectResp1")}
+            <br />
+            <br />
+            {t("help.faq.incomeAffectResp2")}
+            <Link href={"https://www.huduser.gov/portal/datasets/il.html"}>
+              {t("help.faq.incomeAffectResp3")}
+            </Link>
+            {t("help.faq.incomeAffectResp4")}
+            <br />
+            <br />
+            {t("help.faq.incomeAffectResp5")}
+            <ul>
+              <li>{t("help.faq.incomeAffectResp6")}</li>
+              <li>{t("help.faq.incomeAffectResp7")}</li>
+            </ul>
+          </>
+        ),
+      },
+      {
+        question: t("help.faq.section8VoucherDiff"),
+        answer: <>{t("help.faq.section8VoucherDiffResp")}</>,
+      },
+      {
+        question: t("help.faq.scammed"),
+        answer: (
+          <>
+            {t("help.faq.scammedResp")}
+            <ul>
+              <li>{t("help.faq.scammedResp1")}</li>
+              <li>{t("help.faq.scammedResp2")}</li>
+            </ul>
+            <br />
+            {t("help.faq.scammedResp3")}
+            <Link href={"https://consumer.ftc.gov/articles/rental-listing-scams"}>
+              {t("help.faq.scammedResp4")}
+            </Link>
+            {t("help.faq.scammedResp5")}
+          </>
+        ),
+      },
+    ],
   }
   const lotteryResults: FaqCategory = {
     title: t("help.faq.lotteryResults"),
-    faqs: [],
+    faqs: [
+      {
+        question: t("help.faq.lotteryResults.rawRank"),
+        answer: <>{t("help.faq.lotteryResults.rawRankResp1")}</>,
+      },
+      {
+        question: t("help.faq.lotteryResults.preferences"),
+        answer: (
+          <>
+            {t("help.faq.lotteryResults.preferencesResp1")}
+            <br />
+            <br />
+            {t("help.faq.lotteryResults.preferencesResp2")}
+          </>
+        ),
+      },
+      {
+        question: t("help.faq.lotteryResults.order"),
+        answer: (
+          <>
+            {t("help.faq.lotteryResults.orderResp1")}
+            <br />
+            <br />
+            {t("help.faq.lotteryResults.orderResp2")}
+            <ul>
+              <li>{t("help.faq.lotteryResults.orderList1")}</li>
+              <li>{t("help.faq.lotteryResults.orderList2")}</li>
+              <li>{t("help.faq.lotteryResults.orderList3")}</li>
+            </ul>
+          </>
+        ),
+      },
+    ],
   }
 
   return {
--- sites/public/src/static_content/jurisdiction_resources_content.tsx
+++ sites/public/src/static_content/jurisdiction_resources_content.tsx
@@ -1,5 +1,38 @@
+import { t } from "@bloom-housing/ui-components"
+import ResourceCard from "../components/resources/ResourceCard"
 import { ResourceCards } from "../components/resources/Resources"
 
 export const getJurisdictionResourcesContent = (): ResourceCards | null => {
-  return null
+  return {
+    resourceSections: [
+      {
+        sectionTitle: t("resources.immediateHousingTitle"),
+        sectionSubtitle: t("help.housingHelp.immediateHousingAssistanceHeader"),
+        cards: [
+          <ResourceCard
+            title={t("help.housingHelp.immediate.alamedaBACS")}
+            href="https://www.bayareacs.org/"
+            content={t("help.housingHelp.immediate.alamedaBACSinfo")}
+          />,
+          <ResourceCard
+            title={t("help.housingHelp.immediate.alamedaOD")}
+            href="https://operationdignity.org/"
+            content={t("help.housingHelp.immediate.alamedaODinfo")}
+          />,
+          // TODO: add all other content cards here
+        ],
+      },
+      {
+        sectionTitle: t("help.housingHelp.counseling.title"),
+        cards: [
+          <ResourceCard
+            title={t("help.housingHelp.counseling.HUD")}
+            href="https://hudgov-answers.force.com/housingcounseling/"
+            content={t("help.housingHelp.counseling.HUDinfo")}
+          />,
+          // TODO: add all other content cards here
+        ],
+      },
+    ],
+  }
 }
