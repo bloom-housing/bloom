fa3efec82a17543e6cea6cde6ec69a5b1c99ad3b
Eric McGarry <eric@eric.mcgarry.name>
2026-06-03 09:40:15 -0400

    feat: remove uuid typing

--- api/prisma/migrations/60_external_listing_fields/migration.sql
+++ api/prisma/migrations/60_external_listing_fields/migration.sql
@@ -1,9 +1,11 @@
 -- AlterTable
-ALTER TABLE "listing_snapshot" ADD COLUMN     "external_jurisdiction_id" UUID,
-ADD COLUMN     "external_listing_id" UUID,
+ALTER TABLE "listing_snapshot"
+ADD COLUMN     "external_jurisdiction_id" TEXT,
+ADD COLUMN     "external_listing_id" TEXT,
 ADD COLUMN     "external_url" TEXT;
 
 -- AlterTable
-ALTER TABLE "listings" ADD COLUMN     "external_jurisdiction_id" UUID,
-ADD COLUMN     "external_listing_id" UUID,
+ALTER TABLE "listings"
+ADD COLUMN     "external_jurisdiction_id" TEXT,
+ADD COLUMN     "external_listing_id" TEXT,
 ADD COLUMN     "external_url" TEXT;
--- api/src/modules/external-listing.module.ts
+++ api/src/modules/external-listing.module.ts
@@ -2,7 +2,7 @@ import { Logger, Module } from '@nestjs/common';
 import { PermissionModule } from './permission.module';
 import { PrismaModule } from './prisma.module';
 import { ExternalListingController } from '../controllers/external-listing.controller';
-import { ExternalListingService } from 'src/services/external-listing.service';
+import { ExternalListingService } from '../services/external-listing.service';
 
 @Module({
   imports: [PermissionModule, PrismaModule],
