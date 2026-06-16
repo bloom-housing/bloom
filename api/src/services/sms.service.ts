031d71e5d801a34dca0a86312f65731f9b9e1fae
Chad Brokaw <36685920+chadbrokaw@users.noreply.github.com>
2026-06-16 16:20:55 -0600

    feat(6228): add awa sms integration

--- api/src/services/sms.service.ts
+++ api/src/services/sms.service.ts
@@ -1,12 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { AwsCredentialIdentity } from '@aws-sdk/types';
+import {
+  PinpointSMSVoiceV2Client,
+  SendTextMessageCommand,
+} from '@aws-sdk/client-pinpoint-sms-voice-v2';
 import twilio from 'twilio';
 import TwilioClient from 'twilio/lib/rest/Twilio';
 
 @Injectable()
 export class SmsService {
   client: TwilioClient;
+  awsClient: PinpointSMSVoiceV2Client;
   public constructor() {
-    if (process.env.TWILIO_ACCOUNT_SID && process.env.TWILIO_AUTH_TOKEN) {
+    if (process.env.SMS_PROVIDER === 'aws') {
+      let credentials: undefined | AwsCredentialIdentity;
+      const keyId = process.env.AWS_SMS_ACCESS_KEY_ID;
+      const secret = process.env.AWS_SMS_SECRET_ACCESS_KEY;
+      if (keyId && secret) {
+        credentials = {
+          accessKeyId: keyId,
+          secretAccessKey: secret,
+        };
+      }
+      this.awsClient = new PinpointSMSVoiceV2Client({
+        region: process.env.AWS_SMS_REGION,
+        credentials,
+      });
+    } else if (
+      process.env.TWILIO_ACCOUNT_SID &&
+      process.env.TWILIO_AUTH_TOKEN
+    ) {
       this.client = twilio(
         process.env.TWILIO_ACCOUNT_SID,
         process.env.TWILIO_AUTH_TOKEN,
@@ -17,13 +40,27 @@ export class SmsService {
     phoneNumber: string,
     singleUseCode: string,
   ): Promise<void> {
-    if (!this.client) {
-      return;
+    if (process.env.SMS_PROVIDER === 'aws') {
+      if (!this.awsClient) {
+        return;
+      }
+      await this.awsClient.send(
+        new SendTextMessageCommand({
+          DestinationPhoneNumber: phoneNumber,
+          OriginationIdentity: process.env.AWS_SMS_ORIGINATION_NUMBER,
+          MessageBody: `Your Partners Portal account access token: ${singleUseCode}`,
+          MessageType: 'TRANSACTIONAL',
+        }),
+      );
+    } else {
+      if (!this.client) {
+        return;
+      }
+      await this.client.messages.create({
+        body: `Your Partners Portal account access token: ${singleUseCode}`,
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: phoneNumber,
+      });
     }
-    await this.client.messages.create({
-      body: `Your Partners Portal account access token: ${singleUseCode}`,
-      from: process.env.TWILIO_PHONE_NUMBER,
-      to: phoneNumber,
-    });
   }
 }
