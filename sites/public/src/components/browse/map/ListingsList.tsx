242883139073c99b28de9380af19d5aad051dc0a
Morgan Ludtke <ludtkemorgan@gmail.com>
2026-06-16 15:56:45 -0500

    fix: add dynamic listing cards under map listings

--- sites/public/src/components/browse/map/ListingsList.tsx
+++ sites/public/src/components/browse/map/ListingsList.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
-import { Heading } from "@bloom-housing/ui-seeds"
+import { BloomCard } from "@bloom-housing/shared-helpers"
+import { Button, Card, Heading } from "@bloom-housing/ui-seeds"
 import { LoadingOverlay, t } from "@bloom-housing/ui-components"
+import { FeatureFlagEnum } from "@bloom-housing/shared-helpers/src/types/backend-swagger"
 import { getMapListings } from "../../../lib/helpers"
 import { Pagination } from "./Pagination"
 import { useListingsMapContext } from "./ListingsMapContext"
@@ -11,7 +13,8 @@ type ListingsListProps = {
 }
 
 const ListingsList = (props: ListingsListProps) => {
-  const { searchResults, onPageChange, isLoading } = useListingsMapContext()
+  const { searchResults, onPageChange, isLoading, activeFeatureFlags, notificationsSignUpUrl } =
+    useListingsMapContext()
   const loading = props.loading ?? isLoading
   const moreMarkersOnMap = searchResults.markers.length > 0
 
@@ -50,7 +53,71 @@ const ListingsList = (props: ListingsListProps) => {
     </div>
   )
 
-  const infoCards = <div>{/* // Map TODO: Create customizable info cards */}</div>
+  const enableCustomListingNotifications = activeFeatureFlags?.includes(
+    FeatureFlagEnum.enableCustomListingNotifications
+  )
+  const showNotificationsCard = enableCustomListingNotifications || !!notificationsSignUpUrl
+
+  const infoCards = (
+    <div className={styles["listings-list-info-cards"]}>
+      {showNotificationsCard && (
+        <BloomCard
+          iconSymbol="envelope"
+          title={t("welcome.signUp")}
+          variant="block"
+          headingPriority={3}
+          iconClass="card-icon"
+        >
+          <Card.Section>
+            <Button
+              href={
+                enableCustomListingNotifications ? "/account/notifications" : notificationsSignUpUrl
+              }
+              variant="primary-outlined"
+              size="sm"
+            >
+              {t("welcome.signUpToday")}
+            </Button>
+          </Card.Section>
+        </BloomCard>
+      )}
+      {activeFeatureFlags?.includes(FeatureFlagEnum.enableResources) && (
+        <BloomCard
+          iconSymbol="house"
+          title={t("welcome.seeMoreOpportunitiesTruncated")}
+          variant="block"
+          headingPriority={3}
+          iconClass="card-icon"
+        >
+          <Card.Section>
+            <Button href="/additional-resources" variant="primary-outlined" size="sm">
+              {t("welcome.viewAdditionalHousingTruncated")}
+            </Button>
+          </Card.Section>
+        </BloomCard>
+      )}
+      {activeFeatureFlags?.includes(FeatureFlagEnum.enableAdditionalResources) && (
+        <BloomCard
+          iconSymbol="questionMarkCircle"
+          title={t("resources.additionalResourcesTitle")}
+          variant="block"
+          headingPriority={3}
+          iconClass="card-icon"
+        >
+          <Card.Section>
+            <Button
+              href={t("resources.additionalResourcesLink")}
+              variant="primary-outlined"
+              size="sm"
+            >
+              {t("welcome.learnMore")}
+            </Button>
+          </Card.Section>
+        </BloomCard>
+      )}
+      {/* TODO: add the Looking for housing in San Francisco? card here */}
+    </div>
+  )
 
   const pagination =
     searchResults.lastPage !== 0 ? (
