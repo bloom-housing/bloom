2715a08d539315914221f1964ba20b8bee3e7bc2
Mateusz Zduniuk <matzduniuk@gmail.com>
2026-06-24 16:18:13 +0200

    chore: update background jobs module to include new imports

--- api/src/modules/background-jobs.module.ts
+++ api/src/modules/background-jobs.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { BackgroundJobsController } from 'src/controllers/background-jobs.controller';
-import { BackgroundJobsService } from 'src/services/background-jobs.service';
 import { PrismaModule } from './prisma.module';
+import { S3Module } from './s3.module';
+import { PermissionModule } from './permission.module';
+import { BackgroundJobsController } from '../controllers/background-jobs.controller';
+import { BackgroundJobsService } from '../services/background-jobs.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, S3Module, PermissionModule],
   providers: [BackgroundJobsService],
   controllers: [BackgroundJobsController],
   exports: [BackgroundJobsService],
