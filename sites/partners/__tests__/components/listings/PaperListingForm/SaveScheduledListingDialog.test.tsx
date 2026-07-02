c11b72d7b99eacb4239eeb05fd430118c18f7650
Emily Jablonski <emijablonski@gmail.com>
2026-07-01 18:26:20 -0600

    fix: lint error

--- sites/partners/__tests__/components/listings/PaperListingForm/SaveScheduledListingDialog.test.tsx
+++ sites/partners/__tests__/components/listings/PaperListingForm/SaveScheduledListingDialog.test.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { screen } from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
 import SaveScheduledListingDialog from "../../../../src/components/listings/PaperListingForm/dialogs/SaveScheduledListingDialog"
 import { mockNextRouter, render } from "../../../testUtils"
 
