2dceec1d97719ee60732eeba0f49919030e68378
Mateusz Zduniuk <137785676+matzduniuk@users.noreply.github.com>
2026-07-06 09:03:02 +0200

    feat: bulk csv template download (#6423)
    * chore: setup a new application bulk upload service
    * chore: add the new bulk upload CSV template download endpoint to applications controller
    * chore: add basic integration tests for the new service
    * chore: add the export authorization integration tests
    * chore: generate a new backend swagger types
    * fix: fix global import path
    * fix: add hours to the submission date mock
    * fix: update expected strings to match the git action setup
    * chore: update the lottery position source
    * fix: apply PR fixes
    * fix: update application status formatting
    * fix: add application decline reason formatter
    * fix: fix imports
    * fix: fix failing integration tests

---
+++ api/src/modules/application-bulk-upload.module.ts
@@ -0,0 +1,13 @@
+import { Global, Module } from '@nestjs/common';
+import { ApplicationBulkUploadService } from '../services/application-bulk-upload.service';
+import { PrismaModule } from './prisma.module';
+import { ListingModule } from './listing.module';
+import { PermissionModule } from './permission.module';
+
+@Global()
+@Module({
+  imports: [PrismaModule, ListingModule, PermissionModule],
+  providers: [ApplicationBulkUploadService],
+  exports: [ApplicationBulkUploadService],
+})
+export class ApplicationBulkUploadModule {}
