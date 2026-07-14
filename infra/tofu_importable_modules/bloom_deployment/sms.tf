0937ab3e2a814814f7e43909f5a998e17ccab28d
Chad Brokaw <36685920+chadbrokaw@users.noreply.github.com>
2026-07-14 10:40:20 -0600

    fix: lint

--- infra/tofu_importable_modules/bloom_deployment/sms.tf
+++ infra/tofu_importable_modules/bloom_deployment/sms.tf
@@ -1,10 +1,10 @@
 resource "aws_pinpointsmsvoicev2_phone_number" "api_sms" {
-  count            = var.bloom_api_sms_config != null ? 1 : 0
-  region           = var.aws_region
-  iso_country_code = "US"
-  message_type         = "TRANSACTIONAL"
-  number_type          = var.bloom_api_sms_config.number_type
-  number_capabilities  = ["SMS"]
+  count               = var.bloom_api_sms_config != null ? 1 : 0
+  region              = var.aws_region
+  iso_country_code    = "US"
+  message_type        = "TRANSACTIONAL"
+  number_type         = var.bloom_api_sms_config.number_type
+  number_capabilities = ["SMS"]
 }
 
 output "sms_phone_number" {
