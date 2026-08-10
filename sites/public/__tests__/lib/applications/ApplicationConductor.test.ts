d440f86e0aab1e7ad7779f31fbc3b93fbf1b0ee5
krzysztof ziecina <kziecina@airnauts.com>
2026-08-10 17:09:36 +0200

    fix: remove redundant test

--- sites/public/__tests__/lib/applications/ApplicationConductor.test.ts
+++
@@ -1,9 +0,0 @@
-import { listing } from "@bloom-housing/shared-helpers/__tests__/testHelpers"
-import ApplicationConductor from "../../../src/lib/applications/ApplicationConductor"
-
-describe("ApplicationConductor stop light configuration", () => {
-  it("defaults to no enabled stop light rule keys", () => {
-    const conductor = new ApplicationConductor({}, { ...listing })
-    expect(conductor.config.enabledStopLightRuleKeys).toEqual([])
-  })
-})
