f0aba105cf9874b94e15ba27509519625f50b226
Eric McGarry <eric@eric.mcgarry.name>
2026-09-01 15:42:13 -0400

    feat: update translation migration

--- api/prisma/migrations/69_update_lottery_available_email/migration.sql
+++ api/prisma/migrations/69_update_lottery_available_email/migration.sql
@@ -6,11 +6,11 @@ SET
         translations,
         '{lotteryAvailable}',
         COALESCE(translations->'lotteryAvailable', '{}'::jsonb) || '{
-            "duplicatesDetails": "Bloom generally does not accept duplicate applications. A duplicate application is one that has someone who also appears on another application for the same housing opportunity. For more detailed information on how we handle duplicates, see our",
-            "helpCenterUrl": "https://www.exygy.com",
-            "notificationsUrl": "https://www.exygy.com",
+            "duplicatesDetails": "Doorway generally does not accept duplicate applications. A duplicate application is one that has someone who also appears on another application for the same housing opportunity. For more detailed information on how we handle duplicates, see our",
+            "helpCenterUrl": "https://housingbayarea.mtc.ca.gov/faq",
+            "notificationsUrl": "https://public.govdelivery.com/accounts/CAMTC/signup/36832",
             "termsOfUse": "Terms of Use",
-            "termsUrl": "https://www.exygy.com"
+            "termsUrl": "https://mtc.ca.gov/doorway-housing-portal-terms-use"
         }'::jsonb
     )
 WHERE
@@ -24,11 +24,11 @@ SET
         translations,
         '{lotteryAvailable}',
         COALESCE(translations->'lotteryAvailable', '{}'::jsonb) || '{
-            "duplicatesDetails": "بشكل عام، لا تقبل Bloom الطلبات المكررة. ويُقصد بالطلب المكرر ذلك الطلب الذي يتضمن شخصاً يظهر أيضاً في طلب آخر لنفس فرصة السكن. لمزيد من التفاصيل حول كيفية تعاملنا مع الطلبات المكررة، يرجى الاطلاع على",
-            "helpCenterUrl": "https://www.exygy.com",
-            "notificationsUrl": "https://www.exygy.com",
+            "duplicatesDetails": "بشكل عام، لا تقبل Doorway الطلبات المكررة. ويُقصد بالطلب المكرر ذلك الطلب الذي يتضمن شخصاً يظهر أيضاً في طلب آخر لنفس فرصة السكن. لمزيد من التفاصيل حول كيفية تعاملنا مع الطلبات المكررة، يرجى الاطلاع على",
+            "helpCenterUrl": "https://housingbayarea.mtc.ca.gov/faq",
+            "notificationsUrl": "https://public.govdelivery.com/accounts/CAMTC/signup/36832",
             "termsOfUse": "شروط الاستخدام",
-            "termsUrl": "https://www.exygy.com"
+            "termsUrl": "https://mtc.ca.gov/doorway-housing-portal-terms-use"
         }'::jsonb
     )
 WHERE
@@ -41,11 +41,11 @@ SET
         translations,
         '{lotteryAvailable}',
         COALESCE(translations->'lotteryAvailable', '{}'::jsonb) || '{
-            "duplicatesDetails": "ব্লুম সাধারণত একই আবাসন সুবিধার জন্য একাধিক আবেদনে একই ব্যক্তির নাম থাকলে—অর্থাৎ দ্বৈত আবেদন—তা গ্রহণ করে না। দ্বৈত আবেদনের বিষয়টি আমরা কীভাবে পরিচালনা করি সে সম্পর্কে বিস্তারিত তথ্যের জন্য আমাদের",
-            "helpCenterUrl": "https://www.exygy.com",
-            "notificationsUrl": "https://www.exygy.com",
+            "duplicatesDetails": "সাধারণত ডোরওয়ে (Doorway) একই আবাসন সুবিধার জন্য একাধিক আবেদনে একই ব্যক্তির নাম থাকা বা দ্বৈত আবেদন (duplicate application) গ্রহণ করে না। দ্বৈত আবেদনের বিষয়টি আমরা কীভাবে পরিচালনা করি সে সম্পর্কে বিস্তারিত তথ্যের জন্য আমাদের",
+            "helpCenterUrl": "https://housingbayarea.mtc.ca.gov/faq",
+            "notificationsUrl": "https://public.govdelivery.com/accounts/CAMTC/signup/36832",
             "termsOfUse": "ব্যবহারের শর্তাবলি",
-            "termsUrl": "https://www.exygy.com"
+            "termsUrl": "https://mtc.ca.gov/doorway-housing-portal-terms-use"
         }'::jsonb
     )
 WHERE
@@ -58,11 +58,11 @@ SET
         translations,
         '{lotteryAvailable}',
         COALESCE(translations->'lotteryAvailable', '{}'::jsonb) || '{
-            "duplicatesDetails": "Bloom generalmente no acepta solicitudes duplicadas. Una solicitud duplicada es aquella en la que aparece una persona que también aparece en otra solicitud para la misma oportunidad de vivienda. Para obtener información más detallada sobre cómo manejamos las solicitudes duplicadas, consulte nuestros",
-            "helpCenterUrl": "https://www.exygy.com",
-            "notificationsUrl": "https://www.exygy.com",
+            "duplicatesDetails": "Doorway generalmente no acepta solicitudes duplicadas. Una solicitud duplicada es aquella en la que aparece una persona que también aparece en otra solicitud para la misma oportunidad de vivienda. Para obtener información más detallada sobre cómo manejamos las solicitudes duplicadas, consulte nuestros",
+            "helpCenterUrl": "https://housingbayarea.mtc.ca.gov/faq",
+            "notificationsUrl": "https://public.govdelivery.com/accounts/CAMTC/signup/36832",
             "termsOfUse": "Términos de uso",
-            "termsUrl": "https://www.exygy.com"
+            "termsUrl": "https://mtc.ca.gov/doorway-housing-portal-terms-use"
         }'::jsonb
     )
 WHERE
@@ -75,11 +75,11 @@ SET
         translations,
         '{lotteryAvailable}',
         COALESCE(translations->'lotteryAvailable', '{}'::jsonb) || '{
-            "duplicatesDetails": "بلوم عموماً درخواست‌های تکراری را نمی‌پذیرد. درخواست تکراری، درخواستی است که در آن شخصی در درخواست دیگری برای همان فرصت مسکن نیز حضور دارد. برای اطلاعات بیشتر در مورد نحوه رسیدگی ما به درخواست‌های تکراری، به وب‌سایت ما مراجعه کنید",
-            "helpCenterUrl": "https://www.exygy.com",
-            "notificationsUrl": "https://www.exygy.com",
+            "duplicatesDetails": "Doorway معمولاً درخواست‌های تکراری را نمی‌پذیرد. درخواست تکراری، درخواستی است که در آن شخصی در درخواست دیگری برای همان فرصت مسکن نیز حضور دارد. برای اطلاعات بیشتر در مورد نحوه رسیدگی ما به درخواست‌های تکراری، به وب‌سایت ما مراجعه کنید",
+            "helpCenterUrl": "https://housingbayarea.mtc.ca.gov/faq",
+            "notificationsUrl": "https://public.govdelivery.com/accounts/CAMTC/signup/36832",
             "termsOfUse": "شرایط استفاده",
-            "termsUrl": "https://www.exygy.com"
+            "termsUrl": "https://mtc.ca.gov/doorway-housing-portal-terms-use"
         }'::jsonb
     )
 WHERE
@@ -92,11 +92,11 @@ SET
         translations,
         '{lotteryAvailable}',
         COALESCE(translations->'lotteryAvailable', '{}'::jsonb) || '{
-            "duplicatesDetails": "Bloom-ը, որպես կանոն, չի ընդունում կրկնօրինակ դիմումներ: Կրկնօրինակ դիմումը այն դիմումն է, որի դեպքում անձը նույնպես նշված է նույն բնակարանային հնարավորության համար նախատեսված մեկ այլ դիմումում: Կրկնօրինակ դիմումների հետ վարվելու վերաբերյալ ավելի մանրամասն տեղեկությունների համար տե՛ս մեր",
-            "helpCenterUrl": "https://www.exygy.com",
-            "notificationsUrl": "https://www.exygy.com",
+            "duplicatesDetails": "Doorway-ը, որպես կանոն, չի ընդունում կրկնօրինակ դիմումներ: Կրկնօրինակ դիմումը այն դիմումն է, որի դեպքում անձը նույնպես նշված է նույն բնակարանային հնարավորության համար նախատեսված մեկ այլ դիմումում: Կրկնօրինակ դիմումների հետ վարվելու վերաբերյալ ավելի մանրամասն տեղեկությունների համար տե՛ս մեր",
+            "helpCenterUrl": "https://housingbayarea.mtc.ca.gov/faq",
+            "notificationsUrl": "https://public.govdelivery.com/accounts/CAMTC/signup/36832",
             "termsOfUse": "Օգտագործման պայմաններ",
-            "termsUrl": "https://www.exygy.com"
+            "termsUrl": "https://mtc.ca.gov/doorway-housing-portal-terms-use"
         }'::jsonb
     )
 WHERE
@@ -109,11 +109,11 @@ SET
         translations,
         '{lotteryAvailable}',
         COALESCE(translations->'lotteryAvailable', '{}'::jsonb) || '{
-            "duplicatesDetails": "Bloom은 일반적으로 중복 신청을 허용하지 않습니다. 중복 신청이란 동일한 주택 공급 건에 대해 다른 신청서에도 이름이 올라가 있는 지원자가 포함된 경우를 의미합니다. 중복 신청 처리 방식에 대한 자세한 내용은 다음을 참조하십시오",
-            "helpCenterUrl": "https://www.exygy.com",
-            "notificationsUrl": "https://www.exygy.com",
+            "duplicatesDetails": "Doorway은 일반적으로 중복 신청을 허용하지 않습니다. 중복 신청이란 동일한 주택 공급 건에 대해 다른 신청서에도 이름이 올라가 있는 지원자가 포함된 경우를 의미합니다. 중복 신청 처리 방식에 대한 자세한 내용은 다음을 참조하십시오",
+            "helpCenterUrl": "https://housingbayarea.mtc.ca.gov/faq",
+            "notificationsUrl": "https://public.govdelivery.com/accounts/CAMTC/signup/36832",
             "termsOfUse": "이용 약관",
-            "termsUrl": "https://www.exygy.com"
+            "termsUrl": "https://mtc.ca.gov/doorway-housing-portal-terms-use"
         }'::jsonb
     )
 WHERE
@@ -126,11 +126,11 @@ SET
         translations,
         '{lotteryAvailable}',
         COALESCE(translations->'lotteryAvailable', '{}'::jsonb) || '{
-            "duplicatesDetails": "Ang Bloom sa pangkalahatan ay hindi tumatanggap ng mga duplicate na aplikasyon. Ang isang duplicate na aplikasyon ay isa na mayroong isang tao na lumilitaw din sa isa pang aplikasyon para sa parehong pagkakataon sa pabahay. Para sa mas detalyadong impormasyon sa kung paano namin pinangangasiwaan ang mga duplicate, tingnan ang aming",
-            "helpCenterUrl": "https://www.exygy.com",
-            "notificationsUrl": "https://www.exygy.com",
+            "duplicatesDetails": "Ang Doorway sa pangkalahatan ay hindi tumatanggap ng mga duplicate na aplikasyon. Ang isang duplicate na aplikasyon ay isa na mayroong isang tao na lumilitaw din sa isa pang aplikasyon para sa parehong pagkakataon sa pabahay. Para sa mas detalyadong impormasyon sa kung paano namin pinangangasiwaan ang mga duplicate, tingnan ang aming",
+            "helpCenterUrl": "https://housingbayarea.mtc.ca.gov/faq",
+            "notificationsUrl": "https://public.govdelivery.com/accounts/CAMTC/signup/36832",
             "termsOfUse": "Mga Tuntunin ng Paggamit",
-            "termsUrl": "https://www.exygy.com"
+            "termsUrl": "https://mtc.ca.gov/doorway-housing-portal-terms-use"
         }'::jsonb
     )
 WHERE
@@ -143,11 +143,11 @@ SET
         translations,
         '{lotteryAvailable}',
         COALESCE(translations->'lotteryAvailable', '{}'::jsonb) || '{
-            "duplicatesDetails": "Bloom thường không chấp nhận các đơn xin trùng lặp. Một đơn xin trùng lặp là đơn xin có người cũng xuất hiện trên một đơn xin khác cho cùng một cơ hội nhà ở. Để biết thông tin chi tiết hơn về cách chúng tôi xử lý các đơn xin trùng lặp, hãy xem của chúng tôi",
-            "helpCenterUrl": "https://www.exygy.com",
-            "notificationsUrl": "https://www.exygy.com",
+            "duplicatesDetails": "Doorway thường không chấp nhận các đơn xin trùng lặp. Một đơn xin trùng lặp là đơn xin có người cũng xuất hiện trên một đơn xin khác cho cùng một cơ hội nhà ở. Để biết thông tin chi tiết hơn về cách chúng tôi xử lý các đơn xin trùng lặp, hãy xem của chúng tôi",
+            "helpCenterUrl": "https://housingbayarea.mtc.ca.gov/faq",
+            "notificationsUrl": "https://public.govdelivery.com/accounts/CAMTC/signup/36832",
             "termsOfUse": "Điều khoản sử dụng",
-            "termsUrl": "https://www.exygy.com"
+            "termsUrl": "https://mtc.ca.gov/doorway-housing-portal-terms-use"
         }'::jsonb
     )
 WHERE
@@ -160,11 +160,11 @@ SET
         translations,
         '{lotteryAvailable}',
         COALESCE(translations->'lotteryAvailable', '{}'::jsonb) || '{
-            "duplicatesDetails": "Bloom 一般不接受重复申请。重复申请是指申请者与另一份申请者有相同的住房机会。有关我们如何处理重复申请的更多详细信息，请参阅我们的",
-            "helpCenterUrl": "https://www.exygy.com",
-            "notificationsUrl": "https://www.exygy.com",
+            "duplicatesDetails": "Doorway 一般不接受重复申请。重复申请是指申请者与另一份申请者有相同的住房机会。有关我们如何处理重复申请的更多详细信息，请参阅我们的",
+            "helpCenterUrl": "https://housingbayarea.mtc.ca.gov/faq",
+            "notificationsUrl": "https://public.govdelivery.com/accounts/CAMTC/signup/36832",
             "termsOfUse": "使用条款",
-            "termsUrl": "https://www.exygy.com"
+            "termsUrl": "https://mtc.ca.gov/doorway-housing-portal-terms-use"
         }'::jsonb
     )
 WHERE
