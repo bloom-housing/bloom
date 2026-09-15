bc35f8315f67aa8a767f51b8082763ff81c0788b
Yazeed Loonat <yazeedloonat@gmail.com>
2026-09-15 13:53:42 -0700

    fix: adding content type

--- infra/tofu_importable_modules/bloom_deployment/s3.tf
+++ infra/tofu_importable_modules/bloom_deployment/s3.tf
@@ -66,7 +66,8 @@ resource "aws_s3_bucket_cors_configuration" "public" {
   }
 
   cors_rule {
-    allowed_methods = ["GET", "OPTIONS"]
+    allowed_headers = ["Content-Type"]
+    allowed_methods = ["GET"]
     allowed_origins = ["https://partners.${var.domain_name}", "https://${var.domain_name}"]
   }
 }
