28c3901a0367bc959ed9e0c66f965e81080b2abe
Morgan Ludtke <42942267+ludtkemorgan@users.noreply.github.com>
2026-09-15 14:10:40 -0500

    fix: customizeable partner footer (#6690)
    * fix: customizeable partner footer
    
    * fix: make exygy footer optional

---
+++ sites/partners/src/static_content/jurisdiction_footer_content.tsx
@@ -0,0 +1,8 @@
+import { PartnerFooterProps } from "../components/core/PartnerFooter"
+
+export const getJurisdictionFooterLinksContent = (): PartnerFooterProps => {
+  return {
+    links: [],
+    includeExygyFooter: true,
+  }
+}
