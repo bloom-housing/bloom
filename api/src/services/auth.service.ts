7cbf2cd1212add8b764ea4341a25e837516ddefb
Eric McGarry <eric@eric.mcgarry.name>
2026-09-24 10:29:45 -0500

    feat: missing check

--- api/src/services/auth.service.ts
+++ api/src/services/auth.service.ts
@@ -215,6 +215,7 @@ export class AuthService {
 
       if (
         enablePublicTermsOfUse &&
+        !user.agreedToTermsOfService &&
         !agreedToTermsOfService &&
         !(
           user.userRoles?.isAdmin ||
